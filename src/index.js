0fc2d9deb3839df2b3e5d3c32138f56e75c127e5
mixelpixel <mixelpix@mac.com>
2017-07-20 16:50:32 -0400

    rough start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import ToDoClass from './toDoList'
+
 require('!style-loader!css-loader!sass-loader!./index.scss');
 
-ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
+// ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
+ReactDom.render(<ToDoClass />, document.getElementById('root'));
