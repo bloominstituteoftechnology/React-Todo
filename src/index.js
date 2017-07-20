1dbc8ce281d747c03c77f6d477893f5dc3ee9700
Patrick Kennedy <mixelpix@mac.com>
2017-07-20 16:11:09 -0400

    ignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 /*eslint no-unused-vars: "off"*/
 
-/* READ THE README!!! */
 import React from 'react';
 import ReactDom from 'react-dom';
 
 require('!style-loader!css-loader!sass-loader!./index.scss');
-// BLAH BLAH BLAH FROM GITHUB
+
 ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
