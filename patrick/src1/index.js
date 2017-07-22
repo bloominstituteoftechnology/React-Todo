e69c6acc085748e8830b9e275392181b0f08418a
mixelpixel <mixelpix@mac.com>
2017-07-21 20:04:46 -0400

    basic blank page compile

--- patrick/src1/index.js
+++ patrick/src1/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-
+console.log('wutThe WHAAAT?')
+const App = () => ( <div><h1>POOP</h1></div> )
 
 render(<App />, document.getElementById('root'));
