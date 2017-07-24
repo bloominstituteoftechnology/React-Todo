f54280cfaf8b8b44d56a9fc8b2e9690a904576b6
manishavi <manishalal972@gmail.com>
2017-07-24 10:36:48 -0500

    removed lint errors

--- src/index.js
+++ src/index.js
@@ -4,29 +4,29 @@ import { render } from 'react-dom';
 import ListOfNames from './listOfNames';
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+    fontFamily: 'sans-serif',
+    textAlign: 'center',
 };
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      ingredList: [],
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            ingredList: [],
+        };
+    }
 
-  componentWillMount() {
-    this.setState({ingredList: ['Onoin', 'Olive', 'Cheese', 'Chicken', 'Tomatoes']});
-  }
+    componentWillMount() {
+        this.setState({ingredList: ['Onoin', 'Olive', 'Cheese', 'Chicken', 'Tomatoes']});
+    }
 
-  render() {
-    return (
-      <div style={styles}>
-        <h2>Start selecting to make your Pizza {'\u2728'}</h2>
-        <ListOfNames ingredList={this.state.ingredList} />
-      </div>
-      )
+    render() {
+        return (
+            <div style={styles}>
+                <h2>Start selecting to make your Pizza {'\u2728'}</h2>
+                <ListOfNames ingredList={this.state.ingredList} />
+            </div>
+        );
     }
 }
 
