283265d870ee26d4e8ec903d7af3fd2105425c8d
manishavi <manishalal972@gmail.com>
2017-07-23 23:26:04 -0500

    new changes

--- src/index.js
+++ src/index.js
@@ -1,77 +1,33 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
-import ReactDom from 'react-dom';
-import listOfIngreds from './listOfIngreds';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ListOfNames from './listOfNames';
 
-require('!style-loader!css-loader!sass-loader!./index.scss');
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
 
-const CreatePizza = React.createClass({
-  getInitialState: function() {
-    return {
-      first: true
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      ingredList: [],
     };
-  },
-  flip: function() {
-    this.setState({
-      first: !this.state.first
-    });
-  },
-  render: function() {
-    let choice;
-    if(this.state.first) {
-    choice = this.props.ingreds.question;
-    } else {
-       choice = this.props.ingreds.answer;
-    }
-    return(<div>
-      <p>{choice}</p>
-      <button onclick = {this.flip}>Flip</button>
-      <p>Question: {question} </p>
-      <p>Answer: {answer} </p>
-      </div>);
-    }
-});
+  }
 
-// const listOfIngreds = React.createClass({
-//   render: function() {
-//     const list = this.props.ingreds.map(ingred => (
-//       <li>{ingreds.question}</li>
-//     ));
-//     return (
-//       <ul>{list}</ul>
-//     );
-//   },
-// });
+  componentWillMount() {
+    this.setState({ingredList: ['Onoin', 'Olive', 'Cheese', 'Chicken', 'Tomatoes']});
+  }
 
-const App = React.createClass({
-  getInitialState: function(){
-    return {
-      activeIndex : 0
-    };
-  },
-  render: function(){
-    // <div style={styles}>
-    // <h2> Start selecting to have delecious pizza {'\u2728'}</h2></div>
-    const activeIngreds = this.props.ingreds[this.state.activeIndex];
+  render() {
     return (
-      <div>
-      <CreatePizza ingred = {activeIngreds} />
-      <listOfIngreds ingred = {this.prop.ingreds}/>
+      <div style={styles}>
+        <h2>Start selecting to make your Pizza {'\u2728'}</h2>
+        <ListOfNames ingredList={this.state.ingredList} />
       </div>
-    )
-  }
-});
-const ingreds = [
-  {
-      question:"What do you need for pizza?",
-      answer: "Crust"
-  }, {
-       question:"What sauce do you need?",
-       answer:"Marinara"
-  }, {
-       question:"What toppings?",
-       answer:"Cheese, olive, bellpepper"
-  }
-];
+      )
+    }
+}
 
-ReactDom.render(<App ingreds = {ingreds} />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
