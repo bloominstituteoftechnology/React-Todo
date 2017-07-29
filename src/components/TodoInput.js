5a89354b299670e83359432399da0bc56907be7b
aaren-george <aarengeorge@me.com>
2017-07-29 19:02:18 -0400

    styled scss

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -4,7 +4,7 @@ const TodoInput = props => {
     return (
         <div>
             <input value={props.todo} onChange={props.onTodoInput} />
-            <button onClick={props.onTodoSubmit}>Submit todo</button>
+            <button onClick={props.onTodoSubmit}>Submit</button>
         </div>
     );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 const TodoList = props => {
     return (
-        <ol>
-      Task List:
-            {props.todos.map((todo, index) => {
-                return <li value={index}>{todo}</li>;
-            })}
-        </ol>
+        <div>
+            <div>
+                <h1>Welcome to Todo List!</h1>
+            </div>
+            <div>
+                <h2>Task List:</h2>
+                <ol>
+                    {props.todos.map((todo, index) => {
+                        return <li value={index + 1}>{todo}</li>;
+                    })}
+                </ol>
+            </div>
+        </div>
     );
 };
 
