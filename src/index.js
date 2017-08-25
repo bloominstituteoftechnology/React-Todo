1e41e2bd035255a3e66ae4de7330c6da10c485be
phytertek <ryan.phytertek@gmail.com>
2017-08-25 15:33:41 -0400

    change to dark theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
 import { render } from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import createPalette from 'material-ui/styles/palette';
 
 import App from './Container/App';
 
+const theme = createMuiTheme({
+  palette: createPalette({
+    type: 'dark', // Switching the dark mode on is a single property value change.
+  }),
+})
+
 // require('!style-loader!css-loader!sass-loader!./index.scss');
 render(
-  <App/>
+  <MuiThemeProvider theme={theme}>
+    <App/>
+  </MuiThemeProvider>
+
   , 
   document.getElementById('root')
 );
