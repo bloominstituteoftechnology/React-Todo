103f85225067af1e8c5c0c1923dd3fa1a9f240cb
khamp19 <khampton119@gmail.com>
2017-08-26 12:40:21 -0400

    react to do list, no styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
 import ReactDom from 'react-dom';
+import ToDoList from './to_do_components/todoList';
 
 require('!style-loader!css-loader!sass-loader!./index.scss');
 
-ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
+ReactDom.render(<ToDoList />, document.getElementById('root'));
