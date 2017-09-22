f7334b337416c533951ae85198757a1df6b70cfd
Nick Costigan <nick.costigan@gmail.com>
2017-09-22 17:10:47 -0400

    second iteration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import List from './List';
 
 class App extends Component {
-    constructor() {
-        super();
-    }
-    render() {
-        return (
-            <List />
-        )
-    }
+  render() {
+    return (
+      <List />
+    )
+  }
 }
 
 export default App;
