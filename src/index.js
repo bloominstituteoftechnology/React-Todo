3dbb5c8ee535d515607f646663c9625407614a62
Mathew Calkins <mathewpcalkins@gmail.com>
2017-09-25 13:50:26 -0400

    Built to-do list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
-import ReactDom from 'react-dom';
-
-require('!style-loader!css-loader!sass-loader!./index.scss');
-
-ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
+import { render } from 'react-dom';
+import Hello from './Hello';
+import ClassComponentExample from './ClassComponentExample';
+import ToDoList from './ToDoList';
+// App is itself nothing more than a functional component that renders some children components
+const App = () => (
+  <div>
+    <h2>A list of tasks!</h2>
+    <ToDoList/>
+  </div>
+);
+// This `render` method is different from the `render` we saw in the ClassComponentExample
+// This `render` does the work of mounting our root App component to the DOM by targeting 
+// the 'root' div in our index.html file
+render(<App />, document.getElementById('root'));
