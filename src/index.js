07ed93182fba6bede65d23f0d3cc4c331ae4ebd5
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2017-09-25 13:36:05 -0400

    ToDo List

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
-require('!style-loader!css-loader!sass-loader!./index.scss');
+import App from "./Components/App";
 
-ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
+require("!style-loader!css-loader!sass-loader!./index.scss");
+
+ReactDom.render(<App />, document.getElementById("root"));
