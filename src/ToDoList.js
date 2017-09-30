14b639e8bd278f6970ad17aa0bcab99b1f5e57e7
Mathew Calkins <mathewpcalkins@gmail.com>
2017-09-30 17:45:55 -0400

    Did it correctly this time!

---
+++ src/ToDoList.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+class ToDoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      taskNames: [],
+      newTask: ""
+    };
+  }
+  addTask = event => {
+    event.preventDefault();
+    const taskNames = this.state.taskNames;
+    if (!taskNames.includes(this.state.newTask)) {
+      taskNames.push(this.state.newTask);
+    }
+    this.setState({
+      newTask: "",
+      taskNames: taskNames
+    });
+  };
+
+  removeLastTask = event => {
+    event.preventDefault();
+    const taskNames = this.state.taskNames;
+    taskNames.pop();
+    this.setState({
+      newTask: "",
+      taskNames: taskNames
+    });
+  };
+
+  removeThirdTask = event => {
+    event.preventDefault();
+    const taskNames = this.state.taskNames;
+    taskNames.splice(2,1);
+    this.setState({
+      newTask: "",
+      taskNames: taskNames
+    })
+  }
+
+  removeArbitraryTask = (num) => {
+    const taskNames = this.state.taskNames;
+    taskNames.splice(num, 1);
+    this.setState({
+      newTask: "",
+      taskNames: taskNames
+    })
+  }
+
+  handleNewTaskNameInput = event => {
+    this.setState({ newTask: event.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <h2> You have {this.state.taskNames.length} task(s) left to complete!</h2>
+        <table>
+          <tr>
+            <th>Priority</th>
+            <th>Task</th>
+            <th>Remove task?</th>
+          </tr>
+            {this.state.taskNames.map(name => <tr>
+            <td>{this.state.taskNames.indexOf(name)+1}</td>
+            <td>{name}</td>
+            <td><button id="completed-button" onClick={() => this.removeArbitraryTask(this.state.taskNames.indexOf(name))}></button></td>
+            </tr>)}
+        </table>
+        <form onSubmit={this.addTask}>
+          <input
+            type="text"
+            onChange={this.handleNewTaskNameInput}
+            placeholder="Add a new task!"
+            value={this.state.newTask}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+export default ToDoList;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
 import { render } from 'react-dom';
+import ToDoList from './ToDoList'
 // You'll want to import the necessary components you want the App component to render
 
 const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
+   // {/* Replace this div with whatever components you want the App root component to render */}
+   <div>
+   Hello World!
+   <h2> A list of tasks!</h2>
+   <ToDoList/>
+   </div> 
 );
 
 render(<App />, document.getElementById('root'));
