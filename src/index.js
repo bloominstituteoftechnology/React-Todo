a789df595f41c8dc6364499b384746003c3d80e2
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-10-23 15:40:26 -0700

    fixed it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
+import React, {Component} from 'react';
 import { render } from 'react-dom';
 // You'll want to import the necessary components you want the App component to render
+class ToDo extends Component {
+    constructor () {
+        super();
+        this.state = {
+            ToDoList: [],
+        };
+        this.handleToDoList = this.handleToDoList.bind(this);
+        this.handleAddItemToList = this.handleAddItemToList.bind(this);
+        
+    }
 
+handleToDoList(event) {
+    this.setState({ ListItem: event.target.value });
+}
+render() {
+	console.log(this.state)
+    return (
+      <List
+       handleToDoList = {this.handleToDoList}
+       ListItem = {this.state.ListItem}
+       handleAddItemToList = {this.handleAddItemToList}
+       ToDoList = {this.state.ToDoList}
+      />
+    )
+};
+handleAddItemToList() {
+    this.state.ToDoList.push(this.state.ListItem);
+    this.setState({ ToDoList: this.state.ToDoList, ListItem: ''});    
+}
+}
 const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
+   /* Replace this div with whatever components you want the App root component to render */
+   <div>{new ToDo()}</div> 
 );
 
-render(<App />, document.getElementById('root'));
+const List = props => {
+    return (
+        <div>
+            <input type='text' value={props.ListItem} onChange={props.handleToDoList} />
+            <button onClick={props.handleAddItemToList}>Submit Item</button>
+            <ul>
+                {props.ToDoList.map((foofie, i) => {
+                    return <li key={i+1}><input key={i+1} type="checkbox" />{foofie}</li>;
+                })}
+            </ul>
+        </div>
+    );
+};
+
+render(<ToDo />, document.getElementById('root'));
