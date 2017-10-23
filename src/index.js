0605c50947e2a7f312dc7ea678734d54af25fb54
Austin F <austindanielfrench@gmail.com>
2017-10-23 16:20:03 -0700

    ToDo list completed!

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
-/*eslint no-unused-vars: "off"*/
-import React, { Component } from 'react';
+
+import React from 'react';
 import { render } from 'react-dom';
-import toDoList from './toDoList';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      toDoInput = '',
-      toDoList = [];
-    };
-    this.handleToDo = this.handleToDo.bind(this);
-    this.handleAddTodo = this.handleAddTodo.bind(this);
-  }
-  handleToDo () {
-    this.setState({ toDoInput: event.target.value });
-  }
-  handleAddToDo(event) {
-    this.state.toDoList.push(this.state.toDoInput);
-    this.setState{( toDoList, toDoInput: '' )}
-//   }
-//   render() {
-//     return (
-//       <toDoList
-//         changeHandler = {}
-//         addToDo = {}
-//         toDoItem = {}
-//       />
-//     )
-//   }
-// }
+import App from './components/App';
 
-export default App;
+render(<App />, document.getElementById('root'));
