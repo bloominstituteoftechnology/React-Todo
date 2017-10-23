797cf9a99b4f987c8b5bc9eb82a1097b539fe88b
Austin F <austindanielfrench@gmail.com>
2017-10-23 14:00:06 -0700

    Questions filled out

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 // You'll want to import the necessary components you want the App component to render
 
-const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
-);
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div> Hello World! </div>
+    )
+  }
+}
+
+// const App = () => (
+//    {
+//      <div>Hello World!</div>
+//    }
+// );
 
 render(<App />, document.getElementById('root'));
