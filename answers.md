fe89b424fa6677e283c9fbf2eb00acf1da5c977a
Biff <thedonnieutley@gmail.com>
2017-10-23 17:22:46 -0500

    can add items to ToDo list

--- answers.md
+++ answers.md
@@ -1,2 +1,20 @@
 ### All Answers to Partner Study should be filled out in this file.
- * 1. 
+ * 1. Single-page Application -> A single page HTML app that updates the page locally w/o needing to reload/re-connect to server.
+
+ * 2. Compilers -> takes JS code & transforms it in a different format  (commonly transforming ES6 into format older browsers can utilize); ex: Babel for React
+
+ * 3. Bundlers -> takes modules of JS & CSS and combines them down into a few files (optimizing for browsers); ex: Webpack & Browserify for React
+
+ * 4. Elements -> immutable; building blocks of React; not typically used directly but returned from components
+
+ * 5. Components -> reusable pieces of code that return element to be rendered; simplest version of this
+
+ * 6. JSX -> syntax extension to JS (full capability of JS); gets compiled to React.createElement() which returns plain JS objects (React elements)
+
+ * 7. Package Managers -> tools that allow you to manage dependencies in your projects
+
+ * 8. CDN -> deliver static content from network of servers worldwide; increases site response time for users by having static content physically closer to the users
+
+ * 9. Props -> like arguments; inputs to a React component; data passed down from parent component; should not be modified in any way (use state instead)
+
+ * 10. State -> managed by the component itself; keeps track of current state of data within
--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import ToDoList from './components/toDo';
 // You'll want to import the necessary components you want the App component to render
 
-const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
-);
+// const App = () => (
+//    {/* Replace this div with whatever components you want the App root component to render */}
+//    <div>Hello World!</div> 
+// );
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newToDo: '',
+      toDoList: []
+    };
+    this.handleNewToDo = this.handleNewToDo.bind(this);
+    this.handleAddNewToDo = this.handleAddNewToDo.bind(this);
+  }
+
+  handleNewToDo(event) {
+    this.setState({ newToDo: event.target.value });
+  }
+
+  handleAddNewToDo() {
+    let chore = this.state.newToDo;
+    const toDoList = this.state.toDoList;
+    toDoList.push(chore);
+    this.setState({ toDoList, newToDo: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>To Do List</h1>
+        <ToDoList
+          chore={this.state.newToDo}
+          changeHandler={this.handleNewToDo}
+          addTo={this.handleAddNewToDo}
+          toDoList={this.state.toDoList}
+        />
+      </div>
+    );
+  }
+}
 render(<App />, document.getElementById('root'));
