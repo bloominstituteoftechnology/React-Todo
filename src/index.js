81b6d0f765b533baa211e38dfc15e63f1335307f
Jason Crites <cacrites2007@gmail.com>
2017-10-23 17:36:49 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
+import React, {Component} from 'react';
 import { render } from 'react-dom';
 // You'll want to import the necessary components you want the App component to render
+class ToDo extends Component {
+    constructor () {
+        super();
+        this.state = {
+            ToDoList: [],
+        };
+        
+    }
 
+handleToDoList(event) {
+    this.setState({ ListItem: event.target.value });
+}
+render() {
+    return (
+       <List
+       handleToDoList = {this.handleToDoList(this)}
+       ListItem = {this.ListItem}
+       handleAddItemToList = {this.handleAddItemToList(this)}
+       ToDoList = {this.ToDoList}
+        />
+    )
+};
+handleAddItemToList() {
+    this.state.ToDoList.push(this.state.ListItem);
+    this.state({ ToDoList: this.state.ToDoList, ListItem: ''});    
+}
+}
 const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
+   /* Replace this div with whatever components you want the App root component to render */
+   <div>{ToDo}</div> 
 );
+
+const List = props => {
+    return (
+        <div>
+            <input type='text' value={props.ListItem} onChange={props.handleToDoList} />
+            <button onClick={props.handleAddItemToList}>Submit Item</button>
+            <ul>
+                {props.ToDoList.map((foofie, i) => {
+                    return <li key={i+1}>{foofie}</li>;
+                })}
+            </ul>
+        </div>
+    );
+};
 
 render(<App />, document.getElementById('root'));
