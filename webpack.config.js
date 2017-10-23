cf7349b4c3e6ec84ef6f4e0e6275b7795a6402a1
invegat <mark.c.oliver@gmail.com>
2017-10-23 16:41:19 -0400

    build to docs

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = env => {
   return {
     entry: './src/index.js',
     output: {
-      path: path.resolve('dist'),
+      path: path.resolve('docs'),
       filename: 'bundle.js',
     },
     module: {
