f77b298172996c852260591737c68fc1e56646f1
Ryan Hamblin <ryanhca@gmail.com>
2017-10-24 09:07:01 -0600

    boiler was foo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-/*eslint no-unused-vars: "off"*/
 import React from 'react';
 import { render } from 'react-dom';
 // You'll want to import the necessary components you want the App component to render
 
-const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
-);
+const App = () => {
+  return <div>Hello World!</div>;
+};
 
 render(<App />, document.getElementById('root'));
