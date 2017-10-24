aa7bb4023a650be8470b59ea6dc81159b56472aa
Ryan Hamblin <ryanhca@gmail.com>
2017-10-23 19:15:14 -0600

    working solution with extra credit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
 import { render } from 'react-dom';
-// You'll want to import the necessary components you want the App component to render
+import App from './components/App';
 
-const App = () => (
-   {/* Replace this div with whatever components you want the App root component to render */}
-   <div>Hello World!</div> 
-);
+// You'll want to import the necessary components you want the App component to render
 
 render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -11,16 +11,16 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
       }
-    ],
+    ]
   },
-  plugins: [HtmlWebpackPluginConfig],
-}
+  plugins: [HtmlWebpackPluginConfig]
+};
