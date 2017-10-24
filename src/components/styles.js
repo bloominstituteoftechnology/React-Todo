93b46c31a6433f7db9e115f3235ef388c51be464
dan volosnikov <danvolosnikov@gmail.com>
2017-10-23 21:36:52 -0400

    initial commit

---
+++ src/components/styles.js
@@ -0,0 +1,59 @@
+export const titleStyle = { 
+    color: "#fff",
+    'textAlign': 'center'
+};
+
+export const ulStyle = {
+    'listStyleType': 'none',
+    padding: 0,
+    backgroundColor: '#FF8552',
+    width: '400px',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+};
+
+export const liStyle = {
+    textAlign: 'center',
+    fontSize: '30px',
+    border: '3px solid #E6E6E6',
+    color: '#39393A'
+}
+
+export const liStyleCompleted = {
+    textAlign: 'center',
+    fontSize: '30px',
+    border: '3px solid #E6E6E6',
+    color: '#85FFC7'
+}
+
+export const spanStyle = {
+    float: 'right',
+    marginRight: '10px',
+    cursor: 'pointer'
+}
+
+export const formStyle = {
+    textAlign: 'center'
+}
+
+export const inputTextStyle = {
+    height: '30px',
+    width: '280px',
+    color: '#39393A',
+    fontSize: '14px',
+    borderTop: '0',
+    paddingLeft: '10px'
+}
+
+export const inputButtonStyle = {
+    height: '36px',
+    width: '110px',
+    paddingTop: '0',
+    backgroundColor: '#85FFC7',
+    color: '#39393A',
+    border: 'none',
+    padding: '0! important',
+    font: 'inherit',
+    cursor: 'pointer',
+    outline: 'inherit !important'
+}
