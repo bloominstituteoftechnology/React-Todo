6dbfa7774a19873f317c44b4b78c2cf2ed4551ec
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-29 19:55:51 -0700

    its working now

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,52 @@ import React, { Component } from 'react';
 import TodoList from './TodoList';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      todoList: [{id: 0, title: "todo 1"}, {id: 1, title: "todo 2"}],
+      todoList: [],
       todo: ''
     };
     this.handleTodoChange = this.handleTodoChange.bind(this);
     this.handleAddTodo = this.handleAddTodo.bind(this);
     this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
   }
+
   handleRemoveTodo(todoId) {
-    const todoList = this.state.todoList;
-    todoList.forEach((todo, i) => {
-      if (todo.id === todoId) {
-        todoList.splice(i, 1);
+    const newTodoList = this.state.todoList;
+    newTodoList.forEach((currTodo, i) => {
+      if (currTodo.id === todoId) {
+        newTodoList.splice(i, 1);
       }
     });
-    this.setState({ todoList });
+    this.setState({ todoList: newTodoList  });
   }
+
   handleTodoChange(event) {
     this.setState({ todo: event.target.value });
   }
+
   handleAddTodo() {
-    const todo = { id: this.state.todoList.length };
-    todo['title'] = this.state.todo;
-    const todoList = this.state.todoList;
-    todoList.push(todo);
-    this.setState({ todoList, todo: '' });
+    // Create an object with 2 properties id and title.
+    // id assigns a unique id to a newTodo.
+    // title holds the text of the newTodo.
+    const newTodo = {};
+    newTodo['id'] = this.state.todoList.length;
+    newTodo['title'] = this.state.todo;
+
+    // Create an array newTodoList.
+    // push the value of new todo item to this array
+    const newTodoList = this.state.todoList;
+    newTodoList.push(newTodo);
+    this.setState({ todoList:newTodoList, todo: '' });
+    //document.getElementById('inputElem').value = '';
   }
+
   render() {
     return (
       <div>
-        <div>Hi {this.props.username} {this.props.userlastname}</div>
         <input
+          //id = "inputElem"
           type="text"
           value={this.state.todo}
           onChange={this.handleTodoChange}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,36 +1,29 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.remove = this.remove.bind(this);
-  }
+const TodoList = props => {
+  const remove = obj => {
+    props.removeTodo(obj.id);
+  };
 
-  remove(obj) {
-    this.props.removeTodo(obj.id);
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.todoList.map(todo => {
-          return (
-            <div key={todo.title + todo.id}>
-              {todo.title}
-              <button
-                onClick={() => {
-                  remove(todo);
-                }}
-              >
-                Complete
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {props.todoList.map(todo => {
+        return (
+          <div key={todo.title + todo.id}>
+            {todo.title}
+            <button
+              onClick={() => {
+                remove(todo);
+              }}
+            >
+              Complete {todo.id}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default TodoList;
