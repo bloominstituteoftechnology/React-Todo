dbfe8365daba3ed1f17f91319a37743420bf2041
Jesse Hood <jessenoob@gmail.com>
2017-12-04 16:23:55 -0500

    all tests passing, no extra credit completed yet

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,19 +5,36 @@ export default class TodoList extends Component {
   constructor() {
     super();
     this.state = {
-      todos: ['Test Todo'],
+      todos: [],
       newTodo: '',
     }
+  }
+
+  handleTodoInput = (event) => {
+    this.setState({newTodo: event.target.value});
+  }
+
+  addTodo = (event) => {
+    event.preventDefault();
+    const todo = this.state.newTodo;
+    const todos = this.state.todos;
+    this.setState({
+      todos: [...todos, todo],
+      newTodo: ''
+    });
   }
 
   render() {
     return (
       <div>
-      {this.state.todos.map((todo, index) => {
-        return(
-          <Todo key={index} todo={todo} />
-        );
-      })}
+        {this.state.todos.map((todo, index) => {
+          return(
+            <Todo key={index} todo={todo} />
+          );
+        })}
+        <form onSubmit={this.addTodo}>
+          <input onChange={this.handleTodoInput} value={this.state.newTodo} />
+        </form>
       </div>
     );
   }
