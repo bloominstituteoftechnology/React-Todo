cbd3ee9adebc634332c662b864f4f0c93c3729db
Ismail Jadun <1683380+pinwale@users.noreply.github.com>
2017-12-04 20:28:41 -0500

    fixed nonbreaking typo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Todo from './components/TodoList';
+import TodoList from './components/TodoList';
 
 const App = () => (
   <div id="container">
     <h2>a todo app</h2>
-    <Todo />
+    <TodoList />
 
   </div>
 );
