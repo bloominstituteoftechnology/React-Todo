e5d62337e96151e19747b246514d8d15aa2b0442
Aaron Wilder <element1028@gmail.com>
2017-12-05 09:35:58 -0800

    working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,12 +11,18 @@ class Todo extends Component {
     handleClick = () => {
         this.setState({ clicked: !this.state.clicked });
     }
+    delete = () => {
+        console.log('delete it');
+    }
 
     render() {
         const styles = this.state.clicked ? { textDecoration: 'line-through', textDecorationColor: '#9ad3de'} : { textDecoration: 'none' };
         return (
+            <div>
+            <button className="button" onClick={this.delete}>Delete</button>
             <div style={styles} onClick={this.handleClick} className="todoS">
         {this.props.todo}
+      </div>
       </div>
         );
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -28,8 +28,9 @@ class TodoList extends Component {
                 <form onSubmit={this.addTodo}>
                     <input onChange={this.handleTodoInput} placeholder="Add new todo" value={this.state.newTodo} type="text" />
                 </form>
-                {this.state.todos.map((todo, i) => <Todo key={i} index={i} todo={todo} />)}
-            </div>
+               <div className="hope">
+                {this.state.todos.map((todo, i) => <Todo key={i} index={i} todo={todo} />)}</div>
+                </div>
         );
     }
 }
