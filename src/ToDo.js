182812afdb457cce37ad61fe37bd3ef9e7d44303
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-06 09:32:44 -0500

    built foundation for ToDo component

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,1 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            To_Do: [],
+            newToDo: '',
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.To_Do.map(chore => <div>{chore}</div>)}
+            </div>
+        )
+    }
+}
+
+
+export default ToDo;
