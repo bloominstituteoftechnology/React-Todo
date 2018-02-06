75a10526fe131c3fc043fd47f5826e035ae7df3b
Dixie Korley <dixiekorley@gmail.com>
2018-02-06 12:55:38 -0500

    Notetaking for lecture.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,35 @@ import App from './App';
 import './index.css';
 
 class App extends React.Component {
+   state = {
+    movies: [],
+    message: 'old message',
+   };
+
+   handleSubmit() {
+      // event.preventDefault();
+       const newMovie = {
+           id: 4, title: 'Inception'
+       };
+       const newMovies = [...this.state.movies, newMovie];
+       this.setState({ movies: newMovies });
+   }
+
     render() {
         return (
-        
+         <div className="title">
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="title">Title: </label>
+                <input type="text" />
+                <button type="submit">Add Movie</button>
+            </form>
+
+            <ul>
+                {this.state.movies.map(movie => {
+                    return <Movie key={movie.id} movie={movie} />
+                })}
+            </ul>
+         </div>
         );
     }
 }
