b10365cf23432bffe78c1b552f2262c74bd1e1a9
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-02-05 20:40:28 -0500

    WorkingInputForm

--- src/ToDo.js
+++ src/ToDo.js
@@ -5,16 +5,35 @@ class ToDo extends Component {
         super();
 
         this.state = {
-            toDos: ['clean bathroom'],
+            toDos: ['clean bathroom', 'grocery shopping', 'pick up kids'],
             newToDo: ''
         };
     }
 
+    addNewTask = (event) => {
+        event.preventDefault();
+        const toDos = this.state.toDos;
+        toDos.push(this.state.newToDo);
+        this.setState({
+            newToDo: "",
+            toDos: toDos
+        });
+    };
+
+    handleNewTaskInput = (event) => {
+        this.setState({newToDo: event.target.value});
+    };
+
     render() {
         return (
-            <ol>{this.state.toDos.map((toDo) => <li>{toDo}</li>
-            )}
-            </ol>
+            <div>
+                <form onSubmit={this.addNewTask}>
+                    <input type="text" onChange={this.handleNewTaskInput} placeholder="Add New Task!" value={this.state.newToDo} />
+                </form>
+                <ol>{this.state.toDos.map((toDo) => <li>{toDo}</li>
+                )}
+                </ol>
+            </div>    
         );
     }
 }
