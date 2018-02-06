bcf200fcdbd0ad8bfe386d06b2ed99a1d982be37
Res Yeg <shaqk702@gmail.com>
2018-02-05 22:16:02 -0800

    modified TodoList file

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class TodoList extends React.Component {
-    render() {
-        return (
-            <div>This is TodoList Component!</div>
-        );
+    constructor(props) {
+      super(props);
+      this.state = {value:''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
-}
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('An item was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Add a To Do List item:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
 
 export default TodoList;
