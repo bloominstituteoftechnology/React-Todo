962fc0bc56dbc6e6a6f3596ef4d3e5929ced9fd2
Mephestys <mephestys@gmail.com>
2018-02-05 17:30:45 -0700

    Began scaffolding of basic functionality. Added Todo function to Todo.js, added class TodoList to TodoList.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,1 +1,7 @@
 import React from 'react';
+
+function Todo(item) {
+  
+}
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import Todo from './Todo.js';
+
+class TodoList extends React.Component {
+  state = {
+    list: [],
+  }
+}
+
+export default TodoList;
