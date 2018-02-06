c761e9768208fd494bf0230f0cd7b1af373b23e3
Dan-Lara <endtrippy@gmail.com>
2018-02-05 21:33:46 -0700

    Added Form / Able to add new items into todo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import List from './Components/List';
 
+
 class App extends Component {
   constructor() {
     super();
-    this.state = ['testDo'];
+    this.state = {
+      list: [],
+      newItem: '',
+      strikeThrough: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const temp = this.state.list;
+    temp.push(this.state.newItem);
+    this.setState({list: temp});
+    event.preventDefault();
+    console.log(this.state.list);
+  }
+
+  handleChange(event) {
+   this.setState({newItem: event.target.value});
+   console.log(this.state.newItem);
+   event.preventDefault();
   }
 
   render() {
     return (
       <div>
         <h1>Todo App</h1>
-        <List items={this.state} />
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              New Todo Item:
+              <input type="text" value={this.state.newItem} onChange={this.handleChange}/>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        <List items={this.state.list} />
       </div>
     );
   }
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+// const StrikeThrough = (event) => {
+  
+// }
+
+// onClick={StrikeThrough}
+
 const List = (props) => {
   return (
     <div>
-      {props.items.map((element,index) => {
-        return <div key={index}>{element}</div>
+      {props.items.map((element, index) => {
+          return <div key={index}>{element}</div>
+        // return <div onClick={toggleStrike} key={index}>{element}</div>
       })}
     </div>
   );
