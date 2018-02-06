b517624beba4bfff68b26c74dd9f5a09d94f2c0b
ashleijo <shuryjo@gmail.com>
2018-02-05 19:56:52 -0500

    importing todo to todolist. creating a state for handling how component reacts when clicked

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 class Todo extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      clicked: false
+    };
+  }
 
-
-
-
-
+  click = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 }
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Todo from './Todo.js';
 
 class TodoList extends Component {
 
