8601161d7fb090a43bc1573ee728844bb3d6ecc3
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-05 23:27:01 -0600

    Font Update
    rename index.js and app.js to all lowercase
    in index.css and index.html:
    imported and applied Roboto font

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Input from './Input.js';
+import Input from './input.js';
 
-const App = () => (
-  <div>
-  <Input />
-    <h2>Todo App</h2>
-    <ul>
-      <li>Feed Cat</li>
-      <li>Wash Cat</li>
-      <li>Feed Cat To Dog</li>
-    </ul>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Todo App</h2>
+        <Input />
+        <ul>
+          <li>Feed Cat</li>
+          <li>Wash Cat</li>
+          <li>Feed Cat To Dog</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import App from './components/app';
 import './index.css';
 
 render(<App />, document.getElementById('root'));
