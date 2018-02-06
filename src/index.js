3f126f0329137241706be2952e875fca3c913fca
Dixie Korley <dixiekorley@gmail.com>
2018-02-06 08:50:15 -0500

    Take notes, restarting assignment.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import './index.css';
+
+cost lists = [
+    {
+        id: 1,
+        title: 'A'
+    },
+    {
+        id: 2,
+        title: 'B'
+    },
+    {
+        id: 3,
+        title: 'C'
+    },
+]
+
+class App extends React.Component {
+    render() {
+        return (
+        
+        );
+    }
+}
 
 render(<App />, document.getElementById('root'));
