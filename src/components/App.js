dbc4fce77c1c4b3e001585dee8c2c132f13849a7
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-06 08:08:03 -0600

    app.js Methods
    in app.js:
    add method to handle input
    add method to add list item
    add method to remove list item

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,43 @@ import List from './list.js';
 import './app.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: [
+        {
+          id: 1,
+          entry: 'Example Entry',
+        },
+      ],
+      input: '',
+      count: 1,
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  addItem = (event) => {
+    event.preventDefault();
+    const temp = this.state.todo;
+    let tempTwo = this.state.count;
+    tempTwo += 1;
+    temp.push({id: tempTwo, entry: this.state.input})
+    this.setState({
+      todo: temp,
+      input: '',
+      count: tempTwo,
+    });
+  }
+
+  removeItem = (id) => {
+    const temp = this.state.todos;
+    temp.splice(id, 1);
+    this.setState({todo: temp});
+  }
+
   render() {
     return (
       <div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './input.css';
 
 class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <input type="text" name="Todo" placeholder="Add New Activities Here"/>
+        <input type="text" name="Todo" placeholder="Add New Activities Here"/>
     );
   }
 }
