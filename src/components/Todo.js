9d44e8be9cc4da768f86e0f5151ba85b8d5d1839
Charlie Sparks <csparks@antioch.edu>
2018-02-05 20:05:16 -0800

    not working, seeking help

---
+++ src/components/Todo.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+	constructor(){
+		super();
+
+		this.state = {
+			todos: ["Finish coding", "go for a walk", "clean the kitchen"],
+			newTodo: ""
+		}
+	}
+
+  updateString(event){
+    this.setState({newTodo: event.target.value});
+    console.log(this.state);
+  }
+
+  render(){
+    return(
+      <div>
+        <ul>
+          {this.state.todos.map((item, index) => {
+            return <li key={index}>{item}</li>
+          })}
+        </ul>
+
+        <form>
+          <input type="text" onChange={this.updateString} placeholder="enter item here" value={this.state.newTodo} />
+
+        </form>
+      </div>
+      )
+  }
+ }
+
+
+export default TodoList;
