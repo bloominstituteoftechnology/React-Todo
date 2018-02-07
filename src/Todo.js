cd01284e92e60c5c0d9d4480cd69a32e328db5cc
rcs784 <rcstinson@hotmail.com>
2018-02-07 18:49:53 -0500

    stretch problems partially solved

--- src/Todo.js
+++ src/Todo.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { Component } from 'react';
+//import toggleCompleted from './TodoList';
+import styles from './Todo.css';
 
-class Todo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      key: this.props.key,
-      text: this.props.text,
-      completed: false,
-    };
-  }
-  
-  render() {
-    return (
-      <li key={this.state.key} onClick={this.toggleCompleted}>{this.state.completed ? <del>{this.state.text}</del> : this.state.text}</li>
-    );
-  };
-
-  toggleCompleted = () => {
-    this.setState({
-      completed: !this.state.completed,
-    });
-  };
+const Todo = (props) => {
+  return (
+    <li key={props.key} onClick={props.onClick}>{props.completed ? <del>{props.text}</del> : props.text}</li>
+  );
 }
 
 export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -5,8 +5,10 @@ import Todo from './Todo';
 class TodoList extends Component {
   constructor() {
     super();
+    const todos = JSON.parse(window.localStorage.getItem('todos')) || [];
+    alert(JSON.stringify(todos));
     this.state = {
-      todos: [],
+      todos,
       newTodo: '',
     };
   }
@@ -15,8 +17,12 @@ class TodoList extends Component {
     return (
       <div>
         <ul>{this.state.todos.map((todo, i) => {
-          return <Todo key={i} text={todo} />
-        })}</ul>
+          return (
+            <div key={i}>
+              <Todo key={i} text={todo.text} completed={todo.completed} onClick={this.toggleCompleted} />
+              <button key={i} type="button" onClick={this.removeTodo}>x</button>
+            </div>
+          )})}</ul>
         <form onSubmit={this.addNewTodo} onChange={this.updateTodo}>
           Add new todo:<br /><br />
           <input id="input" type="text" placeholder="New todo" /><br /><br />
@@ -35,14 +41,40 @@ class TodoList extends Component {
 
   addNewTodo = (event) => {
     event.preventDefault();
-    const todos = this.state.todos;
-    todos.push(this.state.newTodo);
+    const todos = [...this.state.todos, {
+      text: this.state.newTodo,
+      completed: false,
+    }];
+    window.localStorage.setItem('todos', JSON.stringify(todos));
     this.setState({
       todos,
       newTodo: '',
     });
     document.getElementById('input').value = '';
   };
+
+  removeTodo = (event) => {
+    let todos = this.state.todos;
+    const index = Array.from(event.target.parentNode.parentNode.children).indexOf(event.target.parentNode);
+    todos.splice(index, 1);
+    window.localStorage.setItem(todos, JSON.stringify(todos));
+    this.setState({
+      todos,
+      newTodo: this.state.newTodo,
+    });
+  };
+
+  toggleCompleted = (event) => {
+    let todos = this.state.todos;
+    const index = Array.from(event.target.parentNode.parentNode.children).indexOf(event.target.parentNode);
+    todos[index].completed = !todos[index].completed;
+    window.localStorage.setItem(todos, JSON.stringify(todos));
+    this.setState({
+      todos,
+      newTodo: this.state.newTodo,
+    });
+    
+  }
 }
 
 export default TodoList;
