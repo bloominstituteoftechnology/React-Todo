207959a9ebb5d1cf2ce05ffec4a8c5afdba35fea
kait-schorr <kaitlynschorr@gmail.com>
2018-03-05 14:44:31 -0600

    Added blank ToDo Component

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { Component } from "react";
 
-
+class ToDoList extends Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
 
 export default ToDoList;
