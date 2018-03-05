4c774ead2fd1b35cb4321e5b16f1b57073c2db6c
Charletta <charlettabullard12@outlook.com>
2018-03-05 16:47:17 -0500

    ToDoList.js WIP

---
+++ src/components/ToDoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ToDoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [], 
+      listItems: ''
+    };
+  }
+}
+
+listItemsInput = (event) => {
+  this.setState ({ listItems: event.target.value });
+}
+
+// addlistItems = (event) {
+//   event.preventDefault();
+
+// }
