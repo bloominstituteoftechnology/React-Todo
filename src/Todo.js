acd8b550b0ce9c21733da67d52059d301045bd89
Brendan <bmeredith@openmailbox.org>
2018-03-05 17:15:55 -0600

    Working input

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            todoListItem: ['laundry','washing dishes', 'gardening'],
+            newTodo: ''
+        };
+    }
+
+    handleTodoListInput = (event) => {
+        this.setState({ newTodo: event.target.value });
+    };
+
+    addTodoList = (event) => {
+        event.preventDefault();
+        const todoList = this.state.todoListItem;
+        todoList.push(this.state.newTodo);
+        this.setState({
+            newTodo: '',
+            todoListItem: todoList
+        });
+    };
+
     render() {
         return (
-            <div>Todo Stuff</div>
+            <div>
+                {this.state.todoListItem.map(item => <div>{item}</div>)}
+                <form onSubmit={this.addTodoList}>
+                    <input type="text" onChange={this.handleTodoListInput} placeholder="Add new todo item" value={this.state.newTodo} />
+                </form>
+            </div>
         );
     }
 }
