495a1a3a08b696f65f15cb40d823aa5727bb728b
Cristina Laryea <crispad06@gmail.com>
2018-03-05 16:28:24 -0500

    added todo.js file and enter code

---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+    render() {
+        return (
+            <div>Todo Stuff</div>
+        );
+    }
+}
+
+export default Todo;
