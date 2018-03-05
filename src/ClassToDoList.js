0badba7308f27a7085cc46bd2006ba9a8e685d55
liz <elizabethbraggisgreat@gmail.com>
2018-03-05 17:03:50 -0500

    pre q&a commit

--- src/ClassToDoList.js
+++ src/ClassToDoList.js
@@ -4,18 +4,31 @@ class ClassToDoList extends Component {
     constructor() {
         super ();
         this.state = {
-            toDoTask: ['make to do list', 'sumbit pr', 'praise hallelujah'],
+            toDoTaskList: ['make to do list', 'sumbit pr', 'praise hallelujah'],
             newTask: ''
         };
     }
+    addTask = (event) => {
+        event.preventDefault();
+        const taskList = this.state.toDoTaskList;
+        taskList.push(this.dtate.newTask);
+        this.setState({
+            newTask: '',
+            toDoTaskList: taskList,
+        });
+    };
+
+    handleTaskInput = (event) => {
+        this.setState({ ToDoTask: event.target.value });
+    };
 
 
     render() {
         return (
             <div> 
-                {this.state.toDoTask.map(toDoTask => <div>{toDoTask}</div>)}
+                {this.state.toDoTaskList.map(toDoTaskList => <div>{toDoTaskList}</div>)}
                 <form onSubmit={this.newTask}>
-                    <input type="text" placeholder="Add a new task" value={this.state.newTask}/>
+                    <input type="text" onChange={this.handleTaskInput} placeholder="Add a new task" value={this.state.newTask}/>
                     </form>
             </div>
         )
