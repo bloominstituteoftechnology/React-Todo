608bbb0cb83f910d27baf70557d8ef90d6e5260e
DragonRydrz <DragonRydrz@me.com>
2018-03-05 16:46:42 -0500

    TodoList tentatively completed

--- src/TodoList.js
+++ src/TodoList.js
@@ -13,11 +13,25 @@ class TodoList extends Component {
     handleTodoInput = (event) => {
         this.setState({newTodo: event.target.value})
     } 
+    submitChanges = (event) => {
+        event.preventDefault();
+        // const todoList = this.state.todos;
+
+        this.setState({
+            todos: [ ...this.state.todos, this.state.newTodo ],
+            newTodo: ''
+        })
+    }
     render(){
         return(
             <div>
                 {this.state.todos.map(todosItems => <Todo thing={todosItems}/>)}
+                <form onSubmit={this.submitChanges}>
+                    <input type='text' onChange={this.handleTodoInput} placeholder='Tell me what you need to do'/>
+                </form>
             </div>
         )
     }
 }
+
+export default TodoList;
