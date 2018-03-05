9434d9ad0ac572d7bdfb37cce62694306f48fd52
unknown <adfaris@gmail.com>
2018-03-05 16:59:03 -0500

    Working on the React-I

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      todo: ['Do laundry', 'buy groceries'], 
+      newTask : '',
+    };
+  }
 
-  </div>
-);
+  addNewTask = (event) => {
+    event.preventDefault();
+    const newTaskList = this.state.todo; 
+    newTaskList.push(this.state.newTask);
+    this.setState({
+      todo: newTaskList,
+      newTaskList: '',
+      });
+  };
+
+  handleTodoTaskInput = (event) => {
+    this.setState({newTask: event.target.value});
+  };
+
+  render(){
+    return(
+      <div> 
+        <div> <h2>Todo App</h2></div>
+        {this.state.todo.map((todo,index) => <div key={index}>{todo}</div>)}
+        <form onSubmit = {this.addNewTask}>
+          <input type = "text" onChange={this.handleTodoTaskInput} placeholder="Add a new Task" value={this.state.addNewTask}/>
+        </form>
+      
+      </div>
+    );}
+}
 
 export default App;
