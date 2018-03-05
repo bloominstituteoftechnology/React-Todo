75ad566151028697300abaec1233b3e3ca5b6f2d
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-03-05 18:23:35 -0500

    Stone Chris React-I WIP
    Stone Chris React-I WIP

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
+import ChildComponent from './ChildComponent';
 
 class ToDoList extends Component {
   constructor(){
     super();
     this.state = {
       list: ['clean bathroom', 'mow lawn', 'bath dog'],
-      addToList: ''
+      newTask: '',
     }
   }
 
-  handleClick = () =>{
-    this.setState({ clicked: this.state.clicked });
-    console.log('clicked: ', this.state.clicked);
-  }
+  addTask = (event) => {
+    event.preventDefault();
+    const taskList = this.state.list;
+    taskList.push(this.state.newTask);
+    this.setState({
+      newList: '',
+      list: taskList,
+    });
+  };
+
+  handleTaskInput = (event) => {
+    this.setState({ newList: event.target.value });
+  };
 
   render(){
     return (
       <div> 
-        {this.state.list.map(list2 => <div>{list2}</div>)}
+        {this.state.list.map((task, i) => <ChildComponent key={i} thing={task} />)}
+        <form onSubmit={this.addTask}>
+          <input type="text" onChange={this.handleTaskInput} placeholder="Add a new Task" value={this.state.newTask} />
+        </form>
       </div>)
-    return (
-      <div onClick={this.handleClick}>{this.props.thing}</div>
-    )
   }
 }
 
