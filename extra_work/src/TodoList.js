d1da6e9a826617552d6fcb50a2681559ac4cf8a7
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-03-06 15:21:00 -0800

    rebuilt Todo App and added ability to remove items with filter function

---
+++ extra_work/src/TodoList.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import TodoItems from './TodoItems';
+import './TodoList.css';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    addItem(e) {
+        if (this._inputElement.value !== '') {
+            const newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+
+            this._inputElement.value = '';
+        }
+
+    console.log(this.state.items);
+
+    e.preventDefault();
+    }
+
+    deleteItem(key) {
+        const filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+
+        this.setState({
+            items: filteredItems
+        });
+    }
+    
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a} // Research alternatives to ref
+                            placeholder="enter task">
+                        </input>
+                        <button type="submit">add</button>
+                    </form>
+                </div>
+                <TodoItems entries={this.state.items}
+                            delete={this.deleteItem} />
+            </div>
+        );
+    }
+}
+
+export default TodoList;
