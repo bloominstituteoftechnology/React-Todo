28331bd22b41a5a13802b7cdf507a7fe2840eb77
kait-schorr <kaitlynschorr@gmail.com>
2018-03-06 16:13:23 -0600

    Trying to get session storage to work

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,12 +4,28 @@ import Todo from "./Todo.js";
 class TodoList extends Component {
   constructor() {
     super();
-
     this.state = {
       todos: [],
       newTodo: ""
     };
   }
+  initStorage = () => {
+    if (JSON.parse(sessionStorage.getItem("todos")) === "undefined") {
+      sessionStorage.setItem("todos", JSON.stringify([]));
+    }
+    if (JSON.parse(sessionStorage.getItem("todos")) !== []) {
+      this.setState({
+        todos: this.todos.concat(JSON.parse(sessionStorage.getItem("todos")))
+      });
+    }
+    console.log("Init storage");
+    console.log(sessionStorage);
+  };
+  updateStorage = () => {
+    sessionStorage.setItem("todos", JSON.stringify(this.todos));
+    console.log("Updated storage");
+    console.log(sessionStorage);
+  };
   handleTodoInput = event => {
     this.setState({ newTodo: event.target.value });
   };
@@ -18,17 +34,21 @@ class TodoList extends Component {
     const todoList = this.state.todos;
     todoList.push({ text: this.state.newTodo, completed: false });
     this.setState({ newTodo: "", todos: todoList });
+    this.updateStorage();
   };
   removeTodo = num => {
     console.log(num);
     const todoList = this.state.todos;
     todoList.splice(num, 1);
     this.setState({ todos: todoList });
+    this.updateStorage();
   };
   render() {
     return (
       <div>
-        {this.state.todos.map((todo, i) => (
+        {this.initStorage}
+        {this.updateStorage}
+        {JSON.parse(sessionStorage.getItem("todos")).map((todo, i) => (
           <div>
             <Todo
               key={i}
