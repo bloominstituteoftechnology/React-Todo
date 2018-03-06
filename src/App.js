12ef9bb48514a17cd0b4719f570ead95baa85b99
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-03-06 13:35:47 -0500

    getting syntax, undefined errors, three total, not sure why

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ChildComponent from './ChildComponent';
 
 const App = () => (
   <div>
@@ -6,5 +7,43 @@ const App = () => (
 
   </div>
 );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ''
+    };
+
+addTodo = (event) => {
+    event.preventDefault();
+    const todoList = this.state.todos;
+    todoList.push(this.state.newTodo);
+    this.setState({
+      newTodo: '',
+      todos: todoList
+    });
+  }
+};
+
+handleTodoInput = (event) => {
+  this.setState({ newTodo: event.target.value });
+};
+
+render() {
+    return (
+        <div>
+            {this.state.todos.map(todo => <ChildComponent thing={todo} />)}
+            <form onSubmit={this.addTodo}>
+                <input type="text" onChange={this.handleTodoInput} placeholder="Add a new Todo" value={this.state.newTodo} />
+            </form>
+        </div>
+    );
+}
+}
+
+
+
 
 export default App;
---
+++ src/ChildComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ChildComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  }
+
+  render() {
+    console.log('props: ', this.props);
+    const styles = this.state.clicked ? { textDecoration: 'line-through' } : { textDecoration: 'none' };
+    return (
+      <div style={styles} onClick={this.handleClick}>{this.props.thing}</div>
+    )
+  }
+}
+
+export default ChildComponent;
