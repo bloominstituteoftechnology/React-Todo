4f7b710e71e12e66f69aa9374d0c57a041716224
Edward Manda <lilkups@yahoo.com>
2018-03-06 13:45:14 -0600

    JS on react done!

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,30 +3,34 @@ import React, { Component } from 'react';
 class Todo extends Component {
     constructor() {
         super();
-        this.state = {
-            clicked: false,
-        };
+        this.state = {};
     }
-    completed = (event) => {
-        const clicked = !this.state.clicked;
-        this.setState({
-            clicked: clicked,   
-        });
-        this.props.completed(event);
+    completed = () => {
+        const clicked = !this.props.todo.completed;
+        
+        this.props.completed(this.props.index, clicked);
+    }
+    remove = () => {
+        this.props.remove(this.props.index);
     }
 
     render() {
-        const clicked = this.state.clicked ? 'line-through' :'none';
+        const clicked = this.props.todo.completed ? 'line-through' :'none';
         const parentStyles = {
             textDecoration: clicked,
             padding: '.2rem .2rem .2rem 0.1rem',
             cursor: 'pointer',
         };
-        const xStyles = {
-            color: 'red',
-        }
         return(
-            <div style={parentStyles} onClick={this.completed}>{this.props.todo.text} <span>::</span> <span style={xStyles}>x</span></div>
+            <li>
+                <div style={parentStyles} onClick={this.completed}>
+                    {this.props.todo.text} 
+                </div>
+                <button style={{background: 'red', color: 'white',}} 
+                    onClick={this.remove}>
+                    delete
+                </button>
+            </li>
         );
     }
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,15 +4,58 @@ import Todo from './Todo';
 class TodoList extends Component {
     constructor(){
         super();
-        //this.handler = this.handler.bind(this);
         this.state = {
             todos: [],
             newTodo: {text: '', completed: false},
         };
     }
 
-    completed = (child) => {
-        console.log("from child", child.target);
+    componentDidMount() {
+        this.checkLocalStorage();
+    }
+
+    checkLocalStorage = () => {
+        //get from local storage
+        if(this.getTodos()) {
+            const localTodos = JSON.parse(this.getTodos());
+            this.setState({
+                // set the todos
+                todos: localTodos,
+            });
+        }
+    }
+
+    getTodos = () => {
+        return localStorage.getItem("todos");
+    }
+
+    setTodo = (item) => {
+        return localStorage.setItem('todos', item);
+    }
+
+    removeTodos = () => {
+        return localStorage.removeItem("todos");
+    };
+
+    completed = (index, clicked) => {
+        const newTodos = this.state.todos;
+        newTodos[index].completed = clicked;  
+        this.setState({
+            todos: newTodos,
+        });
+
+        // add todo to local storage
+        this.setTodo(JSON.stringify(newTodos));
+    }
+
+    remove = (index) => {
+        const newTodos = this.state.todos;
+        newTodos.splice(index,1);  
+        this.setState({
+            todos: newTodos,
+        });
+        // add/ update todo to local storage
+        this.setTodo(JSON.stringify(newTodos));
     }
 
     addTodo = (event) => {
@@ -29,6 +72,9 @@ class TodoList extends Component {
             todos: newTodos,
             newTodo: {text: '', completed: false},
         });
+
+        // add/update todo to local storage
+        this.setTodo(JSON.stringify(newTodos));
     }; 
     
     getToDoItemInput = (event) => {
@@ -43,7 +89,7 @@ class TodoList extends Component {
         return (
             <div>
                 {this.state.todos.map((todo, i) => {
-                    return <Todo key={i} todo={todo} completed={this.completed}/>
+                    return <Todo key={i} index = {i} todo={todo} completed={this.completed} remove={this.remove}/>
                 })}
                 <form onSubmit={this.addTodo}>
                     <input text="text" onChange={this.getToDoItemInput} placeholder="Enter a to do item" value={this.state.newTodo.text}/>
