fd6204731d80ba10a3447e27a6a0467c15e581be
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-03-05 21:22:15 -0800

    all tests passed :)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -14,7 +14,7 @@ class Todo extends Component {
     render() {
         const styles = this.state.clicked ? { textDecoration: 'line-through' } : { textDecoration: 'none' };
         return (
-            <div style={styles} onClick={this.handleClick}>{this.props.items}</div>
+            <div style={styles} onClick={this.handleClick}>{this.props.todo}</div>
         );
     }
 }
