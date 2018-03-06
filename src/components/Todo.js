d67d5ca3c7beb00b76d10b23b493b3a865a86566
zackhitch <zackhitch@users.noreply.github.com>
2018-03-06 11:38:08 -0800

    delete and strikethrough functionality completed - ZH

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,17 +13,19 @@ class Todo extends Component {
     this.setState({ clicked: !this.state.clicked });
   };
 
-  handleDel() {
+  handleDel = () => {
     this.props.delTodo.bind(this.props.parent, this.props.index)();
-  }
+  };
 
   render() {
-    // const styles = this.state.clicked
-    //   ? { textDecoration: 'line-through' }
-    //   : { textDecoration: 'none' };
+    const styles = this.state.clicked
+      ? { textDecoration: 'line-through' }
+      : { textDecoration: 'none' };
     return (
-      <div style={styles} onClick={this.handleClick}>
-        {this.props.todo}
+      <div>
+        <div style={styles} onClick={this.handleClick}>
+          {this.props.todo}
+        </div>
         <button onClick={this.handleDel}>x</button>
       </div>
     );
