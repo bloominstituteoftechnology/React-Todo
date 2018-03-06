fdce112cc8bfc8b1d8d68f979f7c08f210839e61
Charletta <charlettabullard12@outlook.com>
2018-03-06 17:23:45 -0500

    Added the Completed.css, answered questions in answers.md, got the x to appear next to items but could not delete items. Have a commented delete code still researching. WIP

--- src/Completed.js
+++ src/Completed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Completed.css';
 
 class Completed extends Component {
   constructor() {
@@ -12,12 +13,18 @@ class Completed extends Component {
     this.setState({ clicked: !this.state.clicked });
   };
 
-
+  handleXClick = () => {
+    this.props.action(this.props.index);
+  }
 
   render() {
     const styles = this.state.clicked ? { textDecoration: 'line-through' } : { textDecoration: 'none' };
     return (
-      <div style={styles} onClick={this.handleClick}>{this.props.itemKey}</div>
+      <div>
+        <div id="one" onClick={this.handleXClick}>x</div>
+        <div id="two" style={styles} onClick={this.handleClick}>{this.props.itemKey}</div>
+        {/* <div style={styles} onClick={this.handleClick}>{this.props.itemKey}</div> */}
+      </div>
     );
   }
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -13,34 +13,49 @@ class ToDoList extends Component {
 
 addItem = event => {
     event.preventDefault();
-    const list = this.state.itemsList;
-    list.push(this.state.newItem);
-    this.setState({
-        newItem: '',
-        itemsList: list
-    });
+    // const list = this.state.itemsList;
+    // list.push(this.state.newItem);
+    if (this.state.newItem !== '') {
+        this.setState({
+            newItem: '',
+            itemsList: [...this.state.itemsList, this.state.newItem]
+        });
+    }   
 };
 
 handleListInput = event => {
     this.setState({ newItem: event.target.value });
 };
 
-    render() {
-        return (
-            <div>
-            {this.state.itemsList.map(item => <Completed itemKey={item} />)}
-            <form onSubmit={this.addItem}>
-            <input
-            type="text"
-            onChange={this.handleListInput}
-            placeholder="Add a new item"
-            value={this.state.newItem}
-            />
-            </form>
-            </div>
-            );
-        }
+completeFunc = index => {
+    this.setState({
+        selected: index
+    });
+}
+
+// delete(itemsList){
+//     const newState = this.state.data.slice();
+//     if (newState.indexOf(itemsList) > -1) {
+//       newState.splice(newState.indexOf(itemsList), 1);
+//       this.setState({data: newState})
+//     }
+//   }
+
+
+render() {
+    return (
+        <div>
+        {this.state.itemsList.map((item, i) => <Completed itemKey={item} index={i} action={this.completeFunc} />)}
+        <form onSubmit={this.addItem}>
+            <input type="text" onChange={this.handleListInput} placeholder="Add a new item" value={this.state.newItem} />
+        </form>
+        </div>
+        );
     }
+}
+
+
+
 
 
 export default ToDoList;
