039a25f1be4e5d320ab1f52af48730e94e439ef6
Viveca Valdez <vivecavaldez@Vivecas-MBP.fios-router.home>
2018-03-05 19:19:17 -0500

    Vivi Valdez & Charletta Bullard WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import ToDoList from './components/ToDoList';
+
+const styles = {
+    fontFamily: 'verdana',
+    textAlign: 'left',
+    fontColor: 'darkPink',
+};
+
 
 render(<App />, document.getElementById('root'));
