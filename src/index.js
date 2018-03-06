13823453e0a97ea7a92b5a5db2a1daecb6c7bc72
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-03-05 16:42:41 -0800

    Completed Project Minus Extra Credit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import Todo from './components/Todo'; // Added
 import TodoList from './components/TodoList'; // Added
+
 
 render(<App />, document.getElementById('root'));
