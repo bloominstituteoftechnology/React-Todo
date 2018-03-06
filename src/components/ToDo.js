dad4224f79d19a5a055713a375edb451aaf0600c
joshuacoyne <josh.j.coyne@gmail.com>
2018-03-05 20:13:19 -0500

    close out functionality working. Needs styling

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+
 class ToDo extends Component {
     constructor() {
         super();
 
         this.state = {
             
-            clicked: false
+            clicked: false,
+            xed: false
         };
     }
 
@@ -14,10 +16,18 @@ class ToDo extends Component {
         this.setState({ clicked: !this.state.clicked });
     };
 
+    xOut = () => { 
+        this.setState({ xed: !this.state.xed});
+    };
+
     render() {
         const styles = this.state.clicked ? { textDecoration: 'line-through'} : { textDecoration: 'none' };
+        const show = this.state.xed ? {display: 'none'} : {display: 'flex'};
         return(
-            <div style={styles} onClick={this.handleClick}>{this.props.text}</div>
+            <div style={show}>
+                <div onClick={this.xOut}>X</div>
+                <div style={styles} onClick={this.handleClick}>{this.props.text}</div>
+            </div>
         );
     }
 }
