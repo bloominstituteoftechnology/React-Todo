74d0e8b3c6fb401fabfdcf9b2fbec3fb337fdcfa
dc5stars <dc5stars@yahoo.com>
2018-03-06 19:11:42 -0500

    Completed form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,38 +2,44 @@ import React, { Component } from 'react';
 
 class ToDoList extends Component {
     constructor() {
-
         super();
 
         this.state = {
-        toDoItems: ['A', 'B', 'C'],
-        newToDo: '',
-
-
-        };    
+            toDoItems: ['A', 'B', 'C'],
+            newToDo: '',
+        };  
     };
-
+    //there was an event passed below
     addToDo = (event) => {
-        //event.preventDefault();
-        console.log('This is the newToDo: ', this.state.newToDo);
-        const toDoItemsList = this.state.toDoItems;
+        event.preventDefault();
+        //console.log('This is the newToDo: ', this.state.newToDo);
+        //const value = document.querySelector('input').value;
+        let toDoItemsList = this.state.toDoItems;
         toDoItemsList.push(this.state.newToDo);
         
+        
         this.setState ({
-            newToDo: '',
+            
             toDoItems: toDoItemsList,
+            newToDo: 'Some random bullshit',
         });
+        
+    };
+
+    handleInput = (event) => {
+        console.log(`This is the onChange symbols: `, event.target.value);
+        //this.newToDo = event.target.value;
+        this.setState ({ newToDo: event.target.value});
     };
     
     render() {
         return (
             <div>
-                {this.state.toDoItems.map((toDoItem) => <div>{toDoItem}</div>)}
-            
-                <p/>
-                <form onSubmit={this.addToDo()}>
-                    <input type='text' placeholder='Please enter some text' value={this.state.newToDo} />
+                {this.state.toDoItems.map((toDoItem, i) => <div key={i}>{toDoItem} </div>)}
+                <br/><form onSubmit={this.addToDo}>
+                <input type='text' placeholder='Please enter some text' onChange={this.handleInput} value={this.state.newToDo}/>
                 </form>
+                {/* {<div>{this.state.newToDo}</div>} */}
                 
             </div>
         );
@@ -41,6 +47,6 @@ class ToDoList extends Component {
     };
         
 
-    };
+};
 
-    export default ToDoList;
+export default ToDoList;
