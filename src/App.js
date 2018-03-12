81cd7c165beecd3ab5594d109293ded569858daf
Jared Cooper <jaredrcoopet@gmail.com>
2018-03-12 16:26:50 -0400

    Everything is working!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import InputBar from './InputBar';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+class App extends Component {
+    constructor() {
+        super();
 
-  </div>
-);
+        this.state = {
+            toDos: [],
+            newtoDos: ''
+        };
+    }
+
+    addtoDos = (event) => {
+        event.preventDefault();
+        const toDossList = this.state.toDos;
+        toDossList.push(this.state.newtoDos);
+        this.setState({
+            newtoDos: '',
+            toDoss: toDossList
+        });
+    };
+
+    handletoDosInput = (event) => {
+        this.setState({ newtoDos: event.target.value });
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.toDos.map((toDos, i) => <InputBar key={i} thing={toDos} />)}
+                <form onSubmit={this.addtoDos}><input type="text" onChange={this.handletoDosInput} placeholder="New task" value={this.state.newtoDos} /></form>
+            </div>
+        );
+    }
+}
 
 export default App;
