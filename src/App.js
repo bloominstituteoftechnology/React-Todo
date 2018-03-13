975eadf0b656d0ff437d99bc8de17ec5bf10dd10
Edson Hutton <SUPATURBOLOCITY@users.noreply.github.com>
2018-03-13 15:41:39 -0400

    Configuring Todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Todolist from './Todolist';
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+  color: 'red',
+};
 
 const App = () => (
-  <div>
-    <h2>Todo App</h2>
-
+  <div style= {styles}>
+    <h2>To Do List</h2>
+    <Todolist/>
   </div>
 );
 
