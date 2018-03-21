72833824aa58297029cc5a46ba5492ed0e7795f5
Stephanie Brooks <sbrooks84591@hotmail.com>
2018-03-20 22:34:10 -0500

    WIP

---
+++ src/Components/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export default class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+}
