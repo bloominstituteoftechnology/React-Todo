8a6f425c1e879826709e710e4d5438a83e426ae9
Lisa Cee <three_nineteen@yahoo.com>
2018-03-20 19:22:58 -0700

    initial commit for tests

--- src/App.js
+++ src/App.js
@@ -7,4 +7,5 @@ const App = () => (
   </div>
 );
 
+
 export default App;
---
+++ src/Components/ToDoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class ToDoList extends Component {
+    constructor() {
+        super();
+        this.state = [];
+    }
+
+    render() {
+        
+    }
+}
+
+export default ToDoList;
