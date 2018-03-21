7ee06deec6f0c53105baa2cc29774afd80172ee6
John Coronel <john.m.coronel@gmail.com>
2018-03-20 19:01:04 -0700

    update

---
+++ src/TodoItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            marked: false
+        }   
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+    
+}
+
+export default TodoItem;
---
+++ src/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+class TodoList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            todoItems:[]
+        }
+    }
+        render() {
+            return (
+                <div
+            )
+        }
+    }
+
+    export default Todolist;
