16fec50c5bb1392d11f08822071819dc7e73aa09
Lisa Cee <three_nineteen@yahoo.com>
2018-03-21 21:02:39 -0700

    5/7 passing, newTodo being rendered

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Todo = (props) => {
-    console.log("PROPS", props);
+    // console.log("PROPS", props);
     return (
         <div>
-            <ul> {this.props.todos.map((todo, i) => {
-                return <li key = {i}>{ todo }</li>
-            })} </ul> 
+            { props.todo }
         </div>    
     );
 }
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
 
-class TodoList extends Component {
+ class TodoList extends Component {
     constructor() {
         super();
         this.state = {
@@ -14,6 +15,7 @@ class TodoList extends Component {
       const todos = this.state.todos;
       console.log("TODOS", todos);
       todos.push(this.state.newTodo);
+
       this.setState({
         newTodo: '',
         todos: todos,
@@ -24,21 +26,26 @@ class TodoList extends Component {
       this.setState({ newTodo: event.target.value });
     };
 
-    completeTodo = (event) => {
-
-    }
-
+  
     render() {
         return (
           <div>
-            { this.state.todos.map(todo => <div>{todo}</div>) }
-        
-            <form onSubmit={this.addTodo}>
-              <input type="text" onChange={this.handleNewTodo} placeholder="Add a task!" value={this.state.newTodo} />
+            { this.state.todos.map((todo, i) => {
+              return (
+              <Todo key={i} todo={todo}/> 
+              )
+            })
+          }
+
+            <form onSubmit={ this.addTodo }>
+              <input type="text" onChange={ this.handleNewTodo } placeholder="Add a task!" value={ this.state.newTodo } />
             </form>
+              
           </div>
-        );
-    }
+        
+      ) 
+    
+}
 }
 
 export default TodoList;
