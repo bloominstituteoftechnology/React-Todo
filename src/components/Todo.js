6446bd64e3627a686585ebaeb53d8f13bc8e11ef
Stephanie <sbowl001@fiu.edu>
2018-03-22 10:39:27 +0800

    solution with annotations

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';  
 
 class Todo extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            clicked: false
+            clicked: false // false until proven true
         };
     }
     handleClick = () => {
-        this.setState({ clicked: !this.state.clicked});
+        this.setState({ clicked: !this.state.clicked}); //now it's true. changing this.state.clicked
     }
-    render() {
+    render() { //about to return dynamic html 
         const styles = this.state.clicked? { textDecoration: 'line-through'} : { textDecoration: 'none'};
         return (
             <div style= {styles} onClick= {this.handleClick}>
             {this.props.todo}
-            </div>
+            </div> //curly brackets- variables. everything else is regular html.  JSX
         );
     }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,18 +5,18 @@ class TodoList extends Component {
     constructor() {
         super();
         this.state = {
-            todos: [],
-            newTodo: '',
+            todos: [], //save
+            newTodo: '', //add
         };
     }
     handleTodoInput = (event) => {
-        this.setState({ newTodo: event.target.value});
+        this.setState({ newTodo: event.target.value}); //handling the typing   //event.target.value = new todo string as I'm typing
     }
-    addTodo = (event) => {
-        event.preventDefault();
+    addTodo = (event) => {   //for submitting the form (when i'm clicking enter)
+        event.preventDefault(); //so it doesn't reset
         const todosList = this.state.todos;
         todosList.push(this.state.newTodo);
-        this.setState({
+        this.setState({  //updating the State, since we don't want to modify this.state directly
             newTodo: '',
             todos: todosList
         });
