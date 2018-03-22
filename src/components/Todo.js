d71b685113a54391fbd115633608853766031910
jnandez0586 <jnandez0586@gmail.com>
2018-03-21 21:08:38 -0500

    Created Todo.js and TodoList.js. Created classes for both files

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Todo extends Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return <div>Todo</div>
+    }
+}
+
+export default Todo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return <div>TodoList</div>
+    }
+}
+
+export default TodoList;
