2d64b9f1365d205aaa2f736ebd7ca79230be9f32
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-22 16:40:59 -0600

    refactor todo generation in own fn, wire up toggleTodo event

---
+++ src/components/Todo.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() { return (<div>todo item</div>) }
+}
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,31 +1,67 @@
 import React, { Component } from 'react'
+import { v4 } from 'uuid'
+//import Todo from './Todo'
+
+// generateTodo :: Function -> String -> Object[Todo]
+function generateTodo(idFn) {
+  return function(text) {
+    return { text, completed: false, id: idFn() }
+  }
+}
+
+// makeTodo :: String -> Object[Todo]
+const makeTodo = generateTodo(v4)
 
 export default class TodosList extends Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      todos: ['todo 1', 'todo 2'],
-      newTodo: ''
+      todos: [ makeTodo('todo A'), makeTodo('todo B')],
+      newTodo: makeTodo('')
     }
   }
 
-  handleChange = (event) => {
-    this.setState({ newTodo: event.target.value })
+  handleChange = (e) => {
+    this.setState({ newTodo: makeTodo(e.target.value) })
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    let todos = this.state.todos
-    todos.push(this.state.newTodo)
-    this.setState({ todos, newTodo: '' })
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState((prevState, props) => {
+      const todo = prevState.newTodo
+      return !todo.text
+	? undefined
+        : {
+	  todos: prevState.todos.concat(makeTodo(todo.text)),
+	  newTodo: makeTodo('')
+	}
+    })
   }
+
+  toggleTodo = (e) => {
+    e.preventDefault()
+    console.log(e)
+    this.setState((prevState, props) => {
+      return prevState.todos.map((todo) => {
+	console.log(e.target)
+
+	todo.id === e.target
+      })
+    })
+  }
+
 
   render() {
     return (
       <div>
-	<ul>{this.state.todos.map((todo, i) => <li key={i}> {todo} </li>)}</ul>
+	<ul>
+	  {this.state.todos.map((todo) => (
+	    <div key={todo.id}>{todo.text} <a onClick={this.toggleTodo} href=''>&times;</a></div>
+	  ))}
+	</ul>
 	<form onSubmit={this.handleSubmit}>
-	  <input onChange={this.handleChange} type="text" value={this.state.newTodo} />
+	<input onChange={this.handleChange} value={this.state.newTodo.text} />
 	</form>
       </div>
     )
