073445674a573eb4f48647158828c90d8efc871e
jnandez0586 <jnandez0586@gmail.com>
2018-03-22 20:39:45 -0500

    commiting so I can git pull

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-
+import TodoList from './TodoList';
 class Todo extends Component {
     constructor(){
         super();
+        this.state = {
+            clicked: false
+        };
     }
+   
 
     render() {
-        return <div>Todo</div>
+        return <div>{this.props.value}</div>
     }
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import Todo from './Todo';
 
 class TodoList extends Component {
-    constructor(){
-        super();
-    }
+    constructor(props){
+        super(props);
+        this.state = ['Wash clothes', 'Buy Milk']
+    };
+
+ // addTask = (event) => {
+    //     event.preventDefault();
+    //     const addTask = this.state.tasks;
+    //     TodoList.push(this.state.newTask);
+    //     this.setState({
+    //         newTask: '',
+    //         tasks: TodoList
+    //     });
+    // }
 
     render() {
-        return <div>TodoList</div>
+        return (
+            <div className='TodoList' >
+                <h2>TodoList</h2>
+                {this.state.map((value, index) => 
+                    <Todo key={index} 
+                        value={ value} />
+                )}
+            </div>
+        )
     }
 }
 
