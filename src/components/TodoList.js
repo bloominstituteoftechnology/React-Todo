a3c8a5d28c4b5d2c57e71756fb6b5c607f1590f4
Enjoyli Harrell <joli@Enjoylis-iMac.local>
2018-03-22 18:45:03 -0700

    Add input field and event handlers

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,26 +7,35 @@ class TodoList extends Component {
         this.state = ['Wash clothes', 'Buy Milk']
     };
 
- // addTask = (event) => {
-    //     event.preventDefault();
-    //     const addTask = this.state.tasks;
-    //     TodoList.push(this.state.newTask);
-    //     this.setState({
-    //         newTask: '',
-    //         tasks: TodoList
-    //     });
-    // }
+ addTask = (event) => {
+        event.preventDefault();
+        const TodoList = this.state.tasks;
+        TodoList.push(this.state.newTask);
+        this.setState({
+            newTask: '',
+            tasks: TodoList
+        });
+    }
+
+handleTaskInput = (event) => {
+    this.setState({ newTask: event.target.value});
+};
 
     render() {
         return (
             <div className='TodoList' >
                 <h2>TodoList</h2>
-                {this.state.map((value, index) => 
-                    <Todo key={index} 
-                        value={ value} />
+                {this.state.map((value, i) => <Todo key={i} value={value} />
                 )}
+
+                <form onSubmit={this.addTask}>
+                    <input type='text'
+                    onChange={this.handleTaskInput}
+                    placeholder="Add new task"
+                    value={this.state.newTask}/>
+                    </form>
             </div>
-        )
+        );
     }
 }
 
