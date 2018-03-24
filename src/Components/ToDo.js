d8693fe4a98f1fc2de69734950a19dfff161c919
Lisa Cee <three_nineteen@yahoo.com>
2018-03-24 10:28:25 -0700

    7/7 passing

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-const Todo = (props) => {
-    console.log("PROPS", props);
-    return (
-        <div className = 'item'>
-            { props.todo }
-        </div>    
-    );
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false,
+            style: 'none',
+        }
+    }
+    clickHandler = (e) => {
+        const clicked = this.state.clicked;
+        const style = !clicked ? "line-through" : "none";
+        this.setState({clicked: !clicked, style});
+    }
+
+    render() {
+        const styles = {textDecoration: this.state.style};
+        return (
+            <div style = {styles} onClick = {this.clickHandler}>{this.props.todo}</div>
+        )
+    }
 }
 
 export default Todo;
