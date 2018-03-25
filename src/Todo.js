6f705fc476b76a5e9ab0ea7f38fe0e38ba64b67d
ubuntu <irishseanvaldivia@gmail.com>
2018-03-25 14:38:33 -0500

    Working on Todo app...

---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Todo exends Component {
+  constructor() {
+    super() 
+    this.state = {
+      todoList = [],
+      newTodo = ""
+    }
+  }
+}
