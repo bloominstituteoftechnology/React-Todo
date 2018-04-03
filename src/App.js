4fe4fd919058bdf291fd2c34e725e99f43a35c3f
totsuya <supertatortots@gmail.com>
2018-04-03 17:55:40 -0400

    added working strikethru

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import Input from './components/Input.js';
 import Todo from './components/Todo.js';
 
+
+
 class App extends Component {
   constructor() {
     super();
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+// const strike = () => {
+//   return ({
+//     'textDecoration': 'line-through'
+//   })
+// }
+
+const lineThrough = (e) => {
+  e.target.style.textDecoration = (e.target.style.textDecoration === 'line-through') ? "none" : "line-through";
+}
+
+// const logThis = (e) => {
+//   console.log(e.target.style);
+// }
+
 const Todo = props => {
-  return (
-    <div>
-    {props.toDos.map((todo, index) => (
-      <div key={index + todo}>{todo}</div>
-    ))}
+  return (<div>
+    {props.toDos.map((todo, index) => (<div onClick={lineThrough} key={index + todo}>{todo}</div>))}
   </div>)
 };
 
