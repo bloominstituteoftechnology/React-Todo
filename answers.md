11fb782085d910f035f9d76d426a4d8ca7d53f11
Henry K <mykim1891@hotmail.com>
2018-04-03 15:59:40 -0700

    Finished creating list, need stirkethrough

--- answers.md
+++ answers.md
@@ -1,8 +1,10 @@
 ### All Answers to Partner Study should be filled out in this file.
 
 1. Single Page Application
+The SPA refers to the applications that were built on HTML to closely resemble to desktop applications. (front-end)
 
 2. Compilers
+A program that transforms source code into object code, meaning it takes instruction from particular programing languages into machine language that computer's processor uses.
 
 3. Bundlers
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './src/index.css';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+// this portion trigger onChnage; refer to render()
+// assigns key to the elements of chores using both 'item' + 'index' values and display 'item'
+const ChoresList = props => {
+  return (
+    <div>
+      <h3 class="blue">{props.newChore}</h3>
+      {props.chores.map((item, index) => (
+        <h2 onClick={App.strikeThrough} key={item + index}>{item}</h2>
+      ))}
+    </div>
+  );
+};
 
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newChore : "",
+      chores:[]
+    };
+  }
+    // setState == refresh of the state
+  handleAddChore = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  // destructuring assignment of chores to this.state
+  // push from chores to this.state.newChore
+  // refresh chores; refresh newchores as ""
+  handleSubmitChore = e => {
+    const { chores } = this.state;
+    chores.push(this.state.newChore);
+    this.setState({ chores, newChore: "" });
+  }
+  
+  // need to fix this one
+  strikeThrough = e => {
+    console.log('this is working')
+    let choreList = this.state.chores
+    for (let i = 0; i < choreList.length; i++) {
+      choreList[i].style.setProperty("text-decoration", "line-through");
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Your To Do List:</h1>
+        <ChoresList {...this.state} />
+        <input type="text" name="newChore" value={this.state.newChore} placeholder="Write it down" onChange={this.handleAddChore} />
+        <button onClick={this.handleSubmitChore}>Add it to your list</button>
+      </div>
+    );
+  }
+}
 
 export default App;
