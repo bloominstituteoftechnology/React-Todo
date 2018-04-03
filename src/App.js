302869ed44ed012a06fcbd878089741d2b47224d
totsuya <supertatortots@gmail.com>
2018-04-03 15:40:57 -0400

    added placeholder tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Input from './components/Input.js';
+import Todo from './components/Todo.js';
+
 
 const App = () => (
   <div>
     <h2>Todo App</h2>
-
+    <Input />
+    <Todo />
   </div>
 );
 
