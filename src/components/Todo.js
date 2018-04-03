4378c5e0cdb43aa23fb8120a682c59ab9187014f
Devon Bacon <devon.bacon@gmail.com>
2018-04-03 16:02:19 -0700

    Added lineThrough function to Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const lineThrough = (e) => {
+  e.target.style.userSelect = 'none';
+  e.target.style.textDecoration = (e.target.style.textDecoration === 'line-through') ? "none" : "line-through";
+}
+
 const Todo = props => {
   return (
     <div>
