8ceb099b3d0e95e1831d3c25a612ef9f0f53b357
Devon Bacon <devon.bacon@gmail.com>
2018-04-03 16:01:02 -0700

    Added return statement to Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Todo = props => {
-  
+  return (
+    <div>
+      {props.toDos.map((todo, index) => (
+        <div onClick={lineThrough} key={index + todo}>{todo}</div>
+      ))}
+    </div>
+  )
 };
 
 export default Todo;
