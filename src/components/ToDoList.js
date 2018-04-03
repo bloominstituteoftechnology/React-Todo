3e2fea0136a0164e8a07b99edbd3bbff5f74139b
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-03 13:56:24 -0700

    Proj is populating tasks

---
+++ src/components/ToDoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ToDoList = props => {
+    return (
+        <div>
+            { props.toDos.map( (task, index) => (
+                <div key={task + index}> {task} </div>
+            ))}
+        </div>
+    );
+};
+
+export default ToDoList;
