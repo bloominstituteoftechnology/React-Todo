2eb9ab77516901b4d557d267aaf7373aadd4f784
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-03 14:15:20 -0700

    finished components.js, app.js and index.js without styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newTodo: "",
+      todoList: []
+    }
+  }
 
-  </div>
-);
+  handleAddItem = e => {
+    this.setState({ [e.target.item]: e.target.value });
+  }
+
+  handleSubmitItem = () => {
+    const { todoList } = this.state;
+    todoList.push(this.state.newTodo);
+    this.setState({ todoList, newTodo: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>My Todo List</h2>
+        <TodoList {...this.state} />
+        <input
+          type="text"
+          name="newTodo"
+          value={this.state.newTodo}
+          placeholder="add item"
+          onChange={this.handleAddItem}
+        />
+        <button onClick={this.handleSubmitItem}>Add Item</button>
+      </div>
+
+    )
+  }
+};
 
 export default App;
--- src/components/Components.js
+++ src/components/Components.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+const TodoList = props => {
+    return (
+        <div>
+            <h3 className="todoItem">{props.newTodo}</h3>
+            {props.todoList.map((item, index) => (
+                <div key={item + index}>{item}</div>
+            ))}
 
+        </div>
+    );
+};
 
 export default Components;
