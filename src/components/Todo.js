f4e29d38d73547bafda148a727164f3cf3513621
Harrison Brock <harrison@harrisonbrock.com>
2018-04-03 18:25:44 -0400

    All test are passing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,14 +3,20 @@ import React from "react"
 class Todo extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            clicked: false
+        }
+    }
+    
+    handleClick = () => {
+        const clicked = this.state.clicked;
+        this.setState({
+            clicked: !clicked});
     }
 
     render() {
-        return (
-            <div>
-            {this.props.todo}
-            </div>
-        );
+        const styles = this.state.clicked ? {textDecoration: 'line-through'} : {textDecoration: 'none'};
+        return <div style={styles} onClick={this.handleClick}>{this.props.todo}</div>
     }
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,21 +11,28 @@ class TodoList extends React.Component {
     }
 
     addTodo = (e) => {
+        e.preventDefault();
         const todos = this.state.todos;
         const newTodo = this.state.newTodo;
         todos.push(newTodo);
         this.setState({todos: todos, newTodo: ''});
     }
+
+    handleInput = (e) => {
+        e.preventDefault();
+        const newTodo = e.target.value;
+        this.setState({newTodo: newTodo});
+    };
     render() {
         return (
             <div>
-    
-            {this.state.todos.map((item, index) => {
-                return(<Todo key={index} todo={item} />)
-            })}
+
+            {this.state.todos.map((todo, i) => <Todo todo={todo} key={i}/>)}
+
             <form onSubmit={this.addTodo}>
-            <input type="text" placeholder="Add Todo" value={ this.state.newTodo } 
-               onChange={this.handleChange} />
+            <input type="text" placeholder="Add Todo" 
+               value={ this.state.newTodo } 
+               onChange={this.handleInput} />
                </form>
             </div>
         );
