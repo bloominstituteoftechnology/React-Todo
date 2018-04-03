02dc728bbad2f0a71250b0ca2d81518917531e1a
Chris <cjmarsh725@gmail.com>
2018-04-03 13:42:54 -0700

    Crucial emoji changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
+const styles = {
+  fontFamily: "sans-serif",
+  margin: "50px auto",
+  width: "200px",
+  fontWeight: "bold",
+  border: "1px solid gray",
+  padding: "20px",
+  paddingLeft: "40px",
+  paddingBottom: "40px",
+  userSelect: "none",
+  backgroundColor: "beige"
+};
+
 const itemClick = event => {
   if (event.target.style.textDecoration !== "line-through") {
     event.target.style.setProperty('text-decoration', 'line-through');
@@ -10,9 +23,11 @@ const itemClick = event => {
 
 const ListItem = props => {
   return (
-    <li onClick={itemClick}>{props.item}</li>
+    <li onClick={itemClick} key={props.key}>{props.item}</li>
   );
 }
+
+const emoji = String.fromCharCode(parseInt('1F4A9',16));
 
 class App extends Component {
   constructor() {
@@ -35,18 +50,19 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div style={styles}>
+        <h2>Shit List &#x1F4A9;</h2>
         <ul>
-          {this.state.items.map(x => <ListItem item={x} />)}
+          {this.state.items.map((x, i) => <ListItem item={x} key={x + i} />)}
         </ul>
         <input
           type="text"
           name="newItem"
           value={this.state.newItem}
-          placeholder="Add item"
+          placeholder="Add some shit"
           onChange={this.handleAddItem}
         />
-        <button onClick={this.handleSubmitItem}>Add Item</button>
+        <button onClick={this.handleSubmitItem}>Add Shit</button>
       </div>
     );
   }
