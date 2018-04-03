a9b062626d78f7ce01e2b591007b9c1d5575208f
Devon Bacon <devon.bacon@gmail.com>
2018-04-03 15:57:49 -0700

    Added input html element to Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Input = props => {
-  
+  return (
+    <div>
+      <input 
+        type="text"
+        name="textInput" 
+        value={props.textInput} 
+        placeholder="Put To Do Here!" 
+        onChange={props.parent.handleAddToDo}
+      />
+    </div>
+  )
 }
 
 export default Input;
