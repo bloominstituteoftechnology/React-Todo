988f7d856adc14cdef9deba2b7575fb8d245d1fd
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-03 13:49:01 -0700

    added todo list input and button to add tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Todo: [],
+      newTodo: ""
+    };
+  }
+
+  handleAddTodo = e => {
+    this.setState({ newTodo: e.target.value });
+  }
+
+  handleSubmitTodo = e => {
+    e.preventDefault();
+    const TodoList = this.state.Todo;
+    TodoList.push(this.state.newTodo);
+    this.setState({ Todo: TodoList, newTodo: "" });
+  }
+
+  render() {
+    return (
+      <div style={styles}>
+        <h2>Todo List</h2>
+        {this.state.Todo.map(todo => <div>{todo}</div>)}
+        <input type="text" value={this.state.newTodo} placeholder="new task" onChange={this.handleAddTodo} />
+        <button onClick={this.handleSubmitTodo}>Add Todo</button>
+      </div>
+    )
+  }
+
+};
 
 export default App;
