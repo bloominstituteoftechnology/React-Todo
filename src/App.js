d773438a5e72da9efa156a12553be726d3a3d4f1
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-03 13:44:10 -0700

    Task Button Added & Input Field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newToDo: "",
+      toDos: ["Finish Project", "Review", "Walk Dog", "Sleep", "Relax"]
+    };
+  }
 
-  </div>
-);
+  handleAddToDo = e => {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+  handleSubmitToDo = () => {
+    const { toDos } = this.state;
+    toDos.push(this.state.newToDo);
+    this.setState({ toDos, newToDo: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Kaitlyn's To Do List</h2>
+
+        <input 
+          type="text" 
+          name="newToDo" 
+          value={this.state.newToDo} 
+          placeholder="Add Task"
+          onChange={this.handleAddToDo}
+        />
+        <button onClick={this.handleSubmitToDo}> Add Task </button>
+      </div>
+    );
+  }
+
+}
 
 export default App;
