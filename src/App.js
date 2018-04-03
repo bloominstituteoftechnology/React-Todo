4a77017bdf7d70452d751ddb63f5dc0daf89443f
Velsu <riftmage50@gmail.com>
2018-04-04 00:49:26 +0200

    Project complete + Stretch Goals + localStorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList";
+import "./style.css";
 
 const App = () => (
-  <div>
-    <h2>Todo App</h2>
-
+  <div className="container">
+    <h2>Reactive Todo App</h2>
+    <TodoList />
   </div>
 );
 
