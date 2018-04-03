6623952399eb1cf5e25edf78e60114b09becb09f
totsuya <supertatortots@gmail.com>
2018-04-03 16:41:31 -0400

    added working text input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Input from './components/Input.js';
 import Todo from './components/Todo.js';
-
 
 class App extends Component {
   constructor() {
@@ -11,12 +10,20 @@ class App extends Component {
       toDos: ["hello"]
     };
   }
+
+  handleAddToDo = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
     return (<div>
       <h2>Todo App</h2>
-      <Input {...this.state}/>
+      <Input {...this.state} parent={this}/>
     </div>)
   }
+
 };
 
 export default App;
--- src/components/input.js
+++ src/components/input.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Input = props => {
   return (<div>
-    <input type="text" name="textInput" value={props.textInput} placeholder="Put To Do Here!" onChange={this.handleAddToDo}/>
-  <button onClick={this.handleSubmitToDo}>Add ToDo</button>
-</div>)
+    <input type="text" name="textInput" value={props.textInput} placeholder="Put To Do Here!" onChange={props.parent.handleAddToDo
+}/>
+    <button onClick={this.handleSubmitToDo}>Add To Do</button>
+  </div>)
 }
+
 export default Input;
