2763e4075857ac1de31ee18cdce4cc144513f74d
unknown <bsa6.23.94@gmail.com>
2018-04-03 17:40:30 -0400

    looking at completed functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import ItemsList from "./ItemsList"
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newListItem: "",
+      listItems: []
+    };
+  }
 
+  addListItem = e => {
+    this.setState({ [e.target.name]: e.target.value});
+  };
+  pushListItems = () => {
+    const { listItems } = this.state;
+    listItems.push(this.state.newListItem);
+    this.setState({ listItems, newListItem: ""});
+  };
+ 
+  render() {
+    return (
+      <div style={{display:'flex',justifyContent:'center', flexDirection: 'column', textAlign: 'center'}}>
+    <h2 style={{fontSize: 40}}>To Do List:</h2>
+    <div style={{fontSize: 40}}>
+    <ItemsList {...this.state} />
+    </div>
+    <input
+    style={{height: 40, textAlign: 'center'}}
+    type="text"
+    name="newListItem"
+    value={this.state.newListItem}
+    placeholder="Add a to do item"
+    onChange={this.addListItem}
+    />
+    <button onClick={this.pushListItems}>Add Item</button>
   </div>
-);
+    )};
+}
 
+
+render(<App />, document.getElementById("root"));
 export default App;
