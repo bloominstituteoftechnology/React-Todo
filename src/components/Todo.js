8229775ff67a977503d7f2238599b9578ffa0362
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-03 18:22:39 -0700

    Added Solution

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,3 +11,28 @@ const Todo = props => {
 };
 
 export default Todo;
+
+// SOLUTION
+// import React, { Component } from 'react';
+
+// class Todo extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             clicked: false
+//         };
+//     }
+
+//     handleClick = () => {
+//         this.setState({ clicked: !this.state.clicked });
+//     };
+
+//     render() {
+//         const styles = this.state.clicked ? { textDecoration: 'line-through' } : { textDecoration: 'none' };
+//         return (
+//             <div style={styles} onClick={this.handleClick}>{this.props.todo}</div>
+//         );
+//     }
+// }
+
+// export default Todo;
