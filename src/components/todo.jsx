29775044f2de7aa3be1404d31e767a409f09eb7e
David A. Morales <you@example.com>
2018-04-03 23:37:59 -0400

    starting to make changes

---
+++ src/components/todo.jsx
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+
+class todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false,
+            todo: this.props.todo
+        };
+    }
+
+    render() {
+        return (
+            <div style=
+        )
+    }
+}
