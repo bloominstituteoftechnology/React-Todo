f827c53b253c957a93f669a345bf8611b67db268
Mark Wong <fmarkwong@gmail.com>
2018-04-04 00:34:55 -0700

    add new todo working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import TodoList from './components/TodoList';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
-    <TodoList />
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      newTodo: '',
+      todoList: ['Task 1', 'Task 2']
+    };
+  }
+
+  handleSubmitTodo = () => {
+    const { todoList } = this.state;
+    todoList.push(this.state.newTodo);
+    this.setState({ todoList, newTodo: '' });
+  }
+
+  handleAddTodo = e => {
+    this.setState({ [e.target.name] : e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Todo App</h2>
+        <TodoList todoList={this.state.todoList}/>
+        <input
+          type="text"
+          name="newTodo"
+          value={this.state.newTodo}
+          placeholder="add animal"
+          onChange={this.handleAddTodo}
+        />
+        <button onClick={this.handleSubmitTodo}>Add Todo</button>
+      </div>
+    );
+  }
+}
 
 export default App;
