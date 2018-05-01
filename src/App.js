e272be60cf6a12912edbf897bfadf1149f867780
emortalix <emortalix@welcometoforever.io>
2018-05-01 19:24:18 -0400

    Testing App

--- src/App.js
+++ src/App.js
@@ -10,11 +10,68 @@ import TodoList from './components/TodoList'
 // Clear Handler
 // Render React Statement
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+import React, { Component } from 'react';
+import TodoList from './components/TodoList'
 
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: '',
+      todos: []
+    };
+  }
+
+  todoInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  submitTodo = event => {
+    event.preventDefault();
+    const { todos } = this.state;
+    const myTodo = { text: this.state.todo, completed: false };
+    todos.push(myTodo);
+    this.setState({ todos, todo: ''});
+  };
+
+  completedTodo = childTodo => {
+    const { todos } = this.state;
+    todos.forEach(todo => {
+      if (todo.text === childTodo.text) {
+        todo.completed = !todo.completed;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearTodos = () => {
+    const { todos } = this.state;
+    const completedTodos = todos.filter(todo => todo.completed === false);
+    this.setState({ todos: completedTodos });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>TODO LIST:</h1>
+        <form onSubmit={this.submitTodo}>
+          <input
+            type="name"
+            placeholder="Add Todo Item"
+            name="todo"
+            value={this.state.todo}
+            onChange={this.todoInput}
+          />
+          <button type="submit">Add Item</button>
+        </form>
+        <button onClick={this.clearTodos}>Clear Completed Items</button>
+        <TodoList
+          completeTodo={this.completedTodo}
+          todos={this.state.todos}
+        />
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+const styles = {
+    textDecoration: 'italic'
+};
 const Todo = props => {
     return (
-        <div> style={props.todo.completed ? styles : null}
-        onClick={() => props.completeTodo(props.todo)} 
+        <div style={props.todo.completed ? styles : null}
+        onClick={() => props.completeTodo(props.todo)}>
         <li>{props.todo.task}</li></div>
     )
 }
