855c96f100135be0e13b00e83eb20239c5c0c108
emortalix <emortalix@welcometoforever.io>
2018-05-01 19:10:36 -0400

    Added Styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Todo = props => {
     return (
-        <li>{props.todo.task}</li>
+        <div> style={props.todo.completed ? styles : null}
+        onClick={() => props.completeTodo(props.todo)} 
+        <li>{props.todo.task}</li></div>
     )
 }
 
