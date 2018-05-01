c015abda3d24a7677a80b4887c74dc8d3db0dd74
Bradiowave <divebombwalker@gmail.com>
2018-05-01 14:19:29 -0700

    75% done with todolist, need to add strikethroughs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Todo from './components/Todo';
 
 const App = () => (
   <div>
-    <h2>Todo App</h2>
+    <Todo />
 
   </div>
 );
---
+++ src/components/Todo.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import TodoList from "./TodoList";
+
+class Todo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            task: "",
+            todos: []
+        };
+    }
+
+    handleNewTask = event => {
+        this.setState({ [event.target.task]: event.target.value});
+    };
+    
+    handleSubmitTask = () => {
+        const todos = this.state.todos;
+        const task = { task: this.state.task, id: todos.length };
+        todos.push(task);
+        this.setState({ todos: todos, task: ""});
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>-----Todo List-----</h1>
+                
+                <TodoList todos={this.state.todos} />
+                <input
+                    task='task'
+                    onChange={this.handleNewTask}
+                    value={this.state.task}
+                    placeholder='Add new task here'
+                />
+                <button onClick={this.handleSubmitTask}>Add Task</button>
+            </div>
+        );
+    }
+}
+
+export default Todo;
