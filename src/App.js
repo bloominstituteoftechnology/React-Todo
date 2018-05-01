025f09f8e09e8838fc9ecec8fb1367cb2edaedb3
kelly <kelfro@fastmail.com>
2018-05-01 14:25:05 -0700

    updating App.js and TodoList.js
    I was looking at the Code Sandbox from earlier today and thinking that maybe if I tried putting my stuff in the App.js instead of TodoList.js, I could more easily compare between what we did in class today and what I'm trying to accomplish now. It's not a 1:1 comparison or anything but I think I was kind of doing it backwards before.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import TodoList from "./components/TodoList";
 
-const App = () => (
+/*const App = () => (
   <div>
     <h2>Todo App</h2>
-
   </div>
 );
+*/
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title:"Things To Do",
+      todos: [],
+      newTodo: ""
+    };
+  }
+
+  handleSubmitTodo = () => {
+    const newTodos = this.state.todos;
+    newTodos.push(this.state.newTodo);
+    this.setState({ todos: newTodos, newTodo: "" });
+  };
+
+  handleNewTodo = event => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  render () {
+    console.log("rendering");
+    return (
+      <div>
+	<input
+	  name="newTodo"
+	  onChange={this.handleNewTodo}
+	  value={this.state.NewTodo}
+	  placeholder="add todo here"
+	/>
+      </div>
+    );
+  };
+};
+      
+      
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,39 +4,11 @@
 import React, { Component } from 'react';
 
 
-class TodoList extends Component {
-  constructor () {
-    super();
-    this.state = {
-      todos: ['Task 1','Task 2'],
-      newTodo: ''
-    };
-  }
-
-  handleSubmitTodo = () => {
-    const newTodos = this.state.todos;
-    newTodos.push(this.state.newTodo);
-    this.setState({ todos: newTodos, newTodo: "" });
-  };
-
-  handleNewTodo = event => {
-    this.setState({ newTodo: event.target.value });
-  };
-
-  render () {
-    console.log("rendering");
-    return (
-      <div>
-	<input
-	  name="newTodo"
-	  onChange={this.handleNewTodo}
-	  value={this.state.NewTodo}
-	  placeholder="add todo here"
-	/>
-      </div>
-    );
-  };
-  
+class TodoList = props => {
+  return (
+  );
 };
+
+
 
 export default TodoList;
