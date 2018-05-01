639cf77634eb22a61cd32ccd5f7f93529f85db28
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-01 15:49:36 -0500

    create input and wiring it up to state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -17,14 +17,28 @@ class TodoList extends Component {
                             id: 2,
                             completed: false
                         }
-            ]
+            ], 
+            newTodo: {task: "", id: "", completed: false}
         }
     }
+
+handleNewTodo = (event) => {
+    const {newTodo} = this.state.newTodo;
+    this.setState({[event.target.name]: event.target.value})
+}
+
+handleSubmitTodo = () => {
+    const {todos} = this.state.todos;
+    this.setState({todos: todos.push(this.state.newTodo)});
+}
+
 
     render() {
         return (
             <div>
                 <ul>{this.state.todos.map((todo) => {return <Todo key={todo.id} todo={todo} />})}</ul>
+                <input type="text" placeholder="new todo" name="newTodo" value={this.state.newTodo}></input>
+                <button onClick={this.handleSubmitTodo}>Add Todo</button>
             </div>
         )
     }
