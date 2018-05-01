77145799272c02ad356ce5f837345362e4babc78
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-01 16:55:21 -0500

    working on todolist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { TodoList } from  './components/component';
 
 const App = () => (
   <div>
     <h2>Todo App</h2>
-
+    <TodoList />
   </div>
 );
 
--- src/components/component.js
+++ src/components/component.js
@@ -1,2 +1,40 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+
+
+class TodoList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            toDos: [],
+            toDo: ''
+        };
+    }
+    handleToDoActionChange = event => {
+        this.setState({ [event.target.toDoAction]: event.target.value });
+    };
+
+    handleSubmit = () => {
+        const toDos = this.state.toDos;
+        const toDo = { toDoAction: this.state.toDo, id: this.state.toDo + toDos.length };
+        toDos.push(toDo);
+        this.setState({ toDos: toDos, toDo: '' });
+    };
+        render() {
+            return (
+            <div>    
+            <toDoList toDos={this.state.toDos} />
+            <input 
+            toDoAction="newToDo"
+            onChange={this.handleToDoActionChange}
+            value={this.state.toDo}
+            placeholder="Add to-do item"
+            />
+            <button onClick={this.handleSubmit}>Add Todo</button>
+            </div>
+        );
+    }
+}
+
+export { TodoList }
