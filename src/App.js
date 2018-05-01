dd8c564e2e4c1bfba1c0fd0e504b1680c6e9239e
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-01 15:37:49 -0700

    complete assignment
    forgot to commit regularly...sorry
    got too caught up in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { Component } from "react";
+import ComponentList from "./components/component";
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      title: "To Do List",
+      task: '',
+      list: []
+    };
+  }
 
-  </div>
-);
+  handleListChange = e => {
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  
+
+  handleButtonClick = e => {
+    const list = this.state.list;
+    const task = {
+      name: this.state.task,
+      id: this.state.list.length,
+      completed: false,
+      strikeThrough: function(i) {
+        const item = document.getElementById(list[i].id)
+        if (list[i].completed === true) {
+          //console.log(item);
+          item.style.textDecoration = "line-through";
+        } else {
+          item.style.textDecoration = "none";
+        }
+      }
+    };
+    list.push(task);
+    this.setState({ list: list, task: '' });
+  }
+
+  render() {
+    return (
+    <div>
+      <h2>{this.state.title}</h2>
+      <ComponentList list={this.state.list}/>
+      <input name="task" 
+        value={this.state.task} 
+        placeholder="enter new task"
+        onChange={this.handleListChange}
+        />
+      <button onClick={this.handleButtonClick}>
+        Add new task!
+      </button>
+    </div>
+    )
+  }
+
+
+}
 
 export default App;
