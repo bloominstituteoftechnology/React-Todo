3a76cec81da18e91bccd311dbcbdcc815a2f0bee
Chris Honda <christopher.honda92@gmail.com>
2018-05-01 14:20:55 -0700

    Initial commit; work with Braden and Jose; need to add strikethrough functionality and enable text entering in input field

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = props => {
+    return (
+        <ul>
+            {props.todos.map(task => {
+                return <li key={task.id}>{task.task}</li>
+            })}
+        </ul>
+    )
+}
+
+export default TodoList;
