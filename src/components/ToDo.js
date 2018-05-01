e6d2d96b096d6c971f50d9dec0e5d2a1f57a547b
Luis <luis@Luiss-MacBook-Air.local>
2018-05-01 15:57:00 -0700

    declared classes & builded out constructor + rendered code

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,2 +1,24 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React, { Component } from 'react'
+class ToDo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      clicked: true
+    }
+  }
+  Clicked = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+    const crossed = this.state.clicked
+      ? { textdecorationline: 'underline line-through' }
+      : { textdecorationline: 'none' }
+    return (
+      <div onClick={this.Clicked}>
+        {this.props.ToDo}
+      </div>
+    )
+  }
+}
+export default ToDo
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,45 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from 'react'
+import ToDo from './ToDo'
+
+class TodoList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      task: [],
+      add: ''
+    }
+  }
+  addTodo = element => {
+    const pushTodo = this.state.task
+    pushTodo.push(this.state.add)
+    this.setState({
+      task: pushTodo,
+      add: ''
+    })
+  }
+
+  submitToDo = element => {
+    this.setState({ add: element.target.value })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.task.map((tasks, index) =>
+          <task key={index} tasks={tasks} />
+        )}
+        <form onSubmit={this.addTodo}>
+          <input
+            type="text"
+            onChange={this.submitToDo}
+            placeholder="Add ToDO Item"
+            value={this.state.add}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default TodoList
