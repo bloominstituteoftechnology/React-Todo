36ba9b8fd23c74279fff65b092ae8ffadaaa2c38
CMVNK <Kopecky12112@gmail.com>
2018-05-01 18:12:16 -0500

    added a render method that returns a div that inputs a task

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,15 +1,34 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-class ToDoList extends Component{
+class ToDo extends Component{
     constructor() {
         super();
         this.state = {
             title: "To Do List",
+            task: "",
+            someVal: "",
             tasks:[]
         };
     }
+    inputSubmitTask = () => {
+        const tasks = this.state.tasks;
+        const task = { name: this.state.task, id: this.state.task + tasks.length };
+        tasks.push(task);
+        this.setState({tasks: tasks, task:""});
+    }
+        
     render(){
-        return <h1>{this.state.title}</h1>
+        return (
+            <div>
+            <TasksList chars={this.state.tasks} />
+            <input
+              name="task" // should be known as the state.value of the thing we update
+              onChange={this.inputSubmitTask}
+              value={this.state.task} // should be bound to the state.value of thing we update
+              placeholder="add task to List"
+            />
+            <button onClick={this.inputSubmitTask}>Add Task</button>
+          </div>
     }
 }
 export default ToDoList;
