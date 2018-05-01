64f9acd027be3e150c4b8b373e286681addb7ca0
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-01 13:44:20 -0700

    changed components.js to ToDoList and put some code in there

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ToDoList from './components/ToDoList.js';
+
 
 const App = () => (
   <div>
     <h2>Todo App</h2>
-
+    <ToDoList />
   </div>
 );
 
---
+++ src/components/ToDoList.js
@@ -0,0 +1,10 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const ToDoList = props => {
+    return (
+        <h1>whasssssssup</h1>
+    )
+};
+export default ToDoList;
