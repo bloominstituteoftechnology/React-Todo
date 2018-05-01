1556e446f468b3e3fc88e48c4480c86c01a238eb
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-01 15:02:13 -0500

    added boilerplate for TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
 
+
+class TodoList extends Component {
+    constructor() {
+        super();
+        this.state = [];
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+
+
+export default TodoList; 
