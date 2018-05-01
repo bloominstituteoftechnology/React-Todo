2f4f0e0372011615c81b928661e505c9ac0f4d68
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-05-02 05:20:33 +0800

    Made the following changes:
    1. Renamed components.js to TodoList.js
    2. Created Todo.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,37 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+
+class TodoList extends Component {
+    constructor () {
+        super();
+        this.state = [
+            {
+                "task": "Pray",
+                "id": 1,
+                "completed": false,
+            },
+            {
+                "task": "Workout",
+                "id": 2,
+                "completed": false,
+            },
+            {
+                "task": "Shower",
+                "id": 3,
+                "completed": false,
+            },
+            {
+                "task": "Breakfast",
+                "id": 4,
+                "completed": false,
+            },
+            {
+                "task": "Read a book",
+                "id": 5,
+                "completed": false,
+            },
+        ];
+    }
+
+    handleNameChange
+}
