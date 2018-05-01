9611b8c54798cad3d7e3bc03acbf4bb1922dfb5d
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-01 15:07:07 -0500

    modified structure of state in TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './components/TodoList.js';
 
 const App = () => (
   <div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,20 @@ import React, { Component } from 'react';
 class TodoList extends Component {
     constructor() {
         super();
-        this.state = [];
+        this.state = {
+            todos: [
+                        {
+                            task: 'Organize Garage',
+                            id: 1,
+                            completed: false
+                        },
+                        {
+                            task: 'Bake Cookies',
+                            id: 2,
+                            completed: false
+                        }
+            ]
+        }
     }
 
     render() {
