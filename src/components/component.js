3b70c3e1d4c4e08ce42c4c4de8b64e48f7735993
dlinz7 <linzdiegoa@gmail.com>
2018-05-01 19:02:30 -0500

    latest update

--- src/components/component.js
+++ src/components/component.js
@@ -1,2 +1,34 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+
+
+
+
+
+
+
+class ToDoList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+            newTodo: "",
+            todos: [],
+
+        };
+    }
+
+
+
+newListItem = event => {                    //the 'event' here is the action of submitting a new list item
+    const updatedTodos = this.state.todos;  // this refers to the array and the current state of it (starts out empty, but after several 
+                                            // items are submitted, the 'state' updates and the array has your previous entries)
+                                                     
+    updatedTodos.push(this.state.newToDo); //this adds our latest list item to our latest version of the array
+    
+    this.setState({ todos: updatedTodos, newTodo: ''})  //
+}
+
+
+}
