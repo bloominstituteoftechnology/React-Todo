072b764cb1e5b2d3d38603b1d1aa3ca44659517d
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-05-01 22:06:45 -0400

    Modified App.js and component.js

--- src/components/component.js
+++ src/components/component.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+const ComponentList = props => {
+  return (
+    <ul>
+      {props.list.map((item, i) => {
+        return (
+          <li key={item.id} id={item.id} onClick={() => {
+            item.completed = !item.completed;
+            item.strikeThrough(i);
+          }}>
+          {item.name}
+          </li>
+        );
+      })}
+    </ul>
+  )
+}
+
+export default ComponentList;
