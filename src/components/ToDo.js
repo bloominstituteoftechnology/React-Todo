099a91b23505a771f0f1a286c2b752ab827d86ad
maribelcuales <maribelcuales@gmail.com>
2018-05-02 21:21:37 +0800

    Added a new To Do box and line-through styling for completed Todo items

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const ListToDo = props => {
-    return <li>{props.todo}</li>; 
+class Todo extends Component {
+  constructor () {
+    super(); 
+    this.state = {
+      clicked: false
+    }; 
+  }
+
+  handleClick = () => {
+      this.setState({ clicked: !this.state.clicked }); 
+  }
+
+  render() {      
+      const styles = this.state.clicked ? {textDecoration: 'line-through'} : { textDecoration: 'none'};      
+      return (
+          <div style={styles} onClick={this.handleClick}>{this.props.todo}</div>
+    );
+  }
 }
+
+export default Todo;
