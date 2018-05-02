811a7a86cb3b7f25726646939350dc4d8fa27895
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-02 18:48:02 -0400

    Blocked out class for TodoList component

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render(){
+    return (
+      <div>I am a Todo List</div>
+    );
+  };
+};
+
+export default TodoList;
