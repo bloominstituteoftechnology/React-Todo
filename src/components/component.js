6328060c7d75a9feb712f349544ac75c683efdfe
Derrick Mei <derrick.mei.168@gmail.com>
2018-05-07 01:39:51 -0500

    I feel awful I need to learn to commit more often.......

--- src/components/component.js
+++ src/components/component.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const AddButton = (props) => <button onClick={props.onClick}>Add</button>;
+
+
+
+export const ListView = (props) => {
+  return (
+    <ul>
+      {
+        props.tasks.map((task, index) => <li key={index} onClick={() => props.onClick(index)}>{task}</li>)
+      }
+    </ul>
+  );
+}
+
+
+export default AddButton;
