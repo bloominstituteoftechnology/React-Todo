b57b4cf3f6b0c4f3244cdbabf831c68aa4ce8bdc
Chris Honda <christopher.honda92@gmail.com>
2018-05-09 20:16:49 -0700

    Tried to add strikethrough functionality, is not working; next commit will be complete refactor

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoList from "./TodoList";
+import Complete from "./Complete";
 
 class Todo extends Component {
     constructor() {
@@ -13,6 +14,29 @@ class Todo extends Component {
     handleNewTask = event => {
         this.setState({ task: event.target.value});
     };
+
+    handleListChange = e => {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handleButtonClick = e => {
+        const todos = this.state.todos;
+        const task = {
+            name: this.state.task,
+            id: this.state.todos.length,
+            completed: false,
+            strikeThrough: function(i) {
+                const item = document.getElementById(todos[i].id)
+                if (todos[i].completed === true) {
+                    item.style.textDecoration = "line-through";
+                } else {
+                    item.style.textDecoration = "none";
+                }
+            }
+        };
+        todos.push(task);
+        this.setState({ todos: todos, task: '' });
+    }
     
     handleSubmitTask = () => {
         const todos = this.state.todos;
