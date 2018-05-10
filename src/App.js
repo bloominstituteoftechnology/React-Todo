9f65f87743db4b19a6af064bc4fcaf2ed6c8439a
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-05-10 16:31:15 -0400

    Reconstructed components to export or render separate peices of UI to the DOM

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
 import React, { Component } from "react";
-import ComponentList from "./components/component";
+import TodoList from "./components/TodoList";
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      title: "To Do List",
-      task: '',
-      list: []
-    };
-  }
-
-  handleListChange = e => {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  handleButtonClick = e => {
-    const list = this.state.list;
-    const task = {
-      name: this.state.task,
-      id: this.state.list.length,
-      completed: false,
-      strikeThrough: function(i) {
-        const item = document.getElementById(list[i].id);
-        if (list[i].completed === true) {
-          item.style.textDecoration = "line-through";
-        }
-        else {
-          item.style.textDecoration = "none";
-        }
-      }
-    };
-    list.push(task);
-    this.setState({list: list, task: ''});
+      title: "To Do List"
+    }
   }
 
   render() {
     return (
       <div>
         <h2>{this.state.title}</h2>
-        <ComponentList list={this.state.list} />
-        <input name="task" value={this.state.task} placeholder="Enter new task" onChange={this.handleListChange} />
-        <button onClick={this.handleButtonClick}>Click to add new task</button>
+        <TodoList />
       </div>
     )
   }
