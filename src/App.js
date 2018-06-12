27fb6665cbc7be1fa2ebc797525e167e51feed5b
Ryan Hamblin <ryanhca@gmail.com>
2018-06-12 09:18:01 -0600

    refactoring WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const App = () => (
-  <div>
-    <h2>Todo App</h2>
-
-  </div>
-);
+class App extends React.Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+  render() {
+    <div>
+      <h2>Welcome to your Todo App!</h2>
+    </div>;
+  }
+}
 
 export default App;
