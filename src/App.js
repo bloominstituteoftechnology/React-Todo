c11d7e8c796eb60fc95e71ee6827c8b01ff4dda8
Luis <luis@Luiss-MacBook-Air.local>
2018-06-13 11:10:45 -0700

    initail commit + imported Component from react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import { Component, React } from 'react'
 
-class App extends React.Component {
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+  render () {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
