b505552f6858474ed5aab3509b30c59da635114f
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-13 17:42:59 -0500

    completed day 1 mvp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 import ToDoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
@@ -7,10 +8,25 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
+    this.state = {
+      'task': [],
+    }
   }
+
+  
+
+  addToDo = addedTask => {
+    const toDos = this.state.task.slice();
+    toDos.push(addedTask);
+    this.setState({ task: toDos });
+  }
+
   render() {
     return (
-      <ToDoForm />
+      <div>
+      <TodoList task={this.state.task} />
+      <ToDoForm FormHandle={this.addToDo} />
+      </div>
     );
   }
 }
