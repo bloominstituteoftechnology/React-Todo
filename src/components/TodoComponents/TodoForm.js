dc4829daec4cff05dd46303db6a07e5924c4458d
Simon <Simon_Freelance@hotmail.com>
2018-06-13 18:27:56 -0400

    ToDo Form rough draft of functionality

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Todo.css';
 
 const TodoForm = (props) => {
-
+   return  (
+    <div>
+    <input onClick = {() => props.inputHandler()} type = "text" placeholder = "...todo" />
+    <button onClick = {() => props.addTask()}>Add Todo</button>
+    <button onClick = {() => props.clearCompleted()}>Clear Completed</button>
+    </div>
+    );
 }
 
 export default TodoForm;
