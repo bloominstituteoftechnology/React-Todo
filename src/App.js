e6de30a90d2e2f00935d7bf13345d630d494fcf0
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-13 15:54:51 -0400

    Blake Fletcher - React-Todo
    First commit
    Created very basic to-do app using today's lecture as inspo
    Created constructor with todos array of string (must change to objects)
    Add input box with typing functionality
    Todos go from input box to bullet list on click
    Added to TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, and any change 
+
+  constructor() {
+    super();
+    this.state = {
+      heading: "To-do List MVP",
+      todos: [
+        "Buy bread",
+        "Drink water",
+        "Do laundry",
+        "Call Susan"
+      ],
+      todo: ""
+    };
+  }
+
+// this lets us type in the input box
+  inputNewTodo = event => {
+    this.setState({ todo: event.target.value });
+  };
+
+// this adds the new todo to the list
+  addTodo = () => {
+    const todos = this.state.todos.slice();
+    todos.push(this.state.todo);
+    this.setState({ todos: todos, todo: "" });
+  };
+
   render() {
+    
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>{this.state.heading}</h2>
+        <input
+          type="text"
+          onChange={this.inputNewTodo}
+          placeholder="Add todo"
+          value={this.state.todo}
+        />
+        <button onClick={this.addTodo}>Add todo</button>
+        <TodoList myTodos=
+        {this.state.todos} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+
+const TodoList = props => {
+  return (
+    <ul>
+      {props.myTodos.map(band => {
+        return <li key={Math.random()}>{band}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default TodoList;
