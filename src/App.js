c47604f988593db1e8403948689d483981842c74
Jameson <jamesonbrown@neolivity.com>
2018-06-13 14:25:09 -0600

    TodoList.js complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
   render() {
     return (
       <div>
         <h1>Todo List: MVP</h1>
+        <TodoList todoArr={this.state.todos} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = props => {
+    return (
+        <ul>
+            {props.todoArr.map(todo => {
+                return <li key={Date.now()}>{todo}</li>;
+            })}
+        </ul>
+    );
+};
+
+export default TodoList;
