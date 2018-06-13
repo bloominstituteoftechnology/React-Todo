1960334c13a9ad00a089b285798b273fc45b885c
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-13 17:43:44 -0500

    completed day 1 mvp

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import './Todo.css';
 
-const ToDoForm = (props) => {
-    return (
-        <div>
-            <form>
-                <input type="text" placeholder="What to do?" />
-                <p>Submit</p>
-                <p>Clear Completed</p>
-            </form>
-        </div>
-    );
-};
+class ToDoForm extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            'input': '',
+        }
+    }
+
+    changeHandler = event => {
+        this.setState({ input: event.target.value });
+      }
+
+    handleAdd = () => {
+        let text = this.state.input;
+        console.log(text);
+        let task = {
+            task: text,
+            id: Date.now(),
+            completed: false,
+        };
+        this.props.FormHandle(task);
+        this.setState({ input: '' })
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                type="text"
+                onChange={this.changeHandler}
+                placeholder="What to do?"
+                value={this.state.input}
+                />
+                <button onClick={this.handleAdd}>Add To Do</button>
+                <button>Clear Completed</button>
+            </div>
+        );
+    }
+}
 
 export default ToDoForm;
