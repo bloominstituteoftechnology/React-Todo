07680885c20618ee5666e9bd9f041a3f18cf08f9
Alec <afroenygam@gmail.com>
2018-06-13 16:05:25 -0400

    TodoList component added, App.js updated

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = props => {
+	return (
+		<ul>
+			{props.listItems.map(item => {
+				return <li key={Date.now()}>{item}</li>;
+				})}
+		</ul>
+	);
+};
+
+export default TodoList;
