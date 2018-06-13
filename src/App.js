3756a6ab94dd489a3b70344da4648c3e7695a2d9
Tai <bowlofryce@gmail.com>
2018-06-13 17:50:40 -0500

    Still working on it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import TodoList from "./components/TodoComponents/TodoList"
+import TodoForm from "./components/TodoComponents/TodoForm"
+import Todo from "./components/TodoComponents/Todo"
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      doList: [
+        
+      ],
+      addItems: ""
+    };
+
+    listChange = event => {
+      console.log(event.target.value);
+      // the way to update state in react! // LIFECYCLE FUNCTION
+      this.setState({ addItems: event.target.value });
+    };
+
+    addTodo = () => {
+      const doList = this.state.doList.slice();
+      doList.push(this.state.addItems);
+      this.setState({ doList: doList, addItems: "" });
+    }
+
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
+        <input 
+          type="text"
+          onChange={this.listChange}
+          placeHolder="Add Todo"
+          value={this.state.addItems}
+        />
+        <button onClick={this.addTodo}>Add Todo..</button>
+        <TodoList listItems={this.state.doList} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <ul>
+          {props.listItems.map(addList => {
+            return <li key={date.now()}>{addList}</li>;
+          })}
+        </ul>   
+      )
+}
+
+default export TodoList;
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
