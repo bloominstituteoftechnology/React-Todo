41580a4eb20c572fea97a033841b6c047451b451
Kevin <brackkevin@gmail.com>
2018-06-13 16:45:51 -0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  this.state = {
+    todoData: [
+      {
+        task: 'Organize Garage',
+      }
+    ]
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
+        <Todo />
+        <TodoForm />
       </div>
     );
   }
