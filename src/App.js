1c92b653e5a65b9a22e2ecabcedc83f089b32efb
Simon <Simon_Freelance@hotmail.com>
2018-06-13 19:12:35 -0400

    Got the task list to at least get one input character

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
-import Todo from './components/TodoComponents/Todo';
 
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      items: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+      ],
+      item: {
+        task: '',
+        id: Date.now(),
+        completed: false
+      }
     };
   }
+
+  inputHandler = (value) => {
+    this.setState({item: {
+      task: value,
+      id: Date.now(),
+      completed: false
+    }});
+  };
+
+  addTodo ()  {
+    const items = this.state.items.slice();
+    items.push(this.state.item);
+    this.setState({items: items, item: {
+      task: '',
+      id: Date.now(),
+      completed: false
+    }});
+  }
+  clearItemsHandler = event => {
+    console.log('clear');
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -18,6 +49,12 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList items={this.state.items} />
+        <TodoForm
+          inputHandler={this.inputHandler}
+          addTask={() => this.addTodo()}
+          clearCompleted={() => this.clearItemsHandler()}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Todo.css';
 
 const TodoForm = (props) => {
-   return  (
-    <div>
-    <input onClick = {() => props.inputHandler()} type = "text" placeholder = "...todo" />
-    <button onClick = {() => props.addTask()}>Add Todo</button>
-    <button onClick = {() => props.clearCompleted()}>Clear Completed</button>
-    </div>
+    return (
+        <div>
+            <input value = "" onChange={(event) => {
+                { props.inputHandler(event.target.value) }
+            }
+        }
+                type="text" placeholder="...todo" />
+            <button onClick={() => props.addTask()}>Add Todo</button>
+            <button onClick={() => props.clearCompleted()}>Clear Completed</button>
+        </div>
     );
 }
 
