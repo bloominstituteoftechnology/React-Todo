724ca4415990817fb8a09e0b44a8dbf30179c16d
Mariana Cerda <raymond_grc@yahoo.com>
2018-06-13 14:51:17 -0500

    Added input form and a sample item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 
+function TodoElement(prop){
+  return <div> {prop.item} </div>
+}
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-constructor(){
-  super();
-  this.state = [
-        'Organize Garage',
-        1528817077286,
-        false
-  ]
+  render() {
+    return (
+      <div>
+        <TodoElement item="Walk the dog" />
+        <input type="text" placeholder="Add item"/>
+      </div>
+  );
+  }
 }
-
-render (){
-  return(
-  <div>
-    {this.state}
-  </div>
-)
-}
-}
-
 export default App;
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
