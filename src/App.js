82d188279805582809041e921c8f4063d3cc9a8f
-tristan- <tristan.linehan@gmail.com>
2018-06-13 15:56:54 -0700

    Tristan Linehan / React-Todo / daily standup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList' ;
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
-}
+  constructor() {
+    super() ;
+    this.state = {
+      tasksList: [
+        {
+          task: 'Default Task Title 1',
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: 'Default Task Title 2',
+          id: Date.now() + 1,
+          completed: false
+        },
 
-export default App;
+      ],
+
+      tasksListNew: ''
+    }
+    console.log(this.state.tasksList[0].task) ;
+  }
+
+  addTask = () => {
+    // copying state, as to not mutate state
+    const tasksList = this.state.tasksList.slice() ;
+    // adding user input to new array, inside state
+    tasksList.push(this.state.tasksListNew) ;
+    // need clarification on setState syntax
+    this.setState({ tasksList: tasksList, band: ''}) ;
+  }
+
+  render() {
+    console.log('Just Rendered!!') ;
+    return(
+      
+      
+      <div>
+        <h1>To Do List: MVP</h1>
+        
+        <TodoList />
+
+        <div className="list-actions-wrapper">
+          <input type="text" className="input-box"/>
+          <button className="button-todo">button-todo</button>
+          <button className="button-clear">button-clear</button>
+        </div>
+      </div> 
+      
+
+    ) 
+  } 
+} 
+
+export default App ;
+      {/* <li>{this.state.tasksList[0].task}</li>          */}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+const TodoList = props => {
+
+    console.log(props) ;
+
+    return(
+        <div className="displayed-list-wrapper">
+            <h3>"PLACEHOLDER: displayed-list"</h3>
+            <ul className="displayed-list"></ul>
+      </div>
+
+    )
+}
+export default TodoList ;
