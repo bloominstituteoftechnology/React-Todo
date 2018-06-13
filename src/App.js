f04580ac80655ce4fe0d23570adb0e01ad4b3ad9
David Layman <davidlayman85@gmail.com>
2018-06-13 17:56:22 -0400

    kind of working...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+constructor(){
+	super();
+	this.state={
+	list:[  
+		  {
+              task: 'Take Nap',
+              id: 100,
+              completed: false,
+          },
+          {
+              task: 'Mow Lawn',
+              id: 101,
+              completed: false,
+          },
+          {
+              task: 'Fix Fence',
+              id: 102,
+              completed: false,
+          },
+          {
+              task: 'Take Nap',
+              id: 103,
+              completed: false,
+          }
+	],
+	newItem:"",
+	newTask:[{
+		task:''
+	}]
+	};
 }
-
+update = event => {
+	this.setState({newItem: event.target.value});
+};
+addItem = () => {
+this.taskCreator(this.newItem);
+const newList = this.state.list.splice('');
+newList.push(this.state.newTask);
+	this.setState({list : newList, newItem : ""});
+};
+taskCreator= () => {
+this.state.newTask.task=this.state.newItem;
+}
+render(){
+return(
+<div>
+<h2> Todo List: </h2>
+<ul>
+      {this.state.list.map((list) => <li key={list.id} className={list.completed}>{list.task}</li>)}
+</ul>
+<input
+type="text"
+placeholder="add item"
+onChange={this.update}
+value={this.state.newItem}
+/>
+<button onClick={this.addItem}>add item</button>
+</div>
+)
+}
+};
 export default App;
