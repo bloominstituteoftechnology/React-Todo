20ea95affffb80f4da95b02a97feb0455fdfdd6b
emaquino44 <prexy44@yahoo.com>
2018-06-13 12:27:41 -0700

    initial push - setup app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      task: 'take out'
+    }
+  }
+
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <ul>{this.state.task}</ul>
       </div>
+
     );
   }
 }
