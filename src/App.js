8e8a1e78007e3cfad9c4d68b7531bc754209e03d
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-13 17:40:48 -0400

    todolist, very messy but works. overly complicated , also finished app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ToDoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,8 +7,8 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="container">
+        <ToDoList />
       </div>
     );
   }
