9edb7721dbb30de69d56419680cd16f104dc84c3
Kevin Tena <kevten09@gmail.com>
2018-06-13 15:22:44 -0500

    Built out todoList.js and set up app.js for this.state

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoList = props => {
+    return (
+        <ul>
+            {props.Todo.map(todo => {
+                return <li key={Math.random()}>{todo}</li>
+        })}
+        </ul>
+    );
+};
+
+export default TodoList;
