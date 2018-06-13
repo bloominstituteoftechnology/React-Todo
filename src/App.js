dd30e342abaf96e73061a581b95ef111277d214d
nickolaussmith <nickolaussmith721@gmail.com>
2018-06-13 17:25:40 -0600

    An attempt was made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [],
+      todo: {},
+      task: '',
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+ 
+ 
   render() {
     return (
       <div>
+        <TodoList/>
         <h2>Welcome to your Todo App!</h2>
       </div>
     );
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                {
+                    this.props.list.map(todo=>{
+                        return <Todo key={todo.id} todo={todo.task} />
+                    })
+                }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
