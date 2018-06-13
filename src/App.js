2d98f4bf59334fe5805715be327f7559c95d50ed
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-13 12:45:28 -0700

    created a an array of list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+
+constructor(){
+	super();
+
+	this.state = {
+
+	   mylist:['Get milk',
+		 'Learn React', 
+		 'Read Thinking Fast and Slow'
+	   ]			
+	};
+
+};	
+		
+ render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
