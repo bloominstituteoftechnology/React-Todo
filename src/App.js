186aa05a08c28240740958eeb879e42c75da4e7a
Sam Khaled <samkhaled@outlook.com>
2018-06-13 19:19:51 -0400

    starting on this

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Welcome to your Todo App!  this is the start</h2>
       </div>
     );
   }
