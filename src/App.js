3b3a0e91a5586e117a7f5f757943108c4a813479
Sania Parekh <sparekh@Sanias-MacBook.home>
2018-06-13 17:55:38 -0500

    didn't get very far today still figuring this out

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Todo from './components/TodoComponents/Todo';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    constructor() {
+      super();
+      this.state = {
+        todolist: [],
+        dataInput: ''
+      }
+    }
+  
+  
+  
+  
   render() {
     return (
       <div>
