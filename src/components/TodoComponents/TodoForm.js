771c624f16c3df90b9e30dcceaecdfb8c1e0bb98
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-13 15:44:35 -0700

    Added new button to TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TodoForm = props => {
   return (
     <form>
-      <input type="text" placeholder="Add To Do" /> <button></button>
+      <input type="text" placeholder="Add To Do" /> <button>Add</button> <button>Clear Completed</button>
     </form>
   );
 }
