10e3f3c477e570b0617086e49c3abc9ba7f9d718
Kevin <brackkevin@gmail.com>
2018-06-13 17:46:37 -0500

    Split TodoList and Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
 
-  this.state = {
-    todoData: [
-      {
-        task: 'Organize Garage',
-      }
-    ]
+  constructor() {
+    super();
+    this.state = {
+      todoData: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
   }
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList />
-        <Todo />
-        <TodoForm />
+        <TodoList todoData={this.state.todoData} />
+        {/*<TodoForm />*/}
       </div>
     );
   }
