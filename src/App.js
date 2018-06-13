3d590b4dc8deabc58a30f7872e68611ef1048559
CLN <cln@Sags-MacBook-Pro.local>
2018-06-13 15:50:04 -0400

    Start, .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import TodoList from "./components/TodoComponents/TodoList";
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
