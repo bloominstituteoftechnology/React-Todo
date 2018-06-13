390e1f685851f39dc635fbcf90fe1f038ad052b8
Ryan Hamblin <ryanhca@gmail.com>
2018-06-12 21:54:18 -0600

    implements MVP without any styling

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+import Todo from './Todo';
+
+const TodoList = props => {
+  return (
+    <div>
+      {props.todos.map(todo => (
+        <Todo
+          handleToggleComplete={props.handleToggleComplete}
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
