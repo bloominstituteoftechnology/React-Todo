4bfa46bd2af4d04a276396980f9960f3f3a1da8c
Erin Meredith <ekmeredith@att.net>
2018-06-14 18:30:07 -0400

    toggle functioning

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -5,7 +5,7 @@ import './Todo.css';
 
 const Todo = props => { //takes in 'todo' data and displays it; use toggle() here.
     return (
-    <div className={props.completed ? 'completed' : 'todo-item'} onClick={props.toggleFlag}>
+    <div key ={props.id}className={props.completed ? 'completed' : 'todo-item'} onClick={()=>props.toggleFlag(props.id)}>
         
        {props.todo}   
     </div>
