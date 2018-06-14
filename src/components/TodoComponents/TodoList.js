5fc211bb1ad4a3b9a612e543ee5cfb97241d9678
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-14 15:52:10 -0500

    minor changes

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ToDo from './Todo';
 
 const ToDoList = props => {
-    console.log(props)
     return props.task.map(task => <ToDo key={task.id} task={task} toggleCompleted={props.toggle} />);
 }
 
