646b677bd688227c43ec2de09189337c09b3738c
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-14 15:29:53 -0500

    completed toggle completed strike through functionality

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Todo.css';
 
-const ToDo = props => <li>{props.task.task}</li>
+const ToDo = props => <li onClick={() => props.toggleCompleted(props.task.id)} style={props.task.completed ? {textDecoration: 'line-through'} : null}>{props.task.task}</li>
 
 export default ToDo;
