83cdb45a27ace29d23bf2788787f193cbbd42ee2
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-14 15:17:18 -0500

    began toggle completed

--- src/App.js
+++ src/App.js
@@ -10,22 +10,43 @@ class App extends React.Component {
     super();
     this.state = {
       'task': [],
+      'item': '',
     }
   }
 
-  
+  changeHandler = event => {
+    this.setState({ item: event.target.value });
+  };
 
-  addToDo = addedTask => {
+  addToDo = () => {
     const toDos = this.state.task.slice();
-    toDos.push(addedTask);
-    this.setState({ task: toDos });
+    toDos.push({ 
+      'task': this.state.item,
+      'id': Date.now(),
+      'completed': false,
+     });
+    this.setState({ task: toDos, item: '' });
   }
+
+ toggleCompleted = id => {
+   let items = this.state.task.slice();
+   items = items.map(item => {
+     if(item.id === id) {
+       item.completed = !item.completed;
+       return item;
+     } else {
+       return item;
+     }
+   });
+  //  this.setState({ task: items });
+ }
+
 
   render() {
     return (
       <div>
-      <TodoList task={this.state.task} />
-      <ToDoForm FormHandle={this.addToDo} />
+      <TodoList task={this.state.task} toggle={this.toggleCompleted} />
+      <ToDoForm changeHandle={this.changeHandler} inputValue={this.state.item} addNew={this.addToDo} />
       </div>
     );
   }
