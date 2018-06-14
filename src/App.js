42c38751069365371ceb9118f5e18c3aef05f06e
Tai <bowlofryce@gmail.com>
2018-06-14 17:50:41 -0500

    I'm gonna have to do some research

--- src/App.js
+++ src/App.js
@@ -1,45 +1,33 @@
-import React from 'react';
-import TodoList from "./components/TodoComponents/TodoList"
-import TodoForm from "./components/TodoComponents/TodoForm"
-import Todo from "./components/TodoComponents/Todo"
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import Todo from "./components/TodoComponents/Todo";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      doList: [
-        
-      ],
+      doList: [],
       addItems: ""
     };
-
-    listChange = event => {
-      console.log(event.target.value);
-      // the way to update state in react! // LIFECYCLE FUNCTION
-      this.setState({ addItems: event.target.value });
-    };
-
-    addTodo = () => {
-      const doList = this.state.doList.slice();
-      doList.push(this.state.addItems);
-      this.setState({ doList: doList, addItems: "" });
-    }
-
   }
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  listChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addTodo = e => {
+    e.preventDefault();
+    const doList = this.state.doList.slice();
+    doList.push({task: this.state.addItems, completed: false, id: Date.now() });
+    this.setState({ doList: doList, addItems: "" });
+  };
+
   render() {
     return (
       <div>
         <h2>Todo List: MVP</h2>
-        <input 
-          type="text"
-          onChange={this.listChange}
-          placeHolder="Add Todo"
-          value={this.state.addItems}
-        />
+        <div><TodoForm /></div>
         <button onClick={this.addTodo}>Add Todo..</button>
         <TodoList listItems={this.state.doList} />
       </div>
