d8260feef3908828f0b5cf15e32ad53e97c2525d
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-14 16:00:50 -0600

    filter and updated formatting in place

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,25 +2,37 @@ import React from "react";
 
 const TodoForm = props => {
   return (
-    <div className="form">
-      <input
-      id="newTodo"
-        type="text"
-        placeholder="Enter todo item"
-        value={props.inputValue}
-        onChange={props.handleNewTodoInput}
-        onKeyPress={(event) => {
-          if (event.key === "Enter") {
-            props.addTodo();
-          }
-        }}
-      />
-      <div className="button" onClick={props.addTodo}>
-        Add
+    <div className="form-container">
+      <div className="input-form form">
+      <div className="button main-button" onClick={props.addTodo}>
+          Add
+        </div>
+        <input
+          id="newTodo"
+          type="text"
+          placeholder="Enter todo item"
+          value={props.inputValue}
+          onChange={props.handleNewTodoInput}
+          onKeyPress={event => {
+            if (event.key === "Enter") {
+              props.addTodo();
+            }
+          }}
+        />
+
+        <div className="button" onClick={props.clearCompleted}>
+          Clear Complete
+        </div>
       </div>
-      <div className="button" onClick={props.clearCompleted}>Clear Complete</div>
-      <div className="search-form">
-        <input id="searchTodo" type="text" placeholder="Search tasks" value={props.searchValue} onChange={props.handleSearchInput}/>
+      <div className="search-form form">
+        <label for="searchTodo">Filter</label>
+        <input
+          id="searchTodo"
+          type="text"
+          placeholder="Filter task"
+          value={props.searchValue}
+          onChange={props.handleSearchInput}
+        />
       </div>
     </div>
   );
