3917ef7fb939eca9d06edadebe1ea16b2e49fd3a
Simon <Simon_Freelance@hotmail.com>
2018-06-14 10:20:41 -0400

    Beautify Code for TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -5,13 +5,13 @@ const TodoForm = (props) => {
 
     return (
         <div>
-            <input type = 'text' onChange={props.changeTodoHandler} 
-                                value = {props.value} 
-                                onKeyDown = {(event) => {
-                                    if(event.keyCode === 13) {
-                                        props.addTodoHandler();
-                                    }
-                                }}/>
+            <input type='text' onChange={props.changeTodoHandler}
+                value={props.value}
+                onKeyDown={(event) => {
+                    if (event.keyCode === 13) {
+                        props.addTodoHandler();
+                    }
+                }} />
             <button onClick={props.addTodoHandler}>Add Todo</button>
             <button>Clear Completed</button>
         </div>
