7d7d8d9556c2f8c300ef29e14e2d338f5bddada2
JJ Ashcraft <jj@jjashcraft.com>
2018-06-14 17:54:45 -0400

    added preventDefault, fixes clear todos

--- src/App.js
+++ src/App.js
@@ -1,95 +1,121 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
-import TodoList from './components/TodoComponents/TodoList'; 
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-        task: '',
-        id: '',
-      
-
+      task: '',
       todos: [{
-          task: 'Organize Garage',
+          task: 'Moon Shoes',
           id: 1528817077286,
           completed: false
         },
         {
-          task: 'Bake Cookies',
+          task: 'Guardians of the Galaxy Soundtrack',
+          id: 1528817084359,
+          completed: false
+        },
+        {
+          task: 'Oxygen',
+          id: 1528817084458,
+          completed: false
+        },
+        {
+          task: 'Space Ice Cream',
           id: 1528817084358,
           completed: false
         }
       ],
     };
-this.toggle = this.toggle.bind(this);
-  
   }
 
-handleChange = (event) => {
-this.setState({ 
-  task : event.target.value ,
-  id: Date.now(),
-  completed: false,
-});
-};
+  handleChange = (event) => {
+    this.setState({
+       [event.target.name]: event.target.value
+    });
+  };
 
-clearComplete = () => {
- let todolist = this.state.todos.slice();
- todolist = todolist.filter(item => 
-   item.completed === false || !item.task.length < 1)
- this.setState({
-   todos: todolist,
-   task: '',
- })
-};
+  clearComplete = e => {
+    e.preventDefault();
+    let todos = this.state.todos.slice();
+    console.log(todos);
+    todos = todos.filter(item =>
+      !item.completed)
+    this.setState({todos})
+  };
 
 
-toggle = (id) => {
- 
-let todos = this.state.todos.slice();
-todos = todos.map(todo => {
-  if (todo.id === id) {
-    todo.completed = !todo.completed;
-    return todo;
-  } else {
-    return todo;
+  toggle = (id) => {
+
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+
+    this.setState({
+      todos
+    });
   }
-});
-
-  this.setState({ todos });
-}
 
 
 
-addToDo = (event) => {
-  event.preventDefault();
- let addToDo = this.state.todos.slice();
- console.log('previous list ', addToDo);
- addToDo.push({task: this.state.task, id: Date.now(), completed: false});
- this.setState({
-   task: '',
-   todos: addToDo,
- })
-
-  console.log('new list ', addToDo);
-}
-
+  addToDo = (event) => {
+    event.preventDefault();
+    let addToDo = this.state.todos.slice();
   
+    addToDo.push({
+      task: this.state.task,
+      id: Date.now(),
+      completed: false
+    });
+    this.setState({
+      task: '',
+      todos: addToDo,
+    })
+
+  }
+
+
 
 
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
-    return (
-      <div className = 'app-wrapper'>
-        {/* render a todo form, and the todo list */}
-       <TodoForm value={this.state.task} clearComplete = {this.clearComplete} addToDo = {this.addToDo} handleChange = {this.handleChange} />
-       <TodoList   toggle={this.toggle} checked = {this.state.CheckboxChecked} todos = {this.state.todos} />
+    return ( <
+      div className = 'app-wrapper' > { /* render a todo form, and the todo list */ } <
+      TodoForm value = {
+        this.state.task
+      }
+      clearComplete = {
+        this.clearComplete
+      }
+      addToDo = {
+        this.addToDo
+      }
+      handleChange = {
+        this.handleChange
+      }
+      /> <
+      TodoList toggle = {
+        this.toggle
+      }
+      
+      todos = {
+        this.state.todos
+      }
+      />
 
-      </div>
+      <
+      /div>
     );
   }
 }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -9,8 +9,10 @@ const TodoForm = (props) => {
             <div className='form-wrapper'>
                <form onSubmit = {props.addToDo}>
                <input onChange = {props.handleChange} className='todo-input' type='text' name = 'task' value={props.value} placeholder=' ready for blast-off...' /> 
-               <button onKeyPress  onClick = {props.addToDo} className='add-todo'>Add Todo</button>
+               <div className='button-flex'>
+               <button  onClick = {props.addToDo} className='add-todo'>Add Todo</button>
                <button onClick = {props.clearComplete} className='clear'>Clear Completed</button>
+               </div>
                </form>
             </div>
         );
