f2d352d1f857aea69c187b7632bf460fc864dead
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-14 17:43:34 -0500

    added some style

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,10 +3,12 @@ import './Todo.css';
 
 const ToDoForm = props => {
     return (
-        <div>
+        <div className='form'>
             <input type="text" placeholder="What to do?" onChange={props.changeHandle} value={props.inputValue}/>
+            <div className="buttons">
             <button onClick={props.addNew}>Add</button>
             <button onClick={props.handleCompleted}>Clear Completed</button>
+            </div>
         </div>
     );
 }
