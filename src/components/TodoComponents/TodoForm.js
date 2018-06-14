ef3f68b156dc2cd1ba1ab85832b72a4390baeed6
Your Name <you@example.com>
2018-06-14 13:24:17 -0700

    Fashoky

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+const TodoForm = props => {
+  return (
+    <form>
+      <input
+        onChange={props.handleTodoChange}
+        type="text"
+        name="todo"
+        value={props.value}
+        placeholder="...todo"
+      />
+      <button onClick={props.handleAddTodo}>Add Todo</button>
+      <button onClick={props.handleClearTodos}>Clear Completed</button>
+    </form>
+  );
+};
+
+export default TodoForm;
