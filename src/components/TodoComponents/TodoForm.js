26b2a6bd77e98083fe950f1125d14833fbcaff3b
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-14 15:17:42 -0500

    refactored to functional component syntax

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,44 +1,14 @@
 import React from 'react';
 import './Todo.css';
 
-class ToDoForm extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            'input': '',
-        }
-    }
-
-    changeHandler = event => {
-        this.setState({ input: event.target.value });
-      }
-
-    handleAdd = () => {
-        let text = this.state.input;
-        console.log(text);
-        let task = {
-            task: text,
-            id: Date.now(),
-            completed: false,
-        };
-        this.props.FormHandle(task);
-        this.setState({ input: '' })
-    }
-
-    render() {
-        return (
-            <div>
-                <input
-                type="text"
-                onChange={this.changeHandler}
-                placeholder="What to do?"
-                value={this.state.input}
-                />
-                <button onClick={this.handleAdd}>Add To Do</button>
-                <button>Clear Completed</button>
-            </div>
-        );
-    }
+const ToDoForm = props => {
+    return (
+        <div>
+            <input type="text" placeholder="What to do?" onChange={props.changeHandle} value={props.inputValue}/>
+            <button onClick={props.addNew}>Add</button>
+            <button>Clear Completed</button>
+        </div>
+    );
 }
 
 export default ToDoForm;
