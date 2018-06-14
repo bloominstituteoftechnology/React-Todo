c1ab414598ae0918892869332f21563d75f11b02
Randy Calderon <randycweb@gmail.com>
2018-06-14 12:52:21 -0700

    Added array to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props) {
+      super()
+      this.todo = [
+        {
+        task: 'Walk the dogs',
+        id: Date.now(),
+        completed: false,
+      }
+    ];
+    
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
