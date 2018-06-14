10ae5b2055e0579b477a612ce0261c0d2fe0e23a
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-14 17:43:21 -0500

    added some style

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Todo.css';
 
-const ToDo = props => <li onClick={() => props.toggleCompleted(props.task.id)} style={props.task.completed ? {textDecoration: 'line-through'} : null}>{props.task.task}</li>
+const ToDo = props => {
+    return (
+    <div className="task">
+    <li onClick={() => props.toggleCompleted(props.task.id)} style={props.task.completed ? {textDecoration: 'line-through'} : null}>{props.task.task}</li>
+    </div>
+    );
+}
 
 export default ToDo;
