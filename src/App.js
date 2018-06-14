3309f1d55747336489bc6a3a1282dd53ccc9605a
Randy Calderon <randycweb@gmail.com>
2018-06-14 16:26:35 -0700

    Changed todo to state and minor changes to components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
+    constructor() {
       super()
-      this.todo = [
-        {
+      this.state = {
+         todos: [
         task: 'Walk the dogs',
         id: Date.now(),
         completed: false,
-      }
-    ];
-    
-  }
+    ]
+  }   
+}
+
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List</h2>
+        <TodoForm />
+        <TodoList tasks={this.state.todos}/>
       </div>
     );
   }
