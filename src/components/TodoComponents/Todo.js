e9b76c51c489b87b59fd2784d9574971258c4ba7
Nicky Chen <nicky.chen0@gmail.com>
2018-06-14 13:33:26 -0700

    refactored day 1 mvp

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Todo = props => {
+    return (
+        <li>{props.singleTodo}</li>
+    )
 
 }
 
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,13 +2,14 @@
 // feel free to change this component.js into TodoList.js
 import React from 'react';
 import './Todo.css';
+import Todo from './Todo';
 
 
 const TodoList = props => {
     return (
     <ul>
         {props.listTasks.map(listed => {
-            return <li key={Math.random()}>{listed.task}</li>;
+            return <Todo key= {Math.random()} singleTodo = {listed.task}/>;
         })}
     </ul>
     );
