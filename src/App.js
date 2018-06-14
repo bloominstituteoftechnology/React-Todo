dbe7c640cf64f71b9ea64c89c22ea11e237545fd
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-14 16:59:06 -0400

    redoing project, finished todo.js:

--- src/App.js
+++ src/App.js
@@ -5,13 +5,40 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      greeting: "Hello To Do List!",
+      todos: [
+        "shopping",
+        "cleaning",
+        "buying new car",
+        "renting a storage unit",
+      ],
+      todo: ""
+    };
+  };
+  
+  changeGreetingHandler = event => {
+    console.log(event.target.value);
+    this.setState({ todo: event.target.value });
+  };
+
   render() {
     return (
-      <div className="container">
-        <ToDoList />
+      <div>
+        <h2>{this.state.greeting}</h2>
+        <input
+          type="text"
+          onChange={this.changeGreetingHandler}
+          placeholder="add todo"
+          value={this.state.todo}
+        />
+        <button onSubmit={this.addToDo}> Add To Do </button>
+        <ToDoList allChores={this.state.bands} />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
