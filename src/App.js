575cf7b25bc8efca3fbfb9bce2615318c27bcab1
chainchompa <chainchompa@gmail.com>
2018-06-14 15:44:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor () {
+    super ();
+    this.state = {
+      todoList: ["eat",
+                "sleep",
+                "cry"
+              ],
+      todo: ""
+    }
+  }
+  addTodo = () => {
+    const todos = this.state.todoList.slice();
+    todos.push(this.state.todo);
+    this.setState ({todoList: todoList});
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
