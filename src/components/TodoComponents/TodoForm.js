9ee1d2046052e9af85277f889348734d3cc6e341
Sania Parekh <sparekh@Sanias-MacBook.home>
2018-06-19 17:51:27 -0500

    made some progress still lots to go in completing requirement for mvp

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Todo.css';
+
+
+const TodoForm = props => { //hold input field and buttons
+    console.log(props);
+    return (
+<div>
+       <input type='text'
+       placeholder='Add Todo'
+       onChange={props.toDoHandler}
+       value={props.dataInput}/>
+       <button onClick={props.addTodo}>Add todo</button>
+       <button onClick={props.clearTodo}> Clear completed </button>
+    </div>
+    )
+}
+
+export default TodoForm;
