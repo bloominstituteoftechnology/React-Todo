486a090f2206b054b24d85ed5f7cc19e7573131f
Your Name <you@example.com>
2018-06-20 15:27:56 -0700

    Fashoky updating app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import ReactDom from "react-dom";
+import TodoList from "./TodoList";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      greeting: "Todo List: MVP",
+      Todo: []
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
         <h1>Learn setstate()</h1>
         <h1> Style my Todo list</h1>
       </div>
