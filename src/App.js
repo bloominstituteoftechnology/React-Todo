097da47769a67add2121353eecc14e5639a0f15b
Kairi <kairijb@gmail.com>
2018-06-20 17:41:57 -0500

    I'm pretty lost

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+
+    this.state = {
+      todo: []
+    }
+
+
+    
+  };
+
+ 
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
       </div>
+      
     );
   }
 }
