53ab8d6e73196c3da1737c498304177ba8a86b21
Katie Gorbell <katiegorbell@gmail.com>
2018-06-20 17:26:08 -0500

    added basic structure to the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import ToDoForm from './components/TodoComponents/ToDoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='app-container'>
+        <h1 className='header'>Todo List: MVP</h1>
+        <TodoList />
+        <ToDoForm />
       </div>
     );
   }
