547774a7906347b1e7faa1f8e9fd01b4193ff1d4
Adrian Adames <adrianadames@gmail.com>
2018-06-20 15:41:03 -0400

    re-starting React-Todo project from scratch

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 import React from 'react';
-import TodoForm from "./components/TodoComponents/TodoForm";
-import Todo from "./components/TodoComponents/Todo";
-
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor(props) {
-    super(props);
-    this.state = [
-      // {
-      //   task: "",
-      //   id: "",
-      //   completed: "false",
-      // }
-    ];
-  }
-
-  addTodoTask = event => {
-    this.setState({task: event.target.value});
-  }
-
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoForm />
-        {/* <br></br> */}
-        
-        <Todo />
       </div>
     );
   }
 }
 
 export default App;
-
-
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,15 +1,1 @@
 // <TodoList /> receives your Todos array and iterates over the list generating a new <Todo /> for each element in the array.
-import React from 'react';
-
-class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-       return (
-           
-       );
-    };
-}
-
