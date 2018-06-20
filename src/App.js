e0ab511fbed08ad4c93d9b8089f7dc466222ab19
M.Real <mreal126@protonmail.com>
2018-06-20 15:40:50 -0400

    Beginning to add components.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
           completed: false
         },
         {
-          task: 'Make video for Sams Birthday',
+          task: '2 hours of Codewars JavaScript',
           id: 3,
           completed: false
         },
@@ -32,50 +32,11 @@ class App extends React.Component {
     };
   }
 
-addTodo = def => {
-    def.preventDefault();
-    const todos = this.state.todos.slice();
-    todos.push({ task: this.state.todo, completed: false, id: Date.now() });
-    this.setState({todos, todo: ""});
-  }
-
-changeTodo = def => this.setState({[def.target.name]: def.target.value});
-
-toggleTodoComplete = id => {
-    let todos = this.state.todos.slice();
-    todos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        return todo;
-      }
-      else {
-        return todo;
-      }
-    });
-    this.setState({todos});
-  };
-
-clearCompletedTodos = def => {
-  def.preventDefault();
-  let todos = this.state.todos.slice();
-  todos = todos.filter(todo => !todo.completed);
-  this.setState({todos});
-};
 
   render() {
     return (
       <div className="container">
       <h2>To-do List</h2>
-      <TodoList 
-      handleToggleComplete={this.toggleTodoComplete}
-      todos={this.state.todos}
-      />
-      <TodoForm
-      value={this.state.todo}
-      handleTodoChange={this.changeTodo}
-      handleAddTodo={this.addTodo}
-      handleClearTodos={this.clearCompletedTodos}
-      />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -6,15 +6,7 @@ import Todo from './Todo';
 const TodoList = props => {
     return (
      <div>
-      {props.todos.map(todo => {
-        return (
-          <Todo
-            handleToggleComplete={props.handleToggleComplete}
-            key={todo.id}
-            todo={todo}
-          />
-        );
-      })}
+       
     </div>
 );
 };
