f2e9012e8412b92270bea616c42734f43bbbcf51
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-20 18:00:05 -0500

    setting up class components

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
+// import App from '../../App.js';
+import React from 'react';
+
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+class TodoList extends App {
+constructor() { 
+    super();
+    console.log(this.state);
+  }
+  render() {
+  
+  }
+} 
