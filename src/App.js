8ff8bfa768ff4c4f8099ad2e38e784646d14dace
Your Name <you@example.com>
2018-06-20 14:21:51 -0700

    app.js fashoky

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -8,8 +8,9 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <h1>Learn setstate()</h1>
+        <h3> Style my Todo list</h3>
       </div>
-      <div></div>
     );
   }
 }
