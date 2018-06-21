27aadd81321fce4fb2770bf83c76f3771c68c9dc
Noah-Amar <ne.amar@outlook.com>
2018-06-21 17:58:18 -0500

    pull request

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+[
+    {
+      task: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    }
+  ];
