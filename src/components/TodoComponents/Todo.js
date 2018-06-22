f1bc479c52a4c2a501261821eee577185baa1b87
sean chavez <seanchavez@seanchavezsMBP.home>
2018-06-21 22:27:19 -0700

    Add strike through completed todo list items

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Todo.css'
 
 const Todo = (props) => {
-    return <div onClick= {() => props.completeHandler(props.singleTodo.id)}>{props.singleTodo.task}</div>
+    return (
+      <div 
+        onClick= {() => props.completeHandler(props.singleTodo.id)}
+        className= {props.singleTodo.completed ? 'completed' : null}
+      >
+        {props.singleTodo.task}
+      </div>
+    );
 };
 
 export default Todo
