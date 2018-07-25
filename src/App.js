79799a546bce6218af7fa5300cbc0477131f617e
Josue Peralta <jperal77@gmail.com>
2018-07-25 13:49:40 -0700

    Initial Commit and skeleton code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {};
+  }
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
+        <TodoList />
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Todo.css';
+
+
+export default TodoForm;
