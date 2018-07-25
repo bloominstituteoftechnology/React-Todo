6d1812ffb07c2fefdce92c708eefbd89b99227c5
szincone <sawyerzincone@gmail.com>
2018-07-25 17:05:42 -0600

    working version of mvp, with no linking of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Todo from './components/TodoComponents/Todo';
-import TodoList from './components/TodoComponents/TodoList';
+// import Todo from './components/TodoComponents/Todo';
+// import TodoList from './components/TodoComponents/TodoList';
 
 const myArr = [
   {
@@ -10,12 +10,6 @@ const myArr = [
     completed: false
   }
 ];
-
-// CCR - "Class", "Constructor", "Render"
-
-const MessageRenderer = props => {
-  return <h1>{props.propsMessage}</h1>;
-};
 
 const TodoTask = props => {
   const { task } = props.todoDataProp;
@@ -36,29 +30,27 @@ class App extends React.Component {
 
   handleUpdateState = () => {
     const todoData = this.state.todoData.slice();
-    
     // clone our state todoData array.
     todoData.push({
-      task: todoData.target,
+      task: this.state.message,
       id: Date.now(),
       completed: false
     });
-    // push our new task item into the todoData array.
+    // push our new animal item into the todoData array.
     this.setState({ todoData: todoData });
     // FROM REACT API the ONLY way to change state.
   };
 
   handleInputChange = event => {
     // update the message field on state.
-    this.setState({ task: event.target.value });
+    this.setState({ message: event.target.value });
   };
 
   render() {
     console.log("STATE: ", this.state);
     return (
       <div>
-        {/* {this.state.todoData.map(task => <Todo todoDataProp={task} />)} */}
-        <TodoList todoArray={this.state} />
+        {this.state.todoData.map(task => <TodoTask todoDataProp={task} />)}
         <input placeholder="...todo" onChange={this.handleInputChange} />
         <button onClick={this.handleUpdateState}>Add Todo</button>
         {/*All events in React go through what's called the Synthetic Event*/}
@@ -69,82 +61,4 @@ class App extends React.Component {
   }
 }
 
-
 export default App;
-
-//// delete everything below here
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import "./styles.css";
-
-// const myArr = [
-//   {
-//     id: 1,
-//     common_name: "Chilean flamingo",
-//     species: "Phoenicopterus chilensis"
-//   }
-// ];
-
-// // CCR - "Class", "Constructor", "Render"
-
-// const MessageRenderer = props => {
-//   return <h1>{props.propsMessage}</h1>;
-// };
-
-// const Animal = props => {
-//   const { common_name, species } = props.animalProp;
-//   return (
-//     <div>
-//       <h4>Name: {common_name}</h4>
-//       <h4>Species: {species}</h4>
-//     </div>
-//   );
-// };
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       message: "Hello",
-//       animals: myArr
-//     };
-//   }
-
-//   handleUpdateState = () => {
-//     const animals = this.state.animals.slice();
-//     // clone our state animals array.
-//     animals.push({
-//       id: 4,
-//       common_name: "Australian brush turkey",
-//       species: "Alectura lathami"
-//     });
-//     // push our new animal item into the animals array.
-//     this.setState({ animals: animals });
-//     // FROM REACT API the ONLY way to change state.
-//   };
-
-//   handleInputChange = event => {
-//     // update the message field on state.
-//     this.setState({ message: event.target.value });
-//   };
-
-//   render() {
-//     console.log("STATE: ", this.state);
-//     return (
-//       <div>
-//         {/*<MessageRenderer propsMessage={this.state.message} />*/}
-//         <div>Hello: {this.state.message}</div>
-//         {this.state.animals.map(animal => <Animal animalProp={animal} />)}
-//         <button onClick={this.handleUpdateState}>Click to Add Animal</button>
-//         {/*All events in React go through what's called the Synthetic Event*/}
-//         {/*Events are tied to user interaction with our code*/}
-//         <button onDoubleClick={this.handleUpdateState}>Click to Double</button>
-//         <input placeholder="capture" onChange={this.handleInputChange} />
-//       </div>
-//     );
-//   }
-// }
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,24 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 
-import React from "react";
-import "./Todo.css";
+// import React from "react";
+// import "./Todo.css";
 
-import Todo from './Todo';
+// import Todo from './Todo';
 
-const TodoList = props => {
-  console.log(props);
-  const todoData = props;
-  return (
-    <div>
-      {/* {props.todoData.map(task => (
-        <Todo
-          // id={props.id}
-          todoDataProp={task}
-        />
-      ))} */}
-    </div>
-  )
-};
+// const TodoList = props => {
+//   console.log(props);
+//   const todoData = props.todoArray;
+//   return (
+//     <div>
+//       {todoData.map(task => (
+//         <Todo
+//           todoDataProp={task}
+//         />
+//       ))}
+//     </div>
+//   )
+// };
 
-export default TodoList;
+// export default TodoList;
