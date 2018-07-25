9f7b897f4dcb0fa0539568492b8ffbe972a24e45
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-25 15:31:56 -0500

    completed todo and todolist functionality

--- src/App.js
+++ src/App.js
@@ -12,16 +12,39 @@ const myArr = [
     id: 1528817084358,
     completed: false
   }
+  
 ];
+
+const TodoList = props => {
+  return <div>{props.toDoListProp.map(todo => <Todo toDoProp={todo} />)}</div>;
+}
+
+const Todo = props => {
+  const { task } = props.toDoProp
+  return (
+    <div>
+      <h4>Task: {task}</h4>
+    </div>);
+}
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: myArr
+    };
+  }
+
   render() {
+    console.log("STATE: ", this.state);
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList toDoListProp={this.state.todos}/>
       </div>
     );
   }
