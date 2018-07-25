2574b41ab7dced0ea1322bb95d55a4fef0d859af
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-25 14:42:47 -0500

    started array of data on app.js for first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+
+const myArr = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
