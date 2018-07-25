0d79fade0925d7c9bc1acf5ff05a9996f2b5cf6d
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-25 16:55:52 -0500

    Working on logic
    trying to figure out how to get these buttons to work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import addTodo from './components/TodoComponents/Todo'; 
 
-// const addTodo = [
-//   {
-//     task: "",
-//     id: Date.now(),
-//     completed: false
-//   }
-// ];
-
-// const newTodo = props =>{
-
-// }
-
-
+const newTodo = props =>{
+  const{task} = props.newTodoProp; 
+  return(
+    <div>
+      <h4>{task}</h4>
+    </div>
+  )
+}
 class App extends React.Component {
   constructor(){
     super();
@@ -22,13 +17,25 @@ class App extends React.Component {
       todo: addTodo, 
     }; 
   }
+  handleEnterTask = event =>{
+   
+  }
+  handleUpdateState = () =>{
+    const todo = this.state.todo.slice(); 
+
+    todo.push({
+    task: "",
+    id: Date.now(),
+    completed: false
+    })
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <div>{this.state.message}</div>
+       <div>{this.state.message}</div>
        <input placeholder="Enter Task" />
        <button>Add a Todo</button>
        <button>Completed Todo</button>
