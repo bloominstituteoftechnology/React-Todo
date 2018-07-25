c5101f38cb1e319c3d74c1e061b055b0cf43b0a5
Arthur Bates <aabatesj@gmail.com>
2018-07-25 16:43:15 -0400

    TodoForm
    added synthetic event that handles change of state to input value

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,12 +3,24 @@ import React from 'react';
 class Form extends React.Component {
 
     state = {
+        inputValue: "Hello you"
 
+    }
+
+    handlechange = () => {
+        console.log('changed');
     }
 
     render() {
         return (
             <div className="Form">
+                <form>
+                    <input
+
+                        onChange={this.handleChange}
+                        value={this.state.inputValue}
+                    />
+                </form>
             </div>
         );
     }
