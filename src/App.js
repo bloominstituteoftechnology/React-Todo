353826ffe52656e7a197325575b3a20a0d18426b
AI2EJAY <JHammett1314@gmail.com>
2018-07-25 18:09:23 -0500

    added boiler plate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
-
-class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+
+//comment
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      thingsToDo: [],
+      temp: '',
+    };
+  }
+
+  handleChange = event => {
+    console.log(event.target.value);
+    this.setState(
+      {temp: event.target.value}
+    )
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <p>{this.state.thingsToDo}</p>
+        <input type="text" value={this.state.temp} onChange={this.handleChange} />
       </div>
     );
   }
 }
-
-export default App;
-
-//comment
