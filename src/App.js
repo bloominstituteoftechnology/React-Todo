e9672484e3255f7b5de189f8dffe3868c0e87328
David White <dw@Davids-iMac.local>
2018-07-25 16:50:23 -0600

    still a the starting point

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
+
+
+const todo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      task: myArr,
+      todo: [],
+      this: ''
+    };
+
+  }
+ 
+  handleAddTask = () => {
+    const task = this.state.task.slice();
+  
+  
+
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        
+
+        <input placeholder="...todo" onchange={} />
+        <button onClick={this.handleUpdateState}>Add ToDo</button>
       </div>
     );
   }
 }
 
-export default App;
+
