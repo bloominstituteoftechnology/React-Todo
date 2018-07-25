5dae7e6b84bd28d6265de0c5138bdec3c9f39bbd
jaredrcooper <jaredrcoopet@gmail.com>
2018-07-25 16:57:19 -0400

    adding todos works, blank strings won't be added, removing currently removes all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from "react";
+import "./App.css";
+
+//implement logic to not add a item if the text input is blank
+
+//remove the todo from array
+
+//stylelize
 
 class App extends React.Component {
   constructor(props) {
@@ -14,10 +21,23 @@ class App extends React.Component {
     });
   }
 
-  addToDo = (input) => {
-    //making a copy of this.state.list so we don't directly change the state
-    let toDoArray = this.state.list;
-    toDoArray.push(input);
+  addToDo = input => {
+    //if the input has a length then execute
+    if (input.length) {
+      let toDoArray = this.state.list;
+      toDoArray.push(input);
+      this.setState({
+        list: toDoArray,
+        //sets
+        userInput: ""
+      });
+    }
+  };
+
+  //making a copy of this.state.list so we don't directly change the state
+
+  removeToDo = () => {
+    let toDoArray = this.state.list.splice();
     this.setState({
       list: toDoArray
     })
@@ -25,26 +45,40 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="main-container">
-        <h1>To Do List by Jared Cooper</h1>
-        <input
-          onChange={e => {
-            this.changeUserInput(e.target.value);
-          }}
-          value={this.state.userInput}
-          type="text"
-          placeholder="Type a todo"
-        />
-        <button onClick={(e) => {
-          this.addToDo(this.state.userInput)
-        }}>Add</button>
+      <div className="wrapper">
+        <div className="main-container">
+          <h1 className="title">To Do List by Jared Cooper</h1>
+          <input
+            className="input-form"
+            onChange={e => {
+              this.changeUserInput(e.target.value);
+            }}
+            value={this.state.userInput}
+            type="text"
+            placeholder="to do..."
+          />
+          <button
+            className="onlyBtn"
+            onClick={e => {
+              this.addToDo(this.state.userInput);
+            }}
+          >
+            Add
+          </button>
 
-        <ul>
-          {this.state.list.map( (item) => 
-            <li>{item}</li>
-          )}
-        </ul>
+          <button
+            className="onlyBtn"
+            onClick={e => {
+              this.removeToDo();
+            }}
+          >
+            Remove
+          </button>
 
+          <ul className="only-ul">
+            {this.state.list.map((item, i) => <li key={i}>{item}</li>)}
+          </ul>
+        </div>
       </div>
     );
   }
