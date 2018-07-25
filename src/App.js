1e695658efd411bbe00e01104f27a56512074308
Carey <careycbaldwin@gmail.com>
2018-07-25 17:06:52 -0400

    managed to mimic behavior of animal example
    need to add items to list instead of top of page

--- src/App.js
+++ src/App.js
@@ -3,35 +3,64 @@ import './App.css';
 import TodoForm from './components/TodoComponents/TodoForm';
 
 
+const todos  = [
+  {
+    task: 'Fold Laundry',
+    id: 1,
+  },
+  {
+    task: 'Wash Car',
+    id: 2,
+  }
+];
+
+const listGenerator = props => {
+  return <div>{props.listItem}</div>
+}
+
+const ListItem = props => {
+  const {task, id} = props.listProp;
+  return(
+    <div>
+      <h4>Item: {task}</h4>
+      <h4>ID: {id}</h4>
+    </div>
+  );
+};
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-    constructor(){
+    constructor() {
       super();
       this.state = {
-        todos: [
-          {
-            task: 'Fold Laundry',
-          },
-          {
-            task: 'Wash Car',
-          },
-        ],
-        todo: ''
+        list: "",
+        todos: todos,
       };
     }
 
     handleUpdateState = () => {
-      const totos = this.state.todos.slice();
-    }
+      const todos = this.state.todos.slice();
+      this.setState({todos: todos});
+    };
+
+    handleInputChange = event => {
+      console.log(event.target.value);
+      this.setState({list: event.target.value });
+    };
  
   render() {
+    console.log("STATE: ", this.state);
     return (
-      <div className = "container">
-        <h1>List of Tasks</h1>
-        <TodoForm />
-      </div>
+      <div>
+       <div>{this.state.list}</div>
+       {this.state.todos.map(todo =><ListItem listProp = {todo}   />)}
+        <button onClick = {this.handleUpdateState}>Click to add item</button>
+        <button>Clear Completed</button>
+        <input placeholder = "enter" onChange = {this.handleInputChange} />
+      </div>  
     );
   }
 }
