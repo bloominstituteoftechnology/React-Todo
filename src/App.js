573b8b430fc573becc1bd698a69b312689fc5b11
jaredrcooper <jaredrcoopet@gmail.com>
2018-07-25 15:53:41 -0400

    adding todos works, needs styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      list: []
+    };
+  }
+  changeUserInput(input) {
+    this.setState({
+      userInput: input
+    });
+  }
+
+  addToDo = (input) => {
+    //making a copy of this.state.list so we don't directly change the state
+    let toDoArray = this.state.list;
+    toDoArray.push(input);
+    this.setState({
+      list: toDoArray
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="main-container">
+        <h1>To Do List by Jared Cooper</h1>
+        <input
+          onChange={e => {
+            this.changeUserInput(e.target.value);
+          }}
+          value={this.state.userInput}
+          type="text"
+          placeholder="Type a todo"
+        />
+        <button onClick={(e) => {
+          this.addToDo(this.state.userInput)
+        }}>Add</button>
+
+        <ul>
+          {this.state.list.map( (item) => 
+            <li>{item}</li>
+          )}
+        </ul>
+
       </div>
     );
   }
