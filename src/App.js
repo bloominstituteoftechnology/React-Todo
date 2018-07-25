f9c7bb2196b5a1e601fefb7cae36ee7e08a59a8f
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-25 16:58:53 -0500

    initial pull...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class App extends React.Component {
+class App extends React.Component { 
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
