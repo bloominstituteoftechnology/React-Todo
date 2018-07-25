75e1c2076329c24284a363f7231cf4edbdc96187
tulf <timzulf@gmail.com>
2018-07-25 17:54:42 -0400

    I still don't know what I'm doing holy crap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+
+const myArr = [
+  {
+    id: 1,
+    common_name: "Chilean flamingo",
+    species: "Phoenicopterus chilensis"
+  }
+];
+
+// CCR - "Class", "Constructor", "Render"
+
+const MessageRenderer = props => {
+  return <h1>{props.propsMessage}</h1>;
+};
+
+const Animal = props => {
+  const { common_name, species } = props.animalProp;
+  return (
+    <div>
+      <h4>Name: {common_name}</h4>
+      <h4>Species: {species}</h4>
+    </div>
+  );
+};
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello",
+      animals: myArr
+    };
+  }
+
+  handleUpdateState = () => {
+    const animals = this.state.animals.slice();
+    // clone our state animals array.
+    animals.push({
+      id: 4,
+      common_name: "Australian brush turkey",
+      species: "Alectura lathami"
+    });
+    // push our new animal item into the animals array.
+    this.setState({ animals: animals });
+    // FROM REACT API the ONLY way to change state.
+  };
+
+  handleInputChange = event => {
+    // update the message field on state.
+    this.setState({ message: event.target.value });
+  };
+
   render() {
+    console.log("STATE: ", this.state);
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        {/*<MessageRenderer propsMessage={this.state.message} />*/}
+        <div>Hello: {this.state.message}</div>
+        {this.state.animals.map(animal => <Animal animalProp={animal} />)}
+        <button onClick={this.handleUpdateState}>Click to Add Animal</button>
+        {/*All events in React go through what's called the Synthetic Event*/}
+        {/*Events are tied to user interaction with our code*/}
+        <button onDoubleClick={this.handleUpdateState}>Click to Double</button>
+        <input placeholder="capture" onChange={this.handleInputChange} />
       </div>
     );
   }
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
