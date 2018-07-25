9f527c1ba12bfa0b8ac3dea07b1210bd08184d30
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-25 15:43:42 -0600

    Added data to todo and instructions in sections.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      // Your todo data should be an array of objects that look a lot like this:
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
     };
   }
   
-  
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Todo = props => {
     return (
         <div>
+            {/* <TodoList /> receives your Todos array and iterates over the list generating a new <Todo /> for each element in the array. */}
             
         </div>
     );
