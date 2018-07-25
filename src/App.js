9c030fc3c00790cd927e0eca2edf87e37c8d9171
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-25 14:40:14 -0500

    Initial PR
    Initial PR
    Added toDos array to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import React from 'react';
 
+const toDos = [
+  {
+    task: 'Open Master Branch',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Fork a copy',
+    id: Date.now(),
+    completed: false
+  }
+  {
+    task: 'Clone your copy',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Make changes and push them up',
+    id: Date.now(),
+    completed: false
+  }
+  {
+    task: 'Complete first pull request',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Knock it out of the park',
+    id: Date.now(),
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
