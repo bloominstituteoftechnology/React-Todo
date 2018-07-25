40088e0e0d725550e6507144b2c731755c739867
keegn <keegan.t.burkett@gmail.com>
2018-07-25 15:45:57 -0500

    scaffolding app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+
+// design `App` to be the parent component of your application
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // state store
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    }
+  }
+
+  // change handlers 
+
+
+ // the this keyword gives you access to the constructor
+// when there are updates to state render gets called again
+// render method
   render() {
     return (
       <div>
