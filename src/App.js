9e8b118aba43bf1edbc50ceb87ce83cad99f6b5f
William <kwontaewoongah@gmail.com>
2018-07-25 14:14:54 -0700

    made place for state to live

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+const Todos=[];
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state={
+      list: Todos
+    }
+  }
   render() {
     return (
       <div>
