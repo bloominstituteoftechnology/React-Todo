478b253adc08011e20a7e6771daf65643677191e
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-25 12:31:49 -0700

    add structure to components

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Todo.css';
+
+const TodoForm = () => {
+    return (
+        <input type="text" placeholder="Add something you need to do!" />
+    );
+}
+
+export default TodoForm;
