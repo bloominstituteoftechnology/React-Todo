64a5da46233bafe7fa66930b783bfab39d662e35
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-25 13:40:57 -0700

    Major changes - TodoForm.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
+//================= My array ====================
+const myTask = [
+  {
+    task: 'Organize Garage',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: Date.now(),
+    completed: false
+  }
+];
 
 class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
+        <TodoForm />
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import '.Todo.css';
+
+// Custom Button
+const TodoForm = props => {
+    return (
+        <div className="my-form">
+            <input placeholder="...todo" />
+            <button className="add-button custom-button">Add Todo</button>
+            <button className="clear-button custom-button">Clear Completed</button>
+        </div>
+    );
+};
+
+export default TodoForm;
