5f0dd360d56ca2c1362c17a47f19cfee1b3ca457
William <kwontaewoongah@gmail.com>
2018-07-25 20:22:47 -0700

    finished keypress functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import ToDoForm from './components/TodoComponents/TodoForm';
+import {ToDoInput, ToDoButton, ClearCompletedButton} from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
-const Todos=[{
-  task: 'Organize Garage',
-  id: 1528817077286,
-  completed: false
-},
-{
-  task: 'Bake Cookies',
-  id: 1528817084358,
-  completed: false
-}];
+
+const Todos=[];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -21,14 +14,27 @@ class App extends React.Component {
       list: Todos
     }
   }
+  updateTodos=()=>{
+    const list=this.state.list;
+    list.push({
+      'task': document.querySelector('.input-field').value,
+      'id':Date.now(),
+      'completed':false
+    });
+    this.setState({list:list});
+    document.querySelector('.input-field').value='';
+  }
   render() {
     return (
       <div>
-        <ToDoForm />
+        <div onKeyPress={ (e) => {if (e.key === 'Enter') {this.updateTodos()}}}><ToDoInput/></div>
+        <ClearCompletedButton />
+        <div onClick={this.updateTodos}><ToDoButton /></div>
         <TodoList taskProp={this.state.list} />
       </div>
     );
   }
+  
 }
 
 export default App;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const ToDoInput=()=><input className='input-field' type='text' placeholder='Enter a Todo'/>
-const ToDoButton=()=><div className='todo-button'>Add Todo</div>
-const ClearCompletedButton=()=><div className='clearcompleted-button'>Clear Completed</div>
+export const ToDoInput=()=><input className='input-field' type='text' placeholder='Enter a Todo'/>
+export const ToDoButton=()=><div className='todo-button' >Add Todo</div>
+export const ClearCompletedButton=()=><div className='clearcompleted-button'>Clear Completed</div>
 
-const ToDoForm=()=><div><ToDoInput /><ToDoButton /><ClearCompletedButton /></div>
 
-export default ToDoForm;
+
