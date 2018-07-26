58877f1a07c423d1d16eb83bcf215479d1af48b5
mkerbleski <mkerbles@gmail.com>
2018-07-26 09:47:17 -0600

    Starting from the top.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
 
-const Task = props => {
-  return <p>Task #{newArr.length}: {props.propsMessage}</p>;
-}
-
-
-
-const newArr = [
-{ id: 0,
-  task: {}}
-];
-
-// const newObj = {
-//   id: 1,
-//   task: {}
-// };
-
 class App extends React.Component {
   constructor () {
     super();
     this.state = {
       message: "this is the message from state",
-      taskList: newArr
     };
   };
 
@@ -29,41 +12,9 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  addTask = () => {
-    console.log('yo');
-    this.setState(newArr.push({
-      id: "hi",
-      task:"bye"
-    }));
-    console.log('bye')
-
-
-  }
-
-  deleteTask = () => {
-    console.log('updateStatus called')
-    this.setState({message: "a task was deleted"});
-    newArr.pop()
-  }
-
-  captureNewTask = (x) => {
-    console.log(x.target.value);
-    this.setState({message: "capture newtask message"})
-    return x.target.value;
-  }
 
   render() {
     return (
-      <div>
-        <h2>Todo App!</h2>
-        <Task propsMessage={this.state.message} />
-        {/* <Task propsMessage={this.state.task[0].id} /> */}
-        {/* {this.state.task.map(x => <Task propsMessage={x.task} />)} */}
-        {this.state.taskList.map(x => <Task propsMessage={this.state.taskList[1]} />)}
-        <input placeholder="newtask" onChange={this.captureNewTask} onClick={this.addTask}></input>
-        <button onClick={this.addTask}>Add task</button>
-        <button onClick={this.deleteTask}>Delete task</button>
-      </div>
     );
   }
 }
