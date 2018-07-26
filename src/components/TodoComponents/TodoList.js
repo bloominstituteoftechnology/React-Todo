8e97bae4721ee0ec3dcbe46e3f583364ea500f9e
cubsfan46350 <cubsfan.46350@msn.com>
2018-07-26 03:18:04 -0500

    added some changes

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,7 +3,7 @@ import Todo from './Todo';
 const TodoList = props => {
     return (
         <div>
-          {props.todos.map(todo => <Todo todo={todo} />)}
+          {props.todos.map(todo => <Todo todo = {todo.task} id = {todo.id} />)}
         </div>
     );
 };
