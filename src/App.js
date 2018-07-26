a70a20c8213c573629a269ad39378df27410a713
keegn <keegan.t.burkett@gmail.com>
2018-07-25 21:59:13 -0500

    playing with styles and getting to know my app component layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
-import TodoForm from './components/TodoComponents/TodoForm'
+import TodoForm from './components/TodoComponents/TodoForm';
+import './App.css'
+
 
 
 // design `App` to be the parent component of your application
@@ -11,12 +13,12 @@ class App extends React.Component {
     this.state = {
       todos: [
         {
-          task: 'Organize Garage',
+          task: 'Placeholder Todo 1',
           id: 1528817077286,
           completed: false
         },
         {
-          task: 'Bake Cookies',
+          task: 'Placeholder Todo 2',
           id: 1528817084358,
           completed: false
         }
@@ -25,35 +27,34 @@ class App extends React.Component {
     }
   }
 
-  // change handlers 
+  // CHANGE HANDLERS
   // ADD TODO
   addTodo = event => {
     event.preventDefault();
     // here we are making a new todos array
     const todos = this.state.todos.slice();
+    console.log(todos)
     // pushing new todo onto sliced todos array
     todos.push({ task: this.state.todo, completed: false, id: Date.now() });
     this.setState({ todos, todo: '' });
   };
-
-  // TODO Change
-  changeTodo = event => this.setState({ [event.target.name]: event.target.value });
+  // HANDLE INPUT CHANGES 
+  updateInput = event => this.setState({ [event.target.name]: event.target.value });
 
 
  // the this keyword gives you access to the constructor
 // when there are updates to state render gets called again
-// render method
+// RENDER METHOD
   render() {
     return (
-      <div>
+      <div className="flex">
       <TodoList
         todos={this.state.todos}
-        
        />
        <TodoForm
         value={this.state.todo}
         handleAddTodo={this.addTodo}
-        handleTodoChange={this.changeTodo}
+        handleInputChange={this.updateInput}
       />
       </div>
     );
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './Todo.css'
 
 const TodoForm = props => {
     return (
         <form>
             <input 
-                onChange={props.handleTodoChange}
+                onChange={props.handleInputChange}
                 type="text"
                 name="todo"
                 value={props.value}
                 placeholder="...todo"
             />
-            <button onClick={props.handleAddTodo}>Add Todo</button>
-            {/* <buttonon Click={props.handleClearTodos}>Clear Completed</button> */}
+            <button onClick={props.handleAddTodo} className={`btn ${props.className}`}>Add Todo</button>
+            <button>Clear Completed</button>
         </form>
     )
 }
