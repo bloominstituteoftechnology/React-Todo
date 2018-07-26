d043df78200d74548874ec3e8c32444b3ca2552f
William <kwontaewoongah@gmail.com>
2018-07-26 01:45:33 -0700

    finished with css

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 
-const Todo=props=><div className="card teal lighten-2"><div class="card-content white-text"><p className='flow-text'>{props.taskProp.task}</p></div></div>
+const Todo=props=><div className="card"><div className="card-content"><p className='flow-text'>{props.taskProp.task}</p></div></div>
 
 export default Todo;
