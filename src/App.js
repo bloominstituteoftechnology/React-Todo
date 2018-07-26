7485ec88ad331fc749526340e7ac1d2203c07938
mkerbleski <mkerbles@gmail.com>
2018-07-25 20:40:25 -0600

    not really any further.

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ const newArr = [
   task: {}}
 ];
 
-const newObj = {
-  id: 1,
-  task: {}
-};
+// const newObj = {
+//   id: 1,
+//   task: {}
+// };
 
 class App extends React.Component {
   constructor () {
     super();
     this.state = {
       message: "this is the message from state",
-      taskList: newArr,
+      taskList: newArr
     };
   };
 
@@ -29,10 +29,15 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  addTask = (y) => {
-    console.log('updateStatus called')
-    this.setState({message: "a task was added"});
-    this.setState({tasklist: newArr.push(newObj.task = "hello")});
+  addTask = () => {
+    console.log('yo');
+    this.setState(newArr.push({
+      id: "hi",
+      task:"bye"
+    }));
+    console.log('bye')
+
+
   }
 
   deleteTask = () => {
@@ -41,8 +46,9 @@ class App extends React.Component {
     newArr.pop()
   }
 
-  changeStateMessage = (x) => {
+  captureNewTask = (x) => {
     console.log(x.target.value);
+    this.setState({message: "capture newtask message"})
     return x.target.value;
   }
 
@@ -54,7 +60,7 @@ class App extends React.Component {
         {/* <Task propsMessage={this.state.task[0].id} /> */}
         {/* {this.state.task.map(x => <Task propsMessage={x.task} />)} */}
         {this.state.taskList.map(x => <Task propsMessage={this.state.taskList[1]} />)}
-        <input placeholder="newtask" onChange={this.changeStateMessage}></input>
+        <input placeholder="newtask" onChange={this.captureNewTask} onClick={this.addTask}></input>
         <button onClick={this.addTask}>Add task</button>
         <button onClick={this.deleteTask}>Delete task</button>
       </div>
