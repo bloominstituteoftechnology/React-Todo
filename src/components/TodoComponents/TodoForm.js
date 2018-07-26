3592d844b56958f861f6c78d502ea5cff982553f
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-25 20:11:30 -0400

    More styling updates

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,7 +4,7 @@ import './Todo.css';
 const TodoForm = props =>{
   return(
     <div className="todo-form-container">
-      <input onKeyPress={props.handleEnter} onChange={props.change} type="text" value={props.text} />
+      <input align="center" className="todo-form-input" onKeyPress={props.handleEnter} onChange={props.change} type="text" value={props.text} />
       <button onClick={props.addClick} className="btn add-btn">Add Todo!</button>
       <button onClick={props.handleCompleted} className="btn clear-btn">Clear Completed!</button>
     </div>
