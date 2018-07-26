0f7fbeaaf564a587873fc60ccb413a3e23eb7404
David White <dw@Davids-iMac.local>
2018-07-26 16:47:40 -0600

    Completed todo form, added to app.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
 import React from 'react';
+import "./Todo.css";
+import TodoForm from "./components/TodoComponents/TodoForm";
 
 
 
-const todo = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  }
-];
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      task: myArr,
-      todo: [],
+      task: [],
+      char: [],
       this: ''
     };
 
   }
  
-  handleAddTask = () => {
-    const task = this.state.task.slice();
-  
-  
+  handleChange = event => {
+    this.setState({ char: event.target.value });
+  };
 
-  }
+  handleSubmit = event => {
+    event.preventDefault();
+    const chars = this.state.chars.slice();
+    chars.push(this.state.char);
+    this.setState({ chars: chars });
+  };
 
-
-  render() {
+render() {
     return (
-      <div>
-        
-
-        <input placeholder="...todo" onchange={} />
-        <button onClick={this.handleUpdateState}>Add ToDo</button>
+      <div classname="App">
+        <header className="App-header">
+            <h1> className="App-title">TODO List</h1>
+        </header>    
+        <TodoForm />
+          <handleSubmit = {this.handleSubmit}/>
+          <handleChange = {this.handleChange}/>
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,4 +1,14 @@
+import React from 'react';
 
+const Harryform = props => {
+   return ( 
+        <form>
+        <input type="text" onChange={props.handleChange} />
+        <button onClick={props.handleSubmit}>Add Todo</button>
+        <button onclick={}>Clear Completed</button>
+        </form>
+   );
+};
 
 
 
