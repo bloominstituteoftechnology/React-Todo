4459a65709435ae41657bd6ad89cb0f050857e44
William <kwontaewoongah@gmail.com>
2018-07-26 14:50:12 -0700

    made some changes

--- src/App.js
+++ src/App.js
@@ -12,37 +12,45 @@ class App extends React.Component {
   constructor() {
     super();
     this.state={
-      list: Todos
+      list: Todos,
+      savedList: localStorage.getItem('taskListItems')
     }
   }
   addTodos=()=>{
-    const list=this.state.list;
-    list.push({
-      'task': document.querySelector('.input-field').value,
-      'id':Date.now(),
-      'completed':false
-    });
-    this.setState({list:list});
-    document.querySelector('.input-field').value='';
+    if (document.querySelector('.input-field').value.length>=1){
+      const list=this.state.list.slice(0);
+      list.push({'task': document.querySelector('.input-field').value, 'id':Date.now(),'completed':false});
+      this.setState({list:list},localStorage.setItem('taskListItems',JSON.stringify(this.state.list)));
+      document.querySelector('.input-field').value='';
+    } 
   }
-  updateTaskStatus=event=>{
-    event.target.classList.toggle('completed');
-    const list=this.state.list;
-    for (let i=0; i<list.length; i++) {
-      if (list[i]['task']===event.target.textContent){
-        event.target.classList.contains('completed') ? list[i]['completed']=true:list[i]['completed']=false;
+  componentDidMount() {
+    console.log(this.state.savedList)
+    if (this.state.savedList) {
+      this.setState({list:JSON.parse(this.state.savedList)});
     }
   }
-    this.setState({list:list});
+ 
+  updateTaskStatus=event=>{
+    const list=this.state.list.slice(0);
+    const task=event.target.textContent;
+    for (let i=0,n=list.length; i<n; i++) {
+      if (list[i]['task']===task){
+        list[i]['completed']=!(list[i]['completed']);
+        list[i]['completed']===true ? event.target.classList.add('completed'):event.target.classList.remove('completed');
+      }
+    }
+    this.setState({list:list},localStorage.setItem('taskListItems',JSON.stringify(this.state.list)));;
   }
   removeCompleted=()=>{
-    let list=this.state.list;
+    let list=this.state.list.slice(0);
     list=list.filter((e)=>e.completed===false);
-    this.setState({list:list});
+    this.setState({list:list},localStorage.setItem('taskListItems',JSON.stringify(this.state.list)));
   }
   render() {
     return (
       <div className='tasklist'>
+        <h1>Task List</h1>
         <div className='input' onKeyPress={ (e) => {if (e.key === 'Enter') {this.addTodos()}}}><ToDoInput/></div>
         <div className="waves-effect waves-light btn ccbtn" onClick={this.removeCompleted}><ClearCompletedButton /></div>
         <div className="waves-effect waves-light btn todobtn" onClick={this.addTodos}><ToDoButton /></div>
@@ -50,7 +58,6 @@ class App extends React.Component {
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 
-const Todo=props=><div className="card"><div className="card-content"><p className='flow-text'>{props.taskProp.task}</p></div></div>
+const Todo=props=><div className="card"><div className="card-content"><p id={props.taskProp.id} className='flow-text'>{props.taskProp.task}</p></div></div>
 
 export default Todo;
