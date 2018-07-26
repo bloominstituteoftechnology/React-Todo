56b1469622ee89f9927fa3faf64b20baec1e3d9e
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-26 17:43:15 -0500

    inital

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,3 +1,15 @@
 import react from "react";
 import './Todo.css';
 import './TodoForm';
+
+const TodoForm = ({handleInputChange})=> {
+    
+    return (
+        <form>
+            <input placeholder= 'To Do Here ' type = 'text' onChange = { handleInputChange} />
+            <button> Add To Do: </button>
+            </form>
+    )
+}
+
+export default TodoForm;
