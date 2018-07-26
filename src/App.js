8448bceb6b710202fc11066fe65d4ad98fbb8563
kamalnrf <kamalnrf@hotmail.com>
2018-07-27 03:39:00 +0530

    Added Search

--- src/App.js
+++ src/App.js
@@ -33,13 +33,25 @@ const days  = ["Sunday 🖖","Monday 💪😀","Tuesday 😜","Wednesday 😌☕
 class App extends React.Component {
   constructor() {
     super();
+    const cache = localStorage.getItem('list')
+    console.log(cache)
     this.state = {
       task: "",
-      list: sample
-    };
+      list: cache == undefined ? cache: sample,
+      search: "",
+      sList: []
+    }
     this.date = new Date();
     this.randomWord = randomWord[Math.floor(Math.random() * (8 - 1) + 1)]
     this.day = days[this.date.getDay()]
+  }
+
+  handleInputChange = event => {
+    this.setState({task: event.target.value})
+  }
+
+  handleSInputChange = event => {
+    this.setState({search: event.target.value})
   }
 
   handleUpdateState = () => {
@@ -53,8 +65,14 @@ class App extends React.Component {
     this.setState({ list: list });
   };
 
-  handleInputChange = event => {
-    this.setState({task: event.target.value})
+  handleSearchUpdate = () => {
+    const list = this.state.list.slice();
+    const newList = list.filter(todo => {
+      console.log("STRING SEARCH")
+      return todo.task.includes(this.state.search)
+    })
+    document.getElementById('searchInput').value = ''
+    this.setState({ sList: list, list: newList });
   }
 
   updateTask = event => {
@@ -76,9 +94,16 @@ class App extends React.Component {
     this.setState({ list: newList});
   }
 
+  handleSearchComplete = () => {
+    this.setState({list: this.state.sList})
+  }
+
   render() {
     console.log("STATE")
     console.log(this.state)
+    localStorage.setItem('list', [this.state.list])
+    const s = localStorage.getItem('list')
+    console.log(s)
     // console.log("STATE: ", this.state);
     return (
       <div class="container">
@@ -86,8 +111,8 @@ class App extends React.Component {
           <h1>{`${this.randomWord} ${this.day}`}</h1>
         </div>
         <div class="todoContainer">
-        <TodoForm inputChange={this.handleInputChange} dataChange={this.handleUpdateState} clearCompleted={this.handleCompleted}/>
-          <TodoList list={this.state.list} updateTask={this.updateTask}/>
+        <TodoForm inputChange={this.handleInputChange} sInputChange={this.handleSInputChange} dataChange={this.handleUpdateState} sdataChange={this.handleSearchUpdate} clearCompleted={this.handleCompleted} sclearCompleted={this.handleSearchComplete}/>
+        <TodoList list={this.state.list} updateTask={this.updateTask}/>
         </div>
       </div>
     );
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,9 +4,16 @@ import ReactDOM from "react-dom";
 function TodoForm (props){
     return (
         <div>
-            <input placeholder="Add todo" onChange={props.inputChange} id="myInput"/>
-            <button onClick={props.dataChange}>Add Todo</button>
-            <button onClick={props.clearCompleted}>Clear Completed Todo</button>
+            <div className="inputContainer">
+                <input placeholder="Add todo" onChange={props.inputChange} id="myInput"/>
+                <button onClick={props.dataChange}>Add Todo</button>
+                <button onClick={props.clearCompleted}>Clear Completed Todo</button>
+            </div>
+            <div className="searchContainer">
+                <input placeholder="search" onChange={props.sInputChange} id="searchInput"/>
+                <button onClick={props.sdataChange}>Search</button>
+                <button onClick={props.sclearCompleted}>Clear Search</button>
+            </div>
         </div>
     )
 }
