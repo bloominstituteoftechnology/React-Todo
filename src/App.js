50488f7b33846beb1e267d0c3d022e74a985180b
Carey <careycbaldwin@gmail.com>
2018-07-26 12:50:01 -0400

    erased previous work and followed solution

--- src/App.js
+++ src/App.js
@@ -1,70 +1,77 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
 
-const todos  = [
-  {
-    task: 'Fold Laundry',
-  },
-  {
-    task: 'Wash Car',
-  }
-];
-
-const listGenerator = props => {
-  return <div>{props.listItem}</div>
-}
-
-const ListItem = props => {
-  const {task, id} = props.listProp;
-  return(
-    <div>
-      <h4>task: {task}</h4>
-    </div>
-  );
-};
-
-
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-    constructor() {
-      super();
-      this.state = {
-        thingsToDo: [],
-        temp: '',
-      };
-    }
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
 
-    addItem = event => {
+    addTodo = event => {
       event.preventDefault();
       const todos = this.state.todos.slice();
-      todos.push({task: this.state.todo });
-      this.setState({todos, todo: ''});
+      todos.push({task: this.state.todo, completed: false, id: Date.now() });
+      this.setState({todos, todo: '' });
     };
 
-    handleChange = event => {
-      console.log(event.target.value);
-      this.setState({temp: event.target.value}
-      );
+    changeTodo = event =>  this.setState({ [event.target.name]: event.target.value});
+  
+    toggleTodoComplete = id => {
+      let todos = this.state.todos.slice();
+      todos = todos.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed;
+          return todo;
+        }else{
+          return todo;
+        }
+      });
+      this.setState({ todos });
     };
 
+    clearCompletedTodos = event => {
+      event.preventDefault();
+      let todos = this.state.todos.slice();
+      todos = todos.filter(todo => !todo.completed);
+      this.setState({ todos });
+    };
 
  
   render() {
     return (
-      <div>
-       
-        <h1> To-do List</h1>
-
-        <p> {this.state.thingsToDo}</p>
-
-        <input type="text" value = {this.state.temp} onChange = {this.handleChange} />
+      <div className = "container">
+        <h1> To-do List </h1>
+        <TodoList
+          handleToggleComplete = {this.toggleTodoComplete}
+          todos = {this.state.todos}
+        />
+        <TodoForm
+          value={this.state.todo}
+          handleTodoChange = {this.changeTodo}
+          handleAddTodo = {this.addTodo}
+          handleClearTodos = {this.clearCompletedTodos}
+          />
       </div>  
     );
   }
 }
+
 
 export default App;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,16 +1,20 @@
 import React from 'react';
-
 import './TodoForm.css';
 
-const TodoForm = props=> {
+const TodoForm = props => {
     return (
         <form className = "form-container">
-            <input type = "text" name = "todo" value = {props.value} placeholder = "task here"/>
-            <button>add task</button>
-        <button>clear completed</button>
+            <input 
+            onChange = {props.handleTodoChange}
+            type = "text" 
+            name = "todo" 
+            value = {props.value} 
+            placeholder = "task here"
+            />
+            <button onClick = {props.handleAddTodo}>add task</button>
+            <button onClick = {props.handleClearTodos}>clear completed</button>
         </form>
-
-        )
-}
+    );
+};
 
 export default TodoForm
