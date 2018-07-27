1ad0460ce035e07c62a06b6fbe154d63c55136a4
cubsfan46350 <cubsfan.46350@msn.com>
2018-07-27 00:07:45 -0500

    attempted assignment

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,11 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+const TodoList = props => (
+    <ul>
+        {props.list.map((item) => <Todo item={item.task} key={item.id}/>)}
+    </ul>
+)
+
+export default TodoList;
