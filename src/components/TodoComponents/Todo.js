f7d88774f7412e451ad043c2f5b9ade36f13b2fb
AlexanderCaves <ahcaves@gmail.com>
2018-08-01 15:24:16 -0700

    set up boilerplate for js files/switched seats

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,2 +1,12 @@
 // here is the start for PR purposes
 import React from 'React';
+
+const Todo = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Todo;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const TodoForm = props => {
+    return (
+        <form>
+            <input type='text' placeholder='Todo'></input>
+            <button>Add Todo</button>
+            <button>Clear Completed</button>
+        </form>
+    );
+};
+
+export default TodoForm;
+
+//<button className={}>{props.text}</button>
