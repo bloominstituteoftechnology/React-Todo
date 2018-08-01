84df5278afa6dcd9bce3ec27dc250951aafee05f
vanceleon <vanceleon44@gmail.com>
2018-08-01 17:09:47 -0400

    array of todos added to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+
+const todoArray = [
+  {
+    task: 'Organize Garage',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 2,
+    completed: false
+  },
+];
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
