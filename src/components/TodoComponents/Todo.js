8a2fe5c32dc16ef5c4778261a9e1adb1a10f8df2
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-01 17:04:04 -0400

    testing push/pull

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+// const tasksArray = [
+//         {
+//           task: 'Organize Garage',
+//           id: 1528817077286,
+//           completed: false
+//         },
+//         {
+//           task: 'Bake Cookies',
+//           id: 1528817084358,
+//           completed: false
+//         },
+// ],
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,6 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+const TodoList = props => {
+    return (tasksArray.map(each) =>
+ )
+}
