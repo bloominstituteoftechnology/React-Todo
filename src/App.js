98e2a0cd22b65dd8d5ba91d3b7e11adccd298094
AlexanderCaves <ahcaves@gmail.com>
2018-08-02 13:47:21 -0700

    new app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React from "react";
+import TodoList from "./components/TodoComponents/TodoList.js";
+import TodoForm from "./components/TodoComponents/TodoForm.js";
 
-const TodoForm = ({ inputVal, updateInputVal, submitNewTask }) => {
-  return (
-    <div>
-      <form onSubmit={submitNewTask} >
-        <input
-          placeholder="Enter new todo..."
-          value={inputVal}
-          onChange={updateInputVal}
+const dummyData = [
+  {
+    task: "Task One",
+    id: new Date(),
+    completed: false
+  },
+  {
+    task: "Task Two",
+    id: new Date(),
+    completed: false
+  },
+  {
+    task: "Task Three",
+    id: new Date(),
+    completed: false
+  }
+];
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: dummyData,
+      inputVal: ""
+    };
+  }
+
+  updateInputVal = event => {
+    const { value } = event.target;
+    this.setState({ inputVal: value });
+  };
+
+  submitNewTask = event => {
+    event.preventDefault();
+
+    this.setState(prevState => ({
+      list: prevState.list.concat({
+        id: new Date(),
+        task: prevState.inputVal,
+        completed: false
+      }),
+      inputVal: ""
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <TodoList taskList={this.state.list} />
+        <TodoForm
+          inputVal={this.state.inputVal}
+          updateInputVal={this.updateInputVal}
+          submitNewTask={this.submitNewTask}
         />
-        <button>Add Todo</button>
-        <button>Clear Completed</button>
-      </form>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
-export default TodoForm;
+export default App;
