f2043fbbdf339895ecccf3aee5f224a3c5ccb85f
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-08-29 18:18:28 -0400

    Todo list displays, no errors, does not save state on input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+import "./components/TodoComponents/Todo.css";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super(); 
+    this.state = {
+      todos: [],
+      inputText: ""
+    };
+  }
+
+
+  addToto = event => {
+    event.preventDefault();
+    if (this.state.inputText) {
+      this.setState({
+        todo: [...this.state.todos, this.state.inputText],
+        inputText: ""
+      });
+    }
+  };
+
+  handleInput = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
+        <TodoForm
+          addTodo={this.addTodo}
+          inputText={this.state.inputText}
+          handleInput={this.handleInput}
+        />
       </div>
     );
   }
 }
+
 
 export default App;
