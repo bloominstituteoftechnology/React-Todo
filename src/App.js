7df89bf3691905db5f2b81748bc32c82d3c47ae5
MikeRGarrity <mikergarrity@gmail.com>
2018-08-29 16:26:28 -0500

    adding form and buttons, working on functionality

--- src/App.js
+++ src/App.js
@@ -7,12 +7,46 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+  
+  addTodo = event => {
+    event.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({task: this.state.todo, completed: false, id: Date.now() });
+    if (this.state.todo) {
+      this.setState({
+        todos: [...this.state.todos, this.state.todo],
+        todo: ""
+      });
+    }
+  };
+
+    
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList />
-        <TodoForm />
+        <TodoForm 
+          handleAddTodo = {this.addTodo}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,7 +4,13 @@ import './TodoForm.css';
 function TodoForm(props) {
     return (
         <form className = "form-container">
-
+        <input 
+            type = "text" 
+            name = "todo" 
+            placeholder = "...todo"
+            />
+          <button onClick = {props.handleAddTodo}>Add Todo</button>
+          <button onClick = {props.handleClearTodos}>Clear Completed</button>
         </form>
     );
 };
