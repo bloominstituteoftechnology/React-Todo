16663b3fc8c0fdc83d9d8d47514121f192e54c3b
CsillaG <csillagok@gmail.com>
2018-08-29 19:28:42 -0400

    MVP is cleard up and done

--- src/App.js
+++ src/App.js
@@ -6,52 +6,77 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-constructor(){
-  super();//let's us access lifestyle methods (hooks)
-  this.state = {
-    todos: [
-      {
-      task: "Organize JS Notes",
-        id: 1,
-        completed:false
-      },
-      {
-     task: "Make Energy Bar",
-      id: 2,
-      completed: false
-    },
-    {
-      task: "Do Shopping",
-      id: 3,
-      completed: false,
-    },
-    {
-    task: "Feed the Cat",
-    id: 4,
-    completed: false,
-    }
-
-    ],
-    todo:""
-  };
+  constructor() {
+    super(); //let's us access lifestyle methods (hooks)
+    this.state = {
+      todos: [
+        {
+          task: "Organize JS Notes",
+          id: 1,
+          completed: false
+        },
+        {
+          task: "Make Energy Bar",
+          id: 2,
+          completed: false
+        },
+        {
+          task: "Do Shopping",
+          id: 3,
+          completed: false
+        },
+        {
+          task: "Feed the Cat",
+          id: 4,
+          completed: false
+        }
+      ],
+      todo: ""
+    };
   }
+  addTodo = element => {
+    element.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({ task: this.state.todo, completed: false, id: Date.now() });
+    this.setState({ todos, todo: "" });
+  };
 
+  changeTodo = element => this.setState({ [element.target.name]: element.target.value });
+
+  toggleTodoComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompletedTodos = element => {
+    element.preventDefault();
+    let todos = this.state.todos.slice();
+    todos = todos.filter(todo => !todo.completed);
+    this.setState({ todos });
+  };
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-      <TodoList handleToggleComplete={this.toggleTodoComplete}
-      todos={this.state.todos} />
-      <TodoForm
-      value={this.state.todo}
-      handleTodoChange={this.changeTodo}
-      handleAddTodo={this.addTodo}
-      handleClearTodos={this.clearCompletedTodos}
-      />
-
-
-        
+        <TodoList
+          handleToggleComplete={this.toggleTodoComplete}
+          todos={this.state.todos}
+        />
+        <TodoForm
+          value={this.state.todo}
+          handleTodoChange={this.changeTodo}
+          handleAddTodo={this.addTodo}
+          handleClearTodos={this.clearCompletedTodos}
+        />
       </div>
     );
   }
