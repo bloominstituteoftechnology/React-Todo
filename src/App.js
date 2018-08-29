0e34369b9dc5176b33957589a8bdf4d91fc7241d
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-29 16:19:24 -0400

    Added input and button to TodoForm.js, render TodoForm in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      inputText: '',
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({ inputText: event.target.value });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoForm
+          addTodo={this.addTodo}
+          inputText={this.state.inputText}
+          handleInput={this.handleInput}
+        />
       </div>
     );
   }
