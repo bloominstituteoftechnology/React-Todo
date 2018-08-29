a233a0cc754e1cdb34c56d380707422e5b0c5f8b
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-08-29 15:08:57 -0500

    Update TodoList.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,8 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import ToDo from "./Todo.js";
+
+function ToDoList(props) {
+  return <ul>{props.todos.map(todo => <ToDo todo={todo} />)}</ul>;
+}
+
+export default ToDoList;
