65c316a65608d27c667eb81cdd73872befb42a53
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-08-29 15:06:43 -0500

    complete App logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React from "react";
+import ToDoList from "./components/TodoComponents/TodoList.js";
+import ToDoForm from "./components/TodoComponents/TodoForm.js";
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+export default class App extends React.Component {
+  constructor() {
+    super(); // let's us access lifecycle methods (hooks)
+    this.state = {
+      todo: [],
+      inputText: ""
+    };
+  }
+
+  // property
+  addToDo = event => {
+    event.preventDefault();
+    if (this.state.inputText) {
+      this.setState({
+        todo: [...this.state.todo, this.state.inputText],
+        inputText: ""
+      });
+    }
+  };
+
+  handleInput = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+  };
+
+  // form is called controlled component (vs. uncontrolled)
   render() {
+    // this is a lifecycle hook
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <ToDoList todos={this.state.todo} />
+        <ToDoForm
+          addToDo={this.addToDo}
+          inputText={this.state.inputText}
+          handleInput={this.handleInput}
+        />
       </div>
     );
   }
 }
-
-export default App;
