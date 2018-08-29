cdb7435939d0a14b7944a260ca4ac7833e0f7776
BrianARuff <thelonewolf198927@gmail.com>
2018-08-29 19:08:46 -0400

    add in comments for the TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -17,11 +17,16 @@ export default class TodoForm extends React.Component {
     }
 
     addTodo = (e) => {
+        // prevent form from submitting to non-existant server
         e.preventDefault();
+        // run the "addTodo" function passed down from App in the following manner:
+        // App --> TodoList --> TodoForm(execute function and it will "bubble back up" like the following:) Todoform --> TodoList --> App
         this.props.addTodo(this.state.inputText);
+        // Reset the value of this components state inputText back to blank
         this.setState({
             inputText: '',
         });
+        // Using the ref I created on the input element and instantiated on line 10 in the constructor, I reset the input value (on the physical web page the user sees) back to blank
         this.input.current.value = '';
     }
     render() {
