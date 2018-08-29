fe9457f999b3d52b28d42c66f484dc8cd73a49e7
JeanetteFernandez <jeanfern555@gmail.com>
2018-08-29 16:30:43 -0500

    Functioning todo list that adds items into a list.

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoForm(props) {
+    return (
+        <form>
+            <input value={props.inputText} onChange={props.handleInput}/>
+            <button onClick={props.addTodo}>Add Todo</button>
+        </form>
+    );
+}
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,9 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+// your components will all go in this TodoList.js.
+import React from 'react';
+import Todo from './Todo';
+
+function TodoList(props) {
+    return <ul>{props.todos.map(todo => <Todo todo={todo}/>)}</ul>
+}
+
+export default TodoList;
