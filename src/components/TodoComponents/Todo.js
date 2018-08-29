e64b85d9c9d69bdf13824a2e9f807248cd937219
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-08-29 18:59:06 -0400

    to do list

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Todo from './Todo';
+import './Todo.css';
+ function TodoList(props) {
+    return (
+        <ul>{props.todos.map(todo => <Todo todo={todo} />)}</ul>
+    );
+};
+ export default TodoList; 
