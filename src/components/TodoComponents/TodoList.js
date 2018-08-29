4ac858ff8df0f5ed894a501f5da6a0d1cf4e2468
Joseph Chretien-Golden <joechre@gmail.com>
2018-08-29 18:17:28 -0500

    still need to set up app js 6:17

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo1 from './Todo';
+
+function TodoList (props) {
+return (
+    <ul>
+        {props.todo4.map(todo2 => <Todo1 todo3={todo2} key={todo2.id} />)}
+    </ul>
+);
+}
+export default TodoList;
+
+
