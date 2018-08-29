93258e32e79df7ec5bc708c61d83a85f1b9c9ba8
Michael Agard <mdagard@gmail.com>
2018-08-29 16:23:01 -0400

    Updated to only be used to display the app.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TodoList from "./components/TodoComponents/TodoList";
-import TodoForm from "./components/TodoComponents/TodoForm";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-// import "./styles.css";
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [],
-      inputText: ""
-    };
-  }
-
-  addTodo = event => {
-    event.preventDefault();
-    if (this.state.inputText) {
-      this.setState({
-        todos: [...this.state.todos, this.state.inputText],
-        inputText: ""
-      });
-    }
-  };
-
-  handleInput = event => {
-    this.setState({
-      inputText: event.target.value
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <TodoList todos={this.state.todos} />
-        <TodoForm
-          addTodo={this.addTodo}
-          inputText={this.state.inputText}
-          handleInput={this.handleInput}
-        />
-      </div>
-    );
-  }
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(<App />, document.getElementById('root'));
