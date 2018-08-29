b1901430a84d401997b112a316f2a39bff9603d6
verydecent <wnjhwng@gmail.com>
2018-08-29 15:18:05 -0700

    Added state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      message: 'THE WORLD IS OVER',
+    }
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>We can do {this.state.message}</h2>
       </div>
     );
   }
