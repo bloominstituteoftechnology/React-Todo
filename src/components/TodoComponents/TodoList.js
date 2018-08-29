8ab24b9d7d02fed257ddf8e62251c5c6ff7a306e
m-j-hacker <mjhacker@gmail.com>
2018-08-29 15:21:25 -0500

    Updated comment on TodoList.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,1 +1,6 @@
 import React from "react";
+
+// This file contains the function that receives
+// the todo array and iterates over the list
+// generating a new <Todo /> for each element
+// in the array
