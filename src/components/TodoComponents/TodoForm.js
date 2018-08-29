917c7621f98cf3c0cb3965ff30ef8ac6714cfd26
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-29 12:49:26 -0700

    adding tofo function

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,7 +4,7 @@ function TodoForm(props) {
   return (
     <form>
       <input />
-      <button onClick={event => console.log(event)}>Add user</button>
+      <button onClick={event => console.log(event)}>Add Todo</button>
       <button onCLick={event => console.log(event)}>Clear Completed</button>
     </form>
   );
