9be9f9b446fb0b98901b37d84868ed34438cc851
alejandrok93 <alejandrok93@gmail.com>
2018-08-29 13:43:50 -0700

    Completed Day 1 project requirements of Todo app

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Todo extends React.Component {
+  render() {
+    return <li class="todo-item">{this.props.item}</li>;
+  }
+}
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo.js";
+import "./Todo.css";
+
+class TodoList extends React.Component {
+  constructor(props) {
+    //console.log(props);
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <ul>
+        {/* loop through list of todos and return a todo component */}
+        {this.props.items.map(item => (
+          <Todo item={item.task} />
+        ))}
+      </ul>
+    );
+  }
+}
+export default TodoList;
