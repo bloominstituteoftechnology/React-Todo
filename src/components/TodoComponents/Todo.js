aa17d553b253221010a644d8005764bb7603c22f
Max <maxkajiwara@gmail.com>
2018-08-30 14:24:13 -0700

    Simplify completed flag

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Todo.css';
 
 const Todo = (props) => {
+  const completed = props.todo.completed ? ' completed' : '';
+
   return (
-    <li className={'todo ' + props.todo.completed}
+    <li className={'todo' + completed}
         id={props.todo.id}
         onClick={props.toggleCompleted}>
       {props.todo.task}
