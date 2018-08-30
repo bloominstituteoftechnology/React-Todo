e2fa7c880ed39263ad4c2b07888e7227090eb594
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-30 18:08:47 -0400

    Added placeholder to input

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,7 +4,11 @@ import './TodoForm.css';
 function TodoForm(props) {
   return (
     <form>
-      <input value={props.inputText} onChange={props.handleInput} />
+      <input
+        value={props.inputText}
+        onChange={props.handleInput}
+        placeholder="...todo"
+      />
       <button onClick={props.addTodo}>Add to-do</button>
       <button onClick={props.removeCompleted}>Remove Completed</button>
       <button onClick={props.removeAll}>Remove All</button>
