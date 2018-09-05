e3b19b58d058babd6a97571f7c62b4998a4f72c7
Matthew Wright <matthewbwright@gmail.com>
2018-09-05 12:50:37 -0700

    sets up initial build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+import './components/TodoComponents/Todo.css';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todo: [],
+      inputText: ''
+    };
+  }
+
+  //property
+  addTodo = event => {
+    event.preventDefault();
+    if (this.state.inputText) {
+      this.setState({
+        todo: [...this.state.todo, this.state.inputText],
+        inputText: ''
+      });
+    }
+  };
+
+  handleInput = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todo={this.state.todo} />
+        <TodoForm
+          addTodo={this.addTodo}
+          inputText={this.state.inputText}
+          handleInput={this.handleInput}
+        />
       </div>
     );
   }
