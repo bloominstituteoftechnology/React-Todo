13843320566c4e5fa5c8dd58511498f8be18002c
Ray Lomeli <raylom96@gmail.com>
2018-09-05 15:31:59 -0500

    created map function for todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super(); //lets us access lifecycle methods
+    this.state = {
+      todoData: [{ todoItem: "Get on computer" }, { todoItem: "Study React" }]
+    };
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        {this.state.todoData.map(todo => {
+          return <TodoList todoProp={todo} />;
+        })}
       </div>
     );
   }
