ed5a3eb54ffdb9b5e0af5b64272f644ff1fd559e
Marious Davis <mlancelot88@gmail.com>
2018-09-05 18:12:58 -0500

    building out my super constructor as well as my TodoForm.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import AddItem from './components/TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      todo: []
+    };
+  }
+  
+  render()
+   {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,19 @@
+// Form Fields Here
+
+// Submit Button
+
+function AddItem(props) {
+    return (
+        <button>{props.AddItem}</button>
+    );
+  }
+
+  export default AddItem;
+  
+  // Clear Button
+  
+   function ClearItem(props) {
+    return (
+        <button>{props.ClearItem}</button>
+    );
+  }
