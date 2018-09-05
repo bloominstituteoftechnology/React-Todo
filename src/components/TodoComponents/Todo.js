ac1a67d0937473619ee9409f848ba82d47d4cb2d
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-05 17:36:17 -0400

    Mapped input/data in to a new array

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Todo extends Component {
+    constructor(props){
+        super();
+        this.state = {
+
+        }
+
+    }
+    render() { 
+        return ( 
+            <div>
+            
+            </div>
+         );
+    }
+}
+ 
+export default Todo ;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,49 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+
+
+
+class TodoList extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            taskList: [
+                {
+                  task: 'Organize Garage',
+                  id: 1528817077286,
+                  completed: false
+                },
+                {
+                  task: 'Bake Cookies',
+                  id: 1528817084358,
+                  completed: false
+                },
+                {
+                    task: 'Clean bathrooms',
+                    id: 1528815984358,
+                    completed: false
+                  },
+                  {
+                    task: 'Rake Leaves',
+                    id: 1528816094358,
+                    completed: false
+                  }
+
+              ]
+        };
+    }
+
+
+    render() { 
+        return (  
+            <div key={this.state.taskList.id}>
+            {this.state.taskList.map(tasks => { 
+                return `${tasks.task}`
+            })
+        } <br />
+                </div>
+        );
+    }
+}
+ 
+export default TodoList;
