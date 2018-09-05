caf8da4528eef8c1b1846053dbc8b13991ea5510
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-05 16:22:20 -0400

    TodoForm mounted on the parent App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Todo.css'
+
+class TodoForm extends Component {
+    constructor(props)  {
+        super(props)
+        this.state = [{textInput: ''}
+            
+          ];
+    }
+
+handleChange = event =>
+    this.setState({value: event.target.textInput});
+
+
+
+
+    render() { 
+        //alert(this.state.value);
+
+        return (
+        
+    <form>
+        <input onChange={this.handleChange} type="text" placeholder='...todo' value={this.state.value}  />
+    
+        <input onSubmit='' type="submit" value="Add Todo" />
+        <input onSubmit='' type="submit" value="Clear Completed" />
+    </form>
+        );
+    }
+}
+ 
+export default TodoForm;
