b7d72e525490361c21fae5c21116eeb4b662d3b6
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-09-25 15:34:03 -0500

    installed yarn and began working on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+  super(); 
+  
+    this.state = {
+      todos: " ",
+      id: " ", 
+      completed: false 
+    }
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="App">
         <h2>Welcome to your Todo App!</h2>
       </div>
     );
