e95c3f99ea40e639310b16b50c34f26ee2a500a3
Mitchell Robles <mitchellr92@live.com>
2018-09-25 16:25:48 -0700

    added text box to App

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  constructor() {
+    super();
+
+    this.state = {
+      text: "...todo",
+    };
+  };
+
+  handleInput = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+  
   render() {
     return (
       <div>
         <TodoList />
+        <input onChange={this.handleInput} />
       </div>
     );
   }
