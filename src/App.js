6ab9bab820aaced928c9ed6fdc465b88ef9c4bd0
cferrell2 <cferrell12182@gmail.com>
2018-09-25 00:16:06 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="init">
         <h2>Welcome to your Todo App!</h2>
       </div>
     );
