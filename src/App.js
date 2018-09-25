364b8d17d0b05a3173fe26a27faaf37d65254f44
Matthew Meitl <darksamus6@gmail.com>
2018-09-25 17:52:21 -0500

    got form, state, and array object working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      formArr: [],
+      count: 0,
+      completed: false
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  handleCheck = e => {
+    let { formArr } = this.state;
+    console.log(e.id);
+    console.log(formArr[0]);
+
+    formArr[e.id].completed = true;
+  };
+
+  handleSubmit = e => {
+    let { formArr, value, count, completed } = this.state;
+    e.preventDefault();
+
+    formArr.push({
+      id: count,
+      text: value,
+      date: Date.now(),
+      completed: completed
+    });
+    console.log(formArr[count]);
+    console.log(formArr[0]);
+    this.setState(prevState => {
+      return { count: prevState.count + 1 };
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div>
+          {this.state.formArr.map(item => (
+            <div key={item.id}>
+              <input
+                id={item.id}
+                type="checkbox"
+                onChange={item => this.handleCheck(item)}
+              />
+              {item.id}: {item.text} @{new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit"
+              }).format(item.date)}
+            </div>
+          ))}
+        </div>
+
+        <form>
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" value="Submit" onClick={this.handleSubmit} />
+        </form>
       </div>
     );
   }
