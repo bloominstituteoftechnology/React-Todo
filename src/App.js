fc5f298bbdfc153151c8cbb386a2089b0c66211a
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-25 22:46:48 -0400

    added sample tasks for test state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,18 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = [];
+    this.state = [ // Test state
+      {
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+      }
+    ];
   }
   render() {
     return (
