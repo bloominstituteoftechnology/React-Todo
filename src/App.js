10a3223df97056becf5f54eef1662163a723975e
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-25 22:12:53 -0400

    created a few placeholder todos and passed value through props

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      todoList: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    };
   }
 
   render() {
     return (
       <div>
-        <TodoList />
+        <TodoList todoList={this.state.todoList} />
       </div>
     );
   }
