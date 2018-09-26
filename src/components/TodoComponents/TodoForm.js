83e4010ff01b28add96610c285e8ce112ac3af21
Denise Escobar <descobar1490@gmail.com>
2018-09-25 21:28:54 -0500

    Created a basic todoform component and began set up for my app.js

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const TodoForm = () =>{
+    return(
+        <div>
+            <form>
+                <input />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default TodoForm;
