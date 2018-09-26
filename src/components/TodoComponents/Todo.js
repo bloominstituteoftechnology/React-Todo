9cfaaefe292d671226f72310465ef10fd9082412
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-25 22:07:55 -0400

    created todoList, form and todo component

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <div>
+            <h2>{props.task}</h2>
+        </div>
+    );
+}
+
+export default Todo
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
+import React from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const TodoList = props => {
+    return (
+        <div>
+            <TodoForm />
+            <Todo />
+        </div>
+    );
+};
+
+export default TodoList;
