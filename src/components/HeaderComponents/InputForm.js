773a061562b6079d00b63ea74f6145f3d727ffc1
Glenn-David Daniel <d2rd@outlook.com>
2018-09-26 14:12:24 -0700

    Added input <form> to InputForm.js

--- src/components/HeaderComponents/InputForm.js
+++ src/components/HeaderComponents/InputForm.js
@@ -2,20 +2,18 @@ import React from "react";
 
 const InputForm = () => {
   return (
-    <div>
       <div>
           {/* creates title for new task */}
           <h2>{this.state.title}</h2> 
         <div>
           {/* <ToDoList list={this.state.list} /> */}
-          <input
-            
-          />
-          <button onClick={this.handleButtonClick}>Add this task</button>
+          <form>
+            <label>New Task: <input type="text" name="newTask" /></label>
+            <input type="submit" value="Submit" />
+            <button onClick={this.handleButtonClick}>Add this task</button>
+          </form>
         </div>
       </div>
-      <InputForm>Input form appears here.</InputForm>
-    </div>
   );
 };
 
