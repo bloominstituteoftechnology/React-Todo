6298db4c69ebc0453d0f746d68a0301f4357adf4
Griffin <griffinshuth@gmail.com>
2018-09-25 20:55:16 -0600

    first commit time to get started.ç

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      todos: [
+        {
+          task: 'warm up',
+          id: 1,
+          completed: false
+        },
+        {
+          task: 'run',
+          id: 2,
+          completed: false
+        },
+        {
+          task: 'sprint',
+          id: 3,
+          completed: false
+        }
+      ]
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+render() {
+  let list = this.state.todos.map((todo) => {
+    return <li key={todo.id}> {todo.task}</li>
+  })
+  return (
+    <div className='App'>
+      <ul>
+        {list}
+      </ul>>
+      <form>
+        To do:
+        <input type='text' name='task' onChange={this.handleChange} value={this.state.text} />
+      </form>
+    </div>
+  );
+}
 }
 
 export default App;
+
