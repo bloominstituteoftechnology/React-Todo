72ed3b7e176781c9cb27ab3cfaa37c83ced94aec
Paul DeYonghe <p.deyonghe@gmail.com>
2018-09-25 22:16:52 -0500

    second pull; form is in place, having trouble with rendering list items

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = (props) => {
+  return (
+    <div>
+    {props.todos.map(x =>
+      <Todo key={x.id} todo={x.task}  />
+    )}
+    </div>
+  );
+
+}
+
+export default TodoList;
