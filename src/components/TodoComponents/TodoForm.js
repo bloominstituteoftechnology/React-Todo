c29b44e85f0f3b8032bae6aaf2d4e33845b7593d
Paul DeYonghe <p.deyonghe@gmail.com>
2018-09-25 20:47:11 -0500

    first pull: got some basic form down

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TodoForm = (props) => {
+  return (
+    <div>
+      <input />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+    </div>
+  );
+
+}
+
+export default TodoForm;
