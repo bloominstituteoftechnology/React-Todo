f0b9a07a6df564b760fc5924e8eb3de4bfe1c767
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-25 22:45:53 -0400

    changed import file path for Todo.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import Todo from './components/TodoComponents/Todo.js'
+import Todo from './Todo.js'
 
 const TodoList = (props) => {
   return (
