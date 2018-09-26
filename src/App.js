fd6d589f42193d7f68a489babb1a4c2237c67e03
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-25 23:24:35 -0400

    Converted TodoForm to class component to add Enter key listener to submit todo item

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ class App extends React.Component {
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   state =   {
-      todo: [{
-        task: 'Organize Garage',
-        id: 1528817077286,
-        completed: false
-      },
-      {
-        task: 'Bake Cookies',
-        id: 1528817084358,
-        completed: false
-      }],
+      todo: [],
   }
   createTodo    =   (value)  =>  {
       let newTodo = this.state.todo;
@@ -31,7 +22,6 @@ class App extends React.Component {
   }
   completeTask  =   (value) =>  {
       let newTodo = this.state.todo;
-      console.log(value);
       newTodo = newTodo.map((todo)  =>  {
           if(value.id === todo.id)  {
               todo.completed = true;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Todo.css';
 
-const TodoForm  =   (props) =>  {
-    return(
-        <div className="todoForm">
-            <input placeholder="...todo" />
-            <div onClick={()    =>  props.clickAdd(document.querySelector("input").value)}>Add Todo</div>
-            <div onClick={()    =>  props.clearComplete()}>Clear Completed</div>
-        </div>
-    )
+class TodoForm extends React.Component {
+    componentDidMount() {
+        const input = document.querySelector("input")
+        document.addEventListener("keypress",(e)  =>  {
+
+            if(e.key === "Enter")   {
+                this.props.clickAdd(input.value);
+                document.querySelector("input").value = "";
+            }
+        })
+    }
+    render(props)    {
+        return(
+            <div className="todoForm">
+                <input placeholder="...todo" />
+                <div onClick={()    =>  this.props.clickAdd(document.querySelector("input").value)}>Add Todo</div>
+                <div onClick={()    =>  this.props.clearComplete()}>Clear Completed</div>
+            </div>
+        )
+    }
 }
 
 export default TodoForm;
