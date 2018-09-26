b1299c640139a851c44c6f19f12231a0c1291fbd
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-09-25 20:46:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import 
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      toDoList: [];
+    }
+  }
   render() {
     return (
       <div>
