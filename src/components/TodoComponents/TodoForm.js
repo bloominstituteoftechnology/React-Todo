0e976101f15131f434375e64d03727b6f7b0f5ee
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-26 14:04:57 -0600

    Form text now clears on submit

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,26 +2,29 @@ import React, { Component } from 'react'
 
 class TodoForm extends Component {
 
-    state = {todo: {}};
+    state = {id: Date.now(), task: "", completed: false};
 
     inputChange = event => {
         this.setState({
-          todo: {id: Date.now(), task: event.target.value, completed: false}
+            task: event.target.value
         });
-      };
+    };
+
+    submitTodo = event => {
+        event.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({task: ""});
+    };
 
     render() {
         return (
-        <div>
-            <form onSubmit={(e) => {
-                this.props.addTodo(this.state.todo);
-                e.preventDefault();
-            }}>
-            <input onChange={this.inputChange} />
-            <button type="submit">Add</button>
-            <button>Clear Selected</button>
-            </form>
-        </div>
+            <div>
+                <form onSubmit={this.submitTodo}>
+                <input value={this.state.task} onChange={this.inputChange} />
+                <button type="submit">Add</button>
+                <button>Clear Selected</button>
+                </form>
+            </div>
         )
     }
 }
