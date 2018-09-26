799f34d9b97ae60b7b317fdc8fb69fe0551c503a
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-25 21:41:22 -0400

    Create base state, remove default html inside of the App component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  state =   {
+      todo: [],
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        
       </div>
     );
   }
