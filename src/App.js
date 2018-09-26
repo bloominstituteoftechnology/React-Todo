fdf77c6e1690c2811c80e70319bc41ad0b7e79a2
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-09-25 20:48:36 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Todo from 'Todo':
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  super();
+
+  this.state = {
+    toDoData:
+    [
+      {
+        task: "Finish TODO list",
+        id: 1,
+        completed: false
+      },
+      {
+        task: "Add more funcitonality",
+        id: 2,
+        completed: false
+      },
+      {
+        task: "Add styling",
+        id: 3,
+        completed: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div>
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Todo = props => {};
+
+export default Todo;
