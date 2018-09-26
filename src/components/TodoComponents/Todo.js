a4a28dcc4f7edb83db8d2e506a6d36b97fc296b6
Robert Billington <chillingtonbillington@gmail.com>
2018-09-26 17:11:51 -0500

    completed ToDo

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,1 +1,11 @@
-import React from React;
+import React from 'react';
+
+const ToDo = (props) =>{
+    return(
+        <div id ={props.id}>
+            <p> {props.item.task}</p>   
+        </div>
+    )
+}
+
+export default ToDo;
