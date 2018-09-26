7d422b2fef59768e83b0ec1dd1bccd0ecc3888af
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-09-26 09:28:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Welcome to your Todo App!.</h2>
       </div>
     );
   }
