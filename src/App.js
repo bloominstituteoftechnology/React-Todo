e4ae457e82387572d00c9838b941397a0475ea28
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-09-25 21:49:33 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: []
+			//values which we are storing
+		};
+	}
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+	render() {
+		return <div />;
+	}
 }
 
 export default App;
+
+// - `<App />` will hold all the data needed for this project.It will also be the container for your Todo Components.
+//   - All of your application data will be stored here on`<App />`.
+//   - All of your`handler` functions should live here on`<App />`.
