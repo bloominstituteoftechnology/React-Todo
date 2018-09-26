da08255669dcef666521186b0c7cf944ad249d3c
Chris Juelfs <CRJuelfs24@comcast.net>
2018-09-25 23:08:01 -0500

    50-ish% done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,44 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      text: '',
+      placeholder: '... to-do',
+      tasks: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+  // function handleInput(event){
+  //   this.setState({
+  //     placeholder: "... to-do"
+  //   });
+  // }
+  handleInput = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
+        <div>{this.state.text}</div>
+        <input placeholder = {this.state.placeholder} onChange = {this.handleInput} />
+        <button>Add Todo</button>
+        <button>Clear Completed</button>
       </div>
     );
   }
