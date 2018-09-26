0e15748086cbef1d6341d690734f472a48107232
Rick Fleming <elfeming111@yahoo.com>
2018-09-25 20:25:41 -0700

    Added App.css for styling app and started styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import './App.css'
 import TodoList from './components/TodoComponents/TodoList.js';
 import TodoForm from './components/TodoComponents/TodoForm.js';
 
 class App extends React.Component {
   constructor(){
     super();
+    this.headers = [
+      'Lazy Day Todo\'s',
+      'Gettin\' Busy',
+      'Full Day, Now You\'ve Gone And Done It',
+      'Well There\'s Always Tomorrow'
+    ]
     this.state = {
+      headerText: this.headers[0],
       todoData: [
-        {
-          task: 'Test 1',
-          id: Date.now(),
-          isCompleted: false
-        }
+        // {
+        //   task: 'Test 1',
+        //   id: Date.now(),
+        //   isCompleted: false
+        // }
       ]
     }
   }
@@ -23,8 +31,12 @@ class App extends React.Component {
       isCompleted: false
     };
 
+    let index = Number.parseInt(this.state.todoData.length / 3, 10);
+    index = index >= this.headers.length ? this.headers.length - 1 : index;
+
     this.setState({
-      todoData: [...this.state.todoData, newTask]
+      todoData: [...this.state.todoData, newTask],
+      headerText: this.headers[index]
     })
   }
 
@@ -51,7 +63,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Todo List</h1>
+        <h1>{this.state.headerText}</h1>
         <TodoList todoData={this.state.todoData} completeTask={this.completeTask}/>
         <TodoForm addTask={this.addTask} clearCompleted={this.clearCompleted}/>
       </div>
