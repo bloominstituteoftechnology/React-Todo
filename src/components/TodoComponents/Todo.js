af5edd8d43c6870c42e43ddaa79494b97285e51a
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-25 22:31:19 -0400

    added Todo passed props to render task data

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Todo = (props) => {
+  return (
+    <div className='todo'>
+      {this.props.data.task}
+    </div>
+  );
+}
+
+export default Todo;
