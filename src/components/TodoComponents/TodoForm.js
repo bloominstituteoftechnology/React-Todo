0b61e0ed5fa09a7a11e9f57d8e123e393c80f877
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-09-25 17:33:55 -0700

    Added elements for todo form component

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ToDoForm = (props) => {
+    return (
+        <div>
+        <input value='...todo'></input>
+            <button>Add Todo</button>
+            <button>Clear Completed</button>
+        </div> 
+    )
+}
+
+export default ToDoForm;
