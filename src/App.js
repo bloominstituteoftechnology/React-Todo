b6302f9450c437ccd5a14ed3f2855063f308acaf
Pascale Pierre <pascaleee@gmail.com>
2018-09-25 21:20:56 -0500

    layed out the code for input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import List from './components/TodoComponents/TodoList';
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: "",
+      item: []
+    };
+  }
+
+  onSubmit= event => {
+    event.preventDefault()
+   this.setState({
+     input: "",
+     item: [this.state.items, this.state.term]
+   });
+
+  };
+
+  onChange= event => {
+   this.setState({input: event.target.value});
+
+  };
+
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      <form className="App">
+       <h1>Pascale's ToDos!!!</h1>
+ <List items={this.state.items} />
+       <input placeholder="enter todo" value={this.state.item} onChange={this.onChange}/>
+       <button onClick = {this.handleClick}> submit</button>
+     </form>
+     </div>
     );
   }
 }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,13 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const List = props => (
+  <ul>
+    {
+      props.items.map((item, index) => <li key={index}>{item}</li>)
+    }
+  </ul>
+);
+
+export default List;
