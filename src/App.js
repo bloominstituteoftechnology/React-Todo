74365dfe27f980317dabb0e9f4e3d3f07759d743
Tommy Collison <tommy@collison.ie>
2018-09-25 18:33:32 -0700

    getting started on form and app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+constructor() { 
+  super(); 
+
+  this.state = { 
+    input: '',
+    todoList: [{
+      task: '',
+      id: 1,
+      completed: false
+    }],
+    }
+  };
+}
+
+handleClick = (event) => { 
+  this.setState({
+    task: event.target.value,
+    id: Date.now(),
+  })
+};
+
+handleInput = (event) => { 
+  console.log(event.target.value);
+  this.SetState({
+    input: event.target.value
+  })
+}
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Oh god it's another to-do app</h2>
+        <Form />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+const Form = (props) => { 
+    return (
+     <div>
+         <input onChange={this.handleInput} />
+         <button>Submit to-do</button>
+         <button>Clear completed to-dos</button>
+     </div>   
+    )
+}
+
+export default Form;
