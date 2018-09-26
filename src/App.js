ce1ddcc5f0ef809979884d1f218d4aa2c60654c9
Robert Billington <chillingtonbillington@gmail.com>
2018-09-26 17:12:39 -0500

    Completedd Day 1 MVP

--- src/App.js
+++ src/App.js
@@ -17,15 +17,37 @@ class App extends React.Component {
           id: 2,
           completed: false
         }
-      ]
+      ],
+      newTask: '',
     }
+  }
+
+  addItem = () =>{
+    const objItem = {
+      task: this.state.newTask,
+      id: Date.now(),
+      completed: false,
+    }
+    
+    this.setState({
+      todo: [...this.state.todo, objItem]
+    });
+    
+  }
+  handleInput = event => {
+    console.log(event.target.value)
+    this.setState({
+      newTask: event.target.value
+    })
+
+    
   }
   render() {
     return (
       <div>
         <h1>Todo List: MVP</h1>
-        <TodoList />
-        <ToDoForm />
+        <TodoList list = {this.state.todo}/>
+        <ToDoForm addItem = {this.addItem} handleInput = {this.handleInput}/>
 
       </div>
     );
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,11 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from 'react';
+import ToDo from './Todo';
+
 
 const TodoList = (props) =>{
     return(
         <div className = "to-do-list">
-            {props.this.state.task}
+            {props.list.map(taskItem => {
+                return(
+                    <ToDo item = {taskItem} key ={taskItem.id}/>
+                ) 
+            })}
         </div>
     )
 }
