07463231578f3937b9e4f0345cb857e06abedaf2
Alex Martin <atonymartin20@yahoo.com>
2018-09-25 20:39:26 -0500

    Started setting up App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		
+		// you will need a place to store your state in this component.
+		// design `App` to be the parent component of your application.
+		// this component is going to take care of state, and any change handlers you need to work with your state
+
+		this.state = {
+			toDoList: [
+				{
+					task: '',
+					id: `Date.now()`,
+					completed: false
+				},
+			]
+		};
+	}
+
+	// - All of your `handler` functions should live here on `<App />`.
+
+	handleClick = (event) => {
+		this.setState({
+			task: event.target.value,
+			id: Date.now(),
+		})
+	};
+
+	handleInput = (event) => {
+		//something
+	};
+	
+	render() {
+		return (
+			<div>
+				<h1>Todo List: MVP</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
