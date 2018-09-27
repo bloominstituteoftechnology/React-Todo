de664776de0484b623add76adda684a661f47bba
Abigayle Franklin <abifranklin@gmail.com>
2018-09-27 10:25:05 -0500

    Hard Reset!

--- src/App.js
+++ src/App.js
@@ -4,46 +4,4 @@ import ToDoForm from './components/TodoComponents/TodoForm';
 
 
 
-
-class App extends React.Component {
-  constructor(){
-      super();
-
-      this.state = {
-        todo: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-        ]
-        }
-  }
-
-  addItemHandler = (event) => {
-    event.preventDefault();
-    this.state.todo.push(
-      {task: event.target.previousSibling.value,
-      id: Date.now(),
-      completed: false}
-    )
-    this.setState();
-    console.log(this.state.todo);
-  }
-
-  render() {
-    return (
-    <div>
-      <ToDoList list={this.state.todo}/>
-      <ToDoForm submit={this.addItemHandler}/>
-    </div>
-    )
-  }
-}
-
 export default App
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,17 +1,5 @@
 import React from 'react';
 
-const ToDoList = (props) => {
-    return (
-        <div>
-        {props.list.map(item => {
-          return (
-            <div key={item.id}>
-              {item.task}
-            </div>
-          );
-        })}
-      </div>
-    )
-}
+
 
 export default ToDoList
