19692990b2d6f8f20271d0cd78d4cc32fdd83bd8
Robin U Familara <robin.familara@cgius.com>
2018-09-27 21:46:12 -0500

    Performed refactoring

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,12 +1,19 @@
 //is a component that takes in the todo data and displays the task to the screen.
 
 import React from "react";
-//to do simply returns the task item
+//to do simply returns the task item name to the screen
 const Todo = (props) => {
+    const message = props.completed ? "done" : "not done";
+
+    const itemClicked = () =>{
+        props.strike(props.id)
+        console.log(props.addClass)
+    }
     console.log("todo", props)
+    console.log()
         return (
-            <p className="todo-item">
-                {props.task}
+            <p className={props.class} onClick={itemClicked}>
+                {props.task} is {message} {props.class}
             </p>
         );
 }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,27 +4,38 @@
 //receives your Todos array and iterates over the list generating a new <Todo /> for each element in the array.
 import React from "react";
 import Todo from "./Todo";
-import TodoForm from "./TodoForm";
+import TodoForm from "./TodoForm"
 
 //todo has a display style that is not formatted yet
 //displays an h1 then maps through prop.values to create new todo components for each item in the array
 //calls todoform to display the user buttons
-const TodoList = (props) => {
-    console.log(props)
-    return(
-        <div className={props.displayStyle}>
-            <h1>Todo List: MVP</h1>
-            <div className='list'>
-                {props.values.map(item => {
-                return (
-                    <Todo key={item.id} id={item.id} task={item.task} completed={item.completed} onClick={props.strike}>
-                    </Todo>
-                );
-                })}
+class TodoList extends React.Component {
+    render() {
+        console.log("list", this.props)
+        return(
+            <div className={this.props.displayStyle}>
+                <h1>Todo List: MVP</h1>
+                <div className="list">
+                    {this.props.state.todos.map(item => {
+                    return (
+                        <Todo key={item.id} 
+                            id={item.id} 
+                            task={item.task} 
+                            completed={item.completed} strike={this.props.strike}
+                            class={this.props.class}
+                        />
+                    );
+                    })}
+                    <TodoForm 
+                    state={this.props.state}
+                    text={this.props.text}
+                    input={this.props.input} 
+                    add={this.props.add} 
+                    remove={this.props.remove}/>
+                </div>
             </div>
-            <TodoForm action={props}/>
-        </div>
-    );
+        );
+    }
 }
 
 export default TodoList;
