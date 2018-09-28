92874a75b4d98826b16a8a6bfa2c20107e801375
Robin U Familara <robin.familara@cgius.com>
2018-09-27 21:10:47 -0500

    Updated and completed To-Do components -Updated App.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,3 +2,15 @@
 // feel free to change this component.js into TodoList.js
 
 //receives your Todos array and iterates over the list generating a new <Todo /> for each element in the array.
+import React from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+ const TodoList = (props) => {
+    console.log("list", props)
+    return(
+        <div className={props.displayStyle}>
+            <TodoForm />
+        </div>
+    );
+}
+ export default TodoList;
