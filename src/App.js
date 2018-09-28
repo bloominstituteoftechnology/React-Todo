7581e9baf4f45affed218773f7f18bf3ff40828b
paulapivat <paul.apivat@gmail.com>
2018-09-28 16:40:37 +0700

    add Clear Completed Task functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
 import React from 'react';
 
-const todoArray1 = [
-  {
-    task: 'Fork & Clone',
-    id: Date.now(),
-    isCompleted: false
-  },
-  {
-    task: 'Submit Pull Request',
-    id: Date.now(),
-    isCompleted: false
-  },
-  {
-    task: 'Create functionality',
-    id: Date.now(),
-    isCompleted: false
-  },
-  {
-    task: 'Add styling',
-    id: Date.now(),
-    isCompleted: false
-  }
-];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -33,22 +11,22 @@ class App extends React.Component {
       todoArray: [
         {
           task: 'Fork & Clone',
-          id: Date.now(),
+          id: 1,
           isCompleted: false
         },
         {
           task: 'Submit Pull Request',
-          id: Date.now(),
+          id: 2,
           isCompleted: false
         },
         {
           task: 'Create functionality',
-          id: Date.now(),
+          id: 3,
           isCompleted: false
         },
         {
           task: 'Add styling',
-          id: Date.now(),
+          id: 4,
           isCompleted: false
         }
       ],
@@ -80,35 +58,52 @@ class App extends React.Component {
     })
   }
 
+  toggleCompleted = (id) => {
+    const todoArr = [...this.state.todoArray];
+    todoArr.forEach(todo => {
+      if (todo.id === id) {
+        todo.isCompleted = !todo.isCompleted;
+        return todo;
+      } 
+    })
+    this.setState({ todoArray: todoArr})
+  }
+
+
+  clearCompleted = (id) => {
+    const todoArr = [...this.state.todoArray];
+    todoArr.filter((todo,index) => {
+      if (todo.isCompleted) {
+        console.log(index)
+        todoArr.splice(index,1)
+      }
+    })
+    this.setState({ todoArray: todoArr})
+  }
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <h3>List starts here:</h3>
+      
           <div>
-            <h2>List internal to App:</h2>
-            {this.state.todoArray.map((e) => {
-              return (
-                <div>
-                  {e.task}
-                </div>
-              );
-            })}
-            <div><h3>{this.state.textInput}</h3></div>
+            <h2>List starts here:</h2>
+            <TodoList 
+                todos={this.state.todoArray} 
+                changeTaskStatus={this.changeTaskStatus} 
+                toggleCompleted={this.toggleCompleted}/>    
+            <TodoForm 
+                addTodoTask={this.addTodoTask} 
+                printTodoTask={this.printTodoTask} 
+                addTextInput={this.state.textInput} 
+                handleClickClear={this.handleClickClear} 
+                textInput={this.state.textInput}
+                clearCompleted={this.clearCompleted}
+                />  
+            <TextInputDisplay 
+                addTextInput={this.state.textInput} />
           </div>
-          <input value={this.state.textInput} onChange={this.printTodoTask}/> <button onClick={this.addTodoTask}> Submit </button> <button onClick={this.handleClickClear}> Clear All </button>
           
-          
-          
-          <h3>This is a new task: {}</h3>
-          <div>
-            <h2>List external to App:</h2>
-            <TodoList todos={this.state.todoArray} />    
-            <TodoForm addTodoTask={this.addTodoTask} printTodoTask={this.printTodoTask} addTextInput={this.state.textInput} handleClickClear={this.handleClickClear} textInput={this.state.textInput}/>  
-            <TextInputDisplay addTextInput={this.state.textInput} />
-          </div>
-           
       </div>
     );
   }
@@ -128,21 +123,27 @@ class App extends React.Component {
 const TodoList = (props) => {
   return props.todos.map((e) => {
     return (
-      <div>
-        <Todo todo={e}/>    
-      </div>
+      <ul>
+        <li><Todo key={e.id} todo={e} toggleCompleted={props.toggleCompleted}/></li>
+      </ul>
     )
   })
 }
 
+
+
+
 // the <Todo /> functional component is defined here; it has access to the properties of each todo 'object' (i.e., task, id)
 // it calls the properties of each todo 'object' through props.todo
-const Todo = (props) => {
-  return (
-    <div>
-        This is the {props.todo.task} task for {props.todo.id} date is {props.todo.isCompleted.toString()}.
-    </div>
-  )
+class Todo extends React.Component {
+  render() {
+
+    return (
+      <div key={this.props.todo.id} onClick={() => this.props.toggleCompleted(this.props.todo.id)} style={{textDecoration: this.props.todo.isCompleted ? 'line-through' : 'none'}}>
+          This is the {this.props.todo.task} task for {this.props.todo.id} date is completed?: {this.props.todo.isCompleted.toString()}.
+      </div>
+    )
+  }
 }
 
 const TextInputDisplay = props => {
@@ -156,16 +157,31 @@ const TextInputDisplay = props => {
 
 // Trick is to set task to 'props.addTextInput', which is the name of the argument AND handler-function (so technically, it's a call-back?) to task. 
 //turn into a class component
-const TodoForm = props => {
-  let currentDate = new Date();
-  return (
-    <div>
-      <input value={props.textInput} onChange={props.printTodoTask} placeholder="placeholder text"/>
-      <button onClick={() => {props.addTodoTask({task: props.addTextInput, id: currentDate.toDateString(), isCompleted: false})}}>Add Todo</button>
-      <button onClick={props.handleClickClear}>Clear Button</button>
-    </div>
-  )
-};
+
+// const TodoForm = props => {
+//   let currentDate = new Date();
+//     return (
+//       <div>
+//           <input value={props.textInput} onChange={props.printTodoTask} placeholder="placeholder text"/>
+//           <button onClick={() => {props.addTodoTask({task: props.addTextInput, id: currentDate.toDateString(), isCompleted: false})}}>Add Todo</button>
+//           <button onClick={props.handleClickClear}>Clear Button</button>
+//           <button>Clear Completed Task</button>
+//       </div>
+//     )
+// };
+
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <div>
+          <input value={this.props.textInput} onChange={this.props.printTodoTask} placeholder="placeholder text"/>
+          <button onClick={() => {this.props.addTodoTask({task: this.props.addTextInput, id: Date.now(), isCompleted: false})}}>Add Todo</button>
+          <button onClick={this.props.handleClickClear}>Clear Button</button>
+          <button onClick={this.props.clearCompleted}>Clear Completed Task</button>
+      </div>
+    )
+  }
+}
 
 
 
