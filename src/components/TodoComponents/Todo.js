2317d05da3fe7a84ed70f12e0cbfa1e57964e227
Kendra Williams <kmonique.williams@gmail.com>
2018-09-27 22:47:10 -0400

    classname changed

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,14 +4,15 @@ import React from "react";
 import "./Todo.css";
 //to do simply returns the task item name to the screen
 //class name toggles the p tag class names depending on its completed flag
+
 const Todo = (props) => {
-    let className = props.completed ? "todo-item completed" : "todo-item";
+    // let className = props.completed ? "todo-item completed" : "todo-item"; <--old code would pass this value to className
 
     const itemClicked = () =>{
         props.toggle(props.id)
     }
         return (
-                <ul className={className} 
+                <ul className={props.completed ? "todo-item completed" : "todo-item"}
                     onClick={itemClicked}>
                     <li>{props.task}</li>
                 </ul>
