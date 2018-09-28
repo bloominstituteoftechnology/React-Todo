46f529a59a469e2da90fd7d44aced4c5617a2df2
Robin U Familara <robin.familara@cgius.com>
2018-09-27 21:32:18 -0500

    Fix something of Todo.js until successful

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,7 @@
 //is a component that takes in the todo data and displays the task to the screen.
 
 import React from "react";
-/to do simply returns the task item
+//to do simply returns the task item
 const Todo = (props) => {
     console.log("todo", props)
         return (
