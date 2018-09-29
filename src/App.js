4bdb4a5b8d3221a567671ea4ba6b1c09041dde92
Wesley Meeks <wesmeeks88@gmail.com>
2018-09-29 09:49:49 -0600

    fixed minor errors, got functionality up and running

--- src/App.js
+++ src/App.js
@@ -1,75 +1,89 @@
-import React from 'react';
-import Todo from './components/TodoComponents/Todo';
-import TodoList from './components/TodoComponents/TodoList';
-import TodoForm from './components/TodoComponents/TodoForm';
+import React from "react";
+import Todo from "./components/TodoComponents/Todo";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
-constructor(){
-  super();
-  this.state = {
-    inputField: '',
+  constructor() {
+    super();
+    this.state = {
+      inputField: "Hello",
 
-    todoList : [
-      {
-        task: 'Organize Garage',
-        id: 1528817077286,
-        completed: false
-      },
-      {
-        task: 'Bake Cookies',
-        id: 1528817084358,
-        completed: false
-      }
-    ],
-    id: 1528817084358,
+      todoList: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: "Bake Cookies",
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      id: 1528817084358
+    };
   }
-}
   render() {
     return (
       <div>
-       <TodoForm
-         inputField = {this.state.inputField}
+        <TodoForm
+          inputField={this.state.inputField}
           clickHandler={this.clickHandler}
           clearClickHandler={this.clearClickHandler}
-          messageChangeHandler={this.messageChangeHandler} />
-        <TodoList todoList={this.state.todoList} itemCompleted={this.itemCompleted} />
-
+          messageChangeHandler={this.messageChangeHandler}
+        />
+        <TodoList
+          todoList={this.state.todoList}
+          itemCompleted={this.itemCompleted}
+        />
       </div>
     );
   }
-  clickHandler = event =>{
-   event.preventDefault();
-   this.setState({todoList: [...this.state.todoList, {
-     task: this.state.inputField,
-     id: this.state.id+1,
-     completed: false}],
-     inputField: ''
-   });
-   this.setState({id: this.state.id+1});
-
- }
- itemCompleted = id =>{
-   let copy = [...this.state.todoList];
-   copy = copy.map(task => {
-     if(task.id === id) {
-       task.completed = !task.completed;
-       return task;
-     } else {
-       return task;
-     }
-   });
-   this.setState({copy});
-
- }
-  messageChangeHandler = event =>{
+  clickHandler = event => {
     event.preventDefault();
-    this.setState({inputField: event.target.value});
+    this.setState({
+      todoList: [
+        ...this.state.todoList,
+        {
+          task: this.state.inputField,
+          id: this.state.id + 1,
+          completed: false
+        }
+      ],
+      inputField: ""
+    });
+    this.setState({ id: this.state.id + 1 });
+  };
+  itemCompleted = id => {
+    let copy = [...this.state.todoList];
+    copy = copy.map(task => {
+      if (task.id === id) {
+        task.completed = !task.completed;
+        return task;
+      } else {
+        return task;
+      }
+    });
+    this.setState({ todoList: copy });
+    console.log(id);
+  };
+  messageChangeHandler = event => {
+    event.preventDefault();
+    this.setState({ inputField: event.target.value });
   };
 
-  clearClickHandler = event =>{
+  clearClickHandler = event => {
     event.preventDefault();
-  }
+    let copy = [];
+    this.state.todoList.forEach(task => {
+      if (!task.completed) {
+        copy.push(task);
+      }
+    });
+    this.setState({ todoList: copy });
+  };
 }
 
 export default App;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,14 +1,13 @@
-import React from 'react';
-// import '.Todo.css';
+import React from "react";
 
-const TodoForm = props =>{
-    return(
-        <form>
-            <input value={props.inputField} onChange={props.messageChangeHandler} />
-            <button onClick={props.clickHandler}>Add Todo</button>
-            <button onClick={props.clearClickHandler}>Clear Completed</button>
-        </form>
-    );
-}
+const TodoForm = props => {
+  return (
+    <form className="form">
+      <input value={props.inputField} onChange={props.messageChangeHandler} />
+      <button onClick={props.clickHandler}>Add Todo</button>
+      <button onClick={props.clearClickHandler}>Clear Completed</button>
+    </form>
+  );
+};
 
 export default TodoForm;
