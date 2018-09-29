187fe13a24f150dd72c951efe64cb83b4a9879c8
alasalle <angela.lasalle91@gmail.com>
2018-09-28 21:11:02 -0400

    completed styling updated README.md

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,13 +3,15 @@ import React from "react";
 class TodoForm extends React.Component {
   render() {
     return (
-      <div>
+      <div className={'formDiv'}>
         <form>
-          <input value={this.props.todo} onChange={this.props.inputChange} />
-          <button type="submit" onClick={this.props.addTodo}>
-            Add Todo
-          </button>
-          <button onClick={this.props.clear}>Clear Completed</button>
+          <input value={this.props.todo} onChange={this.props.inputChange} placeholder={'Todo Item'} />
+          <div className={'buttonDiv'}>
+            <button type="submit" onClick={this.props.addTodo}>
+              Add Todo
+            </button>
+            <button onClick={this.props.clear}>Clear Completed</button>
+          </div>
         </form>
       </div>
     );
