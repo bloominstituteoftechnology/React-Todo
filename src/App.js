e40a2c01456577a1eed9fa6797fd6a58db899084
Emmanium <pradoe.ep@gmail.com>
2018-09-28 22:02:24 -0600

    removed default todos

--- src/App.js
+++ src/App.js
@@ -13,18 +13,7 @@ class App extends React.Component {
     super();
 
     this.state = {
-      todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ],
+      todos: [],
     }
   }
 
