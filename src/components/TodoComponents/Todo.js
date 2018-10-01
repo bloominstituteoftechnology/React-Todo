0555924e0ae222679448c4dd81c4e4a5662c58a1
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-09-30 20:07:35 -0500

    started todo and todolist

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,14 @@
+
+const todolist = [
+    {
+      task: 'Organize Garage',
+      id: 1,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 2,
+      completed: false
+    }
+  ];
+  export default todolist
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import todolist from './Todo';
+import React from 'react';
+
+const TodoList = () => {
+    return (
+    <div>
+          {todolist.map (item => {
+        return  ( <h1>{item.task}</h1> )
+        })}
+    </div>
+    )
+}
+
+
+
+export default TodoList
