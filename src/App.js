8e95e513e6f9b237343b12495d973f5823c7f2fc
Ben <bennybooftw@gmail.com>
2018-09-30 22:42:22 -0400

    Finished App Class Component and functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,78 @@
-import React from 'react';
+import React, { Component }from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+import '/App.css';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      id: Date.now(),
+      completed: false,
+      inputText: ''
+    }
+  }
+
+  addTask = event => {
+    event.preventDefault(); // The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur
+    if(this.state.inputText) { // If not an empty string => (setState)
+      this.setState({
+        id: Date.now(),
+        todos: [...this.state.todos, {inputText: this.state.inputText, completed: this.state.completed, id: this.state.id}],
+        inputText: ''
+      })
+    } 
+  }
+
+  setInput = event => {
+    this.setState({
+      inputText: event.target.value
+    })
+  }
+
+  completedTask = id => {
+    let completedTasks = this.state.todos.slice(); // Copy todos array
+    completedTasks = completedTasks.map(task => {
+      if (task.id === id) { // Change completed properties to true if id matches
+        task.completed = !task.completed;
+        return task;
+      }
+      return task; // Otherwise just return the item without modifying it
+    })
+    this.setState({ todos: completedTasks }); // Set the state of todos with modified completed properties
+  }
+
+  clearCompleted = event => {
+    event.preventDefault();
+    let uncompletedTasks = this.state.todos.slice();
+    uncompletedTasks = uncompletedTasks.filter(task => !task.completed) // Return all tasks whose completed property is false (not yet completed)
+    this.setState({ todos: uncompletedTasks }) // Set the state of todos to only tasks that have not been completed
+  }
+
+  clearTasks = () => {
+    if (this.state.inputText) {
+      this.setState({
+        id: Date.now(),
+        todos: [],
+        inputText: ''
+      })
+    }
+  }
+
   render() {
-    return (
+    return(
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>ToDoList: MVP</h2>
+        <TodoForm
+          addTask={this.addTask}
+          inputText={this.state.inputText}
+          setInput={this.setInput}
+        />
+        <TodoList todos={this.state.todos} />
       </div>
-    );
+    )
   }
 }
 
