5842adcb3e949bcffac826feb46baaf094d94568
Courtney Buratto <you@example.com>
2018-10-01 21:37:42 -0700

    MVP(added inputto TodoForm.js)

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const TodoForm = props => {
     return (
-        <div>    
+        <div>
+            <input placeholder="...todo"></input>
             <button>Add Todo</button>
             <button>Clear Completed</button>
         </div>
