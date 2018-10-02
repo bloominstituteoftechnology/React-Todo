a8799dab8b795659b78b2f331eb0721c81ba6082
Courtney Buratto <you@example.com>
2018-10-01 20:37:00 -0700

    MVP (created todo data array)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
+const myArr = [
+  {
+    task: 'make coffee',
+    id: 1,
+    completed: false,
+  },
+  {
+    task: 'eat empanadas',
+    id: 2,
+    completed: false,
+  },
+  {
+    task: 'call my father',
+    id: 3,
+    completed: false,
+  },
+  {
+    task: 'empty compost',
+    id: 4,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
