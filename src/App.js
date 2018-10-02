6e80c86b3ab42e0d389baa5c2e3e16df623061ce
Courtney Buratto <you@example.com>
2018-10-01 20:45:36 -0700

    MVP (set up state in App.js)

--- src/App.js
+++ src/App.js
@@ -24,7 +24,12 @@ const myArr = [
 ];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
