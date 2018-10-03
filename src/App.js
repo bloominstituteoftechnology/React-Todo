c59ccb0c233aab197efdd502aa8de95d865af558
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-03 14:06:22 -0600

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function TodoForm(props) {
+    return (
+      <form>
+        <input 
+        value={props.task} 
+        onChange={props.handleInputChange} 
+        placeholder="...todo"
+        type="text"
+        name="todo"
+        />
+        <button onClick={props.handleAddTodo}>Add Todo</button>
+        <button onClick={props.handleClearCompleted}>Clear Completed</button>
+      </form>
+    );
+  };
+  
+  export default TodoForm;
