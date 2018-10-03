8be2b830ad9f1d570991470af1888c9626ae1014
Troy Amlee <troyamlee19@gmail.com>
2018-10-03 15:34:30 -0500

    [mvp] Able to add more tasks to list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDoNow from './components/TodoComponents/TodoList';
+import './components/TodoComponents/Todo.css';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDoList: [
+        {taskToDo: 'Wash Dishes'},
+        {taskToDo: 'Sand The Floor'},
+        {taskToDo: 'Paint The Fence'},
+        {taskToDo: 'Wax On'},
+        {taskToDo: 'Wax Off'}
+      ],
+      newTask: ''
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  changeHandler = event => {
+    console.log(event.target.name);
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addNewTask = event => {
+    event.preventDefault();
+    this.setState({
+      toDoList: [
+        ...this.state.toDoList,
+        { taskToDo: this.state.newTask }
+      ],
+      newTask: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
+        <ToDoNow 
+        addNewTask={this.addNewTask}
+        ToDoing={this.state.toDoList}
+        changeHandler={this.changeHandler}
+        newTask={this.state.newTask}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,26 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+const ToDoNow = props => {
+    return (
+        <div>
+        <h4>{props.ToDoing.map(todo => 
+        {return <li>{todo.taskToDo}</li>
+        })}</h4>
+
+            <form onSubmit={props.addNewTask}>
+                    <input 
+                    type = "text"
+                    name = "newTask"
+                    onChange={props.changeHandler}
+                    value={props.newTask}
+                    />
+                    <button type="submit" onClick={props.addNewTask}>
+                    Add Todo
+                    </button>
+            </form>
+        </div>
+    )
+}
+
+export default ToDoNow
