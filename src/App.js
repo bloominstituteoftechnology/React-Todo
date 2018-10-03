38a770066ea57b23a41cdffe66f2bfd9d2452be9
Courtney Buratto <you@example.com>
2018-10-03 11:56:19 -0700

    FIX refactored App.js, added event handlers and applied them to TodoForm

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
 import React from 'react';
+import TodoForm from "./components/TodoComponents/TodoForm";
 
-const myArr = [
-  {
-    task: 'make coffee',
-    id: 1,
-    completed: false,
-  },
-  {
-    task: 'eat empanadas',
-    id: 2,
-    completed: false,
-  },
-  {
-    task: 'call my father',
-    id: 3,
-    completed: false,
-  },
-  {
-    task: 'empty compost',
-    id: 4,
-    completed: false
-  }
-];
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+        item: '', //stores values passed into input
+        todos:[
+          {
+            task: 'Eat Food',
+            id: 1528817077286,
+            completed: false
+          },
+          {
+            task: 'Drink Coffee',
+            id: 1528817084358,
+            completed: false
+          }
+        ], //stores values added to todo list
     };
   }
+
+  //HANDLERS
+
+  //input handler
+  onChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  //add handler
+  onSubmit = (event) => {
+    event.preventDefault();
+    const todos = this.state.todos.slice()
+    todos.push(
+      {task:this.state.item, completed: false, id: Date.now()});
+      this.setState({todos, item: ""});
+  }    
+
+  
+
+ 
+
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoForm
+          value={this.state.item}
+          onChange={this.onChange}
+          onSubmit={this.onSubmit}
+        />
       </div>
     );
   }
