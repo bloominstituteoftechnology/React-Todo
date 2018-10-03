d80da9d2af8ca4b813580b759a172eb9b07504b0
Adam <ajmboosh@gmail.com>
2018-10-03 16:05:02 -0500

    added form element around input and button, and onSubmit function on form to finish functionality. user can now press Enter or click Add Todo button to update TodoList. Day 1 MVP complete

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,15 +3,17 @@ import React from "react";
 const TodoForm = props => {
   return (
     <div className="todo-form">
-      <input
-        type="text"
-        name="newTodo"
-        placeholder="...todo"
-        onChange={props.changeHandler}
-        value={props.newTodo}
-      />
-      <button onClick={props.addNewTodo}>Add Todo</button>
-      <button>Clear Completed</button>
+      <form onSubmit={props.addNewTodo}>
+        <input
+          type="text"
+          name="newTodo"
+          placeholder="...todo"
+          onChange={props.changeHandler}
+          value={props.newTodo}
+        />
+        <button onClick={props.addNewTodo}>Add Todo</button>
+        <button>Clear Completed</button>
+      </form>
     </div>
   );
 };
