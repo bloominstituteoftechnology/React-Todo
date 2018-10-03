463518809e2020ed945ca3e8caf883f303bcfc32
Katie Cleary <katiecleary@airmaster.lan>
2018-10-03 16:28:51 -0400

    initial setup for data and handlers. Started to work through the directions for ToDoList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import ToDoForm from "./components/TodoComponents/TodoForm.js";
+import ToDoList from "./components/TodoComponents/TodoList.js";
 
-class App extends React.Component {
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state =  {
+        todoData:[
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+      ],
+      inputText: '',
+      newToDo: ''
+    }
+
+    changeHandler = event => {
+      this.setState({ [event.target.name]: event.target.value });
+    };
+
+    addNewToDo = event => {
+      event.preventDefault();
+      this.setState({
+        todoData: [
+          ...this.state.todoData,
+          { task: this.state.newToDo,
+            id: Date.now(),
+            completed: false
+           }
+        ],
+        newToDo: ''
+      });
+    };
+    
+    
+  
+    render() {
+      return (
+        <ToDoList></ToDoList>
+        <ToDotForm></ToDoForm>
+      );
+    }
+  }
+}
+  
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
-}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,3 +3,59 @@ import { render } from 'react-dom';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        disneyData: [
+          { characterName: 'Mickey Mouse' },
+          { characterName: 'Minnie Mouse' },
+          { characterName: 'Donald Duck' },
+          { characterName: 'Goofy' },
+          { characterName: 'Pluto' }
+        ],
+        color: 'red',
+        inputText: '',
+        inputTwoText: '',
+        newCharacter: ''
+      };
+    }
+  
+    
+  
+    changeHandler = event => {
+      console.log(event.target.name);
+      this.setState({ [event.target.name]: event.target.value });
+    };
+  
+    addNewCharacter = event => {
+      event.preventDefault();
+      this.setState({
+        disneyData: [
+          ...this.state.disneyData,
+          { characterName: this.state.newCharacter }
+        ],
+        newCharacter: ''
+      });
+    };
+  
+    render() {
+      return (
+        <div className="App">
+          <DisneyNames
+            addNewCharacter={this.addNewCharacter}
+            changeColor={this.changeColor}
+            changeHandler={this.changeHandler}
+            disneyData={this.state.disneyData}
+            inputText={this.state.inputText}
+            newCharacter={this.state.newCharacter}
+            style={this.state.color}
+          />
+        </div>
+      );
+    }
+  }
+  
+  const rootElement = document.getElementById('root');
+  ReactDOM.render(<App />, rootElement);
