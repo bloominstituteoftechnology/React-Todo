649b2baf0314e08927be29ab66d56058e841c94c
Unknown <zachary.arney@gmail.com>
2018-10-03 17:32:47 -0500

    Imported TodoList and TodoForm into App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,10 +9,21 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todo={this.state.todo} newTodoItem={this.state.newTodoItem} />
+        <TodoForm
+          text={this.state.newTodoItem}
+          changeHandler={this.changeHandler}
+          onSubmit={this.addTodoItem}
+          addTodoItem={this.addTodoItem}
+          clearAll={this.clearAll}
+        />
       </div>
     );
   }
 }
 
 export default App;
+
+// App is container
+// TodoList will contain TodoItem components
+// TodoForm will create new TodoItem components and push them into TodoList
