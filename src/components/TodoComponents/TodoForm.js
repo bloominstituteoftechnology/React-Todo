6084bb678f9f7eab5640a7cf69a91c116d01c1f7
Unknown <zachary.arney@gmail.com>
2018-10-03 17:55:10 -0500

    Moved clear all button inside of form

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const TodoForm = (props) => {
   return (
-    <div>
-      <form onSubmit={props.addTodoItem}>
-        <input type="text" value={props.text} onChange={props.changeHandler} />
-        <button type="submit" onClick={props.addTodoItem}>
-          Add Todo!
-        </button>
-      </form>
+    <form onSubmit={props.addTodoItem}>
+      <input type="text" value={props.text} onChange={props.changeHandler} />
+      <button type="submit" onClick={props.addTodoItem}>
+        Add Todo!
+      </button>
       <button onClick={props.clearAll}>Clear All!</button>
-    </div>
+    </form>
   );
 };
 
