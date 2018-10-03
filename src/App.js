6d9619f24040db655b10b4f7b15f131ff4f02ebe
Yusuf Nafey <yusufnafey@yahoo.com>
2018-10-03 09:00:28 -0700

    array put into App, imports on each file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+
+
+const todoListArray = [
+  {
+    task: 'Organize Garage',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 2,
+    completed: false
+  }
+];
+
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todoList: todoListArray
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
       </div>
     );
   }
