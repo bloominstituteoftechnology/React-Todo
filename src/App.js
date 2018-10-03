116d0508d20d6edeffa23008d2d07f39033dd327
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-03 14:51:31 -0500

    Added todoData to state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoData: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+    }
+  }
   render() {
+    // console.log(this.state);  Checks This.state is working 
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
