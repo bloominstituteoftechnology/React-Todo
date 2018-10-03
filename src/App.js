b6e0f6c87e06e72526ad980b1b539d5488eb7444
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-10-03 17:20:06 -0400

    components/classes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      inputText: ''
+    }
+  }
+
+  addTodo = event => {
+    event.preventDefault();
+    if(this.state.inputText) {
+      this.setState({
+todos: [...this.state.todos, {task: this.state.inputText, id: Date.now(), completed: false}], inputText: ''
+      });
+    }
+  };
+
+inputHandler = event => {
+  this.setState({
+    inputText: event.target.value
+  });
+};
+
+completeTodo = id => {
+  let todos = this.state.todos.slice();
+  todos = todos.map(todo => {
+    if(todo.id === id) {
+      todo.completed = !todo.completed;
+      return todo;
+    } else {
+      return todo;
+    }
+  });
+  this.setState({ todos });
+};
+
+clearedTodo = event => {
+  event.preventDefault();
+  let todos = this.state.todos.slice();
+  todos = todos.filter(todo => !todo.completed);
+  this.setState({ todos });
+}
+
+render() {
+  return (
+    <div>
+      <h1>Todo</h1>
+      <TodoList todo4={this.state.todos} 
+      handleComplete = {this.todoComplete} />
+      <TodoForm
+      addTodo={this.addTodo}
+      inputText={this.state.inputText}
+      handleInput={this.handleInput}
+      handleClearTodos={this.clearCompleted} />
+    </div>
     );
   }
 }
