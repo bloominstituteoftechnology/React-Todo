fc0c9cb6f8f550b495af34893db2d1dec1e0b4ca
Ryan Boris <neodynia@icloud.com>
2018-10-03 17:44:33 -0400

    Rebuild Todo Component

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
 import './Todo.css';
 
 const Todo = props => {
-    return (
-        <p className="todo-task">{props.todo.task}</p>
-    )
+
+    return (<li>{props.value}</li>);
 }
 
 export default Todo;
