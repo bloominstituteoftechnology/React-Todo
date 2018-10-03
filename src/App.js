cddb7a86e19e450545e163f44a9c5c828bcac217
Unknown <zachary.arney@gmail.com>
2018-10-03 17:38:41 -0500

    Added methods to App for adding todo item and clearing all items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    todo: [],
+    newTodoItem: '',
+    id: 0,
+  };
+
+  addTodoItem = (event) => {
+    event.preventDefault();
+    if (this.state.newTodoItem.length > 0) {
+      this.setState({
+        todo: [
+          ...this.state.todo,
+          { item: this.state.newTodoItem, id: this.state.id, completed: false },
+        ],
+        newTodoItem: '',
+        id: this.state.id + 1,
+      });
+    }
+  };
+
+  changeHandler = (event) => {
+    this.setState({ newTodoItem: event.target.value });
+  };
+
   render() {
     return (
       <div>
