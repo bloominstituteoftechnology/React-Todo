2ad6f60a13838667079e38a252c9d088fe77d9d7
Ryan Boris <neodynia@icloud.com>
2018-10-03 17:46:55 -0400

    Refactor TodoList Component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+
+class TodoList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.taskLists.map(x => {
+          return <div key={x.id}>{x.task}</div>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
