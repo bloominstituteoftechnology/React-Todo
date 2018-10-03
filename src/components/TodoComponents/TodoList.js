e28ca2f6cc1f2894b931f65340d9f0985d51fe9c
Wanda <doubleuevans@gmail.com>
2018-10-03 16:14:09 -0500

    first commit and pull request

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,13 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const todoList = props => {
+  return (
+    <div>
+    {props.todoArray.map(item => {
+      return <h1>{item.task}</h1>
+    })}
+    </div>
+  )
+}
+
+export default todoList
