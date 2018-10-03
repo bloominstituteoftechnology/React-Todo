5bb015bc4342c2676da78af49af787e937916844
Stublink <chrisrey95@gmail.com>
2018-10-03 12:56:10 -0400

    TodoComopenents Setup

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = ({task}) => {
+    return (
+        <div>{task}</div>
+    );
+}
+
+export default Todo;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="Task" />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default TodoForm;
