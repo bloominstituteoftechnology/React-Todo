c40a59b74835ce612972cb8b0a3a250febedb0ca
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-10-03 17:23:46 -0400

    todolist

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo1 from "./Todo";
+
+
+const TodoList(props) {
+    return <ul>{props.todo4.map(todo2 => 
+        <Todo1 
+            todo3={todo2}
+            key= {todo2.id}
+            handleComplete={props.handleComplete}
+            />
+        )}
+        </ul>;
+}
+
+export default TodoList;
