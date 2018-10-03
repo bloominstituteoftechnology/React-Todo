b0399f0a9bd75dce5b843617f3b2e4127865bc9f
Unknown <zachary.arney@gmail.com>
2018-10-03 17:33:52 -0500

    Created TodoList and imported TodoItem into TodoList and

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = (props) => {
+  return (
+    <div>
+      <h1>Todo List!</h1>
+      {props.todo.map((todo) => {
+        return <TodoItem todoItem={todo.item} key={todo.id} />;
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
