5157fc91619e382a17f0625fa52cbfde03c3f2d4
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-03 13:05:58 -0700

    state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: "",
+      id: Date.now(),
+      completed: false
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
+    console.log(this.state.todo.task);
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
