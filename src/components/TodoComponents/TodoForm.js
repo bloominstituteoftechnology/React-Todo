9a41774a15b546bbec66ce4e05486cf5107a131d
Courtney Buratto <you@example.com>
2018-10-03 11:57:38 -0700

    MVP (added handlers to input and buttons)

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const TodoForm = props => {
     return (
-        <form className = "todo-form">
-            <input value ={props.value} type="text" onChange={props.changeInput} placeholder= "...todo"></input>
-            <button onClick={props.addTodo}>Add Todo</button>
-            <button onClick={clearCompleted}>Clear Completed</button>
-        </form>
+        <div className = "todo-form-wrapper">
+          <input name="item" type="text" onChange={props.onChange} value={props.item} placeholder= "...todo" />
+          <button onClick={props.onSubmit}>Add Todo</button>
+          <button>Clear Completed</button>
+        </div>
     );
 }
 
