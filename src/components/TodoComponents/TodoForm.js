e25b297144e272c374eddb8c5b7f589efd4df600
unknown <jerrardsmith215@gmail.com>
2018-10-02 22:03:36 -0400

    Jerrard Smith React-Todo Updated

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Todo.css';
+
+const TodoForm = props => {
+    return (
+        <div>Waffle</div>
+    )
+}
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import './Todo.css';
+import TodoForm from './TodoForm.js'
+
+const TodoList = () => {
+    return (
+        <div className="display">
+            <h1>Just Do It! <span>the list.</span></h1>
+            
+            <hr/>
+
+            <div className="actions">
+                <TodoForm />
+            </div>
+        </div>
+    )
+}
+
+export default TodoList;
