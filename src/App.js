8a59fc9bf0204b5941bbd23ea89242f7c356ff86
MEBonham <programmer@mckaybonham.com>
2018-10-03 14:21:38 -0600

    Got initial to-do item displaying under title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
+import TodoList from "./components/TodoComponents/TodoList";
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {};
+    
+    let firstItem = {
+      task: "Write to-do list",
+      id: Date.now(),
+      completed: false
+    };
+
+    this.state.list = [firstItem];
+    this.state.newItem = "";
+  }
+  
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="app">
+        <h1>To-Do List: MVP</h1>
+        <TodoList todoList={this.state.list} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,13 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoList = props => {
+    return (
+        <ul>
+            {props.todoList.map(item =>
+                <li>{item.task}</li>
+            )}
+        </ul>
+    );
+}
+
+export default TodoList;
