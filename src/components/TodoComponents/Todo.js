8e522307174c6499aeee00dfa5859c240c40913b
Wanda <doubleuevans@gmail.com>
2018-10-03 17:56:54 -0500

    completed Todo

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//import TodoList from './TodoList';
+
+const Todo = (props) => <h1>{props.todo.task}</h1>;
+
+export default Todo
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Todo from './Todo';
+
 const TodoList = (props) => {
 
+  console.log(props)
   return (
     <div>
-    {props.todoArray.map(item => {
-      return <h1>{item.task}</h1>
+    {props.todoArray.map(todoObj => {
+      return (
+        <div>
+          <Todo todo={todoObj}/>
+        </div>
+        );
     })}
     </div>
   )
