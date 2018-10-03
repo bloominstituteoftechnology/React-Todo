2af0228511454dbbdffcba4db685c6169d26cd27
bkwillia052 <bkwillia052@gmail.com>
2018-10-03 17:48:44 -0500

    refactored To-Do list item component into a class.

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 
 
-const Todo = (props) => {
+class Todo extends React.Component { 
+constructor(props) {
+    super(props);
+    this.state = {done: false};
+    this.toggleDone = this.toggleDone.bind(this);
+}
+
+toggleDone(){
+    if(!this.state.done){
+    this.setState({done: true});
+    }
+    else{
+        this.setState({done: false});
+    }
+}
+
+    render(){
     return (
         
-        <p className="list-item">{props.item}</p>
+        <p className={
+            `list-item 
+            ${this.state.done ? 
+            'done' : ''}`
+        }>
+        
+            {this.props.item}
+        
+        </p>
     )
+}
 }
 
 export default Todo;
