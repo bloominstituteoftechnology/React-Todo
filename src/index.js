dafdcf63357c95a25db47b46ab0fe8d54450125c
kyranmccann <kyran.mccann@gmail.com>
2018-10-03 22:20:19 -0700

    adding a commented out localStorage clear for ease of testing.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import { render } from 'react-dom';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
+
+
+
+//for testing purposes because testing was difficult with local storage.
+// window.onbeforeunload = function() {
+//    localStorage.clear();
+// }
