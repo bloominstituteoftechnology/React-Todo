0c635faabb9c839d1a7405c7057e1ee82c281719
erscriber <erscriber@gmail.com>
2018-10-04 14:24:55 -0400

    Added input data and onClick to the button, still have to build out the functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor () {
+    super();
+  
   // you will need a place to store your state in this component.
-  state = {
-    todos: [
-      {
-        task: 'Organize Garage',
-        id: 1528817077286,
-        completed: false
-      },
-      {
-        task: 'Bake Cookies',
-        id: 1528817084358,
-        completed: false
-      }
-    ],
-  };
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+    };
+  }
+
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
@@ -23,8 +29,13 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <div>
-        <input/>
-        <button>Add Todo</button>
+        <input
+          type="text"
+          onChange=
+          placeholder="....todo"
+          value={this.state.input}
+        />
+        <button onClick={this.addTodo}>Add</button>
         <button>Clear Completed</button>
         </div>
       </div>
