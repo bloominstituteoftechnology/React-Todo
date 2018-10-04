9a32fa44170dc1a84b02e60a17db0b7e3343e6a0
BryanGf <briangf@uw.edu>
2018-10-04 13:29:52 -0700

    Netlify link in index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import { render } from 'react-dom';
 import App from './App';
 import './index.css';
 
+//NETFLIY: https://app.netlify.com/sites/tender-mcclintock-3bf2e9/overview
+
 render(<App />, document.getElementById('root'));
