6cb1164a6600a9d43cd9a4da871a7ef44b874579
unknown <quan.nguyen199712@gmail.com>
2018-10-04 18:02:06 -0400

    Final countdown :p

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,6 +4,7 @@ const TodoList = (props)=>{
     return(
         props.todoData.map((item)=>{
             return <h1 style={item.completed ? {textDecoration: 'line-through'} :null} id={item.id} onClick={(event) => props.completedItem(event)}>{item.task}</h1>
+            // return <h1 className ={props.completed} id={item.id} onClick={(event) => props.completedItem(event)}>{item.task}</h1>
         })
     )
 }
