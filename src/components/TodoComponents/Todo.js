14ea4d96a958f3b974539b2a2e506f48862544e9
ssmodish <smodish@hotmail.com>
2018-10-04 18:34:02 -0400

    line-through achieved!

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import './Todo.css';
 
 function Todo(props) {
-  return (
-    <div className='task'onClick={() => props.completedHandler(props.id)} >
-      {props.task}
-    </div>
-  )
+  if(props.completed){
+    return (
+      <div className='task completed'onClick={() => props.completedHandler(props.id)} >
+        {props.task}
+      </div>
+    )
+  } else {
+    return (
+      <div className='task'onClick={() => props.completedHandler(props.id)} >
+        {props.task}
+      </div>
+    )
+  }
 }
 
 export default Todo;
