e16d84cc91a64f9789c011b4177164e283df25cf
unknown <brandongrych@gmail.com>
2018-10-04 14:03:03 -0500

    working on css and style

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './Todo.css';
 
 const TodoForm = (props) => {
     return (
-            <div className="todo-form">
-                <input type="text" value={props.input} onKeyDown={props.keypressHandler} onChange={props.changeHandler}/>
-                <button type='submit' onClick={props.submitHandler}>Submit</button>
-                <button className='cleared' onClick={props.clearHandler}>Clear Completed</button>
-            </div>
+        <div className="todo-form">
+        <div className="input">
+          <input type="text" value={props.input} onKeyDown={props.keypressHandler} onChange={props.changeHandler} />
+        </div>
+        <div className="buttons">
+          <button type="submit" className="button"onClick={props.submitHandler}>
+            Add Task
+          </button>
+          <button className="button" onClick={props.clearHandler}>
+            Clear Completed
+          </button>
+        </div>
+      </div>
     )
 }
 export default TodoForm
