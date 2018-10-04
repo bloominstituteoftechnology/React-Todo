0ad86f8b3da52e1caadbaa82ecf2cfad625e4f4e
kyranmccann <kyran.mccann@gmail.com>
2018-10-03 21:44:31 -0700

    fix 9

--- src/App.js
+++ src/App.js
@@ -6,23 +6,18 @@ import "./components/TodoComponents/Todo.css";
 
 let initialList = [{
         task: 'enter a new task in the upper left',
-        id: Date.now().toString(),
-        complete: false
+
       },
         {
           task: 'mark it complete (or not) by clicking it',
-          id: Date.now().toString(),
-          complete: false
+
         },
         {
           task: 'use the eraser to clear finished tasks',
-          id: Date.now().toString(),
-          complete: false
+
         },
         {
           task: 'search the list in the upper right',
-          id: Date.now().toString(),
-          complete: false
         },]
 let storedList = localStorage.getItem('todoList');
 if (storedList !== null) {
@@ -33,7 +28,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      todoList: ...initialList,
+      todoList: initialList,
       makeTodo: '',
       searchTerm: ''
     };
