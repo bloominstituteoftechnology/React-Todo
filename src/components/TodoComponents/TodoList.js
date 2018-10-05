63e607f5674734e80792bfaa1a51c0272124e7f9
bummings <eabummings@gmail.com>
2018-10-05 12:21:10 -0400

    added dependencies, writing card functionality

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,7 +3,6 @@ import './Todo.css';
 const ToDoList = (props) => {
   return (
     <div>
-
       {
         props.list.map(item => {
           return <p key={item.id} className={item.completed}>{item.task}</p>
