a08a332ccbf0845472180c343c35d6adfc44e22c
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-05 10:11:56 -0500

    getting started

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'gardening', 
+          completed: false
+        },
+        {
+          task: 'eating food',
+          completed: false
+        }
+      ]
+    }
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos}/>
+        <TodoForm />
       </div>
     );
   }
 }
 
 export default App;
+// this.state.todos.map((item) => {
+//   return item.task;
+// })
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoForm = props => {
+  return (
+    <form>
+      <input/>
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+    </form>
+  )
+}
+
+export default TodoForm;
