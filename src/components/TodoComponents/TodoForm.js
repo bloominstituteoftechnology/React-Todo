74f187e34f9f7ec8d18526e3b4e8ead6776a20f5
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-10-08 09:52:30 -0600

    I forgot to make a form submit on enter. I've added that functionality.

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const TodoForm = props => {
     return (
-        <div className="todo-form">
-            <input onChange={props.onChangeInput} value={props.valueOfState} placeholder="Input task here!" />
-            <button onClick={props.clickEvent}>Add Task to your "To Do" List!</button>
-            <button onClick={props.clearCompleted}>Clear Completed Tasks!</button>
-        </div>
+        <form action="">
+            <div className="todo-form">
+                <input onChange={props.onChangeInput} value={props.valueOfState} placeholder="Input task here!" />
+                <button onClick={props.clickEvent}>Add Task to your "To Do" List!</button>
+                <button onClick={props.clearCompleted}>Clear Completed Tasks!</button>
+            </div>
+        </form>
     )
 }
 
