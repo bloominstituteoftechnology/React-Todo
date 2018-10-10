1bf9e88cab456a775d0f56a91d7964f7b40b54b4
Peter Pham <phamhere@gmail.com>
2018-10-10 16:21:25 -0600

    test styled-components

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: #8C2F5B;
+  font-size: 0.6em;
+  padding: 0.25em 1em;
+  border: 2px solid #8C2F5B;
+  border-radius: 3px;
+`;
 
 const TodoForm = props => {
   return (
     <form>
       <input type="text" id="todoInput" name="todo" placeholder="...todo" onChange={props.inputChange} value={props.todo} />
-      <button onClick={props.addTodo}>Add Todo</button>
-      <button onClick={props.clearTodo}>Clear Completed</button>
+      <Button onClick={props.addTodo}>Add Todo</Button>
+      <Button onClick={props.clearTodo}>Clear Completed</Button>
     </form>
   )
 }
