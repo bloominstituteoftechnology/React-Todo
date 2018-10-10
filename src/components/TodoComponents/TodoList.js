2f11bdaf90d827bf4bef24a1044a933df0610a7e
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-10 15:41:51 -0400

    [added] created todoList component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+const TodoList = props => {
+    return(
+        <div>Test Elements</div>
+    );
+}
+
+export default TodoList;
