2fef50ede4d85a9a3f6629756976ee7c90fc0a0c
jaklundun <govi.escudero@gmail.com>
2018-10-11 20:42:43 -0700

    Set up app container with class component and initial state. Updated app welcome greeting.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [
+        {
+          task: 'Kundalini',
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: 'QiGong',
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: 'Walk Sandy',
+          id: Date.now(),
+          completed: false
+        },
+      ]
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Lambda-Lambda-Lambda Todo App!</h2>
       </div>
     );
   }
