e0f9708000ac9b94f00443dbd50cd89667d77148
Jasmine <jasmineanderson315@yahoo.com>
2018-11-07 17:37:43 -0500

    fixed todolist component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,16 +2,18 @@
 // feel free to change this component.js into TodoList.js
 
 import React from 'react';
-import Todo from './Todo';
 
-function TodoList(props) {
-  return (
-    <div className='todo-list'>
-      {props.tasks.map(task => {
-        return <Todo task={task.task} completed={task.completed} key={task.id} id={task.id} completedHandler={props.completedHandler} />
-      })}
-    </div>
-  );
+class TodoList extends React.Component {
+
+    render() {
+        return (
+            <div>
+                {this.props.taskLists.map(x => {
+                    return <div key={x.id}>{x.task}</div>
+                })}
+            </div>
+        );
+    }
 }
 
 export default TodoList;
