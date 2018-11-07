5cf594ec4d7da722d4f0e8f13f4e9074db755829
darenlarson <larson.daren@gmail.com>
2018-11-07 16:58:39 -0600

    Static form built with some functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 import './components/TodoComponents/Todo.css';
 
 
@@ -13,14 +14,26 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      tasks: taskListArray
+      tasks: taskListArray,
+      inputText: '',
     };
   }
+
+  handleChange = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+    console.log(event.target.name);
+  };
 
   render() {
     return (
       <div className="App">
         <TodoList tasksList={this.state.tasks} />
+        <TodoForm 
+          inputText={this.state.inputText} 
+          handleChange={this.handleChange} 
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,6 +1,20 @@
+import React from 'react';
 
+const TodoForm = props => {
+    return (
+        <div>
+            <form>
+                <input 
+                    type="text"  
+                    name="inputText"
+                    value={props.inputText} 
+                    onChange={props.handleChange}
+                />
 
-const TodoForm
-
+                <button>I'm a button!</button>
+            </form>
+        </div>
+    );
+};
 
 export default TodoForm;
