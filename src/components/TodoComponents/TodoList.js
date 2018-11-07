c843149433fbf967a87a4e2fcbc1b973360de578
steelworx <eileencuevas62@gmail.com>
2018-11-07 16:18:09 -0500

    initial commit

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+    return(
+        <div>
+            {props.todos.map(currentTodo => {
+                return <Todo key={} item={currentTodo} />;
+            })}
+        </div>
+    );
+}
+
+export default TodoList;
