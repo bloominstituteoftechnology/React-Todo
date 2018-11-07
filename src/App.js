3ac1514ff5846766fa8a2aa9a6426558fd2f9299
Trai Lynne <trailynne@outlook.com>
2018-11-07 15:17:19 -0700

    Refactored files for class components Refactored code for  App.js so App class to include constructor method Imported React and styles into component js files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  // Constructor
+  constructor(props) {
+    super();
+    this.state = {
+      currentList: []
+    }
+  }
+
+  // Methods
+
+  // Render
   render() {
     return (
       <div>
