d0f6d55dc433daa51cedae6ba4d810e97e47253d
steelworx <eileencuevas62@gmail.com>
2018-11-07 16:51:22 -0500

    added functionality to TodoForm

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
-const testTodos = [
-  {todoInfo: 'make this work'},
-  {todoInfo: 'drink some water'},
-  {todoInfo: 'go to sleep'}
-];
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: testTodos,
+      todos: [],
       todoText: '',
     }
+  }
+
+  addToTodos = event => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, {todoInfo: this.state.todoText}],
+      todoText: '',
+    });
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -24,7 +34,7 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList todos={this.state.todos}/>
-        <TodoForm todoText={this.state.todoText}/>
+        <TodoForm addToTodos={this.addToTodos} todoText={this.state.todoText} handleChange={this.handleChange}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,8 +3,9 @@ import React from 'react';
 const TodoForm = props => {
     return (
         <div>
-            <form action="">
-                <input type="text" name='todoText' placeholder='add to your Todo list here!'/>
+            <form onSubmit={props.addToTodos}>
+                <input type="text" name='todoText' placeholder='add to your Todo list here!' value={props.todoText} onChange={props.handleChange}/>
+                <button type='submit'>Add Todo</button>
             </form>
         </div>
     );
