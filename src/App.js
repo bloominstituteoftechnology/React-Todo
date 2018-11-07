e7588bef207a29eafb779f30b0c878decee977cd
Trai Lynne <trailynne@outlook.com>
2018-11-07 16:15:56 -0700

    Added code for ToDoForm Successfully passed down props from App.js Successfully renders new item when button is clicked Reset input value back to empty string when button clicked Imported and called in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm'
 
 class App extends Component {
 
@@ -9,12 +10,27 @@ class App extends Component {
     super();
     this.state = {
       currentList: [
-        { todo: 'Make todo list' }
-      ]
+        { todo: 'Make todo list' },
+        { todo: 'add items' }
+      ],
+      inputValue: ''
     }
   }
 
   // Methods
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  addItem = e => {
+    e.preventDefault();
+    this.setState({
+      currentList: [...this.state.currentList, { todo: this.state.inputValue }],
+      inputValue: ''
+    });
+  }
 
   // Render
   render() {
@@ -22,6 +38,11 @@ class App extends Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList list={this.state.currentList} />
+        <TodoForm
+          handleChange={this.handleChange}
+          inputValue={this.state.inputValue}
+          addItem={this.addItem}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,2 +1,19 @@
 import React from "react";
 import './Todo.css';
+
+const TodoForm = props => {
+    return (
+        <form onSubmit={props.addItem}>
+            <input
+                type='text'
+                name='inputValue'
+                placeholder='Add new item to the list here'
+                value={props.inputValue}
+                onChange={props.handleChange}
+            />
+            <button type='submit'>Add to List</button>
+        </form>
+    )
+}
+
+export default TodoForm;
