88e46e79f751811c2a989b732a2e4b326c55f37a
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-07 12:40:22 -0800

    added place holder

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,11 @@ import React from 'react';
 function TodoForm(props) {
   return (
     <form>
-      <input name={props.name} onChange={props.handleChange} value={props.inputText}></input>
+      <input 
+        name={props.name} 
+        placeholder={'Add todo item'}
+        onChange={props.handleChange}
+        value={props.inputText}></input>
       <button>press enter</button>
       <button>Clear All</button>
     </form>
