eca08c84b3543ad095a10f5e8232c71002706ada
Jarrad Miller <jm020801@gmail.com>
2018-11-07 15:48:48 -0700

    mvp

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <p className={props.completed.toString()} key={props.id}>{props.task}</p>
+    );
+}
+
+export default Todo;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const TodoForm = props => {
+  return (
+    <div>
+      <form onSubmit={props.addNewTodo}>
+        <input
+          name="newTodo"
+          placeholder="...todo"
+          type="text"
+          value={props.newTodo}
+          onChange={props.textInputHandler}
+        />
+        <button>Add Todo</button>
+        <button>Clear Completed</button>
+      </form>
+    </div>
+  )
+}
+
+export default TodoForm;
