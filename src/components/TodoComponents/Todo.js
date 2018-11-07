fa9824584cd350ea3cccd633f52ee982f835ac6a
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-11-07 14:17:19 -0600

    render dummy data to Todo app

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = props => {
+  return (
+    <li>{props.todo.todo}</li>
+  );
+};
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from './Todo';
+
+const TodoList = props => {
+  return (
+    <ul>
+      {
+        props.todoList.map( todo => <Todo key={todo.todo} todo={todo}/> )
+      }
+    </ul>
+  );
+};
+
+export default TodoList;
