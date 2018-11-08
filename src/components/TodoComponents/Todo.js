ca94ef1e5f827a01fdcf579f98b8fafd0a37e76a
Aman <amsingh714@gmail.com>
2018-11-08 17:40:59 -0500

    updated styling for list items

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Todo.css';
 
 const Todo = props => {
     return (
         <p 
         onClick={() => props.completedItems(props.item.id)}
-        className={`character-header ${props.item.completed}`}
+        className={`item-text ${props.item.completed}`}
         >
         {props.item.task}
         </p>
