7ade1e7f9089c3426b56640e4164b0a01065e53d
Yasirah Krueng <ykrueng@Lambda-School-Loaner-39s-MacBook-Pro.local>
2018-11-08 12:32:02 -0600

    change styles

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,16 +4,16 @@ const TodoForm = props => {
   return (
     <div>
       <form onSubmit={props.addTodo}>
-        <input
-          type="text"
-          name="inputText"
-          onChange={ (e) => props.handleChange(e) }
-          value={props.inputText}
-          placeholder="Add todo"/>
-        <button type="submit">Add Todo</button>
-        <div>
-          <button onClick={ (e) => props.removeCompleted(e) } >Clear Completed</button>
+        <div className="add--todo">
+          <input
+            type="text"
+            name="inputText"
+            onChange={ (e) => props.handleChange(e) }
+            value={props.inputText}
+            placeholder="Add todo"/>
+          <button type="submit">Add Todo</button>
         </div>
+        <button onClick={ (e) => props.removeCompleted(e) } >Clear Completed</button>
       </form>
     </div>
   );
--- src/components/TodoComponents/TodoSearchBox.js
+++ src/components/TodoComponents/TodoSearchBox.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const TodoSearchBox = props => {
   return (
-    <input 
+    <input
+      className="search"
       type="text" 
       name="filterText"
       onChange={ (e) => props.handleChange(e) }
