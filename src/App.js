b65179a3bc5d58a77f9d6d95f01547efc4283bbc
IshwantSingh <ishwant8@gmail.com>
2018-11-09 16:32:06 +0530

    day3 work done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import TodoList from "../src/components/TodoComponents/TodoList"
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(props) {
+    super(props);
+    this.state= {
+      todos: [
+        {todo: "Do today's Homework"},
+        {todo: "Learn more about React"},
+        {todo: "Start College work"}
+      ],
+      task: "",
+      id: "",
+      newTask:""
+    }
+  }
+
+  changeHandler = event => {
+    console.log(event.target.name);
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  addNewTask = event => {
+    event.preventDefault();
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {todo: this.state.newTask}
+      ],
+      newTask: ""
+    });
+  }
+
+
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List:-</h1>
+
+        <div>
+          <TodoList
+            todos={this.state.todos}
+            task={this.state.task}
+            newTask={this.state.newTask}
+            class={this.state.class}
+            changeHandler={this.changeHandler}
+            addNewTask={this.addNewTask}
+          />
+        </div>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,43 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoList = props => {
+    return (
+        <div>
+           {props.todos.map(item => {
+               return <h4 className={props.class}>{item.todo}</h4>
+           })}
+
+
+        <div>
+
+            <form onSubmit={props.addNewTask}>
+
+           <input
+             type="text"
+             name="newTask"  
+             placeholder="...todo" 
+             value={props.newTask}
+             onChange={props.changeHandler}
+           />  
+
+           </form>
+
+        </div>   
+           
+        
+        <button type="submit" onClick={props.addNewTask}>Add Todo</button>
+        <button>Clear Completed</button>
+
+
+
+
+
+
+
+        </div>
+    );
+}
+
+export default TodoList;
