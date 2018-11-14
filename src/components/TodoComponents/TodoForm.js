f79958dc5832a73023078215db87fcb7d3649be4
1@$@1github <asaofdiamonds@gmail.com>
2018-11-14 15:54:44 -0700

    built TodoForm.js

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const TodoForm = () => {
+const TodoForm = props => {
     return(
         <div className='todo-form'>
+        <form onSubmit={props.addTask}>
+        <input
+            type='text'
+            name='inputText'
+            placeholder='add new task'
+            value={props.inputText}
+            onChange={props.handleChange}
+        />    
+        </form>
         Hello I am the todo form
         </div>
     );
