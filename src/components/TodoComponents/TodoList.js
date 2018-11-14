0ed7a3d7363340695f180a1dff0f54f454843a53
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-14 16:37:18 -0500

    need to figure out toggle. Started Todo

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,7 +1,28 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from 'react';
+import Todo from './Todo';
 
-const TodoList = props => {}
+// Basic Structure:
+// const TodoList = props => {
+//     return (
+//         <div>
+//             {props.blahblah.map()}
+//         </div>
+//     )
+// }
+
+const TodoList = props => {
+    return (
+        <div>
+          {props.todos.map(todo => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+            />
+          ))}
+        </div>
+      );
+}
 
 export default TodoList;
