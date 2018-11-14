08898d2ef3b0fa06dc898cd1d5b215376aa38f3e
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-14 16:13:21 -0500

    adding tasks to state of App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
 
+// const ToDoTasks = [
+//   {
+//     task: 'Watch Arrrested Development',
+//     id: 1528817077286,
+//     completed: false
+//   },
+//   {
+//     task: 'Fry Cornball Snacks',
+//     id: 1528817084358,
+//     completed: false
+//   },
+//   {
+//     task: 'Bandage Resulting Burns',
+//     id: 1528817084358,
+//     completed: false
+//   }
+// ];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
+  constructor () {
+    super ()
+    this.state = {
+      todos: [
+        {
+          task: 'Watch Arrrested Development',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Fry Cornball Snacks',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'Bandage Resulting Burns',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+    todo: ""
   }
 }
 
