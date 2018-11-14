d4a1351788860fc2591a2103d1fce040b633b47d
chrisgins <christiansgina@gmail.com>
2018-11-14 14:09:35 -0700

    head start on styling; adding some color

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TodoForm = props => {
     return (
         <div className='todo-form-container'>
-            <input value={props.userInput} onChange={props.handleSubmit} onKeyPress={props.enterPressed} type='text' placeholder='...todo' />
+            <input value={props.userInput} onChange={props.handleSubmit} onKeyPress={props.enterPressed} type='text' placeholder='what to do?' />
             <button onClick={props.buttonPressed}>Add Todo</button>
             <button className='clear' onClick={props.clearList}>Clear Completed</button>
         </div>
