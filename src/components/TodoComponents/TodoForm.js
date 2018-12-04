f866299bf9021ef3f8e07cbce6139daf315e0c67
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-12-03 21:24:26 -0600

    Created initial layout in TodoForm

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Todo.css';
+
+const TodoForm = props =>{
+  return (
+    <div>
+      <input placeholder="...todo" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+    </div>
+  );
+}
+
+export default TodoForm;
