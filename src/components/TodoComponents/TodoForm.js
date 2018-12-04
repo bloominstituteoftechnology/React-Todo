b52c9553fd1a3076a11e161e1ac3cec74a67e53e
Jason Cook <the.root.user@gmail.com>
2018-12-04 18:48:02 -0500

    Properly call function and remove expression brackets
    This was done to avoid react from barking out a warning;
    Following advice from fellow student

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -5,7 +5,7 @@ const TodoForm = props => {
   return (
     <div className="inputField">
     <form>
-      <input type="text" name="newItem" onKeyPress={(e) => {if (e.keyCode === 13) {props.addNewItem} } } onChange={props.formEventHandler} value={props.newItem} placeholder={props.inputText} />
+      <input type="text" name="newItem" onKeyPress={(e) => {if (e.keyCode === 13) props.addNewItem() } } onChange={props.formEventHandler} value={props.newItem} placeholder={props.inputText} />
       <div className="formButtons">
         <input type="submit" onClick={props.addNewItem} value="Add To List" />
         <input type="button" value="Clear Complete" />
