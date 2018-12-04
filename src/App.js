6e32bff6277bc91a70df46679dd14827d4d58304
ThomasUtsey <tomutsey@yahoo.com>
2018-12-04 13:52:12 -0500

    Add Code into App.js
    Code App js to functionality
    
    Added todolist component to compartmentalize

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import ToDoList from "./components/TodoComponents/TodoList";
+import "./components/TodoComponents/Todo.css"
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      name: "Task List",
+      todos: ["Add A Task", "Add a second Task"],
+      todo: ""
+    };
+  }
+  changeNameHandler = event => {
+    this.setState({ todo: event.target.value });
+  };
+  addTodo = () => {
+    const todos = this.state.todos.slice();
+    todos.push(this.state.todo);
+    this.setState({todos: todos, todo: " "});
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2 key="item1">{this.state.name}</h2>
+        <input
+          type="text"
+          onChange={this.changeNameHandler}
+          placeholder="Add task"
+          value={this.state.todo}
+        />
+        <button onClick = {this.addTodo}>Submit</button>
+        <ToDoList currentToDo={this.state.todos} />
       </div>
     );
   }
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+const ToDoList = props => {
+  return (
+    <ul>
+      {" "}
+      {props.currentToDo.map(task => {
+        return <li key={Math.random()}>{task}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default ToDoList;
