a642f7207c0437da32b820a961e7f356b0b5cb4b
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-12-03 20:44:08 -0700

    added code to TodoList. js to iterate through array of taskItems and return them to the DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
         <TodoForm />
       </div>
     );
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,38 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+
+const taskItems = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+const ListItemRenderer = props => {
+  const {task} = props.taskItems;
+  return (
+    <p>{task}</p>
+  )
+}
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: taskItems
+    }
+  }
+  render() {
+    return (
+      <div class="todoList">
+        {this.state.todo.map(task => <ListItemRenderer taskItems={task}/>)}
+      </div>
+    )
+  }
+}
+
+export default TodoList;
