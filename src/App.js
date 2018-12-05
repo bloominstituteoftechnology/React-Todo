c0e2c9ec65407111821e42192e3991d4f4fff83d
Andrew <andrewryanogle@gmail.com>
2018-12-04 20:37:54 -0800

    trying to figure it out

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div>
