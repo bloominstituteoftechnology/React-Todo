15cc2a419f81bfbbd800ba5ba112634493758f2f
mustafa inaya <moose.inaya@icloud.com>
2018-12-04 16:43:03 -0800

    IMPLEMENT, Implemented a Class Component To Make A Functional Todos List.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			message: ''
+		};
+	}
+	inputText = (event) => {
+		this.setState({ message: event.target.value });
+	};
+
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+	render() {
+		return (
+			<div className="App">
+				<h1>{this.state.message}</h1>
+				<h2>{this.state.message}</h2>
+				<h2>Welcome to your Todo App!</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
