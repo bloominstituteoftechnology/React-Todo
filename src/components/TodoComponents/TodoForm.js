1f21e05e16ee2cb541b6f8c639abd1f323a93a0e
James Wigdor <jpwigdor@gmail.com>
2018-12-04 22:58:07 -0500

    Add basic layout of Text/Buttons

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoForm = props => (
+  <form>
+    <input type="" placeholder="...todo" />
+    <button>Add ToDo</button>
+    <button>Clear Completed</button>
+  </form>
+);
+export default TodoForm;
