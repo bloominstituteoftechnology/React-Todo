226ab92a72b460f91d5ce37570094b61eb976e04
Daniel Lim <danheelim@gmail.com>
2018-12-04 21:31:13 -0800

    finished the adding functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 // import TodoForm from './components/TodoComponents/TodoForm.js';
 import List from './components/TodoComponents/TodoList.js';
 import Task from './components/TodoComponents/Todo.js';
@@ -7,24 +8,27 @@ class App extends React.Component {
   constructor() {
     super();
     this.state= {
-      task: List
+      tasks: List,
+      task: ""
 
     }
   }
 
-  handleUpdateTasks = () => {
-    // const currentTask = this.state.task.slice();
-    // currentTask.push({
-    // })
-    // console.log('hi')
+  handleUpdateState = e => {
+    e.preventDefault();
+    const newTasks = this.state.tasks.slice();
+    newTasks.push({
+      id: new Date(),
+      task: this.state.task,
+      completed: false
+    });
+    this.setState({ tasks: newTasks });
+  };
 
-  }
 
-
-  handleInputChange = event => {
-    console.log(event.target.value)
-  }
-
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -33,18 +37,24 @@ class App extends React.Component {
       <div>
         <h2>Welcome to the TODO APP!</h2>
 
-        {this.state.task.map(task => (
+        {this.state.tasks.map(task => (
           <Task displayTask = {task} />
         ))}
 
-        <input value = { this.state.value } onChange = {this.handleInputChange}type="text"></input>
-
-        <button onClick={this.handleUpdateTasks}>Add To Do</button>
-        <button>Clear Finished</button>
+        <form onSubmit={this.handleUpdateState}>
+          <button>Click to Add 2</button>
+          <input name="task" 
+          value={this.state.task} 
+          onChange={this.handleInputChange} />
+        </form>
       </div>
 
     )
   }
 }
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
