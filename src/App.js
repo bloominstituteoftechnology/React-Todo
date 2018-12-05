8f467b3fff8aa0a1986b6bc1d98c0ce716647f58
brettmadrid <brettmadrid9@gmail.com>
2018-12-04 20:21:51 -0800

    Added To Do add functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,76 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+//import "./styles.css";
+
+const todoArr = [
+  {
+    id: 1528817077286,
+    task: "Organize Garage",
+    completed: false
+  },
+  {
+    id: 1528817084358,
+    task: "Clean Room",
+    completed: false
+  }
+];
+
+const MessageRenderer = props => {
+  return <h1>{props.propsMessage}</h1>;
+};
+
+const Todo = props => {
+  const { task } = props.todoProp;
+  return (
+    <div>
+      <p>{task}</p>
+    </div>
+  );
+};
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todoArr,
+      task: ""
+    };
+  }
+
+  handleUpdateState = e => {
+    e.preventDefault();
+    const todos = this.state.todos.slice();
+    // clone our state todos array.
+    todos.push({
+      id: new Date(),
+      task: this.state.task,
+      completed: false
+    });
+    // push our new todo item into the todos array.
+    this.setState({ todos: todos });
+  };
+
+  handleInputChange = event => {
+    // update the message field on state.
+    this.setState({ [event.target.name]: event.target.value });
+    
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        {this.state.todos.map((todo, i) => <Todo todoProp={todo} />)}
+        <form onSubmit={this.handleUpdateState}>
+          <button>Click to Add 2</button>
+          <input name="task" value={this.state.task} placeholder="capture" onChange={this.handleInputChange} />
+        </form>
       </div>
     );
   }
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
 
 export default App;
