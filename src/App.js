03deb6be5cb3a0cbd4d7981bdd87b39bddab2667
Jonathan Dorety <kyullog@gmail.com>
2018-12-04 22:46:32 -0500

    Make TodoList component render dummy data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {todos: [
+      {
+        task: "Take the trash out",
+        id: 1,
+        completed: false
+      },
+      {
+        task: "Sing with the birds",
+        id: 2,
+        completed: false
+      },
+      {
+        task: "Change the baby's diapers",
+        id: 3,
+        completed: false
+      }
+    ]}
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
+    console.log(this.state.todos);
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        {this.state.todos.map( item => {
+          return (<TodoList listItem={item.task} />);
+        })}
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,10 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import './Todo.css';
+
+const TodoList = props => {
+  return <div>{props.listItem}</div>;
+}
+
+export default TodoList;
