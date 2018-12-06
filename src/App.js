3553f1e0238768ba547ed19b097ce303f74d372f
CatsAreMLG <joshuadgon@gmail.com>
2018-12-05 20:50:33 -0600

    renamed stateList to just list

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      stateList: []
+      list: []
     };
   }
 
   handleInputChange = e => {
     e.preventDefault();
-    this.state.stateList.push({
+    this.state.list.push({
       task: e.target.parentNode.children[0].value,
       id: Date.now(),
       completed: false
     });
     e.target.parentNode.children[0].value = "";
     this.setState({
-      stateList: this.state.stateList.filter(e => {
+      list: this.state.list.filter(e => {
         if (e.task !== "" && e.task !== undefined) {
           return true;
         } else {
@@ -31,9 +31,9 @@ class App extends React.Component {
     });
   };
   handleClear = _ => {
-    if (this.state.stateList.length > 0) {
+    if (this.state.list.length > 0) {
       this.setState({
-        stateList: this.state.stateList.filter(e => {
+        list: this.state.list.filter(e => {
           if (e.task !== "" && e.task !== undefined) {
             if (!e.completed) {
               return true;
@@ -49,7 +49,7 @@ class App extends React.Component {
   };
   handleStrike = id => {
     this.setState({
-      stateList: this.state.stateList.map(e => {
+      list: this.state.list.map(e => {
         if (e.id === id && e.completed === false) {
           return { task: e.task, id: e.id, completed: true };
         } else if (e.id === id && e.completed === true) {
@@ -64,7 +64,7 @@ class App extends React.Component {
     return (
       <div className="todo">
         <h2 className="todo__title">Todo list: MVP</h2>
-        <TodoList list={this.state.stateList} strike={this.handleStrike} />
+        <TodoList list={this.state.list} strike={this.handleStrike} />
         <TodoForm
           change={this.handleInputChange}
           clearCompleted={this.handleClear}
