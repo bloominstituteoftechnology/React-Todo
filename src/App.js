a7e6b4e5e64119eb42f92e9016e01330bcf19353
ThomasUtsey <tomutsey@yahoo.com>
2018-12-06 10:59:17 -0500

    Add App Class
    Add Constructor
    
    Add Super
    
    Add State - create todos array of objects ( this will store the data entered in the list of todos. In addition, the object includes an id and a completed keys within object in order to provide reference points for actions to individual task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: [{
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+      }
+    ]
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
