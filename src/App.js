cbb3764b92f0800409673541f52b0d6d2c417f4a
CatsAreMLG <joshuadgon@gmail.com>
2018-12-05 20:48:11 -0600

    Changed stateList to be it's own array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
-let list = [];
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -9,66 +8,56 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      stateList: list
+      stateList: []
     };
   }
 
   handleInputChange = e => {
     e.preventDefault();
-    list.push({
+    this.state.stateList.push({
       task: e.target.parentNode.children[0].value,
       id: Date.now(),
       completed: false
     });
     e.target.parentNode.children[0].value = "";
     this.setState({
-      stateList: (list = list.filter(e => {
+      stateList: this.state.stateList.filter(e => {
         if (e.task !== "" && e.task !== undefined) {
           return true;
         } else {
           return false;
         }
-      }))
+      })
     });
   };
   handleClear = _ => {
-    if (list.length > 0) {
-      list = list.filter(e => {
-        if (e.task !== "" && e.task !== undefined) {
-          if (!e.completed) {
-            return true;
+    if (this.state.stateList.length > 0) {
+      this.setState({
+        stateList: this.state.stateList.filter(e => {
+          if (e.task !== "" && e.task !== undefined) {
+            if (!e.completed) {
+              return true;
+            } else {
+              return false;
+            }
           } else {
             return false;
           }
-        } else {
-          return false;
-        }
-      });
-      this.setState({
-        stateList: list
+        })
       });
     }
   };
   handleStrike = id => {
-    list = list.map(e => {
-      if (e.id === id && e.completed === false) {
-        return {
-          task: e.task,
-          id: e.id,
-          completed: true
-        };
-      } else if (e.id === id && e.completed === true) {
-        return {
-          task: e.task,
-          id: e.id,
-          completed: false
-        };
-      } else {
-        return e;
-      }
-    });
     this.setState({
-      stateList: list
+      stateList: this.state.stateList.map(e => {
+        if (e.id === id && e.completed === false) {
+          return { task: e.task, id: e.id, completed: true };
+        } else if (e.id === id && e.completed === true) {
+          return { task: e.task, id: e.id, completed: false };
+        } else {
+          return e;
+        }
+      })
     });
   };
   render() {
