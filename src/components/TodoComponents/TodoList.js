2d041d97a052606bce9a9fbf802cb1d1224ad4fe
badCompany55 <43321461+badCompany55@users.noreply.github.com>
2018-12-05 19:20:34 -0600

    Slight list tweak
    finish completed task

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ToDo from "./Todo.js";
 
-const list = [
-  {
-    task: "Finish Laundry",
-    id: 1,
-    completed: false
-  },
-
-  {
-    task: "Cook Food",
-    id: 2,
-    completed: false
-  }
-];
-
 const ToDoList = props => {
   return (
     <div>
