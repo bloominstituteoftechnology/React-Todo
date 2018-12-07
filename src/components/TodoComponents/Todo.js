bfed8112b9418454e10c7cb324abbf9401c9b5ba
Andrew Yurlov <yurlovandrew@gmail.com>
2018-12-06 17:44:27 -0800

    created line-through on click

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,26 @@
 import React from "react";
+import './Todo.css';
 
-function Todo(props) {
-  return <li>{props.todo.task}</li>;
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      marked: false
+    };
+  }
+
+  completedHandler = () => {
+    const currentState = this.state.marked;
+    this.setState({ marked: !currentState });
+  };
+
+  render() {
+    return (
+      <li
+        className={this.state.marked ? 'completed' : null}
+        onClick={this.completedHandler}>{this.props.todo.task}</li>
+    );
+  }
 }
 
 export default Todo;
