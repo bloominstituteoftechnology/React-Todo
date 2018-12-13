8fdaf7a5d4c9779362899a001834fec72cc88db4
Angie Lawrence <lawrenceangie45@gmail.com>
2018-12-12 20:30:52 -0500

    Added to app.js,todo.js, and todolist.js

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <li items = {props.itemsInput}></li>
+    );
+};
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = (props) => {
+  return (
+    <li>
+      {props.items.map(items => (<Todo items = {props.items} id = {props.id} complete = {props.complete} />))};
+    </li>
+  );
+};
+
+export default TodoList;
