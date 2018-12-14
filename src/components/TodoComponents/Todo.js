f2cfab7957b2fa5bde2975ef79b85d79bbc86afc
Andrew <acarmstrong98@gmail.com>
2018-12-13 19:46:58 -0500

    todo.js complete

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,5 +1,17 @@
 import React from 'react';
  const Todo = props => {
    
+    return (
+
+        <div>
+      
+            <h3 onClick={()=> props.handleToggleComplete(props.todoProp.id)}>
+            {props.todoProp.task}
+            </h3>
+       
+        </div>
+      );
+      
+      }
 
  export default Todo; 
