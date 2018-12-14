556fd74af90be25779e071c7026e7dc7dbf1a52d
Andrew <acarmstrong98@gmail.com>
2018-12-13 19:46:05 -0500

    everything is set up and ready to be put into place.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import React from 'react';
-
-class App extends React.Component {
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+  constructor() {
+    
+    super();
+    
+    this.state = {
+      todos: []
+    };
+    
+  }
+   addTodo = event => {
+    event.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({task: this.state.todo, completed: this.state.completed, id: Date.now()});
+    
+    this.setState({todos, todo: ''});
+  };
+
+  changeTodo = event => {
+  this.setState({[event.target.name]: event.target.value});
+  }
+
+  completeTodo = id => {
+    let newTodos = this.state.todos.slice();
+     newTodos = newTodos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({newTodos});
+  }
+
+   clearTodo = event => {
+  event.preventDefault();
+  let todos = this.state.todos.slice();
+  todos = todos.filter(todo => !todo.completed);
+  this.setState({todos});
+  }
+
+
+
+
+   render() {
+    
     return (
       <div>
+
         <h2>Welcome to your Todo App!</h2>
+
       </div>
     );
   }
-}
-
-export default App;
+ }
+ 
+ export default App;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+ const TodoForm = props => {
+
+   
+ export default TodoForm; 
