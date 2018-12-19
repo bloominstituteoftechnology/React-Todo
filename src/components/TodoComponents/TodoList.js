22c94f3b844808670c6d3fe2d4fe0d22a596eace
linkel <ironcarbide@gmail.com>
2018-12-19 12:02:11 -0800

    Components display the app.js state list

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo.js';
+
+class TodoList extends React.Component {
+    render () {
+        return (
+            <div>
+            {this.props.list.map((item) =>
+            <Todo task={item.task}/>
+            )}
+            </div>
+        )
+    }
+}
+
+export default TodoList;
