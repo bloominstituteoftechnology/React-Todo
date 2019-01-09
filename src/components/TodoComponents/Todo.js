472d884d93f2750f1bd368c2f44e67cb31078e88
ajfrierson <alvinfrierson@fuller.edu>
2019-01-09 13:12:42 -0800

    completed todo.js

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Todo.css';
+
+const Todo = (props) => {
+    return (
+        <div>
+            <p className="task" id={props.id} onClick={props.onClick}>
+            - {props.task}
+          </p>
+        </div>
+    );
+};
