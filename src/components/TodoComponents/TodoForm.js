497261dbd3c8b7caef7b23796f566e0d0118bd11
umbur <work1415@gmail.com>
2019-01-09 16:39:33 -0700

    added some styling

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TodoForm = props => {
     return(
         <form onSubmit={props.handleSubmit}>
-            <input placeholder="..todo" value={props.taskText} onChange={props.handleChange}/>
+            <input placeholder="..todo" value={props.taskText} onChange={props.handleChange} className='todo'/>
             <button className='todoButton' onClick={props.handleSubmit}>Add Todo</button>
             <button className='clearButton'>Clear Completed</button>
         </form>
