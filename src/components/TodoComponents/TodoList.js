e176059b10ed9360f19c31a64bfe4a9e5fd75a80
ajfrierson <alvinfrierson@fuller.edu>
2019-01-09 13:27:34 -0800

    todolist.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import './Todo.css';
+import Todo from './Todo.js';
+
+const TodoList = (props) => {
+    return (
+      <div className="list-container">
+          {/* Takes as input (from props) each of the todos in state, iterates through (using .map), and renders them onscreen */}
+          {props.todos.map(todo => {
+            return (
+              <div>
+                <Todo key={todo.id} id={todo.id} task={todo.task} onClick={props.toggleComplete} completed={todo.completed} />
+              </div>
+            )
+          })}
+      </div>
+    )
+  }
+
+  export default TodoList;
