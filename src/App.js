466f99b10404af68e500587d92f0bf0e03f39a25
umbur <work1415@gmail.com>
2019-01-10 14:44:33 -0700

    refactored code, plus added some new sytling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 import './components/TodoComponents/Todo.css'
