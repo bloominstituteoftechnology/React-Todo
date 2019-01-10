85a232f5a197423e911da27a0dfb260cedca28a3
Brandon lent <blent1050@yahoo.com>
2019-01-10 12:27:27 -0500

    React-todolist-updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div>
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,10 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+
+function TodoList(props) {
+  return <div>{props.todos.map(todo => {})}</div>;
+}
+
+export default TodoList;
