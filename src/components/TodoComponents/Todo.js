1219965eee335435229fa389397322b9600330e7
joepound <14357797+joepound@users.noreply.github.com>
2019-01-10 12:15:53 -0700

    Added line-through styling on task details for completed todos

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -8,7 +8,7 @@ const Todo = props => {
       <div className="todo-item-text">
         <h2 className="todo-item-header">{props.isCompleted ? "DONE" : "To Do:"}</h2>
         <div className="todo-item-date">{`Created: ${props.dateCreated}`}</div>
-        <p className="todo-item-details">{props.todoDetails}</p>
+        <p className={props.isCompleted ? "todo-item-details-done" : "todo-item-details"}>{props.todoDetails}</p>
       </div>
       <button 
         className="todo-item-complete-btn"  
