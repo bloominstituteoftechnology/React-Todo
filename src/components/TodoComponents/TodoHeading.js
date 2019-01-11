a20e1d6e9387f56fcd0c9c3b4249d01fa644894c
leiannel <leilouis112@gmail.com>
2019-01-10 21:49:37 -0500

    added styling to page

--- src/components/TodoComponents/TodoHeading.js
+++ src/components/TodoComponents/TodoHeading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TodoHeading = (props) => {
     if(props.data.length === 0) {
         return (
-            <p>Add a to do item</p>
+            <p className='addTodoHeading'>Add a to do item</p>
         )
     } else {
         return null
