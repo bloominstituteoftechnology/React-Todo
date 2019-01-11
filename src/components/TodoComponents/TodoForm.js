93a5f9e421eb617725fdd90ee8ab2d22e336f984
Randy Wilson <phoenixrr2113@gmail.com>
2019-01-10 19:21:38 -0500

    more annimations added

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import posed from 'react-pose';
 
 const TodoForm = props => {
+	const Box = posed.div({
+		hoverable: true,
+		pressable: true,
+		init: {
+			scale: 1,
+		},
+		hover: {
+			scale: 1.3,
+		},
+		press: {
+			scale: 1.1,
+		},
+	});
+	const Button = posed.div({
+		hoverable: true,
+		pressable: true,
+		init: {
+			scale: 1,
+		},
+		hover: {
+			scale: 1.3,
+		},
+		press: {
+			scale: 1.1,
+		},
+	});
 	return (
 		<form className="todo-form">
 			<input
@@ -12,12 +39,16 @@ const TodoForm = props => {
 				placeholder="Enter Task"
 			/>
 			<div className="buttons">
-				<button className="add-btn" onClick={props.addTodo}>
-					Add
-				</button>
-				<button className="clear-complete" onClick={props.clearCompleted}>
-					Clear
-				</button>
+				<Box>
+					<button className="add-btn" onClick={props.addTodo}>
+						<Button>Add</Button>
+					</button>
+				</Box>
+				<Box>
+					<button className="clear-complete" onClick={props.clearCompleted}>
+						<Button>Clear</Button>
+					</button>
+				</Box>
 			</div>
 		</form>
 	);
