d46a8fbfc5c08cb25df16db3b650aeec58a63035
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-02-13 18:04:36 -0500

    day 1 MVP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,68 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
-render(<App />, document.getElementById('root'));
+const todoData = [
+  {
+    task: 'Stuff1',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Stuff2',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Stuff3',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Stuff4',
+    id: Date.now(),
+    completed: false
+  },
+]
+
+console.log(todoData);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: todoData,
+      task: ''
+    };
+  }
+
+  handleChanges = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addNewTodo = event => {
+    event.preventDefault();
+    this.setState({
+      todoList: [
+        ...this.state.todoList,
+        { task: this.state.task }
+      ],
+      task: ''
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TodoList todoDataList={this.state.todoList} />
+        <TodoForm
+          addNewTodo={this.addNewTodo}
+          handleChanges={this.handleChanges}
+          task={this.state.task}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
