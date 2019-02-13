996a439e3b85ad4147b6f72653beda6ef55df0af
RMH <rory.hennessey@gmail.com>
2019-02-13 16:03:32 -0500

    updated is happening live but cannot save the new item to the array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import todo from './components/TodoComponents/Todo';
+//import TodoForm from './components/TodoComponents/TodoForm'
+
+
+// const Task = props => {
+//   const { task } = props.taskProp;
+//   return (
+//     <div>
+//       <h5> {task} </h5>
+//     </div>
+//   );
+// };
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: ""
-    }
+      message: "Todo List: MVP",
+      tasks: todo,
+      updatedTasks: " "
+    };
   }
 
   handleUpdateState = () => {
+    const tasks = this.state.tasks.slice();
+    //this clones our tasks array
+    tasks.push ({
+      updatedTasks: "" //i want to push our new task into the tasks array
+    });
 
+    this.setState({ tasks: tasks });
+    //from react api - the only way to change state
 
   };
 
   handleInputChange = event => {
-    this.setState({ message: event.target.value })
+    //update updatedTasks on state
+    this.setState({ updatedTasks: event.target.value });
   };
   
   // you will need a place to store your state in this component.
@@ -23,7 +46,13 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Todo List: MVP</h1>
+        <h1> {this.state.message} </h1>
+        {this.state.tasks.map(myTask => {
+          return (<div key={myTask.task}>{myTask.task}</div>)
+        })}
+        {/* // <Task taskProp= {task} />)} */}
+        <div>{this.state.updatedTasks} </div>
+        {/* <TodoForm /> this does not work when i try to import from TodoForm*/}
         <input type="text" placeholder="...to do" onChange={this.handleInputChange} />
         <button onClick={this.handleUpdateState}>Add ToDo</button>
         <button>Clear Completed </button>
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,27 @@
+//import React from 'react';
+
+const todo = [
+    {
+        task: 'Learn React',
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: 'Execute Todo App',
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: 'Finish MVPs',
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: 'Figure out how to push new task to list',
+        id: Date.now(),
+        completed: false
+    },
+];
+
+
+export default todo;
