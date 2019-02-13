65bd8e5b990ee6d66b027e8b1c340ea9ada520d2
mark-marshall <mark.marshallgp@gmail.com>
2019-02-13 12:29:38 +0000

    [MVP] Added skeletons of functional components and rendered ToDoForm to screen

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,12 @@
+// `<TodoForm>` will hold your input field and your `Add Todo` and `Clear Completed` buttons.
+import React from 'react';
+
+export default function TodoForm() {
+  return (
+    <div>
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+    </div>
+  );
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,10 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+// `<TodoList />` receives your Todos array and iterates over the list generating a new `<Todo />` for each element in the array.
+import React from 'react';
+
+export default function ToDoList( {toDoArray} ) {
+    return (
+     {
+         toDoArray.map(toDo => <Todo todoData={toDo}/>)
+     }   
+    )
+}
