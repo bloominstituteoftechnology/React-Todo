1375f1a1f2fe6859640cb0dcd91e031201b1762a
RMH <rory.hennessey@gmail.com>
2019-02-13 11:11:08 -0500

    h1 input and buttons in place

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    }
+  }
+
+  handleUpdateState = () => {
+
+
+  };
+
+  handleInputChange = event => {
+    this.setState({ message: event.target.value })
+  };
+  
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <input type="text" placeholder="...to do" onChange={this.handleInputChange} />
+        <button onClick={this.handleUpdateState}>Add ToDo</button>
+        <button>Clear Completed </button>
       </div>
     );
   }
