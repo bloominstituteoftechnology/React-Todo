43bdcb3fc45840a94ce61078be4eb916b3ee5f98
nevets615 <surj316@yahoo.com>
2019-02-13 16:21:25 -0500

    initail pull request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import React from 'react';
 
+const tasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks,
+      id: '',
+      completed: ''
+    }
+  }
+
+  addTask = e => {
+    e.preventDefault();
+    console.log(e.target);
+
+    const newTask = {
+      task: this.state.task,
+      id: this.state.id,
+      completed: this.state.completed
+    };
+    this.setState({
+      tasks: [...this.state.tasks,newTask],
+      task:'',
+      id:'',
+      completed:''
+    });
+  };
+   handleChanges = e => {
+     console.log(e.target.value);
+     this.setState({
+       [e.target.task]: e.target.value
+     });
+   };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
