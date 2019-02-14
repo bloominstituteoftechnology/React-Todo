a1ac7f4fe7f055bb54e0cbb3109eb87553b7a937
cubsfan46350 <cubsfan.46350@msn.com>
2019-02-14 13:44:06 -0600

    created todoform component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      
+      list: [
+        {
+         task: 'Organize Garage',
+         id: 1528817077286,
+         completed: false 
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
     }
+      
   }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoForm = () => {
+    return (
+        <form>
+          <input 
+            type = "text"
+            placeholder = "Task"
+            name = ""
+            value = ""
+            onChange = ""
+          />
+        </form>
+    )
+}
+
+export default TodoForm;
