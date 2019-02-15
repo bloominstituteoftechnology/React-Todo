4f3b202e903f4366c8ab572dfab76c81199857ae
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-02-14 17:00:42 -0800

    added some styling to the todo app.

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -12,11 +12,3 @@ const TodoForm = props => {
 
 
 export default TodoForm;
-  // function TodoForm(props) {
-  //   return (
-  //     <form onSubmit={props.addTodo}>
-  //       <input type="text" onChange={props.handleChanges} name="item" value={props.value} placeholder="Add Todo Here" />
-  //       <button type="submit">Add Todo</button>
-  //     </form>
-  //   );
-  // }
