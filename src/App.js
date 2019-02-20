a9f9e5520101625f3dee4948d0b034e35abc7d94
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-20 13:15:13 -0800

    todos state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Todos from './components/TodoComponents/Todo';
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+      id: 1,
+      title: 'Take out the trash',
+      completed: false,
+      }
+    ]
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>App</h2>
+        <Todos/>
       </div>
     );
   }
