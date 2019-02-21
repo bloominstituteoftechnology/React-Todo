39b08b0246391a3769fd34e25936914061e3aef1
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-21 12:21:01 -0800

    Toggle Completed

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class TodoForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" name="title" placeholder="Add Todo"/>
+      </form>
+    )
+  }
+}
+
+push test
+
+export default TodoForm
