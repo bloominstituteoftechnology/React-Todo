e2c7c821f5eb269e717b4b40c51bce8a45146e48
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-23 14:24:57 -0800

    refactored to use hooks

--- src/App.js
+++ src/App.js
@@ -1,94 +1,73 @@
-import React from 'react';
-import TodoList from './components/TodoComponents/TodoList.js'
-import TodoForm from './components/TodoComponents/TodoForm.js'
-import './components/TodoComponents/Todo.css'
-import './App.css'
+import React, {useState} from "react";
+import TodoList from "./components/TodoComponents/TodoList.js";
+import TodoForm from "./components/TodoComponents/TodoForm.js";
+import "./components/TodoComponents/Todo.css";
+import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      todoData: [],
-      inputTask: '',
+const App = () => {
+  const [todoData, setTodoData] = useState([])
+  const [inputTask, setInputTask] = useState("");
+
+  const changeHandler = event => {
+    setInputTask(event.target.value);
+  };
+
+  const addNewTask = event => {
+    event.preventDefault();
+    if (inputTask === "") {
+      return null;
     }
-  }
 
-// ^^^^ state storage ^^^^^ //
-
-
-
-  changeHandler = event => {
-    this.setState({[event.target.name]: event.target.value });
-  }
-
-addNewTask = (event) => {
-  event.preventDefault();
-  if (this.state.inputTask === '') {
-    return null
-  }
-  
-  this.setState({ 
-    todoData: [
-      ...this.state.todoData, 
+    setTodoData([
+      ...todoData,
       {
-      id: Date.now(),
-      task: this.state.inputTask,
-      completed: false,
-      decoration: 'taskFontNone'}
-    ],
-    inputTask: ''
-  })
-}
-
-changeBool = (index) => {
-  this.setState({
-    todoData: this.state.todoData.map((task, idx) => {
-      if (index !== idx) {
-        return task;
-      } else {
-        return {
-          ...task,
-          completed: task.completed === false ? true : false,
-          decoration: task.decoration === 'taskFontNone' ? 'taskFont' : 'taskFontNone'
-        }
+        id: Date.now(),
+        task: inputTask,
+        completed: false,
+        decoration: "taskFontNone"
       }
-    })
-  })
-}
+    ]);
+    setInputTask("");
+  };
 
-clearTask = (event) => {
-  event.preventDefault();
-  this.setState({
-    todoData: this.state.todoData.filter((task) => !task.completed)})}
-
-
-
-  render() {
-  
-    return (
-      <div>
-          <TodoForm 
-        changeHandler={this.changeHandler}
-        addNewTask={this.addNewTask}
-        inputTask={this.state.inputTask}
-        clearTask={this.clearTask}
-        />
-        
-        <TodoList 
-        taskData={this.state.todoData}
-        changeBool={this.changeBool}
-        
-        />
-      
-      </div>
+  const changeBool = index => {
+    setTodoData(
+      todoData.map((task, idx) => {
+        if (index !== idx) {
+          return task;
+        } else {
+          return {
+            ...task,
+            completed: task.completed === false ? true : false,
+            decoration:
+              task.decoration === "taskFontNone" ? "taskFont" : "taskFontNone"
+          };
+        }
+      })
     );
-  }
-}
+  };
+
+  const clearTask = event => {
+    event.preventDefault();
+    setTodoData(todoData.filter(task => !task.completed));
+  };
+
+  return (
+    <div>
+      <TodoForm
+        changeHandler={changeHandler}
+        addNewTask={addNewTask}
+        inputTask={inputTask}
+        clearTask={clearTask}
+      />
+
+      <TodoList taskData={todoData} changeBool={changeBool} />
+    </div>
+  );
+};
 
 export default App;
 
-
-
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
