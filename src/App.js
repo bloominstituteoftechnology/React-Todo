7d7caed788e6a3626ae13341954c7055a0a18b95
adam <bcuz@users.noreply.github.com>
2019-03-04 23:51:22 -0500

    get something on the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+
+let todos = [
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todos,
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
