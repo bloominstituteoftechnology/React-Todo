5298041b25c994b8435dd7c522d0769f17d0ad4c
JustinAndrade <j.andrade.john@gmail.com>
2019-03-05 03:52:00 -0500

    proper display corrected

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Todo = props => {
     return (
-        <h1> {props.todoTask.task} </h1>
+        <React.Fragment>
+            <h1>
+                <strong>task:</strong> {props.todoTask.task} 
+            </h1>
+            <p> 
+                <strong>id:</strong> {props.todoTask.id} 
+            </p>
+            <p>
+                <strong>completed: </strong> {props.todoTask.completed} 
+            </p>
+        </React.Fragment>
     )
 }
 
