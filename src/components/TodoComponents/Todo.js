96bc4689959ef37f9d16fc94b1ae657e893ceb51
JustinAndrade <j.andrade.john@gmail.com>
2019-03-05 03:44:13 -0500

    basic elements diplaying

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <h1> {props.todoTask.task} </h1>
+    )
+}
+
+export default Todo
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,45 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+const todoList = [
+    {
+      task: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    },
+    {
+        task: 'Study Class Components',
+        id: 152881751447,
+        completed: true
+    },
+  ];
+
+  class TodoList extends React.Component {
+      constructor() {
+          super();
+          this.state = {
+              stateTodoList: todoList,
+              task: '',
+              id: '',
+              completed: '',
+          };
+      }
+
+      render() {
+          return (
+              <div>
+                  {this.state.stateTodoList.map(todoTask => (
+                      <Todo todoTask={todoTask} />
+                      ))}
+              </div>
+          );
+        }
+  }
+
+  export default TodoList
