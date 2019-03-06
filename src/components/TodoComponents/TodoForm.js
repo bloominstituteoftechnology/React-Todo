1a403e6488d04482cc96bdba6a6667f09f9045c7
DiamondPurvis <purvisdiamond@gmail.com>
2019-03-05 23:15:08 -0500

    Organized code for readability

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const TodoForm = props => {
-    return ( <form>
-        <input onChange={props.handleTodoChange} type="text" name="todo" value={props.value} placeholder="...todo"/>
-        <button onClick={props.handleAddTodo}> Add Todo </button>
-        <button onClick={props.handleClearTodos}> CLear Completed </button>
-    </form>);
-};
-
-export default TodoForm;
+    return (
+      <form>
+        <input
+          onChange={props.handleTodoChange}
+          type="text"
+          name="todo"
+          value={props.value}
+          placeholder="...todo"
+        />
+        <button onClick={props.handleAddTodo}>Add Todo</button>
+        <button onClick={props.handleClearTodos}>Clear Completed</button>
+      </form>
+    );
+  };
+  
+  export default TodoForm;
