993d158ce65a7e6106b28ed8311292005e69cf87
focused220 <42449262+focused220@users.noreply.github.com>
2019-03-05 18:49:13 -0800

    MVP for React-Todo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 import React from 'react';
 
+const taskArray = [];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      toDoList: [],
+      task: "",
+      id: "",
+      completed: "",
+    };
+  }
+
+  toDoListHandler = event => {
+    this.setState({[event.target.name]: event.target.value});
+    
+  };
+
+  toDoListSubmit = event => {
+    event.preventDefault();
+    let toDo = {
+      task: this.state.task,
+      id: Date.now(),
+      completed: false,
+    };
+    this.setState(prevState => {
+      return{
+        toDoList: [...prevState.toDoList, toDo],
+        task: "",
+        id: "",
+        completed: false,
+        
+      };      
+    });
+    taskArray.push(this.state.task);
+    console.log(taskArray.length)   
+    console.log(taskArray) 
+  }
+  upDateList = () => {
+    return <h3>{taskArray.pop()}</h3>
+  }
+  
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>The ToDo App</h2>
+        <this.upDateList />
+        <div className="form">
+          <form onSubmit={this.toDoListSubmit}>
+            <label>ToDo Item:</label>
+            <input type="text" name="task" value={this.state.task} onChange={this.toDoListHandler}/>
+            <input type="submit" name="submit" value="Add ToDo"/>
+            <input type="button" name="clear" value="Clear Completed"/>
+          </form>
+        </div>
       </div>
     );
   }
+  
 }
 
 export default App;
