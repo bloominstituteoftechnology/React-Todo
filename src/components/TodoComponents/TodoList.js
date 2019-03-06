e211821b71f506a001bf6d6c2adccf597fb0760d
DiamondPurvis <purvisdiamond@gmail.com>
2019-03-05 23:15:36 -0500

    Fixed typo in import -organized code for readability

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Todo from "todo";
+import Todo from "./Todo";
 
 const TodoList = props => {
-    return ( <div>
-        {props.todos.map(todo => (<Todo handleToggleComplete={props.handleToggleComplete}
-            key={todo.id} todo={todo}/>
-            ))}
-    </div>);
-};
-
-export default TodoList;
+    return (
+      <div>
+        {props.todos.map(todo => (
+          <Todo
+            handleToggleComplete={props.handleToggleComplete}
+            key={todo.id}
+            todo={todo}
+          />
+        ))}
+      </div>
+    );
+  };
+  
+  export default TodoList; 
