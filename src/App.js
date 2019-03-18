5ea6e8b606c9fd63e29ddabb7c91c41599f69d82
James Wigdor <jpwigdor@gmail.com>
2019-03-18 12:21:40 -0400

    Fix Error in App.js
    used 'React' instead of the import of Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, {Component} from "react";
 import TodoForm from "./components/TodoComponents/TodoForm";
 
-class App extends React.Component {
+
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      todos: [
+        {
+          task: 'Be a Lumberjack',
+          id: 1528817078886,
+          completed: false
+        },
+        {
+          task: 'Cook Steak',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Chop Lumber',
+          id: 1528817084358,
+          completed: false
+        }, 
+        {
+          task: 'Wear Flannel',
+          id: 1528817095747,
+          completed: false
+        }
+      ]
+
+    };
   }
 
   render() {
     return (
       <div>
         <h1>ToDo List: MVP</h1>
-
         <TodoForm />
       </div>
     );
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const TodoForm = props => (
   <form>
-    <input type="" placeholder="...todo" />
-    <button>Add ToDo</button>
-    <button>Clear Completed</button>
+    <input  type="text" 
+            placeholder="...todo" 
+            onKeyDown={props.addFromInput}
+            onChange={props.saveInput}
+            value={props.current}/>
+    <button onClick={props.addFromBtn}>Add ToDo</button>
+    <button onCLick={props.removeCompleted}>Clear Completed</button>
   </form>
 );
 export default TodoForm;
