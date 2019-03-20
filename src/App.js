53a83c122bcfc5d27189189080adf1de201d994c
omar okla <omaro@me.com>
2019-03-20 17:12:26 -0400

    App.js basics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React from "react";
+// import TodoList from "./components/TodoComponents/TodoList";
+// import Todo from "./components/TodoComponents/Todo";
+// import TodoForm from "./components/TodoComponents/TodoForm";
+
+const todos = [
+	{
+		task: "Organize Garage",
+		id: 1528817077286,
+		completed: false
+	},
+	{
+		task: "Bake Cookies",
+		id: 1528817084358,
+		completed: false
+	}
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			todoOGList: todos,
+			task: "",
+			id: "",
+			completed: ""
+		};
+	}
+	render() {
+		return (
+			<div className='ToDo-wrapper'>
+				<h1>ToDoooooooooo Title</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
