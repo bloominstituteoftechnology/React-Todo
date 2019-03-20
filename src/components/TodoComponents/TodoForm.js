c87f92f273bca2cf0d170bfa54f512e356353029
Josh Akeman <joshakeman@gmail.com>
2019-03-20 16:53:49 -0400

    got button functionality working

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const TodoForm = () => {
+const TodoForm = props => {
     return (
     <form>
         <div>
             <h3>Enter your Todo</h3>
-            <input type="text" name="todo"></input>
+            <input type="text"
+                value={props.title}
+                name="title"
+                placeholder="Title"
+                onChange={props.handleChanges}
+            />
+            <button onClick={props.updateList}>Add Todo</button>
         </div>
     </form>
     )
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -16,16 +16,50 @@ const todos = [
     },
 ] 
 
+class TodoList extends React.Component {
+    constructor() {
+        super();
 
-const TodoList = () => {
-return (
-    <div>
-        {todos.map(todo => (
-            <Todo todoTitle={todo} />
-        ))}
-        <TodoForm />
-    </div>
-)
+        this.state = {
+            todoList: todos,
+            title: '',
+        }
+    }
+
+    handleChanges = event => {
+        console.log(event.target)
+        console.log(event.target.name)
+        console.log(event.target.value)
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    updateList = event => {
+        event.preventDefault();
+        const newTodo = {
+            title: this.state.title
+        }
+
+        this.setState({
+            todoList: [...this.state.todoList, newTodo]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.todoList.map((todo, index) => (
+                    <Todo key={index} todoTitle={todo} />
+                ))}
+                <TodoForm 
+                    title= {this.state.title}
+                    handleChanges={this.handleChanges}
+                    updateList={this.updateList}
+                />
+            </div>
+        )
+    }
 }
 
 export default TodoList
