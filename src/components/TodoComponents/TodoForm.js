c75c680a9a01c19d19731df37e179b6c29a5b721
Josh Akeman <joshakeman@gmail.com>
2019-03-20 15:54:32 -0400

    got basic jsx structure working

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoForm = () => {
+    return <form>It's a form</form>
+}
+
+export default TodoForm
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,29 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+import Todo from './Todo'
+import TodoForm from './TodoForm'
+
+const todos = [
+    {
+        title: 'Eat Lunch',
+    },
+    {
+        title: 'Eat post-lunch snacks',
+    },
+    {
+        title: "Start thinking about what you'll eat for dinner"
+    },
+] 
+
+
+const TodoList = () => {
+return (
+    <div>
+        <Todo todoTitle={todos.title}/>
+        <TodoForm />
+    </div>
+)
+}
+
+export default TodoList
