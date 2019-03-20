d0570aaeaac3297db15782aab19eb4c9b175bf8e
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-20 16:14:22 -0400

    added todo data to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      TodoArray: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'Clean Room',
+          id: 1528817654367,
+          completed: false 
+        },
+        {
+          task: 'Take out trash',
+          id: 15288178658045,
+          completed: false
+        } 
+      ]
+    
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
