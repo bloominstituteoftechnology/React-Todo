2bc6066b4cc0481ae5ce139d1ddd010abd38b7cb
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-03-20 13:44:38 -0700

    Displayed data on the screen

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+function TodoList(props) {
+  console.log(props);
+  return (
+    <div>
+      <h3>{props.todo.task}</h3>
+      <h3>{props.todo.id}</h3>
+      <h3>{props.todo.completed}</h3>
+    </div>
+  );
+}
+
+export default TodoList;
