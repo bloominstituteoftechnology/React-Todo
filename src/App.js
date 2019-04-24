29db417585cf206dbaa2ea451f8da2547299b9cb
Andy Bettisworth <andy@accreu.com>
2019-04-24 15:52:06 -0500

    Add initial state to App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const React = require('react')
+
+/**
+ * Define initial state
+ */
+
+const initialState = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
+
+/**
+ * Define component
+ */
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = initialState
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+/**
+ * Export component
+ */
+
+module.exports = App
