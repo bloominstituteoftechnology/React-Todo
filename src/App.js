6845e67f09191de0eb5e673b6874c46db2f793ed
angel prats <angelpratz@gmail.com>
2019-05-01 16:33:31 -0400

    Completed App js

--- src/App.js
+++ src/App.js
@@ -14,3 +14,75 @@ class App extends React.Component {
 }
 
 export default App;
+
+const tasks = [
+  {
+    Task: 'Compose Beats',
+    id: 01,
+    completed: false
+  },
+  {
+    Task: 'Mix',
+    id: 01,
+    completed: false
+  },
+  {
+    Task: 'Produce',
+    id: 01,
+    completed: false
+  },
+];
+
+
+
+class ToDo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      TodoList: tasks,
+      Task: "",
+      ID: "",
+      Completed: ""
+
+    };
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addTask = e => {
+    e.preventDefault();
+    this.setState({
+      TodoList: [
+        ...this.state.TodoList,
+        {
+          Task: this.state.Task,
+          ID: this.state.ID,
+          Completed: this.state.Copleted
+        }
+      ],
+      Task: "",
+      ID: "",
+      Completed: "",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>ToDo List</h1>
+        <TaskList TodoList={this.state.TodoList} />
+        <TodoForm
+          addTask={this.addTask}
+          changeHandler={this.changeHandler}
+          Task={this.state.Task}
+          ID={this.state.ID}
+          Completed={this.state.Completed}
+        />
+      </div>
+    );
+  }
+}
