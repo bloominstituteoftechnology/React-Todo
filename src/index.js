033796b18f542ff2894e159ce803a2915d29a84b
Ash <ashley.cruz.crespo@gmail.com>
2019-05-03 08:44:10 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
+import "./components/TodoComponents/Todo.css"
+
 render(<App />, document.getElementById('root'));
