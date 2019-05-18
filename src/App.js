17bf62eec16a96658dc5d6af65297a9c3edb5799
JamesW <westjames2013@gmail.com>
2019-05-18 14:51:15 -0700

    Made adjustments to js files.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
+import { timingSafeEqual } from 'crypto';
 
 class App extends React.Component {
-
+  constructor () {
+    super ();
+    this.state = [
+      {
+        task: "Organize Garage",
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: "Bake Cookies",
+        id: 1528817084358,
+        completed: false
+      }
+    ];
+  }
+  addToDo = (event) => {
+    event.preventDefault()
+    console.log('addToDo')
+  }
   
+  clearComplete = (event) => {
+    event.preventDefault()
+    console.log('complete')
+  }
+
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <TodoForm/>
+        <TodoForm task = {this.state[0].task}
+        addToDo = {this.addToDo} 
+        clearComplete= {this.clearComplete}/>
         <TodoList/>
       </div>
     );
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const TodoForm = props => {
+    console.log(props)
   return (
     <form>
       <input
         type="text"
         name="todo"
-        value={props.todo}
+        value={props.inputText}
         onChange={props.handleChanges}
         placeholder="THINGS I GOT 2 DO"
       />
-      <button>Add Todo</button>
-      <button>Clear Completed</button>
+      <button onClick={props.addToDo}>Add Todo</button>
+      <button onClick={props.clearComplete}>Clear Completed</button>
     </form>
   );
 };
