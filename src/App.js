f3f48a640e47dd8c72d5f05ce974bbb77a496180
quansenB <inaki.breinbauer@gmail.com>
2019-05-29 15:44:58 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import uuid from 'uuid';
 
 const todoList = [
-  {
+  /* {
     task: "Organize Garage",
     id: 1528817077286,
     completed: false
@@ -10,7 +11,7 @@ const todoList = [
     task: "Bake Cookies",
     id: 1528817084358,
     completed: false
-  }
+  } */
 ];
 
 class App extends React.Component {
@@ -29,25 +30,37 @@ class App extends React.Component {
     return (
       <div>
         {this.state.todoList.map(todo => <div>{todo.task}</div>)}
-        <input
+        <input 
+        className="input"
         value={this.state.newTodo}
         onChange={this.changeHandler}
+        onEnter={this.addTodo}
         type="text"
       />
-        <button onClick={this.addTodo}>
+        <button className="btn" onClick={this.addTodo} >
           Add ToDo
         </button>
       </div>
     );
   }
 
-  changeHandler = () => {
+  changeHandler = (event) => {
+
+    this.setState({newTodo: event.target.value})
 
   }
 
   addTodo = () => {
-    this.setState({todoList: this.state.todoList.concat({task: "sweep floor"}) })
+    const newTodo ={
+      task: this.state.newTodo,
+      id: uuid(),
+      completed : false, 
+
+    }
+    this.setState({todoList: this.state.todoList.concat(newTodo)})
+    this.state.newTodo="";
   }
 }
 
 export default App;
+
