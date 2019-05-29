8defb5384236317a63807534076888f6860badc6
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-29 12:33:40 +0100

    Initial Commit with TodoForm inital markup

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Todo.css';
+
+export default () => (
+  <form>
+    <input
+      type='text'
+    />
+    <input
+      type='submit'
+    />
+    <button>Clear Completed</button>
+  </form>
+);
