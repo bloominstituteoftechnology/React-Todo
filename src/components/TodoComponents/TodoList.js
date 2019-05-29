6d0be08d2ff539c702b68b53b0872c2b39765c39
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-29 12:23:15 +0100

    Add TodoList skeleton class

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+class TodoList extends React.Component {
+
+    render() {
+        return(
+            <div>
+                {
+                    this.props.todoList.map((todo) => {
+                        return <Todo id={todo.id} task={todo.task} completed={task.completed} />;
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default TodoList;
