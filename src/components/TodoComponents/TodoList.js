7ca9361928e647114a779dc68b563dbefbf7e194
Awa Melvine <melvineawa@gmail.com>
2019-05-29 12:32:00 +0100

    Create TodoList component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import './Todo.css';
+import React from "react";
+import "./Todo.css";
 
-const TodoList = ({ task }) => ({
-    
-})
+const TodoList = ({ todoData }) => (
+  <div className="todo-list">
+    {todoData.map(item => (
+      <Todo key={item.id} item={item} />
+    ))}
+  </div>
+);
+
+export default TodoList;
