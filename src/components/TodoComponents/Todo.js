28fe1f9e5706c214775e37225e9e94b9438d1665
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-29 14:51:31 +0200

    edit heading

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Todo.css"
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 
@@ -6,7 +7,7 @@ export default function TodoComponent(props) {
   const {toDoList, newToDo, addTodo, clearCompleted, changeHandler, enterHandler} = props;
   return(
     <div className="todo-app">
-      <h2>Todo List: MVP</h2>
+      <h1>REACT TODO LIST</h1>
       <TodoForm newToDo={newToDo} addTodo={addTodo} clearCompleted={clearCompleted} changeHandler={changeHandler} enterHandler={enterHandler} />
       <TodoList toDoList={toDoList}/>
     </div>
