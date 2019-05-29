c381d7f7b5a6cd918740b535afbe87e081e8fa9b
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-29 14:10:16 +0200

    flesh out list component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+export default function TodoList(props) {
+  const {toDoList} = props;
+  return(
+    <ol>
+      {
+        toDoList.map(todo => <li key={todo.id} >{todo.task}</li>)
+      }
+    </ol>
+  )
+}
