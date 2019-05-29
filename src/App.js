2a5563fbc1ba54fdad995b459ecf077742d4e2b4
Awa Melvine <melvineawa@gmail.com>
2019-05-29 12:21:51 +0100

    Create constructor and initialize state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: "",
+      id: null,
+      completed: false
+    };
+  }
   render() {
     return (
       <div>
