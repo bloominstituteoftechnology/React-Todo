473bb430161138632e17ca76e910c49a34cd251a
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-29 14:26:48 +0200

    add ClassName to Container

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -5,7 +5,7 @@ import TodoForm from './TodoForm'
 export default function TodoComponent(props) {
   const {toDoList, newToDo, addTodo, clearCompleted, changeHandler, enterHandler} = props;
   return(
-    <div>
+    <div className="todo-app">
       <h2>Todo List: MVP</h2>
       <TodoForm newToDo={newToDo} addTodo={addTodo} clearCompleted={clearCompleted} changeHandler={changeHandler} enterHandler={enterHandler} />
       <TodoList toDoList={toDoList}/>
