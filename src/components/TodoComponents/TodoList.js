de992fe22fd1ccae4fd0cf1a725370657e8ed43a
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-29 14:51:37 +0200

    add classname

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TodoList(props) {
   const {toDoList} = props;
   return(
-    <ol>
+    <ol className="todo-list">
       {
         toDoList.map(todo => <li key={todo.id} >{todo.task}</li>)
       }
