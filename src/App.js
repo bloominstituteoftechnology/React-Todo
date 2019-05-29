85e7b9b76b0c4c30d0b4677b8c14189184d9b82a
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-05-29 14:57:34 -0700

    working on todo list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+    this.state = {
+      message: 'Hello Lambda',
+      newTodo: '',
+      completed: false,
+    };
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  newTodoSubmit = event =>{
+    console.log(event.target.value);
+    this.setState ({
+      newTodo: event.target.value,
+    })
+  }
+  submitTodo = event => {
+    event.preventDefault();
+    console.log(event);
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>{this.state.message}</h2>
+        <form onSubmit={this.submitTodo}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input placeholder="New Todo" onChange={this.newTodoSubmit} id="newTodo" name="newTodo" />
+          <button type="submit"> Add Todo</button>
+        </form>
       </div>
     );
   }
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const TodoList = {
+todoText: '',
+completed: false,
+}
+
+export default TodoList;
