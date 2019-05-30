ead6e08aa10d0871891d81b2af59cb7ba4f87100
Awa Melvine <melvineawa@gmail.com>
2019-05-30 15:33:17 +0100

    Style the todo form

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,16 +4,26 @@ import "./Todo.css";
 const TodoForm = ({ task, handleTaskChange, addTodo, clearCompleted }) => {
   return (
     <form onSubmit={event => addTodo(event)} className="todo-form">
+      <h3>Add Task</h3>
       <input
         name="task"
         type="text"
+        placeholder="Enter new task"
         value={task}
         onChange={event => handleTaskChange(event)}
       />
-      <button type="submit">Add Todo</button>
-      <button type="button" onClick={() => clearCompleted()}>
-        Clear Completed
-      </button>
+      <div className="buttons">
+        <button
+          type="button"
+          className="clear"
+          onClick={() => clearCompleted()}
+        >
+          Clear Completed
+        </button>
+        <button className="add" type="submit">
+          Add Todo
+        </button>
+      </div>
     </form>
   );
 };
