e3d5de3668462d8e2fd3e836abce71aef3ac3bba
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-30 10:00:38 -0700

    Providing the array of objects

--- src/index.js
+++ src/index.js
@@ -2,4 +2,28 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
+const toDo = 
+[
+    {
+    task: 'Get Up ON TIME!!',
+    id: 14,
+    completed: false   
+    },
+    {
+    task: 'Like For Real This Time (Get Up)',
+    id: 15,
+    completed: false
+    },
+    {
+    task: 'Brush your teeth',
+    id: 16,
+    completed: false
+    },
+    {
+    task: 'Start Doing Parkour',
+    id:340940,
+    completed: false
+    }
+];
+
 render(<App />, document.getElementById('root'));
