b3e0a662b8bf2933c244ec2440eed79cb1d439c6
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-30 10:36:52 -0700

    i saw the simple mistake

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Todo = props => {
-    return <div className={props.todo.completed ? 'purchased': 'todo'} onClick={()=> props.toggleTodo(props.item.id)}>
+    return <div className={props.todo.completed ? 'completed': 'todo'} onClick={()=> props.toggleTodo(props.item.id)}>
         {props.todo.task}
     </div>
 };
