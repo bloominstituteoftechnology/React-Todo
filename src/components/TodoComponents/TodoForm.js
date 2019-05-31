a3b34bb9407027c25c1188b7e102c88b256e7d82
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-30 18:47:59 -0700

    finished todoform.js

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Todo.css";
+
+const TodoForm = props => {
+    return (
+        <form className="form" name="todoForm" onSubmit={props.addTodoHandler}>
+            <input
+                className="text-field"
+                type="text"
+                value={props.task}
+                palceholder="Enter a todo here ..."
+                name="task"
+                onChange={props.changeHandler} />
+            <div>
+                <button className="button"onClick={props.addTodoHandler}>Add Todo</button>
+                <button className="button" onClick={props.deleteCompletedHandler}>Clear Completed</button> 
+            </div>
+        </form>
+    )
+}
+
+export default TodoForm;
