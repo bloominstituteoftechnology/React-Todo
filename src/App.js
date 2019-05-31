b0fcc64d1589c3eb612122ea02c52fa589926380
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-30 17:11:21 -0700

    still working on app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
 import React from 'react';
 
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import "./components/TodoComponents/Todo.css";
+
+const list = [];
+
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      TodoList: list,
+      task: "",
+    }
+  }
+
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
+  addTodoHandler = event => {
+    event.preventDefault();
+    let newTodo - {
+      task: this.state.task,
+      id: Date.now(),
+      completed: false,
+    }
+    this.setState({
+      todoList: [...this.state.todoList, newTodo],
+      task: "",
+    })
+  }
+
+  toggleTodo = (e, taskId) => {
+    let.newTodoList = this.state.todoList.map(task => (task.id === this.setState({
+      todoList: newTodoList
+    })
+  
+  deleteCompletedHandler = e => {
+    e.preventDefault();
+    this.setState({
+      todoList: this.state.todoList.filter(task => !task.completed)
+    });
+  }
+    ))
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
