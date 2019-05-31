c099c61a3b36ac09a9f91a99cf053c221f584488
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-30 18:30:32 -0700

    debugging app.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
 
   addTodoHandler = event => {
     event.preventDefault();
-    let newTodo - {
+    let newTodo = {
       task: this.state.task,
       id: Date.now(),
       completed: false,
@@ -44,15 +44,15 @@ class App extends React.Component {
       todoList: this.state.todoList.filter(task => !task.completed)
     });
   }
-    ))
-  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="container">
+        <h2>ToDo List!</h2>
+        <TodoList todoList={this.state.todoList} toggleTodo={this.toggleTodo} />
+        <TodoForm task={this.state.task} changeHandler={this.changeHandler} addTodoHandler={this.addTodoHandler} deleteCompletedHandler={this.deleteCompletedHandler} />
       </div>
     );
   }
