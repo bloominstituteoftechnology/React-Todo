d82d17ac3bcab2f0c37617154acef5af4ad21520
teej387 <teej387@yahoo.com>
2019-06-03 22:03:57 -0500

    Created to

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,10 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const toDoArray = [
+  {
+    task: '',
+    id: '',
+    completed: false
+  },
+];
+
