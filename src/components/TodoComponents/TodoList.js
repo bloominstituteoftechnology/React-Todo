fc303df4f59ed8226c419862ea9ca124354d482d
teej387 <teej387@yahoo.com>
2019-06-03 23:43:07 -0500

    Created ToDoList class

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const toDoArray = [
-  {
-    task: '',
-    id: '',
-    completed: false
-  },
-];
+const toDoArray = [];
 
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: toDoArray,
+      task: '',
+      id: '',
+      completed: false
+    }
+  };
+}
