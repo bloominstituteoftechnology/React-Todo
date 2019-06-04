243d61032a38a815a4f1cc9d54e6c086321b5449
Tara Sherman <taradelin@gmail.com>
2019-06-03 21:52:03 -0400

    Initial commit: ToDo List items added

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,57 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+const todoList = [
+    {
+        task: 'Get ready for work',
+        id: 600,
+        completed: false
+    },
+    {
+        task: 'Leave for work',
+        id: 700,
+        completed: false
+    },
+    {
+        task: 'Begin work',
+        id: 800,
+        completed: false
+    },
+    {
+        task: 'Lunch break',
+        id: 1200,
+        completed: false
+    },
+    {
+        task: 'Return from break',
+        id: 1300,
+        completed: false
+    },
+    {
+        task: 'Leave work',
+        id: 1700,
+        completed: false
+    },
+    {
+        task: 'Arrive home',
+        id: 1800,
+        completed: false
+    },
+    {
+        task: 'Eat dinner',
+        id: 1830,
+        completed: false
+    },
+    {
+        task: 'Study React',
+        id: 1900,
+        completed: false
+    },
+    {
+        task: 'Class Time',
+        id: 2000,
+        completed: false
+    },
+    {
+        task: 'Bedtime',
+        id: 2330,
+        completed: false
+    }
+];
