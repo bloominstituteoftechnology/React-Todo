f1b44fd517e8a805855279e582608cf96443a447
Sir-Williams <sirjohnwilliams@live.com>
2019-06-05 15:28:30 -0700

    forgot about commits but here is what I did in order: made TodoForum half way; made TodoList; made rest of TodoForum; made Todo; made App

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,32 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const todoList = [
+    {
+        id: 1,
+        text: "Clean Room",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Take Our Recyclables",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Grocery Shopping",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Work On Lambda Project",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Play Minecraft",
+        completed: false
+    }
+  ]
+  
+  export default todoList;
