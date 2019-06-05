5fa3c245ce44348d1e70a306f356c5446dec01c7
brooke <brooke@pop-os.localdomain>
2019-06-04 20:46:02 -0400

    added constructor to class app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
+const listoftodos = [];
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props){
+    super(props);
+   
+      // you will need a place to store your state in this component.
+      // design `App` to be the parent component of your application.
+      // this component is going to take care of state, and any change handlers you need to work with your state
+     this.state = {   
+    todos: listoftodos,
+    task:''
+    
+    
+  };
+  }
+  
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+     
     );
   }
 }
