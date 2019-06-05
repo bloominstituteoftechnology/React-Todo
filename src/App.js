6aaa5f501ca9e0958a53244f827a78dbfa585850
darthMinimus <irishseanvaldivia@gmail.com>
2019-06-04 23:13:43 -0500

    going over some react stuff

--- src/App.js
+++ src/App.js
@@ -5,21 +5,38 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            todos: ['study', 'swim', 'travel'],
+            todos: [],
             newTodo: ''
         }
     }
 
+    addTodo = e => {
+        e.preventDefault();
+        const todos = this.state.todos;
+        const newTodo = this.state.newTodo;
+        todos.push(newTodo);
+        this.setState({
+            todos: todos,
+            newTodo: ''
+        })
+    }
+
+    changeHandler = e => {
+        const newTodo = e.target.value;
+        this.setState({
+            newTodo: newTodo
+        })
+    }
 
     render() {
         return (
             <div>
-                <p>Todos: </p>
-                <ul>
-                  {this.state.todos.map((todo) => {
-                      return <Todo todo={todo}/>;
-                  })}
-                </ul>
+                {this.state.todos.map((todo, index) => {
+                    return <Todo todo={todo} key={index} />
+                })}
+                <form onSubmit={this.addTodo}>
+                  <input type="text" value={this.state.newTodo} onChange={this.changeHandler}/>
+                </form>
             </div>
         )
     }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false,
+            style: 'none'
+        }
+    }
+    clickHandler = (e) => {
+      const clicked = this.state.clicked;
+      const style = !clicked ? 'line-through' : 'none';
+      this.setState({
+          clicked: !clicked,
+          style: style
+      })
+    }
+
     render() {
+        const styles = { textDecoration: this.state.style }; 
         return (
-            <li>
-                {this.props.todo}
-            </li>
+            <div style={styles} onClick={this.clickHandler}>{this.props.todo}</div>
         )
     }
 }
