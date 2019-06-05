a68188a25f6e3fb72a0ce137864f63e629e55c35
teej387 <teej387@yahoo.com>
2019-06-04 20:24:51 -0500

    TodoList.js line 46, error can not map() undefined

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Todo from './Todo'
 
-const toDoArray = [];
+const toDoArray = [{
+  task: 'Wake up!',
+  id: 1,
+  completed: false
+}];
 const newId = Date.now();
 
 class TodoList extends React.Component {
@@ -13,27 +17,48 @@ class TodoList extends React.Component {
       id: { newId },
       completed: false
     }
+    { console.log(this.state.list) }
+  };
+  
+
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value })
   };
 
+  submitHandler = event => {
+    event.preventDefault();
+    const newTask = {
+      task: this.state.task,
+      id: this.state.id,
+      completed: this.state.completed
+    };
 
+    this.setState({
+      toDoArray: [...this.state.toDoArray, newTask]
+    });
+    toDoArray.push(newTask);
+  };
 
   render() {
     return (
-      <div className='todoList'>
-        {/* {this.state.toDoArray.map((item, index) => {
-          return <Todo item={item} key={index} />
-        })
-        }; */}
+      <div>
+        <div className='todoList'>
+          {this.state.toDoArray.map((item, index) => {
+            return <Todo item={item} key={index} />
+          })
+          };
+        </div>
+        <form onSubmit={this.submitHandler}>
+          <input
+            type='text'
+            value={this.state.task}
+            onChange={this.changeHandler}
+            placeholder='New Task'
+            name='task'
+          />
+          <button type='submit'>Add Task</button>
+        </form>
       </div>
-      <form onSubmit={this.submitHandler}>
-        <input
-          type='text'
-          value={this.state.task}
-          onChange={this.changeHandler}
-          placeholder='New Task'
-          name='task'
-        />
-      </form>
     )
   }
 }
