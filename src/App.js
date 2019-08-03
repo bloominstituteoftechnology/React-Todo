93761b77aafda84bd47268116435dabfc50da313
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-03 14:28:00 -0500

    create Todo.js, create input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Todo from './components/TodoComponents/Todo'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state={
+      todo: ""
+    }
+  }
+  handleChangeFunction = event => {
+    this.setState({ todo: event.target.value})
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <Todo propsTodo={this.state.todo} />
+        <input onChange={this.handleChangeFunction}/>
       </div>
     );
   }
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Todo = props => <h4>{props.propsTodo}</h4>
+
+export default Todo;
