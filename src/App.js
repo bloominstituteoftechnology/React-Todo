f5623857e5653773afb4ca6b0081948105bd63d7
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-05 16:47:41 -0400

    tested addTodo

--- src/App.js
+++ src/App.js
@@ -19,16 +19,29 @@ class App extends React.Component {
           completed: false,
         },
       ],
+      newTodoText: "testing",
     };
   }
 
   addTodo() {
-
+    if (this.state.todos.length > 2) return;
+    this.setState({
+      ...this.state,
+      todos: [
+        ...this.state.todos,
+        {
+          task: this.state.newTodoText,
+          id: Math.random() * Number.MAX_VALUE,
+          completed: false,
+        },
+      ],
+    });
   }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
+    this.addTodo();
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
