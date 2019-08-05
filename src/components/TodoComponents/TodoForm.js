c6db22a823584243d0c22a431a50d673d8132176
ebisLab <ebaiden@techie.com>
2019-08-05 15:20:24 -0400

    handlechanges added

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,20 +3,36 @@ import React from 'react';
 import Todo from './Todo';
 import TodoList from './TodoList';
 
-const TodoForm = props => {
+class TodoForm  extends React.Component {
   // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased)
+constructor(){
+    super();
+    this.state = {
+        item: ''
+    };
+}
 
+handleChanges = event => {
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+}
+
+
+
+render() {
   return (
     <form onSubmit={``}>
     <input
       type="text"
-      value={``}
+      value={this.state.item}
       name="item"
-      onChange={``}
+      onChange={this.handleChanges}
     />
     <button>Add</button>
   </form>
   );
 };
+}
 
 export default TodoForm;
