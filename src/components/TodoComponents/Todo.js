84c097f291b0b51aff7a1f006da341a3c17d4621
Armando Roman <armandoroman1016@gmail.com>
2019-08-05 14:47:49 -0700

    items conditionally styled when completed

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,10 @@ import React from 'react'
 const ToDo = props => {
     return (
         <div onClick = {() => props.toggleItem(props.item.id)}>
-            <p>{props.item.task}</p>
+            {props.item.completed ? 
+                <h3 style = {{textDecorationLine : 'line-through' }}>{props.item.task}</h3>
+                : <h3>{props.item.task}</h3>
+            }
         </div>
     )
 }
