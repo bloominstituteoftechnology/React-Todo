c0da2eba7bc6deae66a8fd6b04269c7f0d595269
dangolbeeker <scottb305@outlook.com>
2019-08-05 18:20:56 -0400

    Functionality Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm.js'
+
+function addTask(obj, task) {
+  obj.todo.push({
+    task: task,
+    id: Date.now(),
+    completed: false,
+  })
+  return obj;
+}
+
+function editTask(obj, id) {
+  for (let i=0;i<obj.todo.length;i++) {
+    if (obj.todo[i].id === id) {
+      obj.todo[i].completed = !obj.todo[i].completed
+    }
+  }
+  return obj;
+}
+
+function handleClearing(obj) {
+  const filtered = obj.todo.filter(item => item.completed === false)
+  console.log(filtered)
+  obj.todo = filtered;
+  return obj;
+}
 
 
 class App extends React.Component {
@@ -20,14 +46,28 @@ class App extends React.Component {
       ]
     }
   }
+
+  handleAdd = task => {
+    this.setState(addTask(this.state, task));
+  };
+
+  todoChange = (id) => {
+    this.setState(editTask(this.state, id));
+  }
+
+  handleClear = () => {
+    this.setState(handleClearing(this.state));
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div>
         <h2>Just Another Todo App!</h2>
-        <TodoList list={this.state.todo}/>
+        <TodoList todoChange={this.todoChange} list={this.state.todo}/>
+        <TodoForm handleClear={this.handleClear} onAdd={this.handleAdd}/>
       </div>
     );
   }
 }
-
 export default App;
