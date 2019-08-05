5b48de7dd6fd2b0fad0c476c2e1b57a9dce839fe
Lou <antilou86@gmail.com>
2019-08-05 16:40:45 -0400

    WIP: added handler functions to app.js and passed them down as props to the appropriate components.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Todo from './components/TodoComponents/Todo';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
 
+let todoData = [{
+  task: 'get cookies',
+  id: 5,
+  completed: false
+},{
+  task: 'eat cookies',
+  id: 1,
+  completed: false
+}]
+
 class App extends React.Component {
   
   // you will need a place to store your state in this component.
@@ -11,20 +21,50 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.state = [{
-      task: '',
+    this.state = {
+      todoData,
+    }
+  
+
+  toggleItem = id => {
+    this.setState({
+      todoData: this.state.todoData.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    })
+  }
+
+  addTask = taskName => {
+    const newTask = {
+      name: taskName,
       id: Date.now(),
       completed: false
-    },
-  ]
+    };
+    this.setState({
+      todoData: [...this.state.todoData, newTask]
+    })
   }
+
+  clearCompleted = () => {
+    this.setState({
+      todoData: this.state.todoData.filter(item=> !item.completed)
+    });
+  };
+}
 
   render() {
     return (
       <div>
         <h2>ToDo List: MVP</h2>
-        <Todo />
-        <TodoForm />
+        <TodoList todoData={this.state.todoData} toggleItem={this.toggleItem}/>
+        <TodoForm addTask={this.addTask}/>
       </div>
     );
   }
