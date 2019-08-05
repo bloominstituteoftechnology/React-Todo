751997f207512f7669a19ddefed3af2a3b068cdd
noahfranco <noahfra@gmail.com>
2019-08-05 16:47:19 -0400

    orginizing code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { buildSnapshotResolver } from 'jest-snapshot/build/snapshot_resolver';
-import { TodoList } from "./components/TodoComponents/TodoList.js"; 
+import { TodoListdata } from "./components/TodoComponents/TodoList.js"; 
 
 class App extends React.Component {
   constructor() {
     super(); 
     this.state = {
-      todo: TodoList
+      todo: TodoListdata
     }
     console.log(this.state); 
   }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
   render() {
     return (
-      <div> 
+      <div>
+         {/* Title Section */}
       <div>
         <h2>Welcome to your Todo App!</h2>
       </div>
+  
       <section>
+        {/* Form and button section */}
         <form> 
-        <input /> 
+        <input type="text" placeholder="enter todo here" /> 
         </form>
 
         <div> 
@@ -29,6 +33,10 @@ class App extends React.Component {
         <button> Clear </button>
         </div> 
        </section>
+       {/* Content seciton */}
+       <div>
+             
+       </div>
        </div> 
       
     );
