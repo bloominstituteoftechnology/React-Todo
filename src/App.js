d568e5e70acdf3bcfa88f190caf78b805459953c
noahfranco <noahfra@gmail.com>
2019-08-05 16:05:42 -0400

    added this.state to App.js and added an ArrayObject to TodoList.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { buildSnapshotResolver } from 'jest-snapshot/build/snapshot_resolver';
+import { TodoList } from "./components/TodoComponents/TodoList.js"; 
 
 class App extends React.Component {
+  constructor() {
+    super(); 
+    this.state = {
+      todo: TodoList
+    }
+    console.log(this.state); 
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
+      <div> 
       <div>
         <h2>Welcome to your Todo App!</h2>
       </div>
+      <section>
+        <form> 
+        <input /> 
+        </form>
+
+        <div> 
+        <button> Submit </button>
+        <button> Clear </button>
+        </div> 
+       </section>
+       </div> 
+      
     );
   }
 }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,33 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+export const TodoList = [{
+    task: "Clean House",
+    id: 1,
+    completed: false
+},
+{ 
+    task: "Code App (Work)",
+    id: 2,
+    completed: false
+},
+{
+    task: "Exercise",
+    id: 3, 
+    completed: false
+},
+{
+    task: "Spend time with family",
+    id: 4, 
+    completed: false
+},
+{
+    task: "Work on building Assets", 
+    id: 5, 
+    completed: false
+},
+{
+    task: "Read", 
+    id: 6, 
+    completed: false
+}]; 
