d0841261728ea030df1bc90e7a99343763ebc18d
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-08-05 15:40:30 -0500

    Added basic structure of Todo & TodoList

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    constructor(){super();}
+
+    <h3>{prop.task}</h3>
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Todo from './Todo';
 
 class TodoList extends React.Component {
-    
+    constructor(){super();}
+
+    props.array.map((item) => {
+        return (
+            <Todo task={item.task} />
+        )
+    })
 }
+
+export defualt TodoList;
