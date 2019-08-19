0c80590682dd27e0d5a0442908412b09b632d822
Erika <ito2016@protonmail.ch>
2019-08-19 14:05:39 -0400

    Created constructor with state object inside

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-class App extends React.Component {
+class App extends React.Component { //base class
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+ 
+  //set up state in out constructor method
+  constructor(){
+   super();
+   this.state= {
+     message:"Hello from app"
+   };
+ }
+ 
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>{this.state.message}</h2>
       </div>
     );
   }
