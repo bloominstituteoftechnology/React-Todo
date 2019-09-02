235bdc768d6298b9f34dd79f54907f65c1533eb7
chrismjohnston <cmjohnston@gmail.com>
2019-09-02 17:14:39 -0500

    I can now Toggle task completed

--- src/App.js
+++ src/App.js
@@ -18,18 +18,31 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-constructor() {
-  super();
-  this.state = {
-    todos: todoData,
+  constructor() {
+    super();
+    this.state = {
+      todos: todoData,
+    };
   }
-} 
+
+  toggleCompleted = (id) => {
+    this.state.todos.map(todo => {
+      if (id === todo.id) {
+        return {...todo, completed: !todo.completed};
+      } else {
+        return todo;
+      }
+    });
+  }
+
   render() {
     console.log(this.state);
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList todos={this.state.todos} />
+        <TodoList todos={this.state.todos}
+                  toggleCompleted={this.toggleCompleted}
+        />
       </div>
     );
   }
