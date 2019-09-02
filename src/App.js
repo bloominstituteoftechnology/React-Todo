ef0cb43c563c56f97b9c9997264fb3d92e3f77b4
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-02 15:17:48 -0700

    Added pplication data to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
+
+//Adding Data
+
+const todoData = [
+  
+  {
+    task: 'Meal Prep',
+    id: 2001,
+    completed: false,
+  },
+
+  {
+    task: 'Do Laundry',
+    id: 2002,
+    completed: false,
+  },
+
+  {
+    task: 'Learn React Class Components',
+    id: 2003,
+    completed: false,
+  }
+];
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
