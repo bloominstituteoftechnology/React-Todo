315dec7d7e268509ee0dfd90b885f98cc39e64a2
chrismjohnston <cmjohnston@gmail.com>
2019-09-02 15:56:31 -0500

    TodoList component is rendering on page

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>TodoList</div>
+        )
+    }
+}
+
+export default TodoList;
