f35abb822d57003c63550fe62161075e530b8c0b
Carlos Mitchell <kakismitchell@gmail.com>
2019-09-05 00:02:07 -0400

    initial set up, toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
+// import ReactDOM from "react-dom";
+import TodoList from "./components/TodoComponents/TodoList";
+import "./components/TodoComponents/Todo.css";
+const LearReactClass =[
+  {
+    task: "Run create react app",
+    id:1234,
+    completed:false
+  },
+  {
+    task: "Run npm start",
+    id:12345,
+    completed:false
+  },
+  {
+    task: "Delete trash",
+    id:12346,
+    completed:false
+  },
+  {
+    task: "Create  component folder",
+    id:12347,
+    completed:false
+  },
+]
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+ constructor( ){
+   super();
+   this.state ={
+    LearReactClass
+   };
+ }
+ clearCompleted = e => {
+  e.preventDefault();
+  this.setState({
+    LearReactClass: this.state.LearReactClass.filter(item => !item.completed)
+  });
+};
+
+toggleItem = itemId => {
+  console.log("index.js: App: toggleItem: ", itemId);
+  console.log("index.js: App: this.state: ", this.state);
+  //this.state.groceries;
+  this.setState({
+    LearReactClass: this.state.LearReactClass.map(item => {
+      if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    })
+  });
+};
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+      <TodoList  LearReactClass={this.state.LearReactClass}
+      toggleItem={this.toggleItem}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+import Todo from "./Todo";
+
+
+const GroceryList = props => {
+  // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
+  console.log(props);
+  return (
+    <div className="shopping-list">
+      {props.LearReactClass.map(item => (
+        <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+      ))}
+      <button className="clear-btn" onClick={props.clearCompleted}>
+        Clear Completed
+      </button>
+    </div>
+  );
+};
+
+export default GroceryList;
