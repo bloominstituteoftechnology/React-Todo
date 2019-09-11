49f3a09289e90e0c4f47f00fbbfeb4db01c2f1cb
Shantay Grady <shantaygrady@gmail.com>
2019-09-10 20:03:44 -0400

    Setup  dependencies and title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Title from './components/TodoComponents/Title';
 
-class App extends React.Component {
+import Todo from './components/TodoComponents/Todo.css';
+
+
+
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      titleMessage: 'My TodoList App!'
+    };
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <Title message={this.state.titleMessage} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/Title.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Title = props => <h1>Hello, Welcome to {props.message}!</h1>
+
+export default Title;
