d58af574bd57999de66e271e00cc8b69b9f7b7aa
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-09-14 20:53:26 -0400

    added media queries

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -8,8 +8,9 @@ class TodoForm extends React.Component {
         };
     }
 
+    //will set this.state.newtodo equal to whatever text is currently in the input field.
     handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
+        this.setState({ [event.target.name]: event.target.value }); //sets newtodo: to the value entered in the input field
     }
 
     handleSubmit = (event) => {
@@ -22,9 +23,13 @@ class TodoForm extends React.Component {
         return(
             <form onSubmit = {this.handleSubmit}>
                 <input type = "text" 
-                       name = "newtodo" 
+                       name = "newtodo" //same name as the state variable
                        placeholder = "Enter an item to add to your list"
+
+                       //You can control an <input />‘s text by setting its value attribute.
+                       //any time a user updates <input />, the new text is immediately stored in Input‘s state
                        value = {this.state.newtodo}
+
                        onChange = {this.handleChange}/>
 
                 <button type = "submit">Add Todo</button>
