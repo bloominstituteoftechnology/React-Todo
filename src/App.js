24d05524f358fc233260661488b8b351b68a3b4a
MichaelHMods <mikeguadalupe@gmail.com>
2019-09-30 16:53:22 -0500

    need debug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
 
+const jerseyShore = [
+	{
+    task: 'Go to gym',
+	  id: Date.now(),
+    completed: false
+	},
+
+	{
+    task: "Tan",
+	  id:Date.now(),
+	  completed: false
+	},
+	
+	{
+    task:' Laundry',
+    id:Date.now(),
+    completed: false
+  },
+  
+  {
+    task: 'Find GDTF',
+    id:Date.now(),
+    completed: false
+  },
+
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state ={
+      
+      jerseyShore: jerseyShore
+    };
+  }
+  toggleItem = jerseyShore => {
+    console.log('this is in toggleItem',jerseyShore);
+    this.setState({
+      
+      jerseyShore: this.state.jerseyShore.map(item => {
+        console.log('this is item', item)
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+
+        } else{
+          return item;
+        }
+      })
+    });
+  };
+  
   render() {
     return (
       <div>
