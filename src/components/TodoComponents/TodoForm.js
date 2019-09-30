7e8d295c8f2d47a1c4cd490e1f1a5adc5d934778
Joe <josephc.lambert@gmail.com>
2019-09-30 14:11:54 -0700

    add TodoForm to TodoList. hanlder functions log values and TodoForm updates it's todo state onChange

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -10,17 +10,20 @@ class TodoForm extends React.Component {
 
   handleChange = e => {
     console.log('handleChange: ', e.target.name, e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleSubmit = (e, todo) => {
-    e.preventDefaul();
+    e.preventDefault();
     console.log('handleSubmit: ', todo);
   };
 
   render() {
     return (
-      <form onSubmit={e => this.hanldeSubmit(e, this.state.todo)}>
-        <input type='text' value={this.state.todo} onChange={e => this.handleChange(e)} />
+      <form onSubmit={e => this.handleSubmit(e, this.state.todo)}>
+        <input type='text' value={this.state.todo} onChange={e => this.handleChange(e)} name='todo' />
         <button type='submit'>Add Item</button>
       </form>
     );
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Todo from './Todo.js';
+import TodoForm from './TodoForm.js';
 
 const TodoList = props => (
   <ul>
     {props.todos.map(todo => (
       <Todo key={todo.id} todo={todo} completeTodo={props.completeTodo} />
     ))}
+    <li>
+      <TodoForm />
+    </li>
   </ul>
 );
 
