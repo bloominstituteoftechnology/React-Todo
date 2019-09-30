1700a3f773b9eea9c5238e630d5b4686661c75a3
Joe <josephc.lambert@gmail.com>
2019-09-30 13:50:41 -0700

    todo complete base style

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+import check from '../../assets/check.svg';
+
+const StyledTodo = styled.li`
+  list-style: none;
+  &:not(:last-of-type) {
+    margin-bottom: 20px;
+  }
+
+  input[type='checkbox'] {
+    opacity: 0;
+  }
+
+  label {
+    position: relative;
+    &::before {
+      content: '';
+      transition: 200ms;
+      position: absolute;
+      background-image: url(${check});
+      left: -35px;
+      width: 25px;
+      height: 25px;
+      opacity: 0;
+    }
+  }
+
+  ${props =>
+    props.completed &&
+    css`
+      label {
+        text-decoration: line-through;
+        &::before {
+          opacity: 1;
+        }
+      }
+    `}
+`;
 
 const Todo = props => (
-  <li>
+  <StyledTodo completed={props.todo.completed}>
     <input
       type='checkbox'
       id={props.todo.title}
@@ -9,7 +48,7 @@ const Todo = props => (
       onClick={() => props.completeTodo(props.todo.id)}
     />
     <label htmlFor={props.todo.title}>{props.todo.title}</label>
-  </li>
+  </StyledTodo>
 );
 
 export default Todo;
