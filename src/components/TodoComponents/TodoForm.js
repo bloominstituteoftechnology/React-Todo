0ab4bf8d6768e1cafab0dc24d107c4950f566e5b
Joe <josephc.lambert@gmail.com>
2019-09-30 15:50:24 -0700

    adds focus style on todoform input. handles submit defocus when user submits form with return key

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,4 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledInputContainer = styled.div`
+  position: relative;
+
+  input {
+    border: none;
+    border-bottom: 1px solid black;
+    padding: 10px;
+    transition: 200ms;
+    &::placeholder {
+      opacity: 0;
+    }
+    &:focus {
+      border-color: blue;
+    }
+  }
+  input:focus + label {
+    transform: translate3d(-10px, -15px, 0) scale(0.6);
+  }
+  label {
+    position: absolute;
+    z-index: 100;
+    left: 9px;
+    top: 8px;
+    transition: 200ms;
+    transform: translate3d(0, 0, 0);
+  }
+`;
 
 class TodoForm extends React.Component {
   constructor(props) {
@@ -27,6 +56,7 @@ class TodoForm extends React.Component {
       this.setState(prevState => ({ error: false }));
     }
 
+    e.target[1].focus();
     this.props.addTodo(todo);
     this.setState({
       todo: '',
@@ -36,7 +66,17 @@ class TodoForm extends React.Component {
   render() {
     return (
       <form onSubmit={e => this.handleSubmit(e, this.state.todo)}>
-        <input type='text' value={this.state.todo} onChange={e => this.handleChange(e)} name='todo' />
+        <StyledInputContainer>
+          <input
+            type='text'
+            value={this.state.todo}
+            onChange={e => this.handleChange(e)}
+            name='todo'
+            placeholder='New todo title'
+            id='todo'
+          />
+          <label htmlFor='todo'>New todo title</label>
+        </StyledInputContainer>
         <p>
           {this.state.error &&
             `You're trying to add a todo without a meaningful title. Please amend your todo and add a meaningful title.`}
