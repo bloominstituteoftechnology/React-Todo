7ddbbcee3c3b007e97a4d25aa2be36ab1a42c2f9
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-09-30 13:13:44 -0700

    Initial Commit, toggleItem in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+// - `<App />` will hold all the data needed for this project. It will also be the container for your Todo Components.
+//   - All of your application data will be stored here on `<App />`.
+//   - All of your `handler` functions should live here on `<App />`.
+//======================
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+
+// call constructor class and super to inherit parent properties/methods
+
+//=============================
+
+import React from "react";
+
+const itemData = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todoItems: itemData
+    };
+  }
+
+  
+toggleItem = id => {
+  //update state, loop through array, find which element was clicked, update the completed property
+  console.log("In toggleId. Id=", id)
+
+  this.setState({
+    todoItems: this.state.todoItems.map(item => {
+      if (item.id === id){
+        return{
+          ...item,
+          // same as passing in each item property and current value
+          completed: !item.completed
+        };
+      } else{
+        return item;
+      }
+    })
+  });
+};
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To Do:</h2>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,6 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+// - `<App />` will hold all the data needed for this project. It will also be the container for your Todo Components.
+//   - All of your application data will be stored here on `<App />`.
+//   - All of your `handler` functions should live here on `<App />`.
