6e861f459ea153e8165bf89a9dd519818864a14b
Joe <josephc.lambert@gmail.com>
2019-09-30 13:09:11 -0700

    scaffold TodoList

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo.js';
+
+const TodoList = props => (
+  <ul>
+    {props.todos.map(todo => (
+      <Todo key={todo.id} todo={todo} />
+    ))}
+  </ul>
+);
+
+export default TodoList;
