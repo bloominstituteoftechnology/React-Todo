2342a1554656eecfc9ea9e82062088ac39956ce8
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-30 17:53:04 -0500

    Styling still WIP

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
-import './App.css'
+import './App.css';
+
+
 
 const taskArray = [{
       
