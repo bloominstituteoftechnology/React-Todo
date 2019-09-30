ec25362de9c0ce13c7b9b4dbc654768bd43bbd6e
Funmi7 <talabifunmilayo@gmail.com>
2019-09-30 13:14:20 +0100

    Added a form to the todoform.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 const todoData = [
   {
@@ -16,7 +17,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-
+        <TodoForm />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-
 class TodoForm extends React.Component {
 
+    render() {
+        return(
+            <form>
+                <input type='text' placeholder='...todo' onChange/>
+                <button onClick>Add Todo</button>
+                <button onClick>Clear Completed</button>
+            </form>
+        )
+    }
+
 }
+
+export default TodoForm;
