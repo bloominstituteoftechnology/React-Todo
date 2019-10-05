92be4ee651b1b07d4fded107da011cbc333c833d
Jerome <HaynesJerome@gmail.com>
2019-10-05 01:13:44 -0400

    Added data.js

---
+++ src/components/TodoComponents/data.js
@@ -0,0 +1,33 @@
+const Data = [
+	{
+		task      : 'Clean Room',
+		id        : 1234,
+		completed : false,
+	},
+	{
+		task      : 'Study',
+		id        : 1235,
+		completed : false,
+	},
+	{
+		task      : 'Fix Projects',
+		id        : 1236,
+		completed : false,
+	},
+	{
+		task      : 'Wash Car',
+		id        : 1237,
+		completed : false,
+	},
+	{
+		task      : 'Pick Up Gifts',
+		id        : 1238,
+		completed : false,
+	},
+	{
+		task      : 'Make Dinner',
+		id        : 1239,
+		completed : false,
+	},
+];
+export default Data;
