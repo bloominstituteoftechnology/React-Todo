95aafe63c52d6b199baa610c752d066d60deb455
Anthony Hill <hillan1152@gmail.com>
2019-10-28 16:49:02 -0400

    Set up addTodo in app.js and ToDo individual component

--- src/App.js
+++ src/App.js
@@ -18,11 +18,29 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  
+  constructor() {
+    super();
+    this.state = {
+      todo: data
+    }
+  }
+
+  addTodo = item => {
+    const newTodo ={
+      name: item,
+      id: Date.now(),
+      purchase: false
+    };
+    this.setState({
+      todo: [...this.state.data, newTodo]
+    });
+  }
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h2>Todo List</h2>
+        </div>
       </div>
     );
   }
