b4b6b17b1857441480dd6fdb8c5c2ef509de5281
DeAndre Johnson <dre@phreshr.com>
2019-10-29 05:30:47 +0800

    add todo component

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Todo extends Component {
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    const {
+      todo: { id, task, completed },
+    } = this.props;
+    return (
+      <div
+        style={{ textDecoration: completed ? 'line-through' : 'none' }}
+        onClick={() => this.props.completeTodo(this.props.index)}
+      >
+        {task}
+      </div>
+    );
+  }
+}
