01ed4afb2d1234d48481bfeda3991d4011df3c22
Anthony Hill <hillan1152@gmail.com>
2019-10-28 16:18:54 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+const data = 
+[
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
   render() {
     return (
       <div>
