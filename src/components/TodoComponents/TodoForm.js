2ea0b144222be3e087b92c693be946d546dd1c56
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-10-28 16:52:38 -0400

    added todo form

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            newThings: '',
+            //what is below?
+            moreStateProperties: 9,
+            evenMore: { name: 'Dustin' }
+        };
+    }
+    handleChanges = e => {
+        this.setState({
+            newThings: e.target.value
+        });
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log("On Fire!");
+        // add our typed in item to the grocery list!
+        this.props.addThings(this.state.newThings);
+        this.setState({ newThings: '' });
+    };
+    render() {
+        console.log('rendering form');
+        return (
+            <form onSubmit={this.handleSubmit}>
+
+                <label htmlFor="things">New Tasks</label>
+                <input
+                    type="text"
+                    name="things"
+                    id="Todo"
+                    value={this.state.newThings}
+                    onChange={this.handleChanges}
+                />
+                <button>Add</button>
+            </form>
+        );
+    }
+}
+
+export default TodoForm;
