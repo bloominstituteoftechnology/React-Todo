3a354bbed91998cc591190f5429d969bbaaf5605
DeAndre Johnson <dre@phreshr.com>
2019-10-29 05:31:28 +0800

    complete mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: Date.now(),
+          task: 'Finish Todo MVP project',
+          completed: true,
+        },
+      ],
+    };
+  }
+
+  addTodo = title => {
+    const newTodo = {
+      id: Date.now(),
+      task: title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  completeTodo = index => {
+    const newTodos = [...this.state.todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    this.setState(newTodos);
+  };
+
+  clearTodos = e => {
+    e.preventDefault();
+    this.setState({
+      todos: this.state.todos
+        .filter(todo => todo.completed !== true)
+        .map(todo => todo),
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addTodo={this.addTodo} clearTodos={this.clearTodos} />
+        <TodoList todos={this.state.todos} completeTodo={this.completeTodo} />
       </div>
     );
   }
