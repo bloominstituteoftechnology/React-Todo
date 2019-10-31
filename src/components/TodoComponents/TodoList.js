2154aa825b7bddcd9b1748df7fde048db7fffb8f
Shafi Masoumi <masoumi2017@icloud.com>
2019-10-31 16:57:24 -0700

    Todo List looks OK

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,11 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from "react";
+
 import Todo from "./Todo";
 
 const TodoList = props => {
   return (
     <div className="todo-list">
+      {props.jobs.map(item => (
+        <Todo
+          key={item.id}
+          item={item}
+          toggleCompleted={props.toggleCompleted}
+        />
+      ))}
       <button className="delete-btn" onClick={props.clearCompleted}>
         Delete Job
       </button>
