a6cde57df6a15cd2052d21b760028b6e40c62c52
FreedomWriter <natalienetworks@gmail.com>
2019-10-30 22:22:22 -0500

    added strikethrough for completed items

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Todo = props => {
-  console.log(props.item.id);
+  console.log(props.item.complete);
   const handleClick = e => {
     props.toggleComplete(props.item.id);
   };
   return (
-    <div onClick={handleClick}>
-      <p>I'm from Todo!</p>
+    <div
+      onClick={handleClick}
+      className={`task${props.item.complete ? " complete" : ""}`}
+    >
       <p>{props.item.task}</p>
     </div>
   );
