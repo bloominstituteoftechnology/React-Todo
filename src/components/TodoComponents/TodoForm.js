b70633fa5b0b52b10ee5b014ce9d1bddf75972d9
Shafi Masoumi <masoumi2017@icloud.com>
2019-10-31 16:58:49 -0700

    some updates

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,12 +3,39 @@ import React from "react";
 class TodoForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      itemName: ""
+    };
   }
+
+  handleChanges = e => {
+    this.setState({
+      itemName: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.itemName !== "") {
+      this.props.addItem(this.state.itemName);
+      this.setState({
+        itemName: ""
+      });
+    }
+  };
 
   render() {
     console.log("rendering form");
     return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleChanges}
+          type="text"
+          name="item"
+          value={this.state.itemName}
+        />
         <button>Add Job</button>
+      </form>
     );
   }
 }
