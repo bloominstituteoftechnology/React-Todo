8492227fa8f54b60a4877b6ee5a585e7a0ff3e4c
FreedomWriter <natalienetworks@gmail.com>
2019-11-01 19:15:43 -0500

    no sig changes

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,33 +3,11 @@ import Todo from "./Todo";
 
 const TodoList = props => {
   let test = JSON.parse(localStorage.getItem("tasks"));
-  // const date = localStorage.getItem("tasksDate");
-  // const tasksDate = date && new Date(parseInt(date));
-  // const now = new Date();
-  // const taskAge = Math.round((now - tasksDate) / (1000 * 60));
-  // const procratinating = taskAge >= 1;
-  // if (procratinating) {
-  //   console.log("GET IT DONE");
-  //   // this.isProcrastinating = true;
-  // }
-  // console.log(now);
-  // console.log(tasksDate);
-  // console.log(taskAge);
-  // console.log(test);
+
   return (
     <div>
       {test &&
         test.map(item => {
-          // const date = localStorage.getItem("tasksDate");
-          // const tasksDate = date && new Date(parseInt(date));
-          // const now = new Date();
-          // const taskAge = Math.round((now - tasksDate) / (1000 * 60));
-          // const procratinating = taskAge >= 1;
-          // if (procratinating) {
-          //   console.log("GET IT DONE");
-          //   item.isProcrastinating = true;
-          // }
-          // console.log(item.isProcrastinating);
           return (
             <Todo
               key={item.id}
