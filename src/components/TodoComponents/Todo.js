a16fc0f7df20b9fa284c12bdd33598b88d3b3b4c
imxande <alerodriguezlarosa@gmail.com>
2019-12-02 17:25:27 -0500

    initial commit - created todo data

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,40 @@
+const todo = [
+    {
+        task: 'Workout',
+        id:1,
+        completed: false
+    },
+
+    {
+        task: 'Study Tk',
+        id:2,
+        completed: false
+    },
+
+    {
+        task: 'Study React',
+        id:3,
+        completed: false
+    },
+
+    {
+        task: 'Call my family',
+        id:4,
+        completed: false
+    },
+
+    {
+        task: 'Work on javascript replit challenges',
+        id:5,
+        completed: false
+    },
+
+    {
+        task: 'Read favorite book',
+        id:6,
+        completed: false
+    },
+    
+]
+
+export default todo;
