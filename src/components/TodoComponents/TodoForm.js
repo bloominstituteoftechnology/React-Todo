8621599a89979705df765e4692409a646908a785
Aasa-Christian <aasachristian@yahoo.com>
2019-12-02 17:29:42 -0500

    created form component

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { tsParenthesizedType } from '@babel/types';
+
+class TodoForm extends React.Component {
+
+    constructor(){
+        super();
+        this.state ={
+            newTodo: "",
+        }
+    }
+
+    handleChanges = e => {
+        this.setState({
+            newTodo: e.target.value
+        })
+    };
+
+    submitForm = e => {
+        e.preventDefault();
+        this.props.addTodo(this.state.newTodo)
+        this.setState({
+            newTodo:""
+          })
+
+    }
+
+
+    render() {
+
+    return(
+        <form onSubmit={this.submitForm}>
+            <input
+            type = "text"
+            name = "todo"
+            onChange={this.handleChanges}
+            value = {this.state.newTodo}
+            />
+            <button type ="submit">Add Todo</button>
+            <button type ="submit">Clear Completed</button>
+        </form>
+    )
+    }
+    
+}
+
+export default TodoForm;
+
