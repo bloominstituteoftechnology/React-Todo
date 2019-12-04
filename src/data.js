c1c96999384e70fa800b818e603f3fd5f0fb251e
mandihamza <mandihamza@gmail.com>
2019-12-03 20:11:28 -0700

    Adds data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const todos = [
+    {
+      task: 'Lay on keyboard',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+        task: 'Take a nap',
+        id: 1528817084360,
+        completed: false
+    },
+    {
+      task: 'Meow loudly at nothing',
+      id: 1528817084358,
+      completed: false
+    },
+    {
+        task: 'Take a nap',
+        id: 1528817084375,
+        completed: false
+    },
+    {
+        task: 'Climb Xmas tree',
+        id: 1528817084342,
+        completed: false
+    },
+    {
+        task: 'Take a nap',
+        id: 1528817084389,
+        completed: false
+    },
+    {
+        task: 'Take a bath',
+        id: 1528817084347,
+        completed: false
+    },
+    {
+        task: 'Take a long nap',
+        id: 1528817084399,
+        completed: false
+    },
+  ];
