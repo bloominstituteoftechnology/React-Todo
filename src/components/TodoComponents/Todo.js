48b5b8228f82b54abbc396e20a2dd803198a9a2d
CLYPR Technologies <cbanks@clypr.co>
2019-12-04 20:19:56 -0500

    completed tasks cross out

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Todo.css"
 
 const Todo = props => {
     return (
         <div 
-            className={`todo${props.item.completed ? ".completed" : ""}`}
+            className={`todo${props.item.completed ? " completed" : ""}`}
             onClick={e => props.toggleItem(props.item.id)}  
         >
             {props.item.task}
