07625fdff564cad04f93cb917f2577caafb7c9d1
Your name <a.m.verdine@gmail.com>
2019-12-05 20:00:35 -0500

    Setup App and intialize State object

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+      id: Date.now(),
+      completed: false
+    };
+  }
+
+  taskChangeHandler = event => {
+    this.setState({ task: event.target.value });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <TodoForm updateTaskState={this.taskChangeHandler} />
       </div>
     );
   }
