99573572dc792f1017ac067bfafb96a55e771bac
Zachary Murray <89foxlx@gmail.com>
2019-12-06 21:10:25 -0600

    testing some things for my own understanding of the material

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
+
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+    // you will need a place to store your state in this component.
+    // design `App` to be the parent component of your application.
+    // this component is going to take care of state, and any change handlers you need to work with your state
+
+    constructor() {
+        super();
+        this.state = {
+            name: "Zach"
+        };
+        
+    }
+
+    handleSingleClickEvent = () => {
+        this.setState({name: "Steve"});
+        console.log('testing click');
+    };
+
+    render() {
+        return (
+        <div>
+            <h2>To-do List</h2>
+            <h2 onClick={this.handleSingleClickEvent}>Hello {this.state.name}</h2>
+        </div>
+        );
+    }
 }
+
+
 
 export default App;
