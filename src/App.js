30aac9cefae78cc91adbb5aa7b403a6f2ec72ae1
Hutch Crowley <tothecoda@gmail.com>
2019-12-06 18:40:36 -0600

    Update, create App component, cteate Todo Array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
 import React from 'react';
 
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      list
+    };
+  }
+
+  toggleItem = itemId => {
+    console.log(item.id);
+    this.setState({
+      items: this.state.items.map(item => {
+        if (itemId === item.id) {
+          return {
+            ...item,
+            finished: !item.finished
+          };
+        }
+        return item;
+      })
+    });
+  };
+
+  addItem = itemText => {
+    const newItem = {
+      name: itemText,
+      finished: false,
+      id: Date.now
+    };
+
+    this.setState({
+      groceries: [...this.state.items, newItem]
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='App'>
+        <div className='header'>
+          <h2>Hutch's TODO List:</h2>
+        <TodoForm addItem={this.item} />
+        </div>
+        <TodoList
+        list={this.state.list}
+        toggleItem={this.toggleItem}
+        />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/TodoComponents/Todos.js
@@ -0,0 +1,12 @@
+export const data = [
+  {
+    task: 'Organize Garage',
+    id: 1528807077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528807084358,
+    completed: false
+  }
+];
