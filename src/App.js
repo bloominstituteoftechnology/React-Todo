c9da81e4ce9dd65ac7e1ad9a8fd2eb1dd5064566
chiragthesia <harithesia@gmail.com>
2019-12-08 19:04:51 -0600

    MVP Met except for the todo list is glitched

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
+import ToDoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state ={
+      todos:[
+        {
+          task: 'Complete React Classes Work',
+          id:1234568787,
+          completed: false,
+        },
+        {
+          task:'Work on Tutor App',
+          id:1234568788,
+          completed:false,
+        }
+      ],
+      todo:''
+    };
+  }
+
+  changeTodo = event => this.setState({ [event.target.name]: event.target.value });
+
+  addTodo = event=>{
+    event.preventDefault();
+    const newTodo = {
+      task: this.state.todo,
+      completed: false,
+      id: Date.now()
+    };
+    this.setState({
+      todos:[...this.state.todos, newTodo],
+      todo:''
+    });
+  };
+
+  toggleTodoComplete = id =>{
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo =>{
+      if (todo.id === id){
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompletedTodos = event =>{
+    event.preventDefault();
+    let todos = this.state.todos.filter(todo=> !todo.completed);
+    this.setState({ todos });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <ToDoList>
+          handleToggleComplete = {this.toggleTodoComplete}
+          todos = {this.state.todos}
+        </ToDoList>
+        <TodoForm>
+          value = {this.state.todo}
+          handleTodoChange = {this.changeTodo}
+          handleAddTodo = {this.addTodo}
+          handleClearTodos = {this.clearCompletedTodos}
+        </TodoForm>
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const TodoForm = props=>{
+    return(
+        <form>
+            <input>
+                onChange = {props.handleTodoChange}
+                type="text"
+                name="todo"
+                value={props.value}
+                placeholder="...todo"
+            </input>
+            <button onClick={props.handleAddTodo}>Add Task</button>
+            <button onClick={props.handleClearTodos}>Clear Completed Task</button>
+        </form>
+    )
+}
+
+export default TodoForm;
