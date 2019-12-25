173af7c72a0a97fd8ea779265028a19b457c855b
Aaron Beasley <aaroncbrs@gmail.com>
2019-12-24 21:57:15 -0600

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
