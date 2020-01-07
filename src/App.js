5c229b0ccb5448f9c193bf6d98b7a03e3b22c1b5
Justin A <jarata@gmail.com>
2020-01-07 09:31:20 -0800

    initialized project setup class constructor and state test displaying state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = [
+      {
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+      }
+    ];
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        {this.state.map(tsk => (<p key={tsk.id}>Task: {tsk.task}</p>))}
       </div>
     );
   }
