1e63d6fa686169bea3ade9895f7e56d2933c0b05
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-01-13 14:39:39 -0600

    Most of the functionality is written for AddItem and Clear List. Need to incorporate form for adding item still, and need to add the Filter method on the clear-completed task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+const toDos = [
+  {
+    task: "",
+    id: "",
+    completed: false,
+  },
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // first let's define our state based on the data structure in toDos
+  constructor() {
+    super();
+    this.state = {
+      todoList: toDos,
+    };
+  }
+
+  // Method that will strike out the ToDos. It will change Completed to TRUE, thereby changing the CSS style, marking it out
+  toggleItem = id => {
+    const newTodoList = this.state.toDoList.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      } else {
+        return item;
+      }
+    });
+
+    this.setState({
+      toDoList: newTodoList,
+    });
+  };
+
+  // Method by which we add a new item to the list.
+  addItem = itemName => {
+    const newItem = {
+      task: itemName,
+      id: Date.Now(),
+      completed: false,
+    };
+    this.setState({
+      toDoList: [...this.state.toDoList, newItem],
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todoList={this.state.toDoList} toggleItem={this.toggleItem} />
+
+        <TodoForm addItem={this.addItem} />
       </div>
     );
   }
