1f458963b64db44cd79972c06c70aa299762574b
Nicole white <you@example.com>
2020-01-13 11:14:37 -1000

    got todo data from readme, created basic form

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,3 +2,15 @@ import React from "react";
 //<TodoForm> will hold your input field and your Add Todo and Clear Completed buttons.
 // Your input field should take in user input, and allow a user to press Enter or click on the Submit Button to add a todo to your list.
 // Once a todo is submitted, the Todo List should re-render and show the added todo.
+
+class TodoForm extends React.Component{
+render(){
+    <form>
+        <label>Add Task:
+            <input type="text"></input>
+        </label>
+        <button>Submit Task</button>
+        <button>Clear Finished Task</button>
+    </form>
+}
+}
