0131ea38e05c0353cdc9e21d52fe981d372c5021
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-01-13 16:34:11 -0800

    updating app.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,53 @@ import React from 'react';
 import TodoForm from "./components/TodoComponents/TodoForm";
 import TodoList from "./components/TodoComponents/TodoList";
 
+const tasks = [
+  {
+    task: "Fix ceiling light",
+    id: 1,
+    completed: false
+  },
+  {
+    task: "Eat homemade tamales",
+    id: 2,
+    completed: false
+  },
+  {
+    task: "Play videogames with friends",
+    id: 3,
+    completed: false
+  },
+];
+
+
 class App extends React.Component {
   constructor() {
     super();
-    this.state={
-      todo: [
-        {
-          task: "Fix ceiling light",
-          id: 1,
-          completed: false
-        },
-        {
-          task: "Eat homemade tamales",
-          id: 2,
-          completed: false
-        },
-        {
-          task: "Play videogames with friends",
-          id: 3,
-          completed: false
-        },
-      ],
+    this.state = {
+      todoList: tasks,
     };
   }
+
+  addItem = itemName => {
+    const newItem = {
+      name: itemName,
+      id: Date.now(),
+      done: false
+    };
+    this.setState({
+      todoList: [...this.state.todoList, newItem]
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>My Todo List</h2>
-
-        <TodoList />
-        <TodoForm />
+        <TodoList 
+          addItem = {this.state.tasks} 
+          toggleItem = {this.toggleItem}/>
+        <TodoForm 
+          addItem = {this.AddItem}/>
       </div>
     );
   }
