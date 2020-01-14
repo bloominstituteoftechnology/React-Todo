577f053ea412408250010b7123723d663244752f
Dani Blackwell <bleuxgenius@gmail.com>
2020-01-14 09:41:30 -0600

    update Todo Form
    issue, with brackets, not sure.

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+class Todo extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            todoText: ''
+        };
+    }
+    handleChanges = e =>
+    this.setState({
+        todoText: e.target.value
+    });
+};
+
+handleSubmit = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.todoText);
+    this.setState({
+        todoText: ''
+    })
+};
+
+render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <input 
+        type="text"
+        name="item"
+        placeholder="New Todo Item"
+        value={this.state.todoText}
+        onChange={this.handleChanges}
+        />
+        <button>Add</button>
+        </form>
+        );
+    };
+
+export default TodoForm;
