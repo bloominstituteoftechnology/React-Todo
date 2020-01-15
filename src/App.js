b7836d0288e1b1ff7af70650830f85767cbae92e
Sean Birmingham <seannbirmingham98@gmail.com>
2020-01-15 00:34:22 -0500

    set up Todo and TodoList components, todo placeholder should display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 import { todos } from './data';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
