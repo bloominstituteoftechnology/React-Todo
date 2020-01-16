edec2c6f56ca95bf23c74c058d760e54a9d72409
Leisha <banksleisha@yahoo.com>
2020-01-15 23:50:11 -0500

    edit todoform, todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+
+const itemData = [
+  {
+    task: 'create list',
+    id:  1,
+    completed: false
+  },
+  {
+
+
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  constructor(){
+    super();
+    this.state ={
+      itemData
+    };
+  }
+  
+  toggleItem = itemID => {
+    this.setState({
+      itemData: this.state.itemData.map(item => {
+        if (itemID === item.id){
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } 
+        return item;
+        
+      })
+
+    });
+  };
+
+  addItem
+
   render() {
     return (
       <div>
