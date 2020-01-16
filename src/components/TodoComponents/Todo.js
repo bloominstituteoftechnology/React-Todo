85cbf11875c2afcea6de5ebfb8dc4ccc63a24cd3
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-15 20:21:55 -0800

    Initial commit. Constructed the main components

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todo = props => {
+  return (
+    <div>
+      <p>done</p>
+    </div>
+  );
+};
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo'
+
+ const TodoList = props =>{
+     return (
+         <div>
+             <h3>TO DO LIST</h3>
+             <Todo/>
+         </div>
+     )
+ }
+ export default TodoList;
