eec6bd9473312b7fdd19028af848b4f209a9294f
TREY <hugh.a.lloyd@gmail.com>
2020-02-10 18:27:33 -0600

    building todoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
+todoData = [
+  {
+    task: 'example',
+    id: 999,
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    constructor(){
+      super();
+      this.state = {
+        todoData: todoData
+        //give state to data
+      }
+    }
+
+    toggleItem =  (clickedId) => {
+      const newTodo = this.state.todoData.map((item) => {
+       if (item.id === clickedId){
+         return {
+           ...item,
+           completed: !item.completed
+         };
+       } else {
+         return item;
+       }
+      })
+
+      this.setState({
+        todoData: newTodo
+      })
+    };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todoData={this.state.todoData} toggleItem={this.toggleItem}/>
       </div>
     );
   }
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <div onClick={}>
+            <p></p>
+        </div>
+    )
+}
