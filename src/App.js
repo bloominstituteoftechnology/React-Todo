53a04a5c781295ed8005f6db61d2600130878f48
apple <apple@apples-MacBook-Pro-2.local>
2020-02-10 17:11:05 -0800

    periodic save

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
 import Todo from './components/TodoComponents/Todo';
 
+const todo = [
+  {
+    id: 111,
+    name: 'Walk the Dog',
+  },
+  {
+    id: 222,
+    name: 'Take out trash',
+  },
+  {
+    id: 333,
+    name: 'Was dishes',
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+  
   constructor() {
     super();
       this.state = {
-        
+        todo: todo,
       };
   };
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+ 
   render() {
     return (
       <div>
         <h2>Todo List!</h2>
-        <Todo />
+        <Todo todo={this.state.todo}/>
         <TodoForm />
       </div>
     );
   }
-}
+};
 
 export default App;
