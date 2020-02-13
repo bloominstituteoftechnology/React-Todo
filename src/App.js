27e2f9d5632e6ede5f897a0aa2f5492172ba8212
Germaine Pannell <me@germainep.com>
2020-02-12 19:49:41 -0500

    setup the list files
    added TodoList.js to App create TodoList functional component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
-    super();
+    super( );
     this.state = {
-      taksList: [],
+      taskList: [{task: 'write code', id: 1, completed: false}],
     }
   }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList tasklist={this.state.taskList}>
+
+        </TodoList>
       </div>
     );
   }
