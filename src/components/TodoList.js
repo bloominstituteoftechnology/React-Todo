2d5a455740bb5d887925fcb1477705ba34186b30
Pete1701 <pete.pertongonas@pirron.one>
2020-03-10 11:38:50 +0100

    minor changes for cleaner code

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,8 +3,7 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = props => {
-    // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
+const TodoList = props => {    
     return (
       <div className="todo-list">
         {props.tasks.map(item => (
