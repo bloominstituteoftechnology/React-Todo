f1c9737341f63b6d7d36e76063eac5ce04f2062e
miriam-sullivan <maljayd42@gmail.com>
2020-03-11 21:20:59 -0500

    working on the components think ive got a lot done

---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class ToDoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemName: ""
+    };
+  }
+  handleChanges = e => {
+    this.setState({ itemName: e.target.value });
+  };
+  handleAddItem = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.itemName);
+  };
+  render() {
+    console.log("rendering todo form");
+    return (
+      <form onSubmit={this.handleAddItem}>
+        <input
+          type="text"
+          name="item"
+          value={this.state.itemName}
+          OnChange={this.handleChanges}
+        />
+        <button>Add ToDo</button>
+      </form>
+    );
+  }
+}
+
+export default ToDoForm;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+import Item from "./Item";
+
+const ToDoList = props => {
+  return (
+    <div className="ToDoList">
+      {props.ToDos.map(item => (
+        <Item
+          key={item.id}
+          item={item}
+          toggleCompleted={props.toggleCompleted}
+        />
+      ))}
+      <button className="clear-button" onClick={props.clearCompleted}>
+        Clear Completed
+      </button>
+    </div>
+  );
+};
+
+export default ToDoList;
