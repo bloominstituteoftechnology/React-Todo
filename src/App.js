ff5ab0376f2bf996247461b8299c2659d837c03c
yirano <yirano@protonmail.ch>
2020-03-18 20:05:37 -0400

    Initial commit with project setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: []
+    }
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList />
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div />
+  )
+}
+
+export default Todo
