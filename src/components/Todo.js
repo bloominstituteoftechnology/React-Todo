add3543a42b66002f82ce4929855f8ccdcc8a6d4
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-04-06 11:41:24 -0700

    Todo.js basic structure completed

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Todo =  props => {
+  return (
+    <div
+    className={`todo${props.todo.completed ? ' completed' : ''}`}
+    onClick={() => props.toggleTodo(props.todo.id)}
+    >
+      <p>{props.todo.name}</p>
+    </div>
+  )
+};
+
+export default Todo;
