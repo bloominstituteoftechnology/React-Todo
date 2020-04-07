bf6b5e7ad2ab1651fe96fa4ff4b9e55ac9561f38
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-04-07 09:59:06 -0500

    finished first part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor () {
+    super(); 
+    this.state = [
+      {
+      task: 'make chicken', 
+      id: Date.now(), 
+      completed: false
+    }, {
+      task: 'take a bowl', 
+      id: Date.now(), 
+      completed: false
+    }, ]; 
+  }
+  
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
       </div>
     );
   }
