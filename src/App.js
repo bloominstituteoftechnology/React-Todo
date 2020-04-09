03df53c18ea242d750633a9e45fad1669fef463d
Ryan Erickson <rerickson21@yahoo.com>
2020-04-09 16:16:09 -0400

    MVP Reached

--- src/App.js
+++ src/App.js
@@ -1,16 +1,119 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+import './app.css';
+
+const todoList = [
+  {
+		task: 'Organize Garage',
+		id: 1528817077286,
+		completed: false
+	},
+	{
+		task: 'Bake Cookies',
+		id: 1528817084358,
+		completed: false
+	},
+	{
+		task: 'Study Class Components',
+		id: 152881751447,
+		completed: false
+	}
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+  constructor() {
+		super();
+		this.state = {
+			todoList: todoList,
+			task: ''
+		};
+	}
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+	inputChangeHandler = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+  };
+  formSubmitHandler = (event) => {
+		event.preventDefault();
+		this.setState((prevState) => {
+			return {
+				todoList: [
+					...prevState.todoList,
+					{
+						completed: false,
+						id: Date.now(),
+						task: prevState.task
+					}
+				],
+				task: ''
+			};
+		});
+	};
+
+	toggleTask = (taskId) => {
+		this.setState((prevState) => {
+			return {
+				todoList: prevState.todoList.map((taskItem) => {
+					if (taskItem.id === taskId) {
+						return {
+							task: taskItem.task,
+							id: taskItem.id,
+							completed: !taskItem.completed
+						};
+					} else {
+						return taskItem;
+					}
+				})
+			};
+		});
+  };
+  clearCompleted = () => {
+		this.setState((prevState) => {
+			return {
+				todoList: prevState.todoList.filter((taskItem) => {
+					return !taskItem.completed;
+				})
+			};
+		});
+  };
+  clearAll = () => {
+		this.setState((prevState) => {
+			return {
+				todoList: prevState.todoList.filter((taskItem) => {
+					return taskItem.completed;
+				})
+			};
+		});
+	};
+	render() {
+		return (
+			<div className="app">
+				<div className="notepad">
+					<div className="topSection">
+						<div className="header">
+							<h1>Todo List</h1>
+						</div>
+						<div className="todoForm">
+							<TodoForm
+								task={this.state.task}
+								inputChangeHandler={this.inputChangeHandler}
+								formSubmitHandler={this.formSubmitHandler}
+							/>
+							<button className="clear" onClick={this.clearCompleted}>
+								Clear Completed
+							</button>
+							<button className="clear" onClick={this.clearAll}>
+								Clear All
+							</button>
+						</div>
+					</div>
+					<TodoList todoList={this.state.todoList} toggleTask={this.toggleTask} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/TodoComponents/TodoList.js
@@ -0,0 +1,20 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from './Todo';
+
+import './Todo.css'
+
+function TodoList(props) {
+    return (
+        <div className='todoList'>
+            {props.todoList.map(task => {
+                return <Todo task={task} toggleTask={props.toggleTask} />;
+            })}
+        </div>
+    )
+}
+
+
+  export default TodoList 
