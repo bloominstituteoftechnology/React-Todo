ea6bc791013ff19e2ed24a765b8405d3c45e3571
aland1013 <adam.land@gmail.com>
2020-05-06 01:07:34 -0400

    added strikethrough when Todo clicked

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Todo.css';
 
 const Todo = (props) => {
   const handleClick = e => {
     props.toggleCompleted(props.todo.id);
   }
 
-  return <div onClick={handleClick}>{props.todo.task}</div>;
+  return <div onClick={handleClick} className={`${props.todo.completed ? "completed" : ""}`}>{props.todo.task}</div>;
 }
 
 export default Todo;
