8518ccbaf38024f8d4eb0b504c0d700173d4cf27
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-05-06 21:53:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React from 'react';
 
+const todo = [
+  {
+    task: 'Watch Coding tutorials',
+    id:123,
+    completed: false
+  },
+  {
+    task: 'Apply for Jobs',
+    id:1234,
+    completed: false
+  },
+  {
+    task: 'Update Resume',
+    id:12345,
+    completed: false
+  },
+  {
+    task: 'Surf LinkedIn',
+    id:246,
+    completed: false
+  },
+  {
+    task: 'Reply to emails',
+    id:2468,
+    completed: false
+  },
+  {
+    task: 'Network',
+    id:369,
+    completed: false
+  },
+  {
+    task: 'Attend class',
+    id:258,
+    completed: false
+  },
+  {
+    task: 'Study Hard',
+    id:147,
+    completed: false
+  },
+  {
+    task: 'Write Some Code',
+    id:456,
+    completed: false
+  }
+];
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todo,
+    }
+  }
+
+  
+
   render() {
     return (
       <div>
