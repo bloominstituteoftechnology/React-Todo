b287f6ff53c6ee2d6536b3325af5053eb113af23
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-01 17:07:44 -0500

    field working, not styled, clear button not working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import React from 'react';
+import React from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import "./components/Todo.css";
+
+const tasks = [
+  {
+    task: "Vacation",
+    id: 182,
+    complete: false,
+  },
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks,
+    };
+  }
+
+  toggComplete = (taskId) => {
+    // console.log("bk: index.js: App: toggComplete: taskId ", taskId);
+    this.setState({
+      tasks: this.state.tasks.map((task) => {
+        if (taskId === task.id) {
+          return {
+            ...task,
+            complete: !task.complete,
+          };
+        }
+        return task;
+      }),
+    });
+  };
+
+  addTask = (taskName) => {
+    // console.log("bk: index.js: App: addTask: taskName: ", taskName);
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        { task: taskName, complete: false, id: Date.now() },
+      ],
+    });
+  };
+
+  clearCompleted = () => {
+    // console.log("bk: index.js: clearCompleted");
+    this.setState({
+      tasks: this.state.tasks.map((task) => {
+        return { ...task, complete: !task.complete };
+      }),
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='App'>
+        <div className='header'>
+          <h1>My ToDo App</h1>
+          <TodoForm addTask={this.addTask} />
+        </div>
+        <TodoList
+          toggComplete={this.toggComplete}
+          tasks={this.state.tasks}
+          clearCompleted={this.clearCompleted}
+        />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,59 @@
+// // // your components will all go in this `component` directory.
+// // // feel free to change this component.js into TodoList.js
+
+import React from "react";
+import Todo from "./Todo";
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const TodoList = (props) => {
+  return (
+    <div className='todo-list'>
+      {props.tasks.map((task) => (
+        <Todo toggComplete={props.toggComplete} key={task.id} task={task} />
+      ))}
+      <button className='clear-btn' onClick={props.clearCompleted}>
+        Clear Completed Tasks
+      </button>
+    </div>
+  );
+};
+
+export default TodoList;
+
+// import React from "react";
+
+// import Todo from "./Todo";
+
+// const List = (props) => {
+//   return (
+//     <div className='todo-list'>
+//       {props.List.map((item) => (
+//         <List toggleItem={props.toggleItem} key={item.id} item={item} />
+//       ))}
+//       <button className='clear-btn' onClick={props.clearPurchased}>
+//         Clear Completed
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default List;
+
+// // import React from "react";
+// // import Todo from "./Todo";
+
+// // const TodoList = (props) => {
+// //   // console.log('TodoList.js', props)
+
+// //   return (
+// //     <div className='todo-list'>
+// //       {props.todo.map((todo) => (
+// //         <Todo key={todo.id} todo={todo} toggleItem={props.toggleItem} />
+// //       ))}
+// //     </div>
+// //   );
+// // };
+
+// // export default TodoList;
