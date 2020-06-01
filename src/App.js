bd05ab9f4a6f26f81ac09cea61694887ffd819a1
Impulse2020 <irishht@gmail.com>
2020-06-01 18:03:36 -0400

    builds the todo component and starts the list and form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import TodoList from 'TodoList';
+
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    constructor(){
+      super();
+      this.state = {
+        Tasks: tasks
+      }
+    }
+    addTask = Task => {
+      const newTask = {
+        
+
+
+      }
+
+
+
+    }
+
+
   render() {
+    
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Task from './Todo';
+import './Todo.css';
+
+const TodoList = props =>{
+
+    return(
+        <div className = "todolist">
+            {props.tasks.map(item => (
+            <Task key={task.id}  task={task}     />
+            ))}
+            <button className="clear-btn" onClick={props.completeTask}>
+                Complete task
+            </button>
+        </div>
+
+    );
+
+};
+
+export default TodoList;
