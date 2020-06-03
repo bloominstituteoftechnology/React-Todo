8d8dd7b37436e11fc98db333b6b1e28851ea8612
Katherine-Kumar <Katherine2bk@gmail.com>
2020-06-02 19:11:44 -0700

    created dummy data, created state, added state to TodoList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
+const items = [
+  {
+    task: "Clean kitchen",
+    id: 156,
+    completed: false
+  }, 
+  {
+    task: "Cook dinner",
+    id: 127,
+    completed: false
+  }
+]
+
 class App extends React.Component {
+  constructor() {
+  super();
+  this.state = {
+    items
+  };
+}
+
+
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +30,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList items={this.state.items}/>
       </div>
     );
   }
