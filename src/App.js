6bf90002d84fd52a27a543590cf353d589be7b13
KarmaMichaelis <achantayd@gmail.com>
2020-07-06 19:01:51 -0400

    todoList skeleton connected to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import TodoList from './components/TodoList.js'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state={
+      task:'Bake Cookies',
+      id:Date.now(),
+      completed:'false'
+    }
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList propsList={this.state.id}/>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,25 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+
+
+const TodoList=props=><h4>{props.propsList}</h4>
+
+// const list=[
+//     {
+//         task:'Bake Cookies',
+//         completed:'falsed',
+//         id:Date.now()
+//     },
+//     {
+//         task:'Wash Dog',
+//         completed:'falsed',
+//         id:Date.now()
+//     }
+// ]
+
+
+
+export default TodoList
