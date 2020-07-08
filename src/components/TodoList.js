64e725aea93070a31c139ae2aa1b1cfa7b3041ab
JenVest2020 <jenvest2019@gmail.com>
2020-07-08 12:57:55 -0500

    created TodoList component and tasks array

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,35 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const tasks = [
+    {
+        task: 'Review Lesson of the Day',
+        id: 123,
+        completed: false
+    },
+    {
+        task: 'Complete the Follow Along(s)',
+        id: 1234,
+        completed: false
+    },
+    {
+        task: 'Attend Class Lecture',
+        id: 12345,
+        completed: false
+    },
+    {
+        task: 'Begin Lesson Project',
+        id: 123456,
+        completed: false
+    }
+];
+
+const TodoList = props => {
+    return (
+        <>
+
+        </>
+    )
+};
+
+export default TodoList;
