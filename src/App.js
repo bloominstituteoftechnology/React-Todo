ec1edc780a3551fbadb71fab26f3128827ab46e4
JenVest2020 <jenvest2019@gmail.com>
2020-07-08 14:05:30 -0500

    all errors fixed but still no render to screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import TodoList from './components/TodoList';
 
-
+const tasks = [
+  {
+    task: 'Review Lesson of the Day',
+    id: 123,
+    completed: false
+  },
+  {
+    task: 'Complete the Follow Along(s)',
+    id: 1234,
+    completed: false
+  },
+  {
+    task: 'Attend Class Lecture',
+    id: 12345,
+    completed: false
+  },
+  {
+    task: 'Begin Lesson Project',
+    id: 123456,
+    completed: false
+  }
+];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -11,10 +38,10 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App! :-)</h2>
-        <TodoList tasks={this.tasks} />
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
-}
+};
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import Todo from './Todo';
 
-const tasks = [
-    {
-        task: 'Review Lesson of the Day',
-        id: 123,
-        completed: false
-    },
-    {
-        task: 'Complete the Follow Along(s)',
-        id: 1234,
-        completed: false
-    },
-    {
-        task: 'Attend Class Lecture',
-        id: 12345,
-        completed: false
-    },
-    {
-        task: 'Begin Lesson Project',
-        id: 123456,
-        completed: false
-    }
-];
+
 
 const TodoList = props => {
     return (
         <div>
-            {tasks.map(task => {
+            {props.tasks.map(task => {
                 return (
                     <Todo
                         key={props.id}
