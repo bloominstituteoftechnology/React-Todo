2a72cf93fff6c6b5b687a2559b9ff5ed3c39603d
Terry Edwards <terryedwardsjr113@gmail.com>
2020-08-05 19:53:26 -0400

    started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,64 @@
 import React from 'react';
+import Todo from './Todo';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+
+const data=()=>{
+  let data=[
+    {
+      task: 'Laundry',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Mow the lawn',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Wash Dishes',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Tske out the trash',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Clean the bathroom',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Mop',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Get Groceries',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Pay rent',
+      id: '',
+      completed: false
+    },
+    {
+      task: 'Dust the vents',
+      id: '',
+      completed: false
+    }, 
+    {
+      task: 'Wash the dog',
+      id: '',
+      completed: false
+    }
+  ]
+};
+
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -8,6 +68,9 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+      <TodoForm/>
+      <TodoList/>
+      <Todo/>
       </div>
     );
   }
