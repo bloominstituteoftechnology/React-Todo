12c786cd43516fbace6fcad63b5133f93dde15f2
Mitchell Wright <mitchellbwright@gmail.com>
2020-08-04 19:50:14 -0600

    complete basic todo functionality

---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    handleClick = (e) => {
+        this.props.setCompleted(e.target.id);
+    }
+
+    render() {
+        return (
+            <li id={this.props.id} onClick={this.handleClick}>{this.props.task}</li>
+        )
+    }
+}
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,29 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.todos.map( todo =>
+                        <Todo
+                            key={todo.id}
+                            completed={todo.completed}
+                            task={todo.task}
+                            id={todo.id}
+                            setCompleted={this.props.setCompleted}
+                        />
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
