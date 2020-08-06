e997a0e39491f558eeee2626ed1a31d17e1311eb
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-05 20:00:47 -0500

    completed assignment.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      item: ''
+    };
+  }
+
+  handleChanges = e => {
+    this.setState({ item: e.target.value });
+  };
+
+  submitItem = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.item);
+    this.setState({ item: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.submitItem}>
+        <input
+          type='text'
+          name='item'
+          value={this.state.item}
+          onChange={this.handleChanges}
+        />
+        <button>Add</button>
+      </form>
+    );
+  };
+};
+
+export default TodoForm;
