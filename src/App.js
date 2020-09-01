6ec047053ede6c6a0ac8346cb40cae8edf9d2337
Tevvin <Tevvin7395@icloud.com>
2020-09-01 00:34:23 -0400

    passing state to todolist component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
 
+// Importing COMPONENTS
+import TodoList from './components/TodoList.js';
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor(props) {
     super(props);
-    this.state = [
-      {
-        task: 'Organize Garage',
-        id: 1528817077286,
-        completed: false
-      },
-      {
-        task: 'Bake Cookies',
-        id: 1528817084358,
-        completed: false
-      }
-    ];    
+    this.state = {
+      Todo:  [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    };   
   }
 
 
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todo={this.state.Todo} />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+
+
+  render() {
+    return (
+      <div>
+        {this.state.todo.map((todos) =>
+            <p>{this.state.todos}</p>
+        )};
+      </div>
+    );
+  }
+}
+
+export default TodoList;
