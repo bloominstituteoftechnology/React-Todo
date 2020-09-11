d1b80c89d5b7ad48db21e0b5a79538044466d9b6
Tevvin <Tevvin7395@icloud.com>
2020-09-10 22:49:18 -0400

    Basic logic added to TodoForm component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+
+}
+
+export default TodoForm
