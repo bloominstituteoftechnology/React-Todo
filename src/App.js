fcdb582d9e94736164be6c8e3136841a655ea219
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-28 20:26:17 -0400

    Bare Bones Laid Out

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: '',
+      completed: false,
+      id: ''
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <input />
+        <button>Add Todo</button>
+        <button>Clear Completed</button>
       </div>
     );
   }
