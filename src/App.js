81bc2ead5d8ab47da30ce727936ea75e84a4bb92
Mychael M <mychaelbmenges@gmail.com>
2020-09-30 15:11:45 -0600

    added state to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+const todoData = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false,
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false,
+  },
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+      id: Date.now(),
+      completed: false
+    }
+  }
+
+  //change handlers
+  
+
   render() {
     return (
       <div>
