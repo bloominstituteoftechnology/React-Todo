a94ba3c63c36578faeac7e5f9806d50516e51dd6
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-10-11 23:29:22 -0500

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(props){
+    super (props);
+    this.state = {
+      title: 'Todo List: MVP'
+    };
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>{this.state.title}</h1>
+        <TodoList />
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+    constructor(props){
+        super (props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (<div>
+            <h3>
+                List of todo's
+            </h3>
+        </div>)
+    }
+}
