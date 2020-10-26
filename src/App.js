bdf43dc9b98f75079ef069b237cff6e8972d2fb2
CarlosColindres <stingaree43@gmail.com>
2020-10-26 18:05:42 -0400

    todo list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList'
+
+const initialListState = []
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = {
+        todoList: initialListState
+    }
+  }
+
+  addTodo = (todo) => {
+      this.setState({
+        todoList: [...this.state.todoList, 
+        {
+          name: todo,
+          id: Math.random(),
+          completed: false
+          }
+        ]
+      })
+  }
+
+  finishTodo = todoId => {
+    this.setState({
+      todoList:this.state.todoList.map(item=>{
+        if(item.id === todoId) {
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        } else {
+          return item
+        }
+      })
+    })
+  }
+
+  clearTodos = () => {
+    this.setState({
+      todoList:this.state.todoList.filter(item =>(!item.completed))
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='todos-container'>
+        <h1>Welcome to your Todo App!</h1>
+        <TodoForm addTodo={this.addTodo} todos={this.state.todoList} clearTodos={this.clearTodos}/>
+        <TodoList finishTodo={this.finishTodo} todoList={this.state.todoList} clearTodos={this.clearTodos}/>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react'
+import Todo from './Todo'
+const TodoList = ({todoList, finishTodo}) => {
+    
+    return (
+        <div>
+            {todoList.map(todo => <Todo key={todo.id} todo={todo} finishTodo={finishTodo}/>)}
+        </div>
+    )
+}
+
+export default TodoList
