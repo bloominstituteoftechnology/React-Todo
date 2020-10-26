c38eadb5e95fdd56d4608f9ada98fa12477d9009
Francis Bonacic <francisbonacic@icloud.com>
2020-10-26 16:02:34 -0700

    still working

---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+
+    render(){
+        return (
+            <>
+            <input type="text"/>
+            <button>submit</button>
+            <button>clear all</button>
+            </>
+        )
+    }
+}
+
+export default TodoForm;
