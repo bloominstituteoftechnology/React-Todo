ef39e638342e6018d9281e147d4fc019051b72ea
duointherain <duointherain@gmail.com>
2020-12-01 18:37:30 -0800

    starting late, better late than never

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  //and here we get started on yesterday's homework. 
   render() {
     return (
       <div>
