d766cc84604f436974070095c1dac7f05cddf1d0
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-01-11 15:59:39 -0600

    submit functioning correctly

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import ToDoForm from './components/TodoForm';
-import ToDoList from './components/TodoList';
+import React from "react";
+import ToDoForm from "./components/TodoForm";
+import ToDoList from "./components/TodoList";
 
-const todosArray=[
+const todosArray = [
   {
-  todoName:'test item',
-  id:Date.now() 
+    todoName: "test item",
+    id: Date.now(),
+    completed: false,
   },
-  {
-    todoName:'test item 2',
-    id:Date.now()
-  }
 ];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      todosArray:todosArray
+    this.state = {
+      todosArray: todosArray,
     };
+  }
+  handleAddTodo = (newTodoName) => {
+    const newTodo = {
+      todoName: newTodoName,
+      id: Date.now(),
+      completed: false,
+    };
+
+    const newTodoArray = [...this.state.todosArray, newTodo];
+
+    this.setState({
+      todosArray: newTodoArray,
+    });
   };
-
-
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <ToDoForm></ToDoForm>
+        <ToDoForm handleAddTodo={this.handleAddTodo}></ToDoForm>
         <ToDoList todosArray={this.state.todosArray}></ToDoList>
       </div>
     );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -12,13 +12,19 @@ class ToDoForm extends React.Component{
           inputValue:e.target.value
         });
       }
-    
+    handleSubmitAdd=(e)=>{
+        e.preventDefault();
+        this.props.handleAddTodo(this.state.inputValue);
+        this.setState({
+            inputValue:""
+        })
+    }
 
     render(){
         console.log(this.state)
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleSubmitAdd}>
                 <input type='text' value={this.state.inputValue} onChange={this.handleInputChange}></input>
                 <button>Add item to the todo list</button>
                 </form>
