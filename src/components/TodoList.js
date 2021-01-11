103a3f2dc42ec2fd86e9beca0d3d8616b25b2467
Matthew Justice <matthewajustice@gmail.com>
2021-01-11 15:21:26 -0500

    Initial commit, successfully rendering data using class state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,13 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = (props) => {
+  return (
+    <div>
+      {props.tasks.map((task) => (
+        <h2>{task.name}</h2>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    name: 'test',
+    id: '9000',
+    completed: false,
+  },
+];
+
+export default data;
