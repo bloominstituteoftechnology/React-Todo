27fb71ef7dd6c478e06370d7ba0b5f9e3899def3
adam-rappaport <adamrappaport@icloud.com>
2021-01-11 17:41:07 -0600

    almost done

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Item = (props) =>{
+    const handleClick = () => {
+        props.onComplete(props.item.id)
+    }
+
+    let done = "item"
+
+    if (props.item.completed === true) {
+        done = done + "Completed";
+    }
+
+    return(
+        <div className = 'item'>
+            <h3 className = {done} onClick = {handleClick}>
+                {props.item.task}
+            </h3>
+        </div>
+    )
+
+}
+
+export default Item;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,26 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react'
+import Item from './Todo'
+
+const List = (props) =>{
+
+    return(   <div className = 'list'>
+    {props.state.map(item =>{
+        return(
+            <Item 
+            key= {item.id}
+            item= {item}
+            onComplete = {props.onComplete}
+            />
+        )
+    })}
+
+</div>
+    );
+ 
+
+};  
+
+export default List
