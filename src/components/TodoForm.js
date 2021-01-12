d3f7991f630a0146d98f9a888d844d9fff32ad6e
Mark <mark@Marks-MacBook-Air.local>
2021-01-11 20:24:54 -0500

    finished TodoForm

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,48 @@
 import React from 'react'
 class TodoForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTask: ""
+        }
+    }
 
+    handleInput = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
-addToList = () => {
-    console.log("test")
-    this.setState({})
-}
+    handleSubmit = e => {
+        e.preventDefaul()
+        this.setState({
+            newTask: ""
+        })
+        this.props.handleSubmit(e, this.state.newTask)
+    }
 
     render () {
         return (
             <div>
-                <input 
-                    placeholder="Add New Item"
-                />
-                <button 
-                    onClick={this.addToList}
+                <form 
+                    onSubmit={this.handleSubmit}
                 >
-                    Submit
+                    <input 
+                        value={this.state.newTask}
+                        name="newTask"
+                        placeholder="Add New Item"
+                        onChange={this.handleInput}
+                    />
+                    <button 
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
+                <button 
+                    onclick={this.props.clearCompleted}
+                >
+                    Clear
                 </button>
             </div>
         )
