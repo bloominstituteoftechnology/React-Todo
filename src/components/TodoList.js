96bccd8f3c725a424298733914a25de52bc131de
Karla Cervantes <cervantes.karla14@gmail.com>
2021-01-11 19:49:20 -0600

    completed task for today, working on CSS

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+    const handleClick = () => {
+        props.handleItemCompleted();
+    }
+    return (
+        <div>
+            {props.task.map(item => (
+                <Todo handleItemToggle={props.handleItemToggle} key={item.id} item={item} />
+            ))}
+            <button onClick={handleClick} className='clear-btn'>Clear Completed</button>
+        </div>
+    )
+}
+
+export default TodoList;
