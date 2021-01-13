598ba895b9f514cdae64148d578e1dcbe6e1caa9
ChadDiaz <chadcdiaz@gmail.com>
2021-01-13 14:52:37 -0500

    new package-lock.json install

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
-  constructor(){
-    super();
-    
-  }
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
