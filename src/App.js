0341a79f66e3c55ee91a7f42fbd15b86984b72b5
Alan Mir <alanpaulphotos@gmail.com>
2021-01-13 21:25:20 -0500

    Added constructer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
+const todo = [];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todo: todo
+    };
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>My Personal To-Do List</h2>
       </div>
     );
   }
