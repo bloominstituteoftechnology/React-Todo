ff43965305b2e791c4b5ebbe5eb0b9f9f04df473
Jesse Medrano <Jtamedrano@gmail.com>
2021-01-13 18:48:26 -0800

    refactored tailwind install

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ export default class Todo extends Component {
     return (
       <div
         onClick={() => this.props.toggleTodo(this.props.todo.id)}
-        className="transition transition-colors ease-in-out hover:bg-gray-400 text-center cursor-pointer m-1 lg:text-lg h-20 w-5/6 sm:h-8 flex self-center justify-center items-center rounded-md border border-gray-400 bg-gray-300 p-8 shadow-md"
+        className="transition transition-colors ease-in-out hover:bg-gray-400 text-center cursor-pointer m-1 lg:text-lg h-20 w-5/6 sm:h-8 flex self-center justify-center items-center rounded-md border  border-gray-400 bg-gray-300 p-8 shadow-md"
       >
         <p className={this.props.todo.completed ? 'completed' : ''}>
           {this.props.todo.todo}
