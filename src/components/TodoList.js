48519c266e50863b6c2e7c3b48de86c7258f90d6
Alan Mir <alanpaulphotos@gmail.com>
2021-01-13 22:46:48 -0500

    Fixed some spelling errors

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,15 +5,15 @@ import React from 'react';
 
 import Todo from './Todo';
 
-const TodoList = (props) => {
+const TodoList = props => {
 
     return (
-        <div>
-            {props.todo.map(todo => (
-                <Todo toggleTodo={props.toggleTodo} key={todo.id} todo={todo} />
+        <div className="todolist" >
+            {props.todo.map(task => (
+                <Todo key={task.id} task={task} toggleTask={props.toggleTask} />
             ))}
         </div>
-    )
+    );
 };
 
 export default TodoList;
