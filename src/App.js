df259b4f22da415af3b992fed1f5658c1d45fae8
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-08 15:34:15 -0600

    Add constructor to App.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList"
+import TodoForm from "./components/TodoForm"
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  // Constructor Function:
+  constructor() {
+    super();
+    this.state = {
+      list: [
+        {
+          task: "Bake Cookies",
+          id: 12121,
+          completed: false
+        },
+        {
+          task: "Organize Closet",
+          id: 12130,
+          completed: false
+        },
+      ]
+    }
+  }
+
+  // Handler Functions:
+  handleAdd = () => {
+
+  }
+
+  handleClear = () => {
+    
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="container">
+        <TodoList list={this.state.list}/>
+        <TodoForm />
       </div>
     );
   }
