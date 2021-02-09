ff51512695c0fdcd8920226380afbee6816df42c
Trey <treywroberts@gmail.com>
2021-02-08 19:22:18 -0500

    got it working just needs style

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+import Todo from './Todo'
+
+class TodoList extends React.Component{
+    handleClick=()=>{
+        this.props.handleTaskCompleted()
+    }
+    render(){
+        console.log(this.props)
+        return(
+            <div>
+                {this.props.todo.map(todo => (
+                    // console.log(todo)
+                    <Todo todo={todo} handleToggle={this.props.handleToggle} />
+                ))}
+                <button onClick={this.handleClick}>CLEAR</button>
+            </div>
+        )
+    }
+}
+
+export default TodoList
