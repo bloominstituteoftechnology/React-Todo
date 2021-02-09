a1da43d562c32cef382626917d9f6591aaaf1fe4
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-02-08 20:01:26 -0700

    added default state, set up onchange handler, addtask handler, and clearcompleted handler. added input and button tags to return statement with event handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      task: '',
+      id: Date.now(),
+      completed: false,
+    }
+  }
+
+  onChange = event => {
+    setState({ task: event.target.value })
+  }
+
+  addTask = event => {
+
+  }
+
+  clearCompleted = event => {
+
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +29,10 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        {this.state.task}
+        <input placeholder='Write a To-Do!' onChange={this.onChange}/>
+        <button onClick={this.addTask}></button>
+        <button onClick={this.clearCompleted}></button>
       </div>
     );
   }
