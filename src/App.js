f1cd8bb0acddb5c6c9453c5285ea572428a2dfda
daniel-vazquez <da.vazquez728@gmail.com>
2021-02-10 17:08:42 -0700

    npm installed, added tasks array, function for adding new task to array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
+
+
+
+const tasks = [
+  {
+    name: 'Take out the garbage',
+    id: 4142,
+    completed: false
+  },
+  {
+    name: 'Complete Lambda School Project',
+    id: 4143,
+    completed: false
+  },
+  {
+    name: 'Exercise for 30 minutes',
+    id: 4144,
+    completed: false
+  },
+  {
+    name: 'Wash the car',
+    id: 4145,
+    completed: false
+  }
+];
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+    tasks: tasks
+  }
+}
+
+
+
+  addTask = (taskName, e) => {
+    e.preventDefault();
+    const newTask = {
+      id: Date.now(),
+      name: taskName,
+      completed: false
+    }
+    this.setState({
+      ...this.state,
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+
+
   render() {
     return (
       <div>
