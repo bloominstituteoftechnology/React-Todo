9a2b69c50ec5178c5238e6fb3ce0f4a0b496966c
Durrell Jules <dujules23@gmail.com>
2021-02-10 20:15:01 -0500

    added state to app.js and created todo array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+const todo = [
+  {
+  task: "Bake Cookes",
+  id: 1,
+  completed: false,
+},
+{
+  task: "Clean Drum Equipment",
+  id:2,
+  completed: false,
+}
+]
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+
+//constructor for state
+constructor(){
+  super();
+  this.state={
+    todo: todo
+  }
+}
   render() {
     return (
       <div>
