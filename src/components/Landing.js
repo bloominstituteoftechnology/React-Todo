a32ab058dec772a0a72e6751e9fa4269340b18d6
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-04-15 23:37:42 -0400

    changed the user address bar to use nickname

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,54 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react"
-import React,{useEffect,useState} from "react"
+import React from "react"
 import Logout from "./Logout"
-import TodoForm from "./TodoForm"
-import TodoList from "./TodoList"
-
+import {useNavigate} from "react-router-dom"
 
 function Landing () {
-      const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem("todos");
-    if(savedTodos) {
-        return JSON.parse(savedTodos)
-    } else {
-        return [];
-    }
-  })
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  },[todos])
-
-
-  const toggleTodo = todoId => {
-      console.log("toggle")
-      const updatedTodos = todos.map(todo => {
-          if(todoId === todo.id) {
-            return {...todo, completed : !todo.completed}
-          }
-          return todo
-      })
-      setTodos(updatedTodos)
-  }
-
-  const addTodo = newTodo => {
-      setTodos(
-          [...todos, {
-              task: newTodo.trim(),
-              id: Math.random(),
-              completed: false
-          }]
-      )
-      localStorage.setItem("todos", JSON.stringify(todos));
-  }
-
-  const clearCompleted = () => {
-    setTodos(
-          todos.filter(todo => !todo.completed)
-      )
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
     const {loginWithRedirect, user } = useAuth0()
+console.log("useAuth0",useAuth0())
+    const navigate = useNavigate()
+
     if (!user){           
         return (
             <div>
@@ -63,17 +22,9 @@ function Landing () {
                 <header>
                   <Logout />  
                 </header>
-                <div>
-                    <h1>To-do List</h1>
-                    <TodoForm 
-                        addTodo={addTodo}
-                        clearCompleted={clearCompleted}
-                        />
-                    <TodoList 
-                        todos={todos} 
-                        toggleTodo={toggleTodo}
-                        />
-                </div>
+                <button onClick={() => navigate(`/users/${user.nickname}`)}>
+                    See Todos
+                </button>
             </div>
         )
     }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Logout from './Logout'
+import TodoForm from "./TodoForm"
+import TodoList from "./TodoList"
 
 const Todos = () => {
+
+      const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if(savedTodos) {
+        return JSON.parse(savedTodos)
+    } else {
+        return [];
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos])
+
+
+  const toggleTodo = todoId => {
+      const updatedTodos = todos.map(todo => {
+          if(todoId === todo.id) {
+            return {...todo, completed : !todo.completed}
+          }
+          return todo
+      })
+      setTodos(updatedTodos)
+  }
+
+  const addTodo = newTodo => {
+      setTodos(
+          [...todos, {
+              task: newTodo.trim(),
+              id: Math.random(),
+              completed: false
+          }]
+      )
+      localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const clearCompleted = () => {
+    setTodos(
+          todos.filter(todo => !todo.completed)
+      )
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+    
   return (
       <div>
-        Todos
-        <Logout />
+            <Logout />
+            <div>
+                <h1>To-do List</h1>
+                <TodoForm 
+                    addTodo={addTodo}
+                    clearCompleted={clearCompleted}
+                    />
+                <TodoList 
+                    todos={todos} 
+                    toggleTodo={toggleTodo}
+                    />
+            </div>
       </div>
    
   )
